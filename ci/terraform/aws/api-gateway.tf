2f158adb8f67876f6ed0fdd1cc70c5c23539920e
dbes-gds <daniel.besbrode@digital.cabinet-office.gov.uk>
2021-06-03 13:57:50 +0100

    LTG-267: separate module for api-gateway-root

--- ci/terraform/aws/api-gateway.tf
+++
@@ -1,3 +0,0 @@
-resource "aws_api_gateway_rest_api" "di-authentication-api" {
-  name = format("di-authentication-api-%s", var.environment)
-}
--- ci/terraform/aws/userinfo.tf
+++ ci/terraform/aws/userinfo.tf
@@ -1,9 +1,9 @@
 module "userinfo" {
   source = "../modules/userinfo"
 
-  rest_api_id = aws_api_gateway_rest_api.di-authentication-api.id
-  root_resource_id = aws_api_gateway_rest_api.di-authentication-api.root_resource_id
-  execution_arn = aws_api_gateway_rest_api.di-authentication-api.execution_arn
+  rest_api_id = module.api-gateway-root.di-authentication-api-id
+  root_resource_id = module.api-gateway-root.root_resource_id
+  execution_arn = module.api-gateway-root.execution_arn
   api-deployment-stage-name = var.api-deployment-stage-name
   lambda-zip-file = var.lambda-zip-file
 }
