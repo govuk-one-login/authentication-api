cf9d853957e1cc4fad2756a4e0bf71f1c6f3d4eb
dbes-gds <daniel.besbrode@digital.cabinet-office.gov.uk>
2021-06-03 17:31:12 +0100

    LTG-268: add startup and pre-commit scripts
    pre-commit runs compilation, tests, terraform and an api test
    api test does not fail the pre-commit as the curl tests are temporary and will be replaced by integration tests

--- startup.sh
+++ startup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -eu
 
-./gradlew installDist
+source ./scripts/functions.sh
 
-./build/install/di-authentication-api/bin/di-authentication-api server authentication-api.yml
+startup
