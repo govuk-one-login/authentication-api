040d8e4a8879335b0c82a2d6700701bbda66e96a
Chris Clayson <chris.clayson@digital.cabinet-office.gov.uk>
2021-06-04 10:59:50 +0100

    LTG-257: Add Terraform for authorize endpoint.
    Co-authored-by: James Howes <james.howes@digital.cabinet-office.gov.uk>

--- ci/terraform/localstack/localstack.tf
+++ ci/terraform/localstack/localstack.tf
@@ -29,3 +29,15 @@ module "token" {
   api-deployment-stage-name = var.api-deployment-stage-name
   lambda-zip-file = var.lambda-zip-file
 }
+
+module "authorize" {
+  source = "../modules/authorise"
+  providers = {
+    aws = aws.localstack
+  }
+  rest_api_id = module.api-gateway-root.di-authentication-api-id
+  root_resource_id = module.api-gateway-root.root_resource_id
+  execution_arn = module.api-gateway-root.execution_arn
+  api-deployment-stage-name = var.api-deployment-stage-name
+  lambda-zip-file = var.lambda-zip-file
+}
