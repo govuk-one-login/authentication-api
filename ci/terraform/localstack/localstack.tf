893546c5361e948906ed83559380c2fe4205d1f3
james.howes <james.howes@digital.cabinet-office.gov.uk>
2021-06-07 14:59:13 +0100

    LTG-264 - Write terraform for Signup API gateway and Lambda
    - Add the terraform to create the api gateway and Lambda required for the signup endpoint

--- ci/terraform/localstack/localstack.tf
+++ ci/terraform/localstack/localstack.tf
@@ -86,6 +86,26 @@ module "token" {
   lambda_zip_file           = var.lambda_zip_file
 }
 
+module "signup" {
+  source = "../modules/endpoint-module"
+  providers = {
+    aws = aws.localstack
+  }
+
+  endpoint_name   = "signup"
+  endpoint_method = "POST"
+  handler_environment_variables = {
+    BASE_URL = var.api_base_url
+  }
+  handler_function_name = "uk.gov.di.lambdas.SignUpHandler::handleRequest"
+
+  rest_api_id               = module.api_gateway_root.di_authentication_api_id
+  root_resource_id          = module.api_gateway_root.root_resource_id
+  execution_arn             = module.api_gateway_root.execution_arn
+  api_deployment_stage_name = var.api_deployment_stage_name
+  lambda_zip_file           = var.lambda_zip_file
+}
+
 module "userinfo" {
   source = "../modules/endpoint-module"
   providers = {
