5a5979a05160ad93a295c2d39409ac97b36f321b
Alan Carter <alan.carter@digital.cabinet-office.gov.uk>
2021-06-10 11:39:18 +0100

    LTG-300: Attempt Elasticache connection via lambda
    Co-authored-by: Alex Wilson <alex.wilson@digital.cabinet-office.gov.uk>
    Co-authored-by: Chris Clayson <chris.clayson@digital.cabinet-office.gov.uk>

--- serverless/lambda/src/main/java/uk/gov/di/services/SessionService.java
+++ serverless/lambda/src/main/java/uk/gov/di/services/SessionService.java
@@ -1,5 +1,8 @@
 package uk.gov.di.services;
 
+import io.lettuce.core.RedisClient;
+import io.lettuce.core.api.StatefulRedisConnection;
+import io.lettuce.core.api.sync.RedisStringCommands;
 import uk.gov.di.entity.Session;
 
 public class SessionService {
@@ -8,6 +11,9 @@ public class SessionService {
     }
 
     public void save(Session session) {
-
+        RedisClient client = RedisClient.create("redis://test-sessions-store.rixhjg.ng.0001.euw2.cache.amazonaws.com:6379");
+        StatefulRedisConnection<String, String> connection = client.connect();
+        RedisStringCommands<String, String> sync = connection.sync();
+        sync.set(session.getSessionId(), "???");
     }
 }
