f0bf0c7a813457f6782236c7d77fa40857e05786
Alex Wilson <alex.wilson@digital.cabinet-office.gov.uk>
2021-07-21 15:53:38 +0100

    BAU: Inline `sendRequest` method in `ClientRegistrationIntegrationTest`

--- integration-tests/src/test/java/uk/gov/di/authentication/api/ClientRegistrationIntegrationTest.java
+++ integration-tests/src/test/java/uk/gov/di/authentication/api/ClientRegistrationIntegrationTest.java
@@ -2,14 +2,10 @@ package uk.gov.di.authentication.api;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import jakarta.ws.rs.client.Client;
 import jakarta.ws.rs.client.ClientBuilder;
 import jakarta.ws.rs.client.Entity;
-import jakarta.ws.rs.client.Invocation;
-import jakarta.ws.rs.client.WebTarget;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.MultivaluedHashMap;
-import jakarta.ws.rs.core.MultivaluedMap;
 import jakarta.ws.rs.core.Response;
 import org.junit.jupiter.api.Test;
 import uk.gov.di.authentication.helpers.DynamoHelper;
@@ -34,23 +30,19 @@ public class ClientRegistrationIntegrationTest extends IntegrationTestEndpoints 
                         singletonList("test-client@test.com"),
                         "public-key",
                         singletonList("openid"));
-        Response response = sendRequest(clientRequest);
+
+        Response response =
+                ClientBuilder.newClient()
+                        .target(ROOT_RESOURCE_URL + REGISTER_ENDPOINT)
+                        .request(MediaType.APPLICATION_JSON)
+                        .headers(new MultivaluedHashMap<>())
+                        .post(Entity.entity(clientRequest, MediaType.APPLICATION_JSON));
+
         ClientRegistrationResponse clientResponse =
                 objectMapper.readValue(
                         response.readEntity(String.class), ClientRegistrationResponse.class);
 
         assertEquals(200, response.getStatus());
         assertTrue(DynamoHelper.clientExists(clientResponse.getClientId()));
-    }
-
-    private Response sendRequest(ClientRegistrationRequest clientRequest) {
-        Client client = ClientBuilder.newClient();
-        WebTarget webTarget = client.target(ROOT_RESOURCE_URL + REGISTER_ENDPOINT);
-        Invocation.Builder invocationBuilder = webTarget.request(MediaType.APPLICATION_JSON);
-        MultivaluedMap<String, Object> headers = new MultivaluedHashMap<>();
-
-        return invocationBuilder
-                .headers(headers)
-                .post(Entity.entity(clientRequest, MediaType.APPLICATION_JSON));
     }
 }
