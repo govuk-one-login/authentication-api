48b2956caf0e1c3d09b235f02a05ceb945e5c677
james.howes <james.howes@digital.cabinet-office.gov.uk>
2021-07-21 12:04:59 +0100

    GOVSI-529 - Add Integration test for the Auth Code lambda

--- integration-tests/src/test/java/uk/gov/di/authentication/helpers/RedisHelper.java
+++ integration-tests/src/test/java/uk/gov/di/authentication/helpers/RedisHelper.java
@@ -8,6 +8,8 @@ import uk.gov.di.services.CodeStorageService;
 import uk.gov.di.services.RedisConnectionService;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import static uk.gov.di.entity.NotificationType.VERIFY_EMAIL;
@@ -20,13 +22,33 @@ public class RedisHelper {
     private static final Optional<String> REDIS_PASSWORD =
             Optional.ofNullable(System.getenv("REDIS_PASSWORD"));
 
-    public static String createSession() throws IOException {
+    public static String createSession(String sessionId, String clientSessionId)
+            throws IOException {
         try (RedisConnectionService redis =
                 new RedisConnectionService(REDIS_HOST, 6379, false, REDIS_PASSWORD)) {
-            Session session = new Session(IdGenerator.generate(), IdGenerator.generate());
+            Session session = new Session(sessionId, clientSessionId);
             redis.saveWithExpiry(
                     session.getSessionId(), new ObjectMapper().writeValueAsString(session), 1800);
             return session.getSessionId();
+        }
+    }
+
+    public static String createSession() throws IOException {
+        return createSession(IdGenerator.generate(), IdGenerator.generate());
+    }
+
+    public static void addAuthRequestToSession(
+            String clientSessionId, String sessionId, Map<String, List<String>> authRequest) {
+        try (RedisConnectionService redis =
+                new RedisConnectionService(REDIS_HOST, 6379, false, REDIS_PASSWORD)) {
+            Session session =
+                    new ObjectMapper().readValue(redis.getValue(sessionId), Session.class);
+            session.addClientSessionAuthorisationRequest(clientSessionId, authRequest);
+            redis.saveWithExpiry(
+                    session.getSessionId(), new ObjectMapper().writeValueAsString(session), 1800);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 
