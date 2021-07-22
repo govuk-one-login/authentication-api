3c8c6a7e9f5a458f22783f793912f1151efc88b1
Alex Wilson <alex.wilson@digital.cabinet-office.gov.uk>
2021-07-22 14:53:01 +0100

    BAU: Add unit test for `RequestBodyHelper`

---
+++ serverless/lambda/src/test/java/uk/gov/di/helpers/RequestBodyHelperTest.java
@@ -0,0 +1,18 @@
+package uk.gov.di.helpers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.collection.IsMapContaining.hasEntry;
+import static uk.gov.di.helpers.RequestBodyHelper.parseRequestBody;
+
+class RequestBodyHelperTest {
+
+    @Test
+    void takesLastValueIfMultipleInstancesOfKey() {
+
+        var input = "key=one&key=two";
+
+        assertThat(parseRequestBody(input), hasEntry("key", "two"));
+    }
+}
