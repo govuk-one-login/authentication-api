9808826260e155fd63bd7833c8df0dbbd3fcd87e
Alex Wilson <alex.wilson@digital.cabinet-office.gov.uk>
2021-07-30 12:02:36 +0100

    GOVSI-500: Set up example transitions for `SignUpHandler`

--- serverless/lambda/src/main/java/uk/gov/di/helpers/StateMachine.java
+++ serverless/lambda/src/main/java/uk/gov/di/helpers/StateMachine.java
@@ -1,10 +1,17 @@
 package uk.gov.di.helpers;
 
+import uk.gov.di.entity.SessionState;
+
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
 import static java.util.Collections.emptyList;
+import static java.util.Map.entry;
+import static java.util.Map.ofEntries;
+import static uk.gov.di.entity.SessionState.AUTHENTICATION_REQUIRED;
+import static uk.gov.di.entity.SessionState.TWO_FACTOR_REQUIRED;
+import static uk.gov.di.entity.SessionState.USER_NOT_FOUND;
 
 public class StateMachine<T> {
 
@@ -16,5 +23,14 @@ public class StateMachine<T> {
 
     public boolean isValidTransition(T from, T to) {
         return states.getOrDefault(from, emptyList()).contains(to);
+    }
+
+    public static StateMachine<SessionState> userJourneyStateMachine() {
+        var states =
+                ofEntries(
+                        entry(USER_NOT_FOUND, List.of(TWO_FACTOR_REQUIRED)),
+                        entry(AUTHENTICATION_REQUIRED, List.of(TWO_FACTOR_REQUIRED)));
+
+        return new StateMachine<>(states);
     }
 }
