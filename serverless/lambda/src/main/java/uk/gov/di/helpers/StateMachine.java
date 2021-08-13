52d699436d3032b3618fde7e8715e868d1557469
james.howes <james.howes@digital.cabinet-office.gov.uk>
2021-08-13 15:09:42 +0100

    BAU - Authenticated should be the next state after phone number verified for now
    - At least until T&Cs and Consent are in

--- serverless/lambda/src/main/java/uk/gov/di/helpers/StateMachine.java
+++ serverless/lambda/src/main/java/uk/gov/di/helpers/StateMachine.java
@@ -84,6 +84,9 @@ public class StateMachine<T> {
                                 VERIFY_PHONE_NUMBER_CODE_SENT,
                                 List.of(PHONE_NUMBER_CODE_VERIFIED, PHONE_NUMBER_CODE_NOT_VALID)),
                         entry(
+                                PHONE_NUMBER_CODE_VERIFIED,
+                                List.of(AUTHENTICATED)),
+                        entry(
                                 PHONE_NUMBER_CODE_NOT_VALID,
                                 List.of(
                                         PHONE_NUMBER_CODE_VERIFIED,
