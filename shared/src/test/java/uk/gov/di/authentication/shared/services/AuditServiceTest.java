de10da53f6639a5489621ae58824e5634f070975
Alex Wilson <alex.wilson@digital.cabinet-office.gov.uk>
2021-08-19 14:51:59 +0100

    GOVSI-712: Add capability to add arbitrary key-value pairs to audit events
    Co-authored-by: Alan Carter <alan.carter@digital.cabinet-office.gov.uk>

--- shared/src/test/java/uk/gov/di/authentication/shared/services/AuditServiceTest.java
+++ shared/src/test/java/uk/gov/di/authentication/shared/services/AuditServiceTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import uk.gov.di.authentication.shared.domain.AuditableEvent;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static uk.gov.di.authentication.shared.services.AuditService.MetadataPair.pair;
 import static uk.gov.di.authentication.shared.services.AuditServiceTest.TestEvents.TEST_EVENT_ONE;
 
 class AuditServiceTest {
@@ -19,5 +20,15 @@ class AuditServiceTest {
         assertEquals(
                 auditService.generateLogLine(TEST_EVENT_ONE),
                 "Emitting audit event - TEST_EVENT_ONE");
+    }
+
+    @Test
+    void shouldLogAuditEventWithMetadataPairsAttached() {
+        var auditService = new AuditService();
+
+        assertEquals(
+                auditService.generateLogLine(
+                        TEST_EVENT_ONE, pair("key", "value"), pair("key2", "value2")),
+                "Emitting audit event - TEST_EVENT_ONE => [key: value], [key2: value2]");
     }
 }
