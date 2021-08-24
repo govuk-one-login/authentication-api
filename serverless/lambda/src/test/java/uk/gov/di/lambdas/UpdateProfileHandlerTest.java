0f4b160be192820073456bb1dea727e6143b8171
Alan Carter <alan.carter@digital.cabinet-office.gov.uk>
2021-08-24 16:59:07 +0100

    GOVSI-734: Add auditing for account management

--- serverless/lambda/src/test/java/uk/gov/di/lambdas/UpdateProfileHandlerTest.java
+++ serverless/lambda/src/test/java/uk/gov/di/lambdas/UpdateProfileHandlerTest.java
@@ -12,9 +12,12 @@ import com.nimbusds.openid.connect.sdk.AuthenticationRequest;
 import com.nimbusds.openid.connect.sdk.AuthenticationSuccessResponse;
 import com.nimbusds.openid.connect.sdk.Nonce;
 import com.nimbusds.openid.connect.sdk.OIDCScopeValue;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import uk.gov.di.authentication.shared.services.AuditService;
 import uk.gov.di.authentication.shared.services.ConfigurationService;
+import uk.gov.di.domain.AccountManagementAuditableEvent;
 import uk.gov.di.entity.ClientConsent;
 import uk.gov.di.entity.ClientSession;
 import uk.gov.di.entity.ErrorResponse;
@@ -40,7 +43,9 @@ import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
+import static uk.gov.di.domain.AccountManagementAuditableEvent.ACCOUNT_MANAGEMENT_REQUEST_ERROR;
 import static uk.gov.di.entity.UpdateProfileType.*;
 import static uk.gov.di.helpers.CookieHelper.buildCookieString;
 import static uk.gov.di.matchers.APIGatewayProxyResponseEventMatcher.hasJsonBody;
@@ -63,6 +68,7 @@ class UpdateProfileHandlerTest {
     private final ClientSessionService clientSessionService = mock(ClientSessionService.class);
     private final AuthorizationService authorizationService = mock(AuthorizationService.class);
     private final ConfigurationService configurationService = mock(ConfigurationService.class);
+    private final AuditService auditService = mock(AuditService.class);
     private final AuthorisationCodeService authorisationCodeService =
             mock(AuthorisationCodeService.class);
 
@@ -80,7 +86,13 @@ class UpdateProfileHandlerTest {
                         authenticationService,
                         sessionService,
                         clientSessionService,
-                        configurationService);
+                        configurationService,
+                        auditService);
+    }
+
+    @AfterEach
+    public void afterEach() {
+        verifyNoMoreInteractions(auditService);
     }
 
     @Test
@@ -93,11 +105,15 @@ class UpdateProfileHandlerTest {
                 format(
                         "{ \"email\": \"%s\", \"updateProfileType\": \"%s\", \"profileInformation\": \"%s\" }",
                         TEST_EMAIL_ADDRESS, ADD_PHONE_NUMBER, PHONE_NUMBER));
-        APIGatewayProxyResponseEvent result = handler.handleRequest(event, context);
+        APIGatewayProxyResponseEvent result = makeHandlerRequest(event);
 
         verify(authenticationService).updatePhoneNumber(eq(TEST_EMAIL_ADDRESS), eq(PHONE_NUMBER));
 
         assertThat(result, hasStatus(200));
+
+        verify(auditService)
+                .submitAuditEvent(
+                        AccountManagementAuditableEvent.ACCOUNT_MANAGEMENT_PHONE_NUMBER_UPDATED);
     }
 
     @Test
@@ -112,12 +128,17 @@ class UpdateProfileHandlerTest {
                         TEST_EMAIL_ADDRESS,
                         UPDATE_TERMS_CONDS,
                         UPDATED_TERMS_AND_CONDITIONS_VALUE));
-        APIGatewayProxyResponseEvent result = handler.handleRequest(event, context);
+        APIGatewayProxyResponseEvent result = makeHandlerRequest(event);
 
         verify(authenticationService)
                 .updateTermsAndConditions(eq(TEST_EMAIL_ADDRESS), eq(TERMS_AND_CONDITIONS_VERSION));
 
         assertThat(result, hasStatus(200));
+
+        verify(auditService)
+                .submitAuditEvent(
+                        AccountManagementAuditableEvent
+                                .ACCOUNT_MANAGEMENT_TERMS_CONDS_ACCEPTANCE_UPDATED);
     }
 
     @Test
@@ -154,12 +175,16 @@ class UpdateProfileHandlerTest {
                         "{ \"email\": \"%s\", \"updateProfileType\": \"%s\", \"profileInformation\": \"%s\" }",
                         TEST_EMAIL_ADDRESS, CAPTURE_CONSENT, CONSENT_VALUE));
 
-        APIGatewayProxyResponseEvent result = handler.handleRequest(event, context);
+        APIGatewayProxyResponseEvent result = makeHandlerRequest(event);
 
         verify(authenticationService)
                 .updateConsent(eq(TEST_EMAIL_ADDRESS), any(ClientConsent.class));
 
         assertThat(result, hasStatus(200));
+
+        verify(auditService)
+                .submitAuditEvent(
+                        AccountManagementAuditableEvent.ACCOUNT_MANAGEMENT_CONSENT_UPDATED);
     }
 
     @Test
@@ -172,13 +197,15 @@ class UpdateProfileHandlerTest {
                 format(
                         "{ \"email\": \"%s\", \"updateProfileType\": \"%s\"}",
                         TEST_EMAIL_ADDRESS, ADD_PHONE_NUMBER));
-        APIGatewayProxyResponseEvent result = handler.handleRequest(event, context);
+        APIGatewayProxyResponseEvent result = makeHandlerRequest(event);
 
         verify(authenticationService, never())
                 .updatePhoneNumber(eq(TEST_EMAIL_ADDRESS), eq(PHONE_NUMBER));
 
         assertThat(result, hasStatus(400));
         assertThat(result, hasJsonBody(ErrorResponse.ERROR_1001));
+
+        verify(auditService).submitAuditEvent(ACCOUNT_MANAGEMENT_REQUEST_ERROR);
     }
 
     @Test
@@ -193,10 +220,12 @@ class UpdateProfileHandlerTest {
                 format(
                         "{ \"email\": \"%s\", \"updateProfileType\": \"%s\", \"profileInformation\": \"%s\" }",
                         TEST_EMAIL_ADDRESS, ADD_PHONE_NUMBER, PHONE_NUMBER));
-        APIGatewayProxyResponseEvent result = handler.handleRequest(event, context);
+        APIGatewayProxyResponseEvent result = makeHandlerRequest(event);
 
         assertThat(result, hasStatus(400));
         assertThat(result, hasJsonBody(ErrorResponse.ERROR_1017));
+
+        verify(auditService).submitAuditEvent(ACCOUNT_MANAGEMENT_REQUEST_ERROR);
     }
 
     private void usingValidSession() {
@@ -219,5 +248,15 @@ class UpdateProfileHandlerTest {
         when(clientSessionService.getClientSessionFromRequestHeaders(anyMap()))
                 .thenReturn(Optional.of(clientSession));
         return authRequest;
+    }
+
+    private APIGatewayProxyResponseEvent makeHandlerRequest(APIGatewayProxyRequestEvent event) {
+        var response = handler.handleRequest(event, context);
+
+        verify(auditService)
+                .submitAuditEvent(
+                        AccountManagementAuditableEvent.ACCOUNT_MANAGEMENT_REQUEST_RECEIVED);
+
+        return response;
     }
 }
