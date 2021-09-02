d2151607eb1be030a4df3ca4077ccca1cd855723
james.howes <james.howes@digital.cabinet-office.gov.uk>
2021-09-02 15:09:40 +0100

    GOVSI-628 - Move account management integration tests into its own directory
    - This will allow us to run these tests in isolation and keep the unit of the application seperate

--- account-management-integration-tests/src/test/java/uk/gov/di/accountmanagement/api/AuthenticateIntegrationTest.java
+++ account-management-integration-tests/src/test/java/uk/gov/di/accountmanagement/api/AuthenticateIntegrationTest.java
@@ -1,47 +1,33 @@
 package uk.gov.di.accountmanagement.api;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.ws.rs.core.Response;
 import org.junit.jupiter.api.Test;
 import uk.gov.di.accountmanagement.entity.AuthenticateRequest;
-import uk.gov.di.authentication.api.IntegrationTestEndpoints;
+import uk.gov.di.accountmanagement.helpers.DynamoHelper;
+import uk.gov.di.accountmanagement.helpers.RequestHelper;
 import uk.gov.di.authentication.frontendapi.entity.LoginRequest;
-import uk.gov.di.authentication.frontendapi.entity.LoginResponse;
-import uk.gov.di.authentication.helpers.DynamoHelper;
-import uk.gov.di.authentication.helpers.RequestHelper;
-
-import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static uk.gov.di.authentication.shared.entity.SessionState.LOGGED_IN;
 
 public class AuthenticateIntegrationTest extends IntegrationTestEndpoints {
 
     private static final String LOGIN_ENDPOINT = "/authenticate";
-    private final ObjectMapper objectMapper = new ObjectMapper();
 
     @Test
-    public void shouldCallLoginEndpointAndReturn200WhenLoginIsSuccessful() throws IOException {
+    public void shouldCallLoginEndpointAndReturn200WhenLoginIsSuccessful() {
         String email = "joe.bloggs+3@digital.cabinet-office.gov.uk";
         String password = "password-1";
-        String phoneNumber = "01234567890";
         DynamoHelper.signUp(email, password);
-        DynamoHelper.addPhoneNumber(email, phoneNumber);
 
         Response response =
                 RequestHelper.buildRequest(
                         LOGIN_ENDPOINT, new AuthenticateRequest(email, password));
 
         assertEquals(200, response.getStatus());
-
-        String responseString = response.readEntity(String.class);
-        LoginResponse loginResponse = objectMapper.readValue(responseString, LoginResponse.class);
-        assertEquals(LOGGED_IN, loginResponse.getSessionState());
     }
 
     @Test
-    public void shouldCallLoginEndpointAndReturn401henUserHasInvalidCredentials()
-            throws IOException {
+    public void shouldCallLoginEndpointAndReturn401henUserHasInvalidCredentials() {
         String email = "joe.bloggs+4@digital.cabinet-office.gov.uk";
         String password = "password-1";
         DynamoHelper.signUp(email, "wrong-password");
