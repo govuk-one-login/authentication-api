159d17270966f0eb71c86757b6aac70b4806e5ab
james.howes <james.howes@digital.cabinet-office.gov.uk>
2021-09-02 09:25:35 +0100

    GOVSI-628 - Create basic Update Info lambda

--- account-management-api/src/main/java/uk/gov/di/accountmanagement/lambda/UpdateInfoHandler.java
+++ account-management-api/src/main/java/uk/gov/di/accountmanagement/lambda/UpdateInfoHandler.java
@@ -5,12 +5,14 @@ import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 
-public class HelloWorldHandler
+import static uk.gov.di.authentication.shared.helpers.ApiGatewayResponseHelper.generateApiGatewayProxyResponse;
+
+public class UpdateInfoHandler
         implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
 
     @Override
     public APIGatewayProxyResponseEvent handleRequest(
             APIGatewayProxyRequestEvent input, Context context) {
-        return new APIGatewayProxyResponseEvent().withStatusCode(200).withBody("Hello, world!");
+        return generateApiGatewayProxyResponse(200, "");
     }
 }
