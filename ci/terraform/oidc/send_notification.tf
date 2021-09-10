0e681e0bbac404e7627f4cc6d6c8a8578556541b
james.howes <james.howes@digital.cabinet-office.gov.uk>
2021-09-10 14:16:20 +0100

    GOVSI-790 - Enforce API key authentication
    - Enforce api key authentication for the apis that are only called by our frontend app.
    - These are the following:  client-info, login, mfa, send_notification, signup, update_profile, userexists, verify_code

--- ci/terraform/oidc/send_notification.tf
+++ ci/terraform/oidc/send_notification.tf
@@ -28,6 +28,7 @@ module "send_notification" {
   logging_endpoint_enabled  = var.logging_endpoint_enabled
   logging_endpoint_arn      = var.logging_endpoint_arn
   default_tags              = local.default_tags
+  api_key_required          = true
 
   keep_lambda_warm             = var.keep_lambdas_warm
   warmer_handler_function_name = "uk.gov.di.lambdawarmer.lambda.LambdaWarmerHandler::handleRequest"
