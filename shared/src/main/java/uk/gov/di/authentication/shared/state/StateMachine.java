f20bf39a81d79d4f9f062cc6ec0101ebf25e2e7c
Chris Clayson <chris.clayson@digital.cabinet-office.gov.uk>
2021-09-15 16:51:38 +0100

    BAU: Add fluent builder DSL for the state machine
    - Add builders to the `StateMachine` and `Transition` classes
    - Rewrite `userJourneyStateMachine()` to use the builders so the code is more readable

--- shared/src/main/java/uk/gov/di/authentication/shared/state/StateMachine.java
+++ shared/src/main/java/uk/gov/di/authentication/shared/state/StateMachine.java
@@ -6,14 +6,15 @@ import uk.gov.di.authentication.shared.entity.SessionAction;
 import uk.gov.di.authentication.shared.entity.SessionState;
 import uk.gov.di.authentication.shared.entity.UserProfile;
 
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import static java.util.Collections.emptyList;
-import static java.util.Map.entry;
-import static java.util.Map.ofEntries;
 import static uk.gov.di.authentication.shared.entity.SessionAction.SYSTEM_HAS_ISSUED_AUTHORIZATION_CODE;
 import static uk.gov.di.authentication.shared.entity.SessionAction.SYSTEM_HAS_SENT_EMAIL_VERIFICATION_CODE;
 import static uk.gov.di.authentication.shared.entity.SessionAction.SYSTEM_HAS_SENT_MFA_CODE;
@@ -51,6 +52,7 @@ import static uk.gov.di.authentication.shared.entity.SessionState.TWO_FACTOR_REQ
 import static uk.gov.di.authentication.shared.entity.SessionState.USER_NOT_FOUND;
 import static uk.gov.di.authentication.shared.entity.SessionState.VERIFY_EMAIL_CODE_SENT;
 import static uk.gov.di.authentication.shared.entity.SessionState.VERIFY_PHONE_NUMBER_CODE_SENT;
+import static uk.gov.di.authentication.shared.state.Transition.on;
 
 public class StateMachine<T, A, C> {
 
@@ -63,16 +65,16 @@ public class StateMachine<T, A, C> {
     }
 
     public T transition(T from, A action, Optional<C> context) {
-            T to =
-                    states.getOrDefault(from, emptyList()).stream()
-                            .filter(t -> t.getAction().equals(action))
-                            .findFirst()
-                            .orElseThrow(() -> handleBadStateTransition(from, action))
-                            .getNextState();
+        T to =
+                states.getOrDefault(from, emptyList()).stream()
+                        .filter(t -> t.getAction().equals(action))
+                        .findFirst()
+                        .orElseThrow(() -> handleBadStateTransition(from, action))
+                        .getNextState();
 
-            LOGGER.info("Session transitioned from {} to {} on action {}", from, to, action);
+        LOGGER.info("Session transitioned from {} to {} on action {}", from, to, action);
 
-            return to;
+        return to;
     }
 
     public T transition(T from, A action) {
@@ -80,146 +82,127 @@ public class StateMachine<T, A, C> {
     }
 
     public static StateMachine<SessionState, SessionAction, UserProfile> userJourneyStateMachine() {
-        Map<SessionState, List<Transition<SessionState, SessionAction, UserProfile>>> states =
-                ofEntries(
-                        entry(
-                                NEW,
-                                List.of(
-                                        new Transition<>(
-                                                USER_ENTERED_UNREGISTERED_EMAIL_ADDRESS,
-                                                USER_NOT_FOUND))),
-                        entry(
-                                USER_NOT_FOUND,
-                                List.of(
-                                        new Transition<>(
-                                                USER_ENTERED_UNREGISTERED_EMAIL_ADDRESS,
-                                                USER_NOT_FOUND),
-                                        new Transition<>(
-                                                USER_ENTERED_REGISTERED_EMAIL_ADDRESS,
-                                                AUTHENTICATION_REQUIRED),
-                                        new Transition<>(
-                                                SYSTEM_HAS_SENT_EMAIL_VERIFICATION_CODE,
-                                                VERIFY_EMAIL_CODE_SENT))),
-                        entry(
-                                VERIFY_EMAIL_CODE_SENT,
-                                List.of(
-                                        new Transition<>(
-                                                USER_ENTERED_VALID_EMAIL_VERIFICATION_CODE,
-                                                EMAIL_CODE_VERIFIED),
-                                        new Transition<>(
-                                                USER_ENTERED_INVALID_EMAIL_VERIFICATION_CODE,
-                                                EMAIL_CODE_NOT_VALID))),
-                        entry(
-                                EMAIL_CODE_NOT_VALID,
-                                List.of(
-                                        new Transition<>(
-                                                USER_ENTERED_VALID_EMAIL_VERIFICATION_CODE,
-                                                EMAIL_CODE_VERIFIED),
-                                        new Transition<>(
-                                                USER_ENTERED_INVALID_EMAIL_VERIFICATION_CODE,
-                                                EMAIL_CODE_NOT_VALID),
-                                        new Transition<>(
-                                                USER_ENTERED_INVALID_EMAIL_VERIFICATION_CODE_TOO_MANY_TIMES,
-                                                EMAIL_CODE_MAX_RETRIES_REACHED))),
-                        entry(EMAIL_CODE_MAX_RETRIES_REACHED, Collections.emptyList()),
-                        entry(
-                                EMAIL_CODE_VERIFIED,
-                                List.of(
-                                        new Transition<>(
-                                                USER_ENTERED_INVALID_EMAIL_VERIFICATION_CODE,
-                                                EMAIL_CODE_NOT_VALID),
-                                        new Transition<>(
-                                                USER_HAS_CREATED_A_PASSWORD, TWO_FACTOR_REQUIRED))),
-                        entry(
-                                TWO_FACTOR_REQUIRED,
-                                List.of(
-                                        new Transition<>(
-                                                USER_ENTERED_A_NEW_PHONE_NUMBER,
-                                                ADDED_UNVERIFIED_PHONE_NUMBER))),
-                        entry(
-                                ADDED_UNVERIFIED_PHONE_NUMBER,
-                                List.of(
-                                        new Transition<>(
-                                                SYSTEM_HAS_SENT_PHONE_VERIFICATION_CODE,
-                                                VERIFY_PHONE_NUMBER_CODE_SENT))),
-                        entry(
-                                VERIFY_PHONE_NUMBER_CODE_SENT,
-                                List.of(
-                                        new Transition<>(
-                                                USER_ENTERED_VALID_PHONE_VERIFICATION_CODE,
-                                                PHONE_NUMBER_CODE_VERIFIED),
-                                        new Transition<>(
-                                                USER_ENTERED_INVALID_PHONE_VERIFICATION_CODE,
-                                                PHONE_NUMBER_CODE_NOT_VALID))),
-                        entry(
-                                PHONE_NUMBER_CODE_VERIFIED,
-                                List.of(
-                                        new Transition<>(
-                                                SYSTEM_HAS_ISSUED_AUTHORIZATION_CODE,
-                                                AUTHENTICATED))),
-                        entry(
-                                PHONE_NUMBER_CODE_NOT_VALID,
-                                List.of(
-                                        new Transition<>(
-                                                USER_ENTERED_VALID_PHONE_VERIFICATION_CODE,
-                                                PHONE_NUMBER_CODE_VERIFIED),
-                                        new Transition<>(
-                                                USER_ENTERED_INVALID_PHONE_VERIFICATION_CODE,
-                                                PHONE_NUMBER_CODE_NOT_VALID),
-                                        new Transition<>(
-                                                USER_ENTERED_INVALID_PHONE_VERIFICATION_CODE_TOO_MANY_TIMES,
-                                                PHONE_NUMBER_CODE_MAX_RETRIES_REACHED))),
-                        entry(PHONE_NUMBER_CODE_MAX_RETRIES_REACHED, Collections.emptyList()),
-                        entry(
-                                AUTHENTICATION_REQUIRED,
-                                List.of(
-                                        new Transition<>(USER_ENTERED_VALID_CREDENTIALS, LOGGED_IN),
-                                        new Transition<>(
-                                                USER_ENTERED_UNREGISTERED_EMAIL_ADDRESS,
-                                                USER_NOT_FOUND))),
-                        entry(
-                                LOGGED_IN,
-                                List.of(
-                                        new Transition<>(
-                                                SYSTEM_HAS_SENT_MFA_CODE, MFA_SMS_CODE_SENT))),
-                        entry(
-                                MFA_SMS_CODE_SENT,
-                                List.of(
-                                        new Transition<>(
-                                                SYSTEM_HAS_SENT_MFA_CODE, MFA_SMS_CODE_SENT),
-                                        new Transition<>(
-                                                USER_ENTERED_VALID_MFA_CODE, MFA_CODE_VERIFIED),
-                                        new Transition<>(
-                                                USER_ENTERED_INVALID_MFA_CODE,
-                                                MFA_CODE_NOT_VALID))),
-                        entry(
-                                MFA_CODE_NOT_VALID,
-                                List.of(
-                                        new Transition<>(
-                                                USER_ENTERED_VALID_MFA_CODE, MFA_CODE_VERIFIED),
-                                        new Transition<>(
-                                                USER_ENTERED_INVALID_MFA_CODE, MFA_CODE_NOT_VALID),
-                                        new Transition<>(
-                                                USER_ENTERED_INVALID_MFA_CODE_TOO_MANY_TIMES,
-                                                MFA_CODE_MAX_RETRIES_REACHED))),
-                        entry(MFA_CODE_MAX_RETRIES_REACHED, Collections.emptyList()),
-                        entry(
-                                MFA_CODE_VERIFIED,
-                                List.of(
-                                        new Transition<>(
-                                                SYSTEM_HAS_ISSUED_AUTHORIZATION_CODE,
-                                                AUTHENTICATED))),
-                        entry(
-                                AUTHENTICATED,
-                                List.of(
-                                        new Transition<>(
-                                                SYSTEM_HAS_ISSUED_AUTHORIZATION_CODE,
-                                                AUTHENTICATED))));
 
-        return new StateMachine<>(states);
+        return builder()
+                .when(NEW)
+                .allow(on(USER_ENTERED_UNREGISTERED_EMAIL_ADDRESS).then(USER_NOT_FOUND))
+                .when(USER_NOT_FOUND)
+                .allow(
+                        on(USER_ENTERED_UNREGISTERED_EMAIL_ADDRESS).then(USER_NOT_FOUND),
+                        on(USER_ENTERED_REGISTERED_EMAIL_ADDRESS).then(AUTHENTICATION_REQUIRED),
+                        on(SYSTEM_HAS_SENT_EMAIL_VERIFICATION_CODE).then(VERIFY_EMAIL_CODE_SENT))
+                .when(VERIFY_EMAIL_CODE_SENT)
+                .allow(
+                        on(USER_ENTERED_VALID_EMAIL_VERIFICATION_CODE).then(EMAIL_CODE_VERIFIED),
+                        on(USER_ENTERED_INVALID_EMAIL_VERIFICATION_CODE).then(EMAIL_CODE_NOT_VALID))
+                .when(EMAIL_CODE_NOT_VALID)
+                .allow(
+                        on(USER_ENTERED_VALID_EMAIL_VERIFICATION_CODE).then(EMAIL_CODE_VERIFIED),
+                        on(USER_ENTERED_INVALID_EMAIL_VERIFICATION_CODE).then(EMAIL_CODE_NOT_VALID),
+                        on(USER_ENTERED_INVALID_EMAIL_VERIFICATION_CODE_TOO_MANY_TIMES)
+                                .then(EMAIL_CODE_MAX_RETRIES_REACHED))
+                .when(EMAIL_CODE_MAX_RETRIES_REACHED)
+                .deadEnd()
+                .when(EMAIL_CODE_VERIFIED)
+                .allow(
+                        on(USER_ENTERED_INVALID_EMAIL_VERIFICATION_CODE).then(EMAIL_CODE_NOT_VALID),
+                        on(USER_HAS_CREATED_A_PASSWORD).then(TWO_FACTOR_REQUIRED))
+                .when(TWO_FACTOR_REQUIRED)
+                .allow(on(USER_ENTERED_A_NEW_PHONE_NUMBER).then(ADDED_UNVERIFIED_PHONE_NUMBER))
+                .when(ADDED_UNVERIFIED_PHONE_NUMBER)
+                .allow(
+                        on(SYSTEM_HAS_SENT_PHONE_VERIFICATION_CODE)
+                                .then(VERIFY_PHONE_NUMBER_CODE_SENT))
+                .when(VERIFY_PHONE_NUMBER_CODE_SENT)
+                .allow(
+                        on(USER_ENTERED_VALID_PHONE_VERIFICATION_CODE)
+                                .then(PHONE_NUMBER_CODE_VERIFIED),
+                        on(USER_ENTERED_INVALID_PHONE_VERIFICATION_CODE)
+                                .then(PHONE_NUMBER_CODE_NOT_VALID))
+                .when(PHONE_NUMBER_CODE_VERIFIED)
+                .allow(on(SYSTEM_HAS_ISSUED_AUTHORIZATION_CODE).then(AUTHENTICATED))
+                .when(PHONE_NUMBER_CODE_NOT_VALID)
+                .allow(
+                        on(USER_ENTERED_VALID_PHONE_VERIFICATION_CODE)
+                                .then(PHONE_NUMBER_CODE_VERIFIED),
+                        on(USER_ENTERED_INVALID_PHONE_VERIFICATION_CODE)
+                                .then(PHONE_NUMBER_CODE_NOT_VALID),
+                        on(USER_ENTERED_INVALID_PHONE_VERIFICATION_CODE_TOO_MANY_TIMES)
+                                .then(PHONE_NUMBER_CODE_MAX_RETRIES_REACHED))
+                .when(PHONE_NUMBER_CODE_MAX_RETRIES_REACHED)
+                .deadEnd()
+                .when(AUTHENTICATION_REQUIRED)
+                .allow(
+                        on(USER_ENTERED_VALID_CREDENTIALS).then(LOGGED_IN),
+                        on(USER_ENTERED_UNREGISTERED_EMAIL_ADDRESS).then(USER_NOT_FOUND))
+                .when(LOGGED_IN)
+                .allow(on(SYSTEM_HAS_SENT_MFA_CODE).then(MFA_SMS_CODE_SENT))
+                .when(MFA_SMS_CODE_SENT)
+                .allow(
+                        on(SYSTEM_HAS_SENT_MFA_CODE).then(MFA_SMS_CODE_SENT),
+                        on(USER_ENTERED_VALID_MFA_CODE).then(MFA_CODE_VERIFIED),
+                        on(USER_ENTERED_INVALID_MFA_CODE).then(MFA_CODE_NOT_VALID))
+                .when(MFA_CODE_NOT_VALID)
+                .allow(
+                        on(USER_ENTERED_VALID_MFA_CODE).then(MFA_CODE_VERIFIED),
+                        on(USER_ENTERED_INVALID_MFA_CODE).then(MFA_CODE_NOT_VALID),
+                        on(USER_ENTERED_INVALID_MFA_CODE_TOO_MANY_TIMES)
+                                .then(MFA_CODE_MAX_RETRIES_REACHED))
+                .when(MFA_CODE_MAX_RETRIES_REACHED)
+                .deadEnd()
+                .when(MFA_CODE_VERIFIED)
+                .allow(on(SYSTEM_HAS_ISSUED_AUTHORIZATION_CODE).then(AUTHENTICATED))
+                .when(AUTHENTICATED)
+                .allow(on(SYSTEM_HAS_ISSUED_AUTHORIZATION_CODE).then(AUTHENTICATED))
+                .build();
     }
 
     public static class InvalidStateTransitionException extends RuntimeException {}
+
+    public static <T, A, C> Builder builder() {
+        return new Builder<>();
+    }
+
+    public static class Builder<T, A, C> {
+        private final Map<T, List<Transition<T, A, C>>> states = new HashMap<>();
+
+        public StateRuleBuilder<T, A, C> when(T state) {
+            return new StateRuleBuilder<>(this, state);
+        }
+
+        protected void addStateRule(T state, List<Transition<T, A, C>> transitions) {
+            this.states.put(state, transitions);
+        }
+
+        public StateMachine<T, A, C> build() {
+            return new StateMachine<>(states);
+        }
+    }
+
+    public static class StateRuleBuilder<T, A, C> {
+        private final Builder<T, A, C> stateMachineBuilder;
+        private final T state;
+
+        @SafeVarargs
+        public final Builder<T, A, C> allow(final Transition.Builder<T, A, C>... transitions) {
+            stateMachineBuilder.addStateRule(
+                    state,
+                    Arrays.asList(transitions).stream()
+                            .map(b -> b.build())
+                            .collect(Collectors.toList()));
+            return stateMachineBuilder;
+        }
+
+        protected StateRuleBuilder(Builder<T, A, C> stateMachineBuilder, T state) {
+            this.stateMachineBuilder = stateMachineBuilder;
+            this.state = state;
+        }
+
+        public Builder<T, A, C> deadEnd() {
+            stateMachineBuilder.addStateRule(state, Collections.emptyList());
+            return stateMachineBuilder;
+        }
+    }
 
     private InvalidStateTransitionException handleBadStateTransition(T from, A action) {
         LOGGER.error("Session attempted invalid transition from {} on action {}", from, action);
--- shared/src/main/java/uk/gov/di/authentication/shared/state/Transition.java
+++ shared/src/main/java/uk/gov/di/authentication/shared/state/Transition.java
@@ -26,4 +26,37 @@ public class Transition<T, A, C> {
     public Condition<C> getCondition() {
         return condition;
     }
+
+    public static <T, A, C> Builder on(A action) {
+        return new Builder<>(action);
+    }
+
+    public static class Builder<T, A, C> {
+        private A action;
+        private T nextState;
+        private Condition<C> condition;
+
+        protected Builder(A action) {
+            this.action = action;
+        }
+
+        public Builder<T, A, C> ifCondition(Condition<C> condition) {
+            this.condition = condition;
+            return this;
+        }
+
+        public Builder<T, A, C> then(T nextState) {
+            this.nextState = nextState;
+            return this;
+        }
+
+        public Builder<T, A, C> byDefault() {
+            this.condition = new Default<>();
+            return this;
+        }
+
+        public Transition<T, A, C> build() {
+            return new Transition<>(action, nextState, condition);
+        }
+    }
 }
