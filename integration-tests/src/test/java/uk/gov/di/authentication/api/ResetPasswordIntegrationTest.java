0a1692450375e09aee5688b4260104a95afe19d3
Chris Clayson <chris.clayson@digital.cabinet-office.gov.uk>
2021-09-23 11:58:12 +0100

    BAU: Add Notify Stub to PasswordResetRequestHandler

--- integration-tests/src/test/java/uk/gov/di/authentication/api/ResetPasswordIntegrationTest.java
+++ integration-tests/src/test/java/uk/gov/di/authentication/api/ResetPasswordIntegrationTest.java
@@ -7,11 +7,18 @@ import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.MultivaluedHashMap;
 import jakarta.ws.rs.core.MultivaluedMap;
 import jakarta.ws.rs.core.Response;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
 import uk.gov.di.authentication.frontendapi.entity.ResetPasswordWithCodeRequest;
 import uk.gov.di.authentication.helpers.DynamoHelper;
 import uk.gov.di.authentication.helpers.RedisHelper;
+import uk.gov.di.authentication.helpers.httpstub.HttpStubExtension;
 
+import static java.util.concurrent.TimeUnit.MINUTES;
+import static org.awaitility.Awaitility.await;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static uk.gov.di.authentication.api.IntegrationTestEndpoints.API_KEY;
 import static uk.gov.di.authentication.api.IntegrationTestEndpoints.ROOT_RESOURCE_URL;
@@ -23,8 +30,17 @@ public class ResetPasswordIntegrationTest {
     private static final String PASSWORD = "Pa55word";
     private static final String CODE = "0123456789";
 
+    @RegisterExtension
+    public static final HttpStubExtension notifyStub = new HttpStubExtension(8888);
+
+    @AfterEach
+    public void resetStub() {
+        notifyStub.reset();
+    }
+
     @Test
     public void shouldUpdatePasswordAndReturn200() {
+        registerEmail();
         String subject = "new-subject";
         ResetPasswordWithCodeRequest requestBody = new ResetPasswordWithCodeRequest(CODE, PASSWORD);
         DynamoHelper.signUp(EMAIL_ADDRESS, "password-1", new Subject(subject));
@@ -37,7 +53,31 @@ public class ResetPasswordIntegrationTest {
                         .request(MediaType.APPLICATION_JSON)
                         .headers(headers)
                         .post(Entity.entity(requestBody, MediaType.APPLICATION_JSON));
+        await().atMost(1, MINUTES)
+                .untilAsserted(() -> assertThat(notifyStub.getCountOfRequests(), equalTo(1)));
 
         assertEquals(200, response.getStatus());
+    }
+
+    private void registerEmail() {
+        notifyStub.register(
+                "/v2/notifications/email",
+                201,
+                "application/json",
+                "{"
+                        + "  \"id\": \"740e5834-3a29-46b4-9a6f-16142fde533a\","
+                        + "  \"reference\": \"STRING\","
+                        + "  \"content\": {"
+                        + "    \"subject\": \"SUBJECT TEXT\","
+                        + "    \"body\": \"MESSAGE TEXT\",\n"
+                        + "    \"from_email\": \"SENDER EMAIL\""
+                        + "  },"
+                        + "  \"uri\": \"http://localhost:8888/v2/notifications/a-message-id\","
+                        + "  \"template\": {"
+                        + "    \"id\": \"f33517ff-2a88-4f6e-b855-c550268ce08a\","
+                        + "    \"version\": 1,"
+                        + "    \"uri\": \"http://localhost:8888/v2/template/f33517ff-2a88-4f6e-b855-c550268ce08a\""
+                        + "  }"
+                        + "}");
     }
 }
--- integration-tests/src/test/java/uk/gov/di/authentication/api/ResetPasswordRequestIntegrationTest.java
+++ integration-tests/src/test/java/uk/gov/di/authentication/api/ResetPasswordRequestIntegrationTest.java
@@ -4,15 +4,22 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.ws.rs.core.MultivaluedHashMap;
 import jakarta.ws.rs.core.MultivaluedMap;
 import jakarta.ws.rs.core.Response;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
 import uk.gov.di.authentication.frontendapi.entity.ResetPasswordRequest;
 import uk.gov.di.authentication.helpers.DynamoHelper;
 import uk.gov.di.authentication.helpers.RedisHelper;
 import uk.gov.di.authentication.helpers.RequestHelper;
+import uk.gov.di.authentication.helpers.httpstub.HttpStubExtension;
 import uk.gov.di.authentication.shared.entity.BaseAPIResponse;
 
 import java.io.IOException;
 
+import static java.util.concurrent.TimeUnit.MINUTES;
+import static org.awaitility.Awaitility.await;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static uk.gov.di.authentication.shared.entity.SessionState.AUTHENTICATION_REQUIRED;
 import static uk.gov.di.authentication.shared.entity.SessionState.NEW;
@@ -23,8 +30,17 @@ public class ResetPasswordRequestIntegrationTest extends IntegrationTestEndpoint
     private static final String RESET_PASSWORD_ENDPOINT = "/reset-password-request";
     private final ObjectMapper objectMapper = new ObjectMapper();
 
+    @RegisterExtension
+    public static final HttpStubExtension notifyStub = new HttpStubExtension(8888);
+
+    @AfterEach
+    public void resetStub() {
+        notifyStub.reset();
+    }
+
     @Test
     public void shouldCallResetPasswordEndpointAndReturn200() throws IOException {
+        registerEmail();
         String email = "joe.bloggs+3@digital.cabinet-office.gov.uk";
         String password = "password-1";
         String phoneNumber = "01234567890";
@@ -39,6 +55,8 @@ public class ResetPasswordRequestIntegrationTest extends IntegrationTestEndpoint
         Response response =
                 RequestHelper.request(
                         RESET_PASSWORD_ENDPOINT, new ResetPasswordRequest(email), headers);
+        await().atMost(1, MINUTES)
+                .untilAsserted(() -> assertThat(notifyStub.getCountOfRequests(), equalTo(1)));
 
         assertEquals(200, response.getStatus());
 
@@ -50,6 +68,7 @@ public class ResetPasswordRequestIntegrationTest extends IntegrationTestEndpoint
 
     @Test
     public void shouldCallResetPasswordEndpointAndReturn400WhenInvalidState() throws IOException {
+        registerEmail();
         String email = "joe.bloggs+3@digital.cabinet-office.gov.uk";
         String password = "password-1";
         String phoneNumber = "01234567890";
@@ -67,5 +86,27 @@ public class ResetPasswordRequestIntegrationTest extends IntegrationTestEndpoint
                         RESET_PASSWORD_ENDPOINT, new ResetPasswordRequest(email), headers);
 
         assertEquals(400, response.getStatus());
+    }
+
+    private void registerEmail() {
+        notifyStub.register(
+                "/v2/notifications/email",
+                201,
+                "application/json",
+                "{"
+                        + "  \"id\": \"740e5834-3a29-46b4-9a6f-16142fde533a\","
+                        + "  \"reference\": \"STRING\","
+                        + "  \"content\": {"
+                        + "    \"subject\": \"SUBJECT TEXT\","
+                        + "    \"body\": \"MESSAGE TEXT\",\n"
+                        + "    \"from_email\": \"SENDER EMAIL\""
+                        + "  },"
+                        + "  \"uri\": \"http://localhost:8888/v2/notifications/a-message-id\","
+                        + "  \"template\": {"
+                        + "    \"id\": \"f33517ff-2a88-4f6e-b855-c550268ce08a\","
+                        + "    \"version\": 1,"
+                        + "    \"uri\": \"http://localhost:8888/v2/template/f33517ff-2a88-4f6e-b855-c550268ce08a\""
+                        + "  }"
+                        + "}");
     }
 }
