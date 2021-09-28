9f19d9e18c0af941d7ab7d5c53dfa67e408e5914
dbes-gds <daniel.besbrode@digital.cabinet-office.gov.uk>
2021-09-28 14:38:51 +0100

    GOVSI-748: create shared terraform folder to support common services
    Common services used by different apis move to a shared terraform folder that runs before api creation.
    Means that common services can be created once and then re-used in different apis.
    
    To be accompanied by pipeline changes to run the terraform correctly in the build.

--- ci/terraform/oidc/outputs.tf
+++ ci/terraform/oidc/outputs.tf
@@ -7,17 +7,7 @@ output "api_gateway_root_id" {
 }
 
 output "token_signing_key_alias" {
-  value = aws_kms_alias.id_token_signing_key_alias.name
-}
-
-output "stub_rp_client_credentials" {
-  value = [for i, rp in var.stub_rp_clients : {
-    client_name = rp.client_name
-    client_id   = random_string.stub_rp_client_id[i].result
-    private_key = tls_private_key.stub_rp_client_private_key[i].private_key_pem
-    public_key  = tls_private_key.stub_rp_client_private_key[i].public_key_pem
-  }]
-  sensitive = true
+  value = local.id_token_signing_key_alias_name
 }
 
 output "frontend_api_key" {
