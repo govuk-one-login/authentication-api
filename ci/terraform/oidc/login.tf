4d82cfad4a9907861fb612b95d2154d31da90a3c
dbes-gds <daniel.besbrode@digital.cabinet-office.gov.uk>
2021-09-28 14:38:51 +0100

    GOVSI-748: create shared terraform folder to support common services
    Common services used by different apis move to a shared terraform folder that runs before api creation.
    Means that common services can be created once and then re-used in different apis.
    
    To be accompanied by pipeline changes to run the terraform correctly in the build.

--- ci/terraform/oidc/login.tf
+++ ci/terraform/oidc/login.tf
@@ -4,14 +4,16 @@ module "login" {
   endpoint_name   = "login"
   path_part       = "login"
   endpoint_method = "POST"
+  environment     = var.environment
+
   handler_environment_variables = {
     ENVIRONMENT              = var.environment
     BASE_URL                 = local.api_base_url
     EVENTS_SNS_TOPIC_ARN     = aws_sns_topic.events.arn
     LOCALSTACK_ENDPOINT      = var.use_localstack ? var.localstack_endpoint : null
-    REDIS_HOST               = var.use_localstack ? var.external_redis_host : aws_elasticache_replication_group.sessions_store[0].primary_endpoint_address
-    REDIS_PORT               = var.use_localstack ? var.external_redis_port : aws_elasticache_replication_group.sessions_store[0].port
-    REDIS_PASSWORD           = var.use_localstack ? var.external_redis_password : random_password.redis_password.result
+    REDIS_HOST               = var.external_redis_host
+    REDIS_PORT               = var.external_redis_port
+    REDIS_PASSWORD           = var.external_redis_password
     REDIS_TLS                = var.redis_use_tls
     DYNAMO_ENDPOINT          = var.use_localstack ? var.lambda_dynamo_endpoint : null
     TERMS_CONDITIONS_VERSION = var.terms_and_conditions
@@ -23,10 +25,9 @@ module "login" {
   execution_arn             = aws_api_gateway_rest_api.di_authentication_api.execution_arn
   api_deployment_stage_name = var.api_deployment_stage_name
   lambda_zip_file           = var.frontend_api_lambda_zip_file
-  security_group_id         = aws_vpc.authentication.default_security_group_id
-  subnet_id                 = aws_subnet.authentication.*.id
-  environment               = var.environment
-  lambda_role_arn           = aws_iam_role.lambda_iam_role.arn
+  security_group_id         = var.authentication_security_group_id
+  subnet_id                 = var.authentication_subnet_ids
+  lambda_role_arn           = var.lambda_iam_role_arn
   logging_endpoint_enabled  = var.logging_endpoint_enabled
   logging_endpoint_arn      = var.logging_endpoint_arn
   default_tags              = local.default_tags
