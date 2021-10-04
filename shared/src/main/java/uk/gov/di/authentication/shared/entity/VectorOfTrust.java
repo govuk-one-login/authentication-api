6ba0246d87b68514d0db884925bd859ad5a199f9
james.howes <james.howes@digital.cabinet-office.gov.uk>
2021-10-04 15:20:12 +0100

    GOVSI-871 - Add VTR to AuthorisationIntegrationTest
    - Add the vtr attribute to the shouldRedirectToLoginWhenNoCookie test to ensure it gets processed correctly
    - Add extra logging

--- shared/src/main/java/uk/gov/di/authentication/shared/entity/VectorOfTrust.java
+++ shared/src/main/java/uk/gov/di/authentication/shared/entity/VectorOfTrust.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import net.minidev.json.JSONArray;
 import net.minidev.json.parser.JSONParser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,6 +15,8 @@ import static net.minidev.json.parser.JSONParser.DEFAULT_PERMISSIVE_MODE;
 import static uk.gov.di.authentication.shared.entity.CredentialTrustLevel.retrieveCredentialTrustLevel;
 
 public class VectorOfTrust {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(VectorOfTrust.class);
 
     @JsonProperty("credential_trust_level")
     private final CredentialTrustLevel credentialTrustLevel;
@@ -30,6 +34,9 @@ public class VectorOfTrust {
 
     public static final VectorOfTrust parseFromAuthRequestAttribute(List<String> vtr) {
         if (Objects.isNull(vtr) || vtr.isEmpty()) {
+            LOGGER.info(
+                    "VTR attribute is not present so defaulting to {}",
+                    CredentialTrustLevel.getDefault().getValue());
             return new VectorOfTrust(CredentialTrustLevel.getDefault());
         }
         JSONParser parser = new JSONParser(DEFAULT_PERMISSIVE_MODE);
@@ -37,13 +44,18 @@ public class VectorOfTrust {
         try {
             vtrJsonArray = (JSONArray) parser.parse(vtr.get(0));
         } catch (net.minidev.json.parser.ParseException | ClassCastException e) {
+            LOGGER.error("Error when parsing vtr attribute", e);
             throw new IllegalArgumentException("Invalid VTR attribute", e);
         }
         List<String> vtrSets = new ArrayList<>();
         for (int i = 0; i < vtrJsonArray.size(); i++) {
             vtrSets.add((String) vtrJsonArray.get(i));
         }
-        return new VectorOfTrust(retrieveCredentialTrustLevel(vtrSets));
+        CredentialTrustLevel credentialTrustLevel = retrieveCredentialTrustLevel(vtrSets);
+        LOGGER.info(
+                "VTR has been processed at credentialTrustLevel {}",
+                credentialTrustLevel.getValue());
+        return new VectorOfTrust(credentialTrustLevel);
     }
 
     public static VectorOfTrust getDefaults() {
