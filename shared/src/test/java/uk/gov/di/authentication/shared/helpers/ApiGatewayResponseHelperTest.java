e0df0ede9f06d1ab4670c53a56dfa4384a3c5a9e
dannyhayes-gds <daniel.hayes@digital.cabinet-office.gov.uk>
2021-10-05 16:29:03 +0100

    Add default security headers to all api gateway responses

---
+++ shared/src/test/java/uk/gov/di/authentication/shared/helpers/ApiGatewayResponseHelperTest.java
@@ -0,0 +1,64 @@
+package uk.gov.di.authentication.shared.helpers;
+
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import org.apache.http.HttpHeaders;
+import org.junit.jupiter.api.Test;
+import uk.gov.di.authentication.shared.entity.ErrorResponse;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.collection.IsMapContaining.hasEntry;
+
+public class ApiGatewayResponseHelperTest {
+
+    @Test
+    void ShouldAddDefaultSecurityHeadersForErrorResponses() {
+        APIGatewayProxyResponseEvent result =
+                ApiGatewayResponseHelper.generateApiGatewayProxyErrorResponse(
+                        404, ErrorResponse.ERROR_1000);
+
+        assertThat(result.getHeaders(), hasEntry(HttpHeaders.CACHE_CONTROL, "no-cache, no-store"));
+        assertThat(result.getHeaders(), hasEntry(HttpHeaders.PRAGMA, "no-cache"));
+        assertThat(result.getHeaders(), hasEntry("X-XSS-Protection", "1; mode=block"));
+        assertThat(result.getHeaders(), hasEntry("X-Content-Type-Options", "nosniff"));
+        assertThat(
+                result.getHeaders(), hasEntry("Content-Security-Policy", "frame-ancestors 'none'"));
+        assertThat(
+                result.getHeaders(),
+                hasEntry("Strict-Transport-Security", "max-age=31536000; includeSubDomains"));
+        assertThat(result.getHeaders(), hasEntry("X-Frame-Options", "DENY"));
+    }
+
+    @Test
+    void ShouldAddDefaultSecurityHeadersForSuccessResponses() {
+        APIGatewayProxyResponseEvent result =
+                ApiGatewayResponseHelper.generateEmptySuccessApiGatewayResponse();
+
+        assertThat(result.getHeaders(), hasEntry(HttpHeaders.CACHE_CONTROL, "no-cache, no-store"));
+        assertThat(result.getHeaders(), hasEntry(HttpHeaders.PRAGMA, "no-cache"));
+        assertThat(result.getHeaders(), hasEntry("X-XSS-Protection", "1; mode=block"));
+        assertThat(result.getHeaders(), hasEntry("X-Content-Type-Options", "nosniff"));
+        assertThat(
+                result.getHeaders(), hasEntry("Content-Security-Policy", "frame-ancestors 'none'"));
+        assertThat(
+                result.getHeaders(),
+                hasEntry("Strict-Transport-Security", "max-age=31536000; includeSubDomains"));
+        assertThat(result.getHeaders(), hasEntry("X-Frame-Options", "DENY"));
+    }
+
+    @Test
+    void shouldAddDefaultSecurityHeadersForAllResponses() {
+        APIGatewayProxyResponseEvent result =
+                ApiGatewayResponseHelper.generateApiGatewayProxyResponse(200, "test-body", null);
+
+        assertThat(result.getHeaders(), hasEntry(HttpHeaders.CACHE_CONTROL, "no-cache, no-store"));
+        assertThat(result.getHeaders(), hasEntry(HttpHeaders.PRAGMA, "no-cache"));
+        assertThat(result.getHeaders(), hasEntry("X-XSS-Protection", "1; mode=block"));
+        assertThat(result.getHeaders(), hasEntry("X-Content-Type-Options", "nosniff"));
+        assertThat(
+                result.getHeaders(), hasEntry("Content-Security-Policy", "frame-ancestors 'none'"));
+        assertThat(
+                result.getHeaders(),
+                hasEntry("Strict-Transport-Security", "max-age=31536000; includeSubDomains"));
+        assertThat(result.getHeaders(), hasEntry("X-Frame-Options", "DENY"));
+    }
+}
