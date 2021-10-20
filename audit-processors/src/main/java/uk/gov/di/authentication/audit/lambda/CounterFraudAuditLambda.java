d3d573138f62c551a632577d6013d08d8ec17744
Alan Carter <alan.carter@digital.cabinet-office.gov.uk>
2021-10-20 14:50:29 +0100

    GOVSI-865: Create CounterFraudAuditLambda
    * Add CounterFraudAuditLambda and get it to handle any fields which don't need to be hashed.
    * Some fields from User are not handled yet as they will require hashing.

---
+++ audit-processors/src/main/java/uk/gov/di/authentication/audit/lambda/CounterFraudAuditLambda.java
@@ -0,0 +1,38 @@
+package uk.gov.di.authentication.audit.lambda;
+
+import org.apache.logging.log4j.message.ObjectMessage;
+import uk.gov.di.audit.AuditPayload.AuditEvent;
+import uk.gov.di.authentication.shared.services.ConfigurationService;
+import uk.gov.di.authentication.shared.services.KmsConnectionService;
+
+import java.util.HashMap;
+
+public class CounterFraudAuditLambda extends BaseAuditHandler {
+
+    CounterFraudAuditLambda(
+            KmsConnectionService kmsConnectionService, ConfigurationService service) {
+        super(kmsConnectionService, service);
+    }
+
+    CounterFraudAuditLambda() {
+        super();
+    }
+
+    @Override
+    void handleAuditEvent(AuditEvent auditEvent) {
+        var eventData = new HashMap<String, String>();
+
+        eventData.put("event-id", auditEvent.getEventId());
+        eventData.put("request-id", auditEvent.getRequestId());
+        eventData.put("session-id", auditEvent.getSessionId());
+        eventData.put("client-id", auditEvent.getClientId());
+        eventData.put("timestamp", auditEvent.getTimestamp());
+        eventData.put("event-name", auditEvent.getEventName());
+
+        AuditEvent.User user = auditEvent.getUser();
+        // TODO - hash other field from the user object and include them too.
+        eventData.put("user.ip-address", user.getIpAddress());
+
+        LOG.info(new ObjectMessage(eventData));
+    }
+}
