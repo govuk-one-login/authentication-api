54fe5629af831288e58db700cc81d4035fccde66
Alex Wilson <alex.wilson@digital.cabinet-office.gov.uk>
2021-10-22 11:42:22 +0100

    GOVSI-865: Apply HMAC-SHA256 to sensitive fields before logging

--- audit-processors/src/test/java/uk/gov/di/authentication/audit/lambda/CounterFraudAuditLambdaTest.java
+++ audit-processors/src/test/java/uk/gov/di/authentication/audit/lambda/CounterFraudAuditLambdaTest.java
@@ -9,8 +9,11 @@ import org.apache.logging.log4j.core.Logger;
 import org.apache.logging.log4j.core.appender.AbstractAppender;
 import org.apache.logging.log4j.core.config.Property;
 import org.apache.logging.log4j.core.impl.MutableLogEvent;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import uk.gov.di.audit.AuditPayload.AuditEvent;
+import uk.gov.di.audit.AuditPayload.AuditEvent.User;
 import uk.gov.di.audit.AuditPayload.SignedAuditEvent;
 import uk.gov.di.authentication.shared.services.ConfigurationService;
 import uk.gov.di.authentication.shared.services.KmsConnectionService;
@@ -33,20 +36,23 @@ public class CounterFraudAuditLambdaTest {
 
     private final KmsConnectionService kms = mock(KmsConnectionService.class);
     private final ConfigurationService config = mock(ConfigurationService.class);
+    private final ListAppender appender = new ListAppender();
 
-    @Test
-    void handlesRequestsAppropriately() {
+    @BeforeEach
+    public void setUp() {
         Logger logger = (Logger) LogManager.getLogger(CounterFraudAuditLambda.class);
 
-        var appender = new ListAppender();
         appender.start();
-
         logger.addAppender(appender);
 
         when(config.getAuditSigningKeyAlias()).thenReturn("key_alias");
+        when(config.getAuditHmacSecret()).thenReturn("i-am-a-fake-hash-key");
         when(kms.validateSignature(any(ByteBuffer.class), any(ByteBuffer.class), anyString()))
                 .thenReturn(true);
+    }
 
+    @Test
+    void handlesRequestsAppropriately() {
         var handler = new CounterFraudAuditLambda(kms, config);
 
         var payload =
@@ -61,6 +67,50 @@ public class CounterFraudAuditLambdaTest {
         LogEvent logEvent = appender.getEvents().get(1);
 
         assertThat(logEvent, hasMDCProperty("event-id", "foo"));
+    }
+
+    @Test
+    void shouldHashSensitiveFields() {
+        var handler = new CounterFraudAuditLambda(kms, config);
+
+        var payload =
+                SignedAuditEvent.newBuilder()
+                        .setSignature(ByteString.copyFrom("signature".getBytes()))
+                        .setPayload(
+                                AuditEvent.newBuilder()
+                                        .setEventId("foo")
+                                        .setUser(
+                                                User.newBuilder()
+                                                        .setEmail(
+                                                                "test-example@digital.cabinet-office.gov.uk")
+                                                        .setId("some-id")
+                                                        .setPhoneNumber("some-phone-number")
+                                                        .build())
+                                        .build()
+                                        .toByteString())
+                        .build();
+
+        handler.handleRequest(inputEvent(payload), null);
+
+        System.out.println(appender.getEvents());
+
+        LogEvent logEvent = appender.getEvents().get(1);
+
+        assertThat(
+                logEvent,
+                hasMDCProperty(
+                        "user.email",
+                        "dbc2c80d5e663075eb736f52df8446c109878f1a27b9d2f7db634d4e64923c94"));
+        assertThat(
+                logEvent,
+                hasMDCProperty(
+                        "user.id",
+                        "0e49411b4a5da564d867bef289f129fe7faa1d3341a458344e790c522d451a20"));
+        assertThat(
+                logEvent,
+                hasMDCProperty(
+                        "user.phone",
+                        "f264cf9189f466ecdec47c450dfd0e13a59f85dfc1e63ef93d3870ef6b927821"));
     }
 
     private SNSEvent inputEvent(SignedAuditEvent payload) {
@@ -94,5 +144,11 @@ public class CounterFraudAuditLambdaTest {
         public List<LogEvent> getEvents() {
             return events;
         }
+    }
+
+    @AfterEach
+    public void tearDown() {
+        Logger logger = (Logger) LogManager.getLogger(CounterFraudAuditLambda.class);
+        logger.removeAppender(appender);
     }
 }
