5b300a0bfd97bd098d7eb9066c2d2798803aba40
Alex Wilson <alex.wilson@digital.cabinet-office.gov.uk>
2021-10-26 18:45:24 +0100

    GOVSI-1025: Grant lambda roles access to the audit signing key in account management

--- ci/terraform/account-management/events.tf
+++ ci/terraform/account-management/events.tf
@@ -4,4 +4,5 @@ data "aws_sns_topic" "events" {
 
 locals {
   audit_signing_key_alias_name = data.terraform_remote_state.shared.outputs.audit_signing_key_alias_name
+  audit_signing_key_arn        = data.terraform_remote_state.shared.outputs.audit_signing_key_arn
 }
