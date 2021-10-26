f686b59c561bbabce7473d577d278e67b445afb5
Alex Wilson <alex.wilson@digital.cabinet-office.gov.uk>
2021-10-26 17:05:43 +0100

    GOVSI-1025: Prepare account management API for auditing

---
+++ account-management-api/src/main/java/uk/gov/di/accountmanagement/domain/AccountManagementAuditableEvent.java
@@ -0,0 +1,7 @@
+package uk.gov.di.accountmanagement.domain;
+
+import uk.gov.di.authentication.shared.domain.AuditableEvent;
+
+public enum AccountManagementAuditableEvent implements AuditableEvent {
+
+}
