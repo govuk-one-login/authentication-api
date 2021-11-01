ebff2d681f937d9846dcac729bbadc6f122269f5
Alex Wilson <alex.wilson@digital.cabinet-office.gov.uk>
2021-11-01 09:55:22 +0000

    BAU: Programmatically fill payload fields

--- shared/src/test/java/uk/gov/di/authentication/audit/AuditPayloadConsistencyTest.java
+++ shared/src/test/java/uk/gov/di/authentication/audit/AuditPayloadConsistencyTest.java
@@ -10,8 +10,11 @@ import uk.gov.di.audit.AuditPayload.AuditEvent;
 import uk.gov.di.audit.AuditPayload.AuditEvent.User;
 import uk.gov.di.audit.AuditPayload.SignedAuditEvent;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 
@@ -28,28 +31,13 @@ class AuditPayloadConsistencyTest {
 
     @Test
     void shouldHandleFullObjects() {
+        var eventBuilder = fillFieldsWithTestInput(AuditEvent.newBuilder());
+        var userBuilder = fillFieldsWithTestInput(User.newBuilder());
 
-        var emptyObject =
-                AuditEvent.newBuilder()
-                        .setEventId("test-setEventId")
-                        .setRequestId("test-setRequestId")
-                        .setSessionId("test-setSessionId")
-                        .setClientId("test-setClientId")
-                        .setTimestamp("test-setTimestamp")
-                        .setEventName("test-setEventName")
-                        .setUser(
-                                User.newBuilder()
-                                        .setId("test-setId")
-                                        .setEmail("test-setEmail")
-                                        .setIpAddress("test-setIpAddress")
-                                        .setPhoneNumber("test-setPhoneNumber")
-                                        .build())
-                        .putExtensions("test-putExtensions", "test-putExtensions")
-                        .putPlatform("test-putPlatform", "test-putPlatform")
-                        .build();
+        var realObject = eventBuilder.setUser(userBuilder.build()).build();
 
         assertThat(
-                eventWithSignature("signature", emptyObject),
+                eventWithSignature("signature", realObject),
                 hasHashValue("c988acfe4e7f90d39d5749f8842573d8d091cdd9e5e5a5d79ca65b59508e82a5"));
     }
 
@@ -93,5 +81,36 @@ class AuditPayloadConsistencyTest {
                         .appendText("]");
             }
         };
+    }
+
+    private <T> T fillFieldsWithTestInput(T builder) {
+        Arrays.stream(builder.getClass().getMethods())
+                .filter(this::isAWriterMethod)
+                .filter(this::hasStringParameter)
+                .forEach(
+                        method -> {
+                            try {
+                                if (method.getName().startsWith("set")) {
+                                    method.invoke(builder, "test-" + method.getName());
+                                } else {
+                                    method.invoke(
+                                            builder,
+                                            "test-" + method.getName(),
+                                            "test-" + method.getName());
+                                }
+                            } catch (IllegalAccessException | InvocationTargetException e) {
+                                throw new RuntimeException(e);
+                            }
+                        });
+
+        return builder;
+    }
+
+    private boolean hasStringParameter(Method method) {
+        return method.getParameterTypes()[0].equals(String.class);
+    }
+
+    private boolean isAWriterMethod(Method method) {
+        return method.getName().startsWith("set") || method.getName().startsWith("put");
     }
 }
