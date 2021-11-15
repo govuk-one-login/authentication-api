89dc2f8c64636dadf9aebe3134f24a1be7c39dbb
Alex Wilson <alex.wilson@digital.cabinet-office.gov.uk>
2021-11-15 17:52:37 +0000

    GOVSI-1055: Do not read role name or arns from shared state.

--- ci/terraform/oidc/shared.tf
+++ ci/terraform/oidc/shared.tf
@@ -20,14 +20,6 @@ locals {
   authentication_vpc_arn                 = data.terraform_remote_state.shared.outputs.authentication_vpc_arn
   authentication_security_group_id       = data.terraform_remote_state.shared.outputs.authentication_security_group_id
   authentication_subnet_ids              = data.terraform_remote_state.shared.outputs.authentication_subnet_ids
-  lambda_iam_role_arn                    = data.terraform_remote_state.shared.outputs.lambda_iam_role_arn
-  lambda_iam_role_name                   = data.terraform_remote_state.shared.outputs.lambda_iam_role_name
-  dynamo_sqs_lambda_iam_role_arn         = data.terraform_remote_state.shared.outputs.dynamo_sqs_lambda_iam_role_arn
-  dynamo_sqs_lambda_iam_role_name        = data.terraform_remote_state.shared.outputs.dynamo_sqs_lambda_iam_role_name
-  sqs_lambda_iam_role_arn                = data.terraform_remote_state.shared.outputs.sqs_lambda_iam_role_arn
-  sqs_lambda_iam_role_name               = data.terraform_remote_state.shared.outputs.sqs_lambda_iam_role_name
-  email_lambda_iam_role_arn              = data.terraform_remote_state.shared.outputs.email_lambda_iam_role_arn
-  token_lambda_iam_role_arn              = data.terraform_remote_state.shared.outputs.token_lambda_iam_role_arn
   id_token_signing_key_alias_name        = data.terraform_remote_state.shared.outputs.id_token_signing_key_alias_name
   id_token_signing_key_alias_arn         = data.terraform_remote_state.shared.outputs.id_token_signing_key_alias_arn
   audit_signing_key_alias_name           = data.terraform_remote_state.shared.outputs.audit_signing_key_alias_name
