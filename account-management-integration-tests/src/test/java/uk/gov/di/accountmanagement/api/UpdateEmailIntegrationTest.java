720555b1b8ac89c37763357ed9786e1caf1c95bd
Chris Clayson <chris.clayson@digital.cabinet-office.gov.uk>
2021-11-18 22:15:03 +0000

    GOVSI-1113: Capture Notify request on Update Email integration test
    - Add a Notify stub to the integration and capture the e-mail request
    - Assert that the e-mail request contains the new e-mail address

--- account-management-integration-tests/src/test/java/uk/gov/di/accountmanagement/api/UpdateEmailIntegrationTest.java
+++ account-management-integration-tests/src/test/java/uk/gov/di/accountmanagement/api/UpdateEmailIntegrationTest.java
@@ -1,11 +1,16 @@
 package uk.gov.di.accountmanagement.api;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.nimbusds.oauth2.sdk.id.Subject;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
 import uk.gov.di.accountmanagement.entity.UpdateEmailRequest;
 import uk.gov.di.accountmanagement.lambda.UpdateEmailHandler;
+import uk.gov.di.authentication.shared.helpers.ObjectMapperFactory;
 import uk.gov.di.authentication.sharedtest.basetest.ApiGatewayHandlerIntegrationTest;
+import uk.gov.di.authentication.sharedtest.extensions.NotifyStubExtension;
 import uk.gov.di.authentication.sharedtest.helper.DynamoHelper;
 import uk.gov.di.authentication.sharedtest.helper.RedisHelper;
 
@@ -13,7 +18,10 @@ import java.util.Map;
 import java.util.Optional;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 import static uk.gov.di.authentication.sharedtest.matchers.APIGatewayProxyResponseEventMatcher.hasStatus;
+import static uk.gov.di.authentication.sharedtest.matchers.JsonMatcher.hasField;
+import static uk.gov.di.authentication.sharedtest.matchers.JsonMatcher.hasFieldWithValue;
 
 public class UpdateEmailIntegrationTest extends ApiGatewayHandlerIntegrationTest {
 
@@ -21,13 +29,24 @@ public class UpdateEmailIntegrationTest extends ApiGatewayHandlerIntegrationTest
     private static final String NEW_EMAIL_ADDRESS = "joe.b@digital.cabinet-office.gov.uk";
     private static final Subject SUBJECT = new Subject();
 
+    @RegisterExtension
+    private static final NotifyStubExtension notify =
+            new NotifyStubExtension(8888, ObjectMapperFactory.getInstance());
+
     @BeforeEach
     void setup() {
         handler = new UpdateEmailHandler(configurationService);
+        notify.init();
+    }
+
+    @AfterEach
+    void resetStub() {
+        notify.reset();
     }
 
     @Test
-    public void shouldCallUpdateEmailEndpointAndReturn204WhenLoginIsSuccessful() {
+    public void shouldCallUpdateEmailEndpointAndReturn204WhenLoginIsSuccessful()
+            throws JsonProcessingException {
         String publicSubjectID = DynamoHelper.signUp(EXISTING_EMAIL_ADDRESS, "password-1", SUBJECT);
         String otp = RedisHelper.generateAndSaveEmailCode(NEW_EMAIL_ADDRESS, 300);
         var response =
@@ -41,5 +60,11 @@ public class UpdateEmailIntegrationTest extends ApiGatewayHandlerIntegrationTest
                         Map.of("principalId", publicSubjectID));
 
         assertThat(response, hasStatus(204));
+
+        var notifyRequest = notify.waitForRequest(60);
+
+        assertThat(notifyRequest, hasField("personalisation"));
+        var personalisation = notifyRequest.get("personalisation");
+        assertThat(personalisation, hasFieldWithValue("email-address", equalTo(NEW_EMAIL_ADDRESS)));
     }
 }
