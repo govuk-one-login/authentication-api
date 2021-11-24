4eb4e1b06a8590d37ad5a4bcc9e3817c2919ea99
Chris Clayson <chris.clayson@digital.cabinet-office.gov.uk>
2021-11-24 14:39:51 +0000

    GOVSI-1114: Use custom JsonMatcher in AM NotificationHandler integration tests
    - Swap the assertions on the received Notify requests to use the new custom JsonMatcher
    - Refactor generation of code for tests and assertions on code

--- account-management-integration-tests/src/test/java/uk/gov/di/accountmanagement/queuehandlers/NotificationHandlerIntegrationTest.java
+++ account-management-integration-tests/src/test/java/uk/gov/di/accountmanagement/queuehandlers/NotificationHandlerIntegrationTest.java
@@ -8,46 +8,55 @@ import uk.gov.di.accountmanagement.lambda.NotificationHandler;
 import uk.gov.di.authentication.shared.entity.NotifyRequest;
 import uk.gov.di.authentication.sharedtest.basetest.NotifyIntegrationTest;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.security.SecureRandom;
+
+import static java.lang.String.format;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 import static org.mockito.Mockito.mock;
 import static uk.gov.di.authentication.shared.entity.NotificationType.VERIFY_EMAIL;
 import static uk.gov.di.authentication.shared.entity.NotificationType.VERIFY_PHONE_NUMBER;
+import static uk.gov.di.authentication.sharedtest.matchers.JsonMatcher.hasField;
+import static uk.gov.di.authentication.sharedtest.matchers.JsonMatcher.hasFieldWithValue;
 
 public class NotificationHandlerIntegrationTest extends NotifyIntegrationTest {
 
     private static final String TEST_PHONE_NUMBER = "01234567811";
     private static final String TEST_EMAIL_ADDRESS = "joe.bloggs@example.com";
-    private static final int VERIFICATION_CODE_LENGTH = 6;
 
     private static final NotificationHandler handler =
             new NotificationHandler(configurationService);
+    public final String CODE = format("%06d", new SecureRandom().nextInt(999999));
 
     @Test
     void shouldCallNotifyWhenValidEmailRequestIsAddedToQueue() throws JsonProcessingException {
-        NotifyRequest notifyRequest = new NotifyRequest(TEST_EMAIL_ADDRESS, VERIFY_EMAIL, "162534");
+        NotifyRequest notifyRequest = new NotifyRequest(TEST_EMAIL_ADDRESS, VERIFY_EMAIL, CODE);
 
         handler.handleRequest(createSqsEvent(notifyRequest), mock(Context.class));
 
         JsonNode request = notifyStub.waitForRequest(60);
+
+        assertThat(request, hasField("personalisation"));
         JsonNode personalisation = request.get("personalisation");
-        assertEquals(TEST_EMAIL_ADDRESS, request.get("email_address").asText());
-        assertEquals(TEST_EMAIL_ADDRESS, personalisation.get("email-address").asText());
-        assertEquals(
-                VERIFICATION_CODE_LENGTH, personalisation.get("validation-code").asText().length());
+        assertThat(
+                personalisation, hasFieldWithValue("email_address", equalTo(TEST_EMAIL_ADDRESS)));
+        assertThat(
+                personalisation, hasFieldWithValue("email-address", equalTo(TEST_EMAIL_ADDRESS)));
+        assertThat(personalisation, hasFieldWithValue("validation-code", equalTo(CODE)));
     }
 
     @Test
     void shouldCallNotifyWhenValidPhoneNumberRequestIsAddedToQueue()
             throws JsonProcessingException {
         NotifyRequest notifyRequest =
-                new NotifyRequest(TEST_PHONE_NUMBER, VERIFY_PHONE_NUMBER, "162534");
+                new NotifyRequest(TEST_PHONE_NUMBER, VERIFY_PHONE_NUMBER, CODE);
 
         handler.handleRequest(createSqsEvent(notifyRequest), mock(Context.class));
 
         JsonNode request = notifyStub.waitForRequest(60);
+        assertThat(request, hasField("personalisation"));
         JsonNode personalisation = request.get("personalisation");
-        assertEquals(TEST_PHONE_NUMBER, request.get("phone_number").asText());
-        assertEquals(
-                VERIFICATION_CODE_LENGTH, personalisation.get("validation-code").asText().length());
+        assertThat(personalisation, hasFieldWithValue("phone_number", equalTo(TEST_PHONE_NUMBER)));
+        assertThat(personalisation, hasFieldWithValue("validation-code", equalTo(CODE)));
     }
 }
