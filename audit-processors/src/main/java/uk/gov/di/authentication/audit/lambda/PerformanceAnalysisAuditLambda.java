ac886457031ded7aa1d24229a98349b72c214748
Alex Wilson <alex.wilson@digital.cabinet-office.gov.uk>
2022-01-13 16:59:17 +0000

    AUTH-866: Log out fields for performance and insight analysis

--- audit-processors/src/main/java/uk/gov/di/authentication/audit/lambda/PerformanceAnalysisAuditLambda.java
+++ audit-processors/src/main/java/uk/gov/di/authentication/audit/lambda/PerformanceAnalysisAuditLambda.java
@@ -1,8 +1,15 @@
 package uk.gov.di.authentication.audit.lambda;
 
+import org.apache.logging.log4j.message.ObjectMessage;
 import uk.gov.di.audit.AuditPayload.AuditEvent;
+import uk.gov.di.audit.AuditPayload.AuditEvent.User;
 import uk.gov.di.authentication.shared.services.ConfigurationService;
 import uk.gov.di.authentication.shared.services.KmsConnectionService;
+
+import java.util.HashMap;
+
+import static org.apache.commons.codec.binary.Hex.encodeHexString;
+import static uk.gov.di.authentication.audit.helper.HmacSha256Helper.hmacSha256;
 
 public class PerformanceAnalysisAuditLambda extends BaseAuditHandler {
 
@@ -17,6 +24,22 @@ public class PerformanceAnalysisAuditLambda extends BaseAuditHandler {
 
     @Override
     void handleAuditEvent(AuditEvent auditEvent) {
-        LOG.info("Hello, world!");
+        var eventData = new HashMap<String, String>();
+
+        eventData.put("event-id", auditEvent.getEventId());
+        eventData.put("request-id", auditEvent.getRequestId());
+        eventData.put("session-id", auditEvent.getSessionId());
+        eventData.put("timestamp", auditEvent.getTimestamp());
+        eventData.put("event-name", auditEvent.getEventName());
+
+        User user = auditEvent.getUser();
+
+        var hmacKey = this.service.getAuditHmacSecret();
+
+        if (user.getId() != null && !user.getId().isBlank()) {
+            eventData.put("user-id", encodeHexString(hmacSha256(user.getId(), hmacKey)));
+        }
+
+        LOG.info(new ObjectMessage(eventData));
     }
 }
