419b19703629c5879c0e96ad3a777f7b5133c7d5
james.howes <james.howes@digital.cabinet-office.gov.uk>
2022-02-09 14:35:27 +0000

    AUTH-1174 - Create new Lambda role for authorize
    - Limit the permissions given to the authorize lambda but creating a new role specific to it.
    - Create new policies for read access to the client registration and user tables. The majority of the lambdas only need READ access.

--- ci/terraform/oidc/dynamo-policies.tf
+++ ci/terraform/oidc/dynamo-policies.tf
@@ -41,6 +41,28 @@ data "aws_iam_policy_document" "dynamo_access_policy_document" {
   }
 }
 
+data "aws_iam_policy_document" "dynamo_user_read_policy_document" {
+  statement {
+    sid    = "AllowAccessToDynamoTables"
+    effect = "Allow"
+
+    actions = [
+      "dynamodb:BatchGetItem",
+      "dynamodb:DescribeStream",
+      "dynamodb:DescribeTable",
+      "dynamodb:Get*",
+      "dynamodb:Query",
+      "dynamodb:Scan",
+    ]
+    resources = [
+      data.aws_dynamodb_table.user_credentials_table.arn,
+      data.aws_dynamodb_table.user_profile_table.arn,
+      "${data.aws_dynamodb_table.user_profile_table.arn}/index/*",
+      "${data.aws_dynamodb_table.user_credentials_table.arn}/index/*",
+    ]
+  }
+}
+
 data "aws_iam_policy_document" "dynamo_client_registration_policy_document" {
   statement {
     sid    = "AllowAccessToDynamoTables"
@@ -64,6 +86,23 @@ data "aws_iam_policy_document" "dynamo_client_registration_policy_document" {
   }
 }
 
+data "aws_iam_policy_document" "dynamo_client_registration_read_policy_document" {
+  statement {
+    sid    = "AllowAccessToDynamoTables"
+    effect = "Allow"
+
+    actions = [
+      "dynamodb:BatchGetItem",
+      "dynamodb:DescribeTable",
+      "dynamodb:Get*",
+      "dynamodb:Query",
+      "dynamodb:Scan",
+    ]
+    resources = [
+      data.aws_dynamodb_table.client_registry_table.arn,
+    ]
+  }
+}
 
 data "aws_iam_policy_document" "dynamo_spot_write_access_policy_document" {
   statement {
@@ -108,6 +147,22 @@ resource "aws_iam_policy" "dynamo_client_registry_access_policy" {
   description = "IAM policy for managing write permissions to the Dynamo Client Registration table"
 
   policy = data.aws_iam_policy_document.dynamo_client_registration_policy_document.json
+}
+
+resource "aws_iam_policy" "dynamo_client_registry_read_access_policy" {
+  name_prefix = "dynamo-client-registry-read-policy"
+  path        = "/${var.environment}/oidc-shared/"
+  description = "IAM policy for managing read permissions to the Dynamo Client Registration table"
+
+  policy = data.aws_iam_policy_document.dynamo_client_registration_read_policy_document.json
+}
+
+resource "aws_iam_policy" "dynamo_user_read_access_policy" {
+  name_prefix = "dynamo-user-read-policy"
+  path        = "/${var.environment}/oidc-shared/"
+  description = "IAM policy for managing read permissions to the Dynamo User tables"
+
+  policy = data.aws_iam_policy_document.dynamo_user_read_policy_document.json
 }
 
 resource "aws_iam_policy" "dynamo_spot_write_access_policy" {
