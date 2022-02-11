3cebc5d493c5fcf53905393717295ce290311fa7
james.howes <james.howes@digital.cabinet-office.gov.uk>
2022-02-11 13:10:13 +0000

    AUTH-1174 - Create new role for Userinfo lambda

--- ci/terraform/oidc/userinfo.tf
+++ ci/terraform/oidc/userinfo.tf
@@ -1,3 +1,18 @@
+module "oidc_userinfo_role" {
+  source      = "../modules/lambda-role"
+  environment = var.environment
+  role_name   = "oidc-userinfo-role"
+  vpc_arn     = local.authentication_vpc_arn
+
+  policies_to_attach = [
+    aws_iam_policy.oidc_token_kms_signing_policy.arn,
+    aws_iam_policy.audit_signing_key_lambda_kms_signing_policy.arn,
+    aws_iam_policy.dynamo_user_read_access_policy.arn,
+    aws_iam_policy.dynamo_client_registry_read_access_policy.arn,
+    aws_iam_policy.redis_parameter_policy.arn
+  ]
+}
+
 module "userinfo" {
   source = "../modules/endpoint-module"
 
@@ -28,7 +43,7 @@ module "userinfo" {
     local.authentication_oidc_redis_security_group_id,
   ]
   subnet_id                              = local.authentication_subnet_ids
-  lambda_role_arn                        = module.oidc_default_role.arn
+  lambda_role_arn                        = module.oidc_userinfo_role.arn
   logging_endpoint_enabled               = var.logging_endpoint_enabled
   logging_endpoint_arn                   = var.logging_endpoint_arn
   cloudwatch_key_arn                     = data.terraform_remote_state.shared.outputs.cloudwatch_encryption_key_arn
