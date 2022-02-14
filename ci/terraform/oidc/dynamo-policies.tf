38b7928b3b50453c8f495a887b1ba608c7827a6e
james.howes <james.howes@digital.cabinet-office.gov.uk>
2022-02-14 11:51:30 +0000

    AUTH-1174 - Remove unused Dynamo policy

--- ci/terraform/oidc/dynamo-policies.tf
+++ ci/terraform/oidc/dynamo-policies.tf
@@ -14,33 +14,6 @@ data "aws_dynamodb_table" "spot_credential_table" {
   name = "${var.environment}-spot-credential"
 }
 
-data "aws_iam_policy_document" "dynamo_access_policy_document" {
-  statement {
-    sid    = "AllowAccessToDynamoTables"
-    effect = "Allow"
-
-    actions = [
-      "dynamodb:BatchGetItem",
-      "dynamodb:DescribeStream",
-      "dynamodb:DescribeTable",
-      "dynamodb:DeleteItem",
-      "dynamodb:Get*",
-      "dynamodb:Query",
-      "dynamodb:Scan",
-      "dynamodb:BatchWriteItem",
-      "dynamodb:UpdateItem",
-      "dynamodb:PutItem",
-    ]
-    resources = [
-      data.aws_dynamodb_table.user_credentials_table.arn,
-      data.aws_dynamodb_table.user_profile_table.arn,
-      "${data.aws_dynamodb_table.user_profile_table.arn}/index/*",
-      "${data.aws_dynamodb_table.user_credentials_table.arn}/index/*",
-      data.aws_dynamodb_table.client_registry_table.arn,
-    ]
-  }
-}
-
 data "aws_iam_policy_document" "dynamo_user_write_policy_document" {
   statement {
     sid    = "AllowAccessToDynamoTables"
@@ -164,14 +137,6 @@ data "aws_iam_policy_document" "dynamo_spot_read_access_policy_document" {
       data.aws_dynamodb_table.spot_credential_table.arn,
     ]
   }
-}
-
-resource "aws_iam_policy" "dynamo_access_policy" {
-  name_prefix = "dynamo-access-policy"
-  path        = "/${var.environment}/oidc-default/"
-  description = "IAM policy for managing Dynamo connection for a lambda"
-
-  policy = data.aws_iam_policy_document.dynamo_access_policy_document.json
 }
 
 resource "aws_iam_policy" "dynamo_client_registry_access_policy" {
