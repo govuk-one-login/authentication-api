20ea1fd73e85e102be097a5bbc4f80bfe904b091
Chris Clayson <chris.clayson@digital.cabinet-office.gov.uk>
2022-02-14 11:29:05 +0000

    AUTH-1261: Add and populate an S3 bucket for OIDC Lambda sources
    - Add a versioned S3 bucket into the OIDC Terraform, this will be used
    to store lambda source ZIP files
    - Use Terraform to copy up the provided ZIP files to the new bucket

---
+++ ci/terraform/oidc/s3-objects.tf
@@ -0,0 +1,52 @@
+resource "aws_s3_bucket" "source_bucket" {
+  bucket_prefix = "${var.environment}-lambda-source-"
+
+  versioning {
+    enabled = true
+  }
+}
+
+resource "aws_s3_bucket_object" "oidc_api_release_zip" {
+  bucket = aws_s3_bucket.source_bucket.bucket
+  key    = "oidc-api-release.zip"
+
+  server_side_encryption = "AES256"
+  source                 = var.oidc_api_lambda_zip_file
+  source_hash            = filemd5(var.oidc_api_lambda_zip_file)
+}
+
+resource "aws_s3_bucket_object" "client_api_release_zip" {
+  bucket = aws_s3_bucket.source_bucket.bucket
+  key    = "client-api-release.zip"
+
+  server_side_encryption = "AES256"
+  source                 = var.client_registry_api_lambda_zip_file
+  source_hash            = filemd5(var.client_registry_api_lambda_zip_file)
+}
+
+resource "aws_s3_bucket_object" "frontend_api_release_zip" {
+  bucket = aws_s3_bucket.source_bucket.bucket
+  key    = "frontend-api-release.zip"
+
+  server_side_encryption = "AES256"
+  source                 = var.frontend_api_lambda_zip_file
+  source_hash            = filemd5(var.frontend_api_lambda_zip_file)
+}
+
+resource "aws_s3_bucket_object" "warmer_release_zip" {
+  bucket = aws_s3_bucket.source_bucket.bucket
+  key    = "warmer-release.zip"
+
+  server_side_encryption = "AES256"
+  source                 = var.lambda_warmer_zip_file
+  source_hash            = filemd5(var.lambda_warmer_zip_file)
+}
+
+resource "aws_s3_bucket_object" "ipv_api_release_zip" {
+  bucket = aws_s3_bucket.source_bucket.bucket
+  key    = "ipv-api-release.zip"
+
+  server_side_encryption = "AES256"
+  source                 = var.ipv_api_lambda_zip_file
+  source_hash            = filemd5(var.ipv_api_lambda_zip_file)
+}
