a3ddf2e84561acef2ef069e4edb02ef1cf1c2380
Chris Clayson <chris.clayson@digital.cabinet-office.gov.uk>
2022-02-16 14:17:02 +0000

    AUTH-1293: Pass CSC value to IPV API lambdas
    - Pass the CSC ARN to the IPV API lambdas
    - Update how the IPV API filename is determined, in the deploy task,
    as the filename won't be fixed when we switch to the AWS signed
    bucket. The release ZIP file will be the ONLY ZIP file in the S3
    bucket resource.

--- ci/terraform/oidc/ipv-callback.tf
+++ ci/terraform/oidc/ipv-callback.tf
@@ -47,6 +47,7 @@ module "ipv-callback" {
   lambda_zip_file_version        = aws_s3_bucket_object.ipv_api_release_zip.version_id
   warmer_lambda_zip_file         = aws_s3_bucket_object.warmer_release_zip.key
   warmer_lambda_zip_file_version = aws_s3_bucket_object.warmer_release_zip.version_id
+  code_signing_config_arn        = local.lambda_code_signing_configuration_arn
 
   authentication_vpc_arn = local.authentication_vpc_arn
   security_group_ids = [
