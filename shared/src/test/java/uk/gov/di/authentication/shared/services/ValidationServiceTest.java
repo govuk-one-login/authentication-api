f8c8ef2a8151addb3f5bc90c7f6d184d4f1a7aee
Chris Clayson <chris.clayson@digital.cabinet-office.gov.uk>
2022-03-16 16:27:21 +0000

    AUTH-1439: Refactor ValidationService unit tests
    - Refactor ValidationService email code unit tests to be paramterizable.
    - Add scenarios for the new `RESET_PASSWORD_WITH_CODE` notification
    type.

--- shared/src/test/java/uk/gov/di/authentication/shared/services/ValidationServiceTest.java
+++ shared/src/test/java/uk/gov/di/authentication/shared/services/ValidationServiceTest.java
@@ -2,8 +2,10 @@ package uk.gov.di.authentication.shared.services;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import uk.gov.di.authentication.shared.entity.ErrorResponse;
+import uk.gov.di.authentication.shared.entity.NotificationType;
 import uk.gov.di.authentication.shared.entity.Session;
 
 import java.util.Optional;
@@ -11,14 +13,20 @@ import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import static uk.gov.di.authentication.shared.entity.NotificationType.MFA_SMS;
+import static uk.gov.di.authentication.shared.entity.NotificationType.RESET_PASSWORD_WITH_CODE;
 import static uk.gov.di.authentication.shared.entity.NotificationType.VERIFY_EMAIL;
 import static uk.gov.di.authentication.shared.entity.NotificationType.VERIFY_PHONE_NUMBER;
 
 public class ValidationServiceTest {
 
+    public static final String VALID_CODE = "123456";
+    public static final Optional<String> STORED_VALID_CODE = Optional.of(VALID_CODE);
+    public static final String INVALID_CODE = "654321";
+    private static final Optional<String> NO_CODE_STORED = Optional.empty();
     private final ValidationService validationService = new ValidationService();
 
     private static Stream<String> blankEmailAddresses() {
@@ -140,40 +148,68 @@ public class ValidationServiceTest {
                         VERIFY_PHONE_NUMBER, Optional.of("654321"), "123456", session, 5));
     }
 
-    @Test
-    void shouldReturnNoErrorWhenEmailCodeMatchesStored() {
-        assertEquals(
-                Optional.empty(),
-                validationService.validateVerificationCode(
-                        VERIFY_EMAIL, Optional.of("123456"), "123456", mock(Session.class), 5));
+    private static Stream<Arguments> emailCodeTestParameters() {
+        return Stream.of(
+                arguments(VERIFY_EMAIL, Optional.empty(), VALID_CODE, 0, STORED_VALID_CODE),
+                arguments(
+                        RESET_PASSWORD_WITH_CODE,
+                        Optional.empty(),
+                        VALID_CODE,
+                        0,
+                        STORED_VALID_CODE),
+                arguments(
+                        VERIFY_EMAIL,
+                        Optional.of(ErrorResponse.ERROR_1036),
+                        VALID_CODE,
+                        0,
+                        NO_CODE_STORED),
+                arguments(
+                        RESET_PASSWORD_WITH_CODE,
+                        Optional.of(ErrorResponse.ERROR_1039),
+                        VALID_CODE,
+                        0,
+                        NO_CODE_STORED),
+                arguments(
+                        VERIFY_EMAIL,
+                        Optional.of(ErrorResponse.ERROR_1036),
+                        INVALID_CODE,
+                        1,
+                        STORED_VALID_CODE),
+                arguments(
+                        RESET_PASSWORD_WITH_CODE,
+                        Optional.of(ErrorResponse.ERROR_1039),
+                        INVALID_CODE,
+                        1,
+                        STORED_VALID_CODE),
+                arguments(
+                        VERIFY_EMAIL,
+                        Optional.of(ErrorResponse.ERROR_1033),
+                        INVALID_CODE,
+                        6,
+                        STORED_VALID_CODE),
+                arguments(
+                        RESET_PASSWORD_WITH_CODE,
+                        Optional.of(ErrorResponse.ERROR_1040),
+                        INVALID_CODE,
+                        6,
+                        STORED_VALID_CODE));
     }
 
-    @Test
-    void shouldReturnCorrectErrorWhenStoredEmailCodeIsEmpty() {
-        assertEquals(
-                Optional.of(ErrorResponse.ERROR_1036),
-                validationService.validateVerificationCode(
-                        VERIFY_EMAIL, Optional.empty(), "123456", mock(Session.class), 5));
-    }
-
-    @Test
-    void shouldReturnCorrectErrorWhenStoredEmailCodeDoesMatchInputAndRetryLimitHasNotBeenReached() {
+    @ParameterizedTest
+    @MethodSource("emailCodeTestParameters")
+    void shouldReturnCorrectErrorForEmailValidation(
+            NotificationType notificationType,
+            Optional<ErrorResponse> expectedResult,
+            String input,
+            int previousAttempts,
+            Optional<String> storedCode) {
         Session session = mock(Session.class);
-        when(session.getRetryCount()).thenReturn(1);
-        assertEquals(
-                Optional.of(ErrorResponse.ERROR_1036),
-                validationService.validateVerificationCode(
-                        VERIFY_EMAIL, Optional.of("654321"), "123456", session, 5));
-    }
+        when(session.getRetryCount()).thenReturn(previousAttempts);
 
-    @Test
-    void shouldReturnCorrectErrorWhenStoredEmailCodeDoesMatchInputAndRetryLimitHasBeenReached() {
-        Session session = mock(Session.class);
-        when(session.getRetryCount()).thenReturn(6);
         assertEquals(
-                Optional.of(ErrorResponse.ERROR_1033),
+                expectedResult,
                 validationService.validateVerificationCode(
-                        VERIFY_EMAIL, Optional.of("654321"), "123456", session, 5));
+                        notificationType, storedCode, input, session, 5));
     }
 
     @Test
