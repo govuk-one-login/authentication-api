0ca9c9c9da77334395a06610098762f726baa158
james.howes <james.howes@digital.cabinet-office.gov.uk>
2022-04-13 16:09:53 +0100

    BAU - Give ipv-authorize write access to the user tables
    - The ipv-authorize needs write permissions as if a user does not have a salt, one will be created and then the user-profile table will be updated.

--- ci/terraform/oidc/ipv-authorize.tf
+++ ci/terraform/oidc/ipv-authorize.tf
@@ -8,6 +8,7 @@ module "ipv_authorize_role" {
     aws_iam_policy.audit_signing_key_lambda_kms_signing_policy.arn,
     aws_iam_policy.dynamo_client_registry_read_access_policy.arn,
     aws_iam_policy.dynamo_user_read_access_policy.arn,
+    aws_iam_policy.dynamo_user_write_access_policy.arn,
     aws_iam_policy.lambda_sns_policy.arn,
     aws_iam_policy.redis_parameter_policy.arn,
     aws_iam_policy.ipv_token_auth_kms_policy.arn,
