4fb305d0be61acd5789213ac85a07f9a95f8d4a7
james.howes <james.howes@digital.cabinet-office.gov.uk>
2022-04-22 08:25:36 +0100

    AUT-307 - Deploy doc app lambdas to staging
    - Start deploying the doc app lambdas to the staging environment only
    - Remove the condition on the dynamo tables so that they are built in every environment. This is similar to what we done for the SPOT tables.

--- ci/terraform/shared/variables.tf
+++ ci/terraform/shared/variables.tf
@@ -117,11 +117,6 @@ variable "provision_dynamo" {
   default = false
 }
 
-variable "doc_app_api_enabled" {
-  type    = bool
-  default = false
-}
-
 variable "ipv_api_enabled" {
   default = false
 }
