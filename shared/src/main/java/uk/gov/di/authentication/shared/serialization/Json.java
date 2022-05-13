ea06002e6a9faa8e8c952d40f52fda84a3b37fcb
Alex Wilson <alex.wilson@digital.cabinet-office.gov.uk>
2022-05-13 14:56:33 +0100

    BAU: Implement Json interface using Jackson

--- shared/src/main/java/uk/gov/di/authentication/shared/serialization/Json.java
+++ shared/src/main/java/uk/gov/di/authentication/shared/serialization/Json.java
@@ -1,5 +1,8 @@
 package uk.gov.di.authentication.shared.serialization;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import uk.gov.di.authentication.shared.helpers.ObjectMapperFactory;
+
 public interface Json {
     <T> T readValue(String body, Class<T> klass) throws JsonException;
 
@@ -9,5 +12,27 @@ public interface Json {
         public JsonException(Exception e) {
             super(e);
         }
+    }
+
+    static Json jackson() {
+        return new Json() {
+            @Override
+            public <T> T readValue(String body, Class<T> klass) throws JsonException {
+                try {
+                    return ObjectMapperFactory.getInstance().readValue(body, klass);
+                } catch (JsonProcessingException e) {
+                    throw new JsonException(e);
+                }
+            }
+
+            @Override
+            public String writeValueAsString(Object object) throws JsonException {
+                try {
+                    return ObjectMapperFactory.getInstance().writeValueAsString(object);
+                } catch (JsonProcessingException e) {
+                    throw new JsonException(e);
+                }
+            }
+        };
     }
 }
