9e2de5507961111a6e996a3e3e4979d6ad8ead31
Chris Clayson <chris.clayson@digital.cabinet-office.gov.uk>
2022-05-26 16:10:19 +0100

    AUT-356: Update DocApp API to GSON
    - Update entities to use GSON annotations
    - Update lambda to use `SerializationService` instead of Jackson

--- doc-checking-app-api/src/main/java/uk/gov/di/authentication/app/entity/DocAppAuthorisationResponse.java
+++ doc-checking-app-api/src/main/java/uk/gov/di/authentication/app/entity/DocAppAuthorisationResponse.java
@@ -1,10 +1,14 @@
 package uk.gov.di.authentication.app.entity;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
 
 public class DocAppAuthorisationResponse {
 
     @JsonProperty("redirectUri")
+    @SerializedName("redirectUri")
+    @Expose
     private String redirectUri;
 
     public DocAppAuthorisationResponse(
