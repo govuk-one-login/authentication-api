bb09e69c7819276021a512f5c16a0ed10ec9adaa
Chris Clayson <chris.clayson@digital.cabinet-office.gov.uk>
2022-05-30 17:56:08 +0100

    AUT-356: Use custom validator in SerializationService
    - Replace use of Hibernate Validator with our custom `RequiredFieldValidator` class.
    - Allow override of validator deserialization time.

--- shared/src/main/java/uk/gov/di/authentication/shared/serialization/Json.java
+++ shared/src/main/java/uk/gov/di/authentication/shared/serialization/Json.java
@@ -1,13 +1,21 @@
 package uk.gov.di.authentication.shared.serialization;
 
+import uk.gov.di.authentication.shared.validation.Validator;
+
 public interface Json {
     <T> T readValue(String body, Class<T> klass) throws JsonException;
+
+    <T> T readValue(String body, Class<T> klass, Validator validator) throws JsonException;
 
     String writeValueAsString(Object object) throws JsonException;
 
     class JsonException extends Exception {
         public JsonException(Exception e) {
             super(e);
+        }
+
+        public JsonException(String message) {
+            super(message);
         }
     }
 }
