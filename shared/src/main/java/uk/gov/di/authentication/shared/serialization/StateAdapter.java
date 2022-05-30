681839436cae4846d6de7af1d0240275a070c2ae
Chris Clayson <chris.clayson@digital.cabinet-office.gov.uk>
2022-05-30 15:44:59 +0100

    AUT-356: Make StateAdapter compatible with Jackson serialized states
    - Jackson serialized the state as an array rather than a simple
    string, we need to handle this case when reading existing JSON

--- shared/src/main/java/uk/gov/di/authentication/shared/serialization/StateAdapter.java
+++ shared/src/main/java/uk/gov/di/authentication/shared/serialization/StateAdapter.java
@@ -3,6 +3,7 @@ package uk.gov.di.authentication.shared.serialization;
 import com.google.gson.TypeAdapter;
 import com.google.gson.stream.JsonReader;
 import com.google.gson.stream.JsonWriter;
+import com.google.gson.stream.MalformedJsonException;
 import com.nimbusds.oauth2.sdk.id.State;
 
 import java.io.IOException;
@@ -15,6 +16,18 @@ public class StateAdapter extends TypeAdapter<State> {
 
     @Override
     public State read(JsonReader in) throws IOException {
-        return new State(in.nextString());
+        var token = in.peek();
+        switch (token) {
+            case BEGIN_ARRAY:
+                in.beginArray();
+                var state = in.nextString();
+                in.endArray();
+                return new State(state);
+            case STRING:
+                return new State(in.nextString());
+            default:
+                throw new MalformedJsonException(
+                        "Expected BEGIN_ARRAY or STRING got " + token.name());
+        }
     }
 }
