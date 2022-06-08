67dfda9f3d1ba77b3356ef26748eb2dd29feb855
DTaylorGDS <dominic.taylor@digital.cabinet-office.gov.uk>
2022-06-08 22:27:08 +0100

    AUT-30: Add password validator class
    - Add password validator class
    - This is intended to replace the basic password helper function as we now need to do a database table lookup so testing is easier when removed into an instantiable class

--- shared/src/main/java/uk/gov/di/authentication/shared/validation/PasswordValidator.java
+++ shared/src/main/java/uk/gov/di/authentication/shared/validation/PasswordValidator.java
@@ -1,2 +1,42 @@
-package uk.gov.di.authentication.shared.validation;public class PasswordValidator {
+package uk.gov.di.authentication.shared.validation;
+
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import uk.gov.di.authentication.shared.entity.ErrorResponse;
+import uk.gov.di.authentication.shared.services.CommonPasswordsService;
+
+import java.util.Optional;
+import java.util.regex.Pattern;
+
+public class PasswordValidator {
+
+    private static Logger LOG = LogManager.getLogger(PasswordValidator.class);
+    private static final Pattern PASSWORD_REGEX = Pattern.compile(".*\\d.*");
+    private CommonPasswordsService commonPasswordsService;
+
+    PasswordValidator(CommonPasswordsService commonPasswordsService){
+        this.commonPasswordsService = commonPasswordsService;
+    }
+
+    public Optional<ErrorResponse> validate(String password) {
+
+        if (password == null || password.isBlank()) {
+            return Optional.of(ErrorResponse.ERROR_1005);
+        }
+        if (password.length() < 8) {
+            return Optional.of(ErrorResponse.ERROR_1006);
+        }
+        if (!PASSWORD_REGEX.matcher(password).matches()) {
+            return Optional.of(ErrorResponse.ERROR_1007);
+        }
+
+        if (commonPasswordsService.isCommonPassword(password)) {
+            return Optional.of(ErrorResponse.ERROR_1040);
+        }
+
+        return Optional.empty();
+    }
+
+
 }
--- shared/src/test/java/uk/gov/di/authentication/shared/validation/PasswordValidatorTest.java
+++ shared/src/test/java/uk/gov/di/authentication/shared/validation/PasswordValidatorTest.java
@@ -1,4 +1,59 @@
+package uk.gov.di.authentication.shared.validation;
+
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import uk.gov.di.authentication.shared.entity.ErrorResponse;
+
+import uk.gov.di.authentication.shared.services.CommonPasswordsService;
+
+import java.util.Optional;
+import java.util.stream.Stream;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+
 class PasswordValidatorTest {
-  
+    PasswordValidator validator;
+
+    @BeforeEach
+    public void setUp(){
+        CommonPasswordsService mockDynamoService = mock(CommonPasswordsService.class);
+        when(mockDynamoService.isCommonPassword("TestCommonPassword1")).thenReturn(true);
+        this.validator = new PasswordValidator(mockDynamoService);
+
+    }
+
+    private static Stream<Arguments> invalidPasswords() {
+        return Stream.of(
+                Arguments.of("", ErrorResponse.ERROR_1005),
+                Arguments.of(null, ErrorResponse.ERROR_1005),
+                Arguments.of("passw0r", ErrorResponse.ERROR_1006),
+                Arguments.of("TestCommonPassword1", ErrorResponse.ERROR_1040)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("invalidPasswords")
+    void shouldRejectInvalidPasswords(String password, ErrorResponse expectedResponse) {
+/*        Optional<ErrorResponse> check = validator.validate("TestCommonPassword1");
+        assertEquals(Optional.of(ErrorResponse.ERROR_1040), check);*/
+        assertEquals(Optional.of(expectedResponse), validator.validate(password));
+    }
+
+
+    private static Stream<String> validPasswords() {
+        return Stream.of("+pa?55worD", "computer-1", "passsssssssssssswwwwoooordddd-2", "TestCommonPassword2");
+    }
+
+    @ParameterizedTest
+    @MethodSource("validPasswords")
+    void shouldAcceptValidPassword(String password) {
+        assertEquals(Optional.empty(), validator.validate(password));
+    }
 }
