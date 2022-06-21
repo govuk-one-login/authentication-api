9d0dab03e6d9a5f1d7abd60b936528298bd6da3b
DTaylorGDS <dominic.taylor@digital.cabinet-office.gov.uk>
2022-06-21 09:41:36 +0100

    AUT-429: Add factory to generate appropriate code validators based on MFA Method
    - Add factory to generate appropriate code validators based on MFA Method
    - Currently only AuthAppCodeValidator, inputting the max code retries as this varies depending on whether or not the journey is a registration journey

---
+++ shared/src/main/java/uk/gov/di/authentication/shared/validation/MfaCodeValidatorFactory.java
@@ -0,0 +1,39 @@
+package uk.gov.di.authentication.shared.validation;
+
+import uk.gov.di.authentication.shared.entity.MFAMethodType;
+import uk.gov.di.authentication.shared.services.CodeStorageService;
+import uk.gov.di.authentication.shared.services.ConfigurationService;
+import uk.gov.di.authentication.shared.services.DynamoService;
+import uk.gov.di.authentication.shared.state.UserContext;
+
+import java.util.Optional;
+
+public class MfaCodeValidatorFactory {
+
+    public static Optional<MfaCodeValidator> getMfaCodeValidator(
+            MFAMethodType mfaMethodType,
+            boolean isRegistration,
+            UserContext userContext,
+            CodeStorageService codeStorageService,
+            ConfigurationService configurationService,
+            DynamoService dynamoService) {
+
+        switch (mfaMethodType) {
+            case AUTH_APP:
+                int codeMaxRetries =
+                        isRegistration
+                                ? configurationService.getCodeMaxRetriesRegistration()
+                                : configurationService.getCodeMaxRetries();
+                return Optional.of(
+                        new AuthAppCodeValidator(
+                                mfaMethodType,
+                                userContext,
+                                codeStorageService,
+                                configurationService,
+                                dynamoService,
+                                codeMaxRetries));
+        }
+
+        return Optional.empty();
+    }
+}
