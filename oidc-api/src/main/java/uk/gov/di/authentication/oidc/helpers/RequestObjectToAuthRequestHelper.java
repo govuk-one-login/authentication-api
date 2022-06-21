cc6f7cdf292848ba85fe126f0923f5558a397bb3
james.howes <james.howes@digital.cabinet-office.gov.uk>
2022-06-21 09:33:45 +0100

    BAU - Create helper class to transform a request object JWT to an auth request object
    - We will be offering 2 ways for RPs to send an Auth Request. They can either specify each value as their own query parameter or alternatively send a signed request object. This brings 2 different ways to parse each of these and introduces different code paths throughout the application depending on what type they have used.
    - We can simply this by transforming the secure request object JWT into an auth request object once it has been validated in authorize. This will then mean we can restrict the amount of logic needed within our code base and retrieve the query parameters only from the auth request object.

---
+++ oidc-api/src/main/java/uk/gov/di/authentication/oidc/helpers/RequestObjectToAuthRequestHelper.java
@@ -0,0 +1,49 @@
+package uk.gov.di.authentication.oidc.helpers;
+
+import com.nimbusds.jwt.SignedJWT;
+import com.nimbusds.oauth2.sdk.ResponseType;
+import com.nimbusds.oauth2.sdk.Scope;
+import com.nimbusds.oauth2.sdk.id.ClientID;
+import com.nimbusds.oauth2.sdk.id.State;
+import com.nimbusds.openid.connect.sdk.AuthenticationRequest;
+import com.nimbusds.openid.connect.sdk.Nonce;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.net.URI;
+import java.text.ParseException;
+import java.util.Objects;
+
+public class RequestObjectToAuthRequestHelper {
+
+    private static final Logger LOG = LogManager.getLogger(RequestObjectToAuthRequestHelper.class);
+
+    private RequestObjectToAuthRequestHelper() {}
+
+    public static AuthenticationRequest transform(AuthenticationRequest authRequest) {
+        if (Objects.isNull(authRequest.getRequestObject())) {
+            return authRequest;
+        }
+        try {
+            var signedJWT = (SignedJWT) authRequest.getRequestObject();
+            var jwtClaimsSet = signedJWT.getJWTClaimsSet();
+            var responseType =
+                    ResponseType.parse(jwtClaimsSet.getClaim("response_type").toString());
+            return new AuthenticationRequest.Builder(
+                            responseType,
+                            Scope.parse(jwtClaimsSet.getClaim("scope").toString()),
+                            new ClientID(jwtClaimsSet.getClaim("client_id").toString()),
+                            URI.create((String) jwtClaimsSet.getClaim("redirect_uri")))
+                    .state(new State(jwtClaimsSet.getClaim("state").toString()))
+                    .nonce(new Nonce(jwtClaimsSet.getClaim("nonce").toString()))
+                    .requestObject(authRequest.getRequestObject())
+                    .build();
+        } catch (ParseException | com.nimbusds.oauth2.sdk.ParseException e) {
+            LOG.error("Parse exception thrown whilst converting RequestObject to Auth Request", e);
+            throw new RuntimeException(e);
+        } catch (Exception e) {
+            LOG.error("Unexpected exception thrown", e);
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ oidc-api/src/test/java/uk/gov/di/authentication/oidc/helpers/RequestObjectToAuthRequestHelperTest.java
@@ -0,0 +1,88 @@
+package uk.gov.di.authentication.oidc.helpers;
+
+import com.nimbusds.jose.JOSEException;
+import com.nimbusds.jwt.JWTClaimsSet;
+import com.nimbusds.oauth2.sdk.ResponseType;
+import com.nimbusds.oauth2.sdk.Scope;
+import com.nimbusds.oauth2.sdk.id.ClientID;
+import com.nimbusds.oauth2.sdk.id.State;
+import com.nimbusds.openid.connect.sdk.AuthenticationRequest;
+import com.nimbusds.openid.connect.sdk.Nonce;
+import com.nimbusds.openid.connect.sdk.OIDCScopeValue;
+import org.junit.jupiter.api.Test;
+import uk.gov.di.authentication.sharedtest.helper.KeyPairHelper;
+
+import java.net.URI;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static uk.gov.di.authentication.oidc.helper.RequestObjectTestHelper.generateSignedJWT;
+
+class RequestObjectToAuthRequestHelperTest {
+
+    private static final ClientID CLIENT_ID = new ClientID("test-id");
+    private static final String AUDIENCE = "https://localhost/authorize";
+    private static final URI REDIRECT_URI = URI.create("https://localhost:8080");
+    private static final State STATE = new State();
+    private static final Nonce NONCE = new Nonce();
+
+    @Test
+    void shouldConvertRequestObjectToAuthRequest() throws JOSEException {
+        var keyPair = KeyPairHelper.GENERATE_RSA_KEY_PAIR();
+        var scope = new Scope(OIDCScopeValue.OPENID, OIDCScopeValue.EMAIL);
+        var jwtClaimsSet =
+                new JWTClaimsSet.Builder()
+                        .audience(AUDIENCE)
+                        .claim("redirect_uri", REDIRECT_URI.toString())
+                        .claim("response_type", ResponseType.CODE.toString())
+                        .claim("scope", scope.toString())
+                        .claim("nonce", NONCE)
+                        .claim("state", STATE)
+                        .claim("client_id", CLIENT_ID.getValue())
+                        .issuer(CLIENT_ID.getValue())
+                        .build();
+        var signedJWT = generateSignedJWT(jwtClaimsSet, keyPair);
+        var authRequest =
+                new AuthenticationRequest.Builder(
+                                ResponseType.CODE,
+                                new Scope(OIDCScopeValue.OPENID),
+                                CLIENT_ID,
+                                REDIRECT_URI)
+                        .requestObject(signedJWT)
+                        .build();
+
+        var transformedAuthRequest = RequestObjectToAuthRequestHelper.transform(authRequest);
+
+        assertThat(transformedAuthRequest.getState(), equalTo(STATE));
+        assertThat(transformedAuthRequest.getNonce(), equalTo(NONCE));
+        assertThat(transformedAuthRequest.getRedirectionURI(), equalTo(REDIRECT_URI));
+        assertThat(transformedAuthRequest.getScope(), equalTo(scope));
+        assertThat(transformedAuthRequest.getClientID(), equalTo(CLIENT_ID));
+        assertThat(transformedAuthRequest.getResponseType(), equalTo(ResponseType.CODE));
+        assertThat(transformedAuthRequest.getRequestObject(), equalTo(signedJWT));
+    }
+
+    @Test
+    void shouldReturnAuthRequestWhenNoRequestObjectIsPresent() {
+        Scope scope = new Scope(OIDCScopeValue.OPENID, OIDCScopeValue.PHONE);
+        var authRequest =
+                new AuthenticationRequest.Builder(ResponseType.CODE, scope, CLIENT_ID, REDIRECT_URI)
+                        .state(STATE)
+                        .nonce(NONCE)
+                        .build();
+
+        var transformedAuthRequest = RequestObjectToAuthRequestHelper.transform(authRequest);
+
+        assertNull(transformedAuthRequest.getRequestObject());
+        assertThat(transformedAuthRequest.getState(), equalTo(authRequest.getState()));
+        assertThat(transformedAuthRequest.getNonce(), equalTo(authRequest.getNonce()));
+        assertThat(transformedAuthRequest.getClientID(), equalTo(authRequest.getClientID()));
+        assertThat(
+                transformedAuthRequest.getRedirectionURI(),
+                equalTo(authRequest.getRedirectionURI()));
+        assertThat(
+                transformedAuthRequest.getResponseType(), equalTo(authRequest.getResponseType()));
+        assertThat(transformedAuthRequest.getScope(), equalTo(authRequest.getScope()));
+    }
+}
