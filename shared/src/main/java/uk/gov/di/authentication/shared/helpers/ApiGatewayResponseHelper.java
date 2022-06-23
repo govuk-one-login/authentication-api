0ece90ccf6742abefa26f60ee00f5c61a1850fd2
Alex Wilson <alex.wilson@digital.cabinet-office.gov.uk>
2022-06-23 10:58:47 +0100

    BAU: Generate security headers map from enum

--- shared/src/main/java/uk/gov/di/authentication/shared/helpers/ApiGatewayResponseHelper.java
+++ shared/src/main/java/uk/gov/di/authentication/shared/helpers/ApiGatewayResponseHelper.java
@@ -9,8 +9,10 @@ import uk.gov.di.authentication.shared.serialization.Json;
 import uk.gov.di.authentication.shared.serialization.Json.JsonException;
 import uk.gov.di.authentication.shared.services.SerializationService;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class ApiGatewayResponseHelper {
 
@@ -31,22 +33,12 @@ public class ApiGatewayResponseHelper {
             this.headerName = headerName;
             this.headerValue = headerValue;
         }
+
+        public static Map<String, String> headers() {
+            return Arrays.stream(SecurityHeaders.values())
+                    .collect(Collectors.toMap(x -> x.headerName, x -> x.headerValue));
+        }
     }
-
-    private static final String XSS_PROTECTION_HEADER_NAME = "X-XSS-Protection";
-    private static final String CONTENT_TYPE_OPTIONS_HEADER_NAME = "X-Content-Type-Options";
-    private static final String CONTENT_SECURITY_POLICY_HEADER_NAME = "Content-Security-Policy";
-    private static final String STRICT_TRANSPORT_SECURITY_HEADER_NAME = "Strict-Transport-Security";
-    private static final String X_FRAME_OPTIONS_HEADER_NAME = "X-Frame-Options";
-
-    private static final String CACHE_CONTROL_HEADER_VALUE = "no-cache, no-store";
-    private static final String PRAGMA_HEADER_VALUE = "no-cache";
-    private static final String XSS_PROTECTION_HEADER_VALUE = "1; mode=block";
-    private static final String CONTENT_TYPE_OPTIONS_HEADER_VALUE = "nosniff";
-    private static final String CONTENT_SECURITY_POLICY_HEADER_VALUE = "frame-ancestors 'none'";
-    private static final String STRICT_TRANSPORT_SECURITY_HEADER_VALUE =
-            "max-age=31536000; includeSubDomains; preload";
-    private static final String X_FRAME_OPTIONS_HEADER_VALUE = "DENY";
 
     private static final Logger LOG = LogManager.getLogger(ApiGatewayResponseHelper.class);
     private static final Json objectMapper = SerializationService.getInstance();
@@ -94,22 +86,7 @@ public class ApiGatewayResponseHelper {
             apiGatewayProxyResponseEvent.setMultiValueHeaders(multiValueHeaders);
         }
 
-        Map<String, String> securityHeaders =
-                Map.ofEntries(
-                        Map.entry(HttpHeaders.CACHE_CONTROL, CACHE_CONTROL_HEADER_VALUE),
-                        Map.entry(HttpHeaders.PRAGMA, PRAGMA_HEADER_VALUE),
-                        Map.entry(XSS_PROTECTION_HEADER_NAME, XSS_PROTECTION_HEADER_VALUE),
-                        Map.entry(
-                                CONTENT_TYPE_OPTIONS_HEADER_NAME,
-                                CONTENT_TYPE_OPTIONS_HEADER_VALUE),
-                        Map.entry(
-                                CONTENT_SECURITY_POLICY_HEADER_NAME,
-                                CONTENT_SECURITY_POLICY_HEADER_VALUE),
-                        Map.entry(
-                                STRICT_TRANSPORT_SECURITY_HEADER_NAME,
-                                STRICT_TRANSPORT_SECURITY_HEADER_VALUE),
-                        Map.entry(X_FRAME_OPTIONS_HEADER_NAME, X_FRAME_OPTIONS_HEADER_VALUE));
-        apiGatewayProxyResponseEvent.setHeaders(securityHeaders);
+        apiGatewayProxyResponseEvent.setHeaders(SecurityHeaders.headers());
 
         return apiGatewayProxyResponseEvent;
     }
