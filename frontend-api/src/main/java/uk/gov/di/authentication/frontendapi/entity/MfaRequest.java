3aff45fbcda45ecd59881d36a739f1ecb270c1aa
DTaylorGDS <dominic.taylor@digital.cabinet-office.gov.uk>
2022-07-27 12:04:46 +0100

    AUT-393: Add optional isResendCodeRequest field to MFA Handler
    - Add optional isResendCodeRequest field to MFA Handler
    - This will allow it to be called and simple to re-issue to same phone verification code as is reqested in the account creation journey when phone number is given
    - The existing lambda in that other case - SendNotificationHandler - cannot be used because a SendNotificationRequest must have a phone number, which is not availble to the front end at that point of the journey
    - Similarly the MfaHandler cannot be used without modfication as it checks Redis for a code specifically associated with MFA_SMS NotificationType, whereas the existing code will have been stored in association with the VERIFY_PHONE_NUMBER notification type, so attempting to use it without differentiation would result in a new code being issued, which would not then be 'found' when submitted on the 'enter code' page (again because of mismatched expectation of NotificationType)

--- frontend-api/src/main/java/uk/gov/di/authentication/frontendapi/entity/MfaRequest.java
+++ frontend-api/src/main/java/uk/gov/di/authentication/frontendapi/entity/MfaRequest.java
@@ -1,5 +1,22 @@
 package uk.gov.di.authentication.frontendapi.entity;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
 import uk.gov.di.authentication.shared.entity.BaseFrontendRequest;
 
-public class MfaRequest extends BaseFrontendRequest {}
+public class MfaRequest extends BaseFrontendRequest {
+    @Expose
+    @SerializedName("isResendCodeRequest")
+    private boolean isResendCodeRequest = false;
+
+    public MfaRequest() {}
+
+    public MfaRequest(String email, boolean isResendCodeRequest) {
+        this.email = email;
+        this.isResendCodeRequest = isResendCodeRequest;
+    }
+
+    public boolean isResendCodeRequest() {
+        return isResendCodeRequest;
+    }
+}
