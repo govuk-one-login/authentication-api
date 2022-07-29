96f9341414eed8f7347faf2c4cecba3148c7e6af
Alex Wilson <alex.wilson@digital.cabinet-office.gov.uk>
2022-07-29 11:15:27 +0100

    AUT-584: Create encryption key for queue and grant access

---
+++ ci/terraform/modules/txma-audit-queue/key.tf
@@ -0,0 +1,47 @@
+resource "aws_kms_key" "txma_audit_queue_encryption_key" {
+  count                    = var.use_localstack ? 0 : 1
+  description              = "KMS signing key for encrypting TxMA audit queue at rest"
+  deletion_window_in_days  = 30
+  customer_master_key_spec = "SYMMETRIC_DEFAULT"
+  key_usage                = "ENCRYPT_DECRYPT"
+
+  policy = data.aws_iam_policy_document.txma_audit_queue_encryption_key_access_policy
+
+  tags = var.default_tags
+}
+
+data "aws_caller_identity" "current" {}
+
+data "aws_iam_policy_document" "txma_audit_queue_encryption_key_access_policy" {
+  count = var.use_localstack ? 0 : 1
+
+  statement {
+    sid    = "DefaultAccessPolicy"
+    effect = "Allow"
+
+    actions = [
+      "kms:*"
+    ]
+    resources = ["*"]
+
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"]
+    }
+  }
+
+  statement {
+    sid    = "AllowTxmaAccessToKmsAuditEncryptionKey-${var.environment}"
+    effect = "Allow"
+
+    actions = [
+      "kms:Decrypt"
+    ]
+    resources = ["*"]
+
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::${var.txma_account_id}:root"]
+    }
+  }
+}
---
+++ ci/terraform/modules/txma-audit-queue/variables.tf
@@ -0,0 +1,18 @@
+variable "environment" {
+  type = string
+}
+
+variable "default_tags" {
+  default     = {}
+  type        = map(string)
+  description = "Default tags to apply to all resources"
+}
+
+variable "txma_account_id" {
+  type        = string
+  description = "Account id of the corresponding TxMA processor"
+}
+
+variable "use_localstack" {
+  type = bool
+}
