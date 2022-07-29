54c7292427d8d0eb33261919776b218756eaec7e
Alex Wilson <alex.wilson@digital.cabinet-office.gov.uk>
2022-07-29 11:23:16 +0100

    AUT-584: Output ARNs for queue and key

---
+++ ci/terraform/modules/txma-audit-queue/output.tf
@@ -0,0 +1,7 @@
+output "queue_arn" {
+  value = aws_sqs_queue.txma_audit_queue.arn
+}
+
+output "kms_key_arn" {
+  value = var.use_localstack ? aws_kms_key.txma_audit_queue_encryption_key[0].arn : null
+}
