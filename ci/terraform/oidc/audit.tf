efbbda6b406d4ad032271e4914ce01d7da47fb91
Alex Wilson <alex.wilson@digital.cabinet-office.gov.uk>
2022-07-29 16:15:20 +0100

    AUT-584: Deploy TxMA audit queue to staging

--- ci/terraform/oidc/audit.tf
+++ ci/terraform/oidc/audit.tf
@@ -1,5 +1,5 @@
 module "oidc_txma_audit" {
-  count           = contains(["build"], var.environment) ? 1 : 0
+  count           = contains(["build", "staging"], var.environment) ? 1 : 0
   source          = "../modules/txma-audit-queue"
   environment     = var.environment
   txma_account_id = var.txma_account_id
