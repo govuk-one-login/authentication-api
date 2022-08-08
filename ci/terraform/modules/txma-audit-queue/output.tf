77ff0567f308d7bd9be8a3db7e27cbaa1df75063
Alex Wilson <alex.wilson@digital.cabinet-office.gov.uk>
2022-08-08 10:54:05 +0100

    AUT-584: Expose `queue_url` from txma module

--- ci/terraform/modules/txma-audit-queue/output.tf
+++ ci/terraform/modules/txma-audit-queue/output.tf
@@ -2,6 +2,10 @@ output "queue_arn" {
   value = aws_sqs_queue.txma_audit_queue.arn
 }
 
+output "queue_url" {
+  value = aws_sqs_queue.txma_audit_queue.url
+}
+
 output "kms_key_arn" {
   value = var.use_localstack ? aws_kms_key.txma_audit_queue_encryption_key[0].arn : null
 }
