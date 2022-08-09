829ab4b7c5289cdb1e7861e7ecd567c16656048b
Alex Wilson <alex.wilson@digital.cabinet-office.gov.uk>
2022-08-09 09:17:43 +0100

    AUT-584: Deploy audit queues into all environments
    Nothing is publishing to them yet, but this will simplify the Terraform

--- ci/terraform/oidc/audit.tf
+++ ci/terraform/oidc/audit.tf
@@ -1,5 +1,4 @@
 module "oidc_txma_audit" {
-  count           = contains(["build", "staging"], var.environment) ? 1 : 0
   source          = "../modules/txma-audit-queue"
   environment     = var.environment
   txma_account_id = var.txma_account_id
