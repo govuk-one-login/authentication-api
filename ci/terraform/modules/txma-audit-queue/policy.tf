eb6825127375ea5e676b1985175e241201e9d615
Alex Wilson <alex.wilson@digital.cabinet-office.gov.uk>
2022-08-09 10:14:06 +0100

    AUT-584: Create and expose access policy ARN for consumers

---
+++ ci/terraform/modules/txma-audit-queue/policy.tf
@@ -0,0 +1,19 @@
+data "aws_iam_policy_document" "txma_audit_queue_access_policy_document" {
+  version   = "2012-10-17"
+  policy_id = "${var.environment}-txma-audit-queue-access-policy"
+
+  statement {
+    effect    = "Allow"
+    sid       = "AllowWriteAccessToTxmaAuditQueue"
+    actions   = ["sqs:SendMessage", ]
+    resources = [aws_sqs_queue.txma_audit_queue.arn]
+  }
+}
+
+resource "aws_iam_policy" "txma_audit_queue_access_policy" {
+  name_prefix = "txma-audit-queue-access-"
+  path        = "/${var.environment}/"
+  description = "IAM Policy for write access to the TxMA audit queue"
+
+  policy = data.aws_iam_policy_document.txma_audit_queue_access_policy_document.json
+}
