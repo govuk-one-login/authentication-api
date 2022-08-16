cede129f68b07028d03215e00275444f0d3679d1
Alex Wilson <alex.wilson@digital.cabinet-office.gov.uk>
2022-08-16 14:43:15 +0100

    AUT-584: Publish TxMA events from `UpdatePasswordHandler`

--- ci/terraform/account-management/update-password.tf
+++ ci/terraform/account-management/update-password.tf
@@ -9,6 +9,7 @@ module "account_management_api_update_password_role" {
     aws_iam_policy.dynamo_am_user_write_access_policy.arn,
     aws_iam_policy.audit_signing_key_lambda_kms_signing_policy.arn,
     aws_iam_policy.dynamo_common_passwords_read_access_policy.arn,
+    module.acct_mgmt_txma_audit.access_policy_arn
   ]
 }
 
@@ -24,6 +25,8 @@ module "update_password" {
     LOCALSTACK_ENDPOINT     = var.use_localstack ? var.localstack_endpoint : null
     EMAIL_QUEUE_URL         = aws_sqs_queue.email_queue.id
     EVENTS_SNS_TOPIC_ARN    = data.aws_sns_topic.events.arn
+    TXMA_AUDIT_ENABLED      = contains(["staging"], var.environment)
+    TXMA_AUDIT_QUEUE_URL    = module.acct_mgmt_txma_audit.queue_url
     AUDIT_SIGNING_KEY_ALIAS = local.audit_signing_key_alias_name
   }
   handler_function_name = "uk.gov.di.accountmanagement.lambda.UpdatePasswordHandler::handleRequest"
