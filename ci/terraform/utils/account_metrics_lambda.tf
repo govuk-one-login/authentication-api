5bc2e17321bedc7b9a98a6fa94c92b5f71c2cad7
Chris Clayson <chris.clayson@digital.cabinet-office.gov.uk>
2022-08-23 09:53:58 +0100

    AUT-661: Add Terraform to create account metrics lambda
    - Create lambda with permission to describe the UserProfile table
    - Schedule the lambda to run every hour

---
+++ ci/terraform/utils/account_metrics_lambda.tf
@@ -0,0 +1,96 @@
+data "aws_iam_policy_document" "account_metrics_dynamo_access" {
+  statement {
+    sid    = "AllowAccessToDescribeUserProfileTable"
+    effect = "Allow"
+
+    actions = [
+      "dynamodb:DescribeTable",
+    ]
+
+    resources = [
+      data.aws_dynamodb_table.user_profile.arn,
+    ]
+  }
+}
+
+resource "aws_iam_policy" "account_metrics_dynamo_access" {
+  name_prefix = "account-metrics-dynamo-access-policy"
+  description = "IAM policy for managing permissions to the Dynamo User Profile table"
+
+  policy = data.aws_iam_policy_document.account_metrics_dynamo_access.json
+}
+
+module "account_metrics_update_lambda_role" {
+  source = "../modules/lambda-role"
+
+  environment = var.environment
+  role_name   = "account-metrics-lambda-role"
+
+  policies_to_attach = [
+    aws_iam_policy.account_metrics_dynamo_access.arn,
+  ]
+}
+
+resource "aws_lambda_function" "account_metrics_lambda" {
+  function_name = "${var.environment}-account-metrics-publish-lambda"
+  role          = module.account_metrics_update_lambda_role.arn
+  handler       = "uk.gov.di.authentication.utils.lambda.AccountMetricPublishHandler::handleRequest"
+  timeout       = 900
+  memory_size   = 4096
+  runtime       = "java11"
+  publish       = true
+
+  s3_bucket         = aws_s3_object.utils_release_zip.bucket
+  s3_key            = aws_s3_object.utils_release_zip.key
+  s3_object_version = aws_s3_object.utils_release_zip.version_id
+
+  environment {
+    variables = merge({
+      ENVIRONMENT = var.environment
+    })
+  }
+
+  tags = local.default_tags
+}
+
+
+resource "aws_cloudwatch_log_group" "account_metrics_lambda_log_group" {
+  count = var.use_localstack ? 0 : 1
+
+  name              = "/aws/lambda/${aws_lambda_function.account_metrics_lambda.function_name}"
+  kms_key_id        = local.cloudwatch_encryption_key_arn
+  retention_in_days = var.cloudwatch_log_retention
+
+  tags = local.default_tags
+}
+
+resource "aws_cloudwatch_log_subscription_filter" "account_metrics_log_subscription" {
+  count           = length(var.logging_endpoint_arns)
+  name            = "${aws_lambda_function.account_metrics_lambda.function_name}-log-subscription-${count.index}"
+  log_group_name  = aws_cloudwatch_log_group.account_metrics_lambda_log_group[0].name
+  filter_pattern  = ""
+  destination_arn = var.logging_endpoint_arns[count.index]
+
+  lifecycle {
+    create_before_destroy = false
+  }
+}
+
+resource "aws_cloudwatch_event_rule" "account_metrics_schedule" {
+  name                = "${var.environment}-account-metrics-publish-schedule"
+  schedule_expression = "rate(1 hour)"
+}
+
+resource "aws_cloudwatch_event_target" "account_metrics_schedule_target" {
+  arn       = aws_lambda_function.account_metrics_lambda.arn
+  rule      = aws_cloudwatch_event_rule.account_metrics_schedule.name
+  target_id = aws_lambda_function.account_metrics_lambda.version
+}
+
+resource "aws_lambda_permission" "allow_cloudwatch_to_call_account_metrics_lambda" {
+  statement_id  = "AllowExecutionFromCloudWatch"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.account_metrics_lambda.function_name
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.account_metrics_schedule.arn
+}
