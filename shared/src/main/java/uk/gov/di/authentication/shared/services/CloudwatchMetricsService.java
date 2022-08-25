60498118126734b499508ee3ea87a1261d4f66cc
Chris Clayson <chris.clayson@digital.cabinet-office.gov.uk>
2022-08-25 18:40:05 +0100

    AUT-621: Add method to output a new metric
    - Add a new metric to the `CloudwatchMetricsService` to output a new
    `AuthenticationSuccess` metric with common dimensions

--- shared/src/main/java/uk/gov/di/authentication/shared/services/CloudwatchMetricsService.java
+++ shared/src/main/java/uk/gov/di/authentication/shared/services/CloudwatchMetricsService.java
@@ -3,6 +3,7 @@ package uk.gov.di.authentication.shared.services;
 import software.amazon.cloudwatchlogs.emf.logger.MetricsLogger;
 import software.amazon.cloudwatchlogs.emf.model.DimensionSet;
 import software.amazon.cloudwatchlogs.emf.model.Unit;
+import uk.gov.di.authentication.shared.entity.Session;
 
 import java.util.Map;
 
@@ -10,7 +11,15 @@ import static uk.gov.di.authentication.shared.helpers.InstrumentationHelper.segm
 
 public class CloudwatchMetricsService {
 
-    public CloudwatchMetricsService() {}
+    private final ConfigurationService configurationService;
+
+    public CloudwatchMetricsService() {
+        configurationService = ConfigurationService.getInstance();
+    }
+
+    public CloudwatchMetricsService(ConfigurationService configurationService) {
+        this.configurationService = configurationService;
+    }
 
     public void putEmbeddedValue(String name, double value, Map<String, String> dimensions) {
         segmentedFunctionCall(
@@ -30,5 +39,28 @@ public class CloudwatchMetricsService {
 
     public void incrementCounter(String name, Map<String, String> dimensions) {
         putEmbeddedValue(name, 1, dimensions);
+    }
+
+    public void incrementAuthenticationSuccess(
+            Session.AccountState accountState,
+            String clientId,
+            String requestedLevelOfConfidence,
+            boolean isTestJourney,
+            boolean mfaRequired) {
+        incrementCounter(
+                "AuthenticationSuccess",
+                Map.of(
+                        "Account",
+                        accountState.name(),
+                        "Environment",
+                        configurationService.getEnvironment(),
+                        "Client",
+                        clientId,
+                        "IsTest",
+                        Boolean.toString(isTestJourney),
+                        "RequestedLevelOfConfidence",
+                        requestedLevelOfConfidence,
+                        "MfaRequired",
+                        Boolean.toString(mfaRequired)));
     }
 }
