15030945301949970ffc84c82d0fdb3aa62daae9
Alex Wilson <alex.wilson@digital.cabinet-office.gov.uk>
2022-09-01 15:12:59 +0100

    AUT-584: Remove SNS publishing and KMS signing from `AuditService`
    The connections / service classes will remain until a later PR

--- shared/src/test/java/uk/gov/di/authentication/shared/services/AuditServiceTest.java
+++ shared/src/test/java/uk/gov/di/authentication/shared/services/AuditServiceTest.java
@@ -1,47 +1,26 @@
 package uk.gov.di.authentication.shared.services;
 
-import com.google.protobuf.InvalidProtocolBufferException;
-import org.hamcrest.CustomTypeSafeMatcher;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Captor;
 import org.mockito.MockitoAnnotations;
-import software.amazon.awssdk.core.SdkBytes;
-import software.amazon.awssdk.services.kms.model.SignRequest;
-import software.amazon.awssdk.services.kms.model.SignResponse;
-import uk.gov.di.audit.AuditPayload.SignedAuditEvent;
 import uk.gov.di.authentication.shared.domain.AuditableEvent;
 
 import java.time.Clock;
 import java.time.Instant;
 import java.time.ZoneId;
-import java.util.Base64;
 import java.util.Optional;
-import java.util.UUID;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.core.Is.is;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.when;
 import static uk.gov.di.authentication.shared.services.AuditService.MetadataPair.pair;
 import static uk.gov.di.authentication.shared.services.AuditServiceTest.TestEvents.TEST_EVENT_ONE;
-import static uk.gov.di.authentication.sharedtest.matchers.AuditMessageMatcher.hasClientId;
-import static uk.gov.di.authentication.sharedtest.matchers.AuditMessageMatcher.hasEmail;
-import static uk.gov.di.authentication.sharedtest.matchers.AuditMessageMatcher.hasEventName;
-import static uk.gov.di.authentication.sharedtest.matchers.AuditMessageMatcher.hasIpAddress;
-import static uk.gov.di.authentication.sharedtest.matchers.AuditMessageMatcher.hasMetadataPair;
-import static uk.gov.di.authentication.sharedtest.matchers.AuditMessageMatcher.hasPersistentSessionId;
-import static uk.gov.di.authentication.sharedtest.matchers.AuditMessageMatcher.hasPhoneNumber;
-import static uk.gov.di.authentication.sharedtest.matchers.AuditMessageMatcher.hasRequestId;
-import static uk.gov.di.authentication.sharedtest.matchers.AuditMessageMatcher.hasSessionId;
-import static uk.gov.di.authentication.sharedtest.matchers.AuditMessageMatcher.hasSubjectId;
-import static uk.gov.di.authentication.sharedtest.matchers.AuditMessageMatcher.hasTimestamp;
 import static uk.gov.di.authentication.sharedtest.matchers.JsonMatcher.asJson;
 import static uk.gov.di.authentication.sharedtest.matchers.JsonMatcher.hasFieldWithValue;
 import static uk.gov.di.authentication.sharedtest.matchers.JsonMatcher.hasNumericFieldWithValue;
@@ -57,7 +36,6 @@ class AuditServiceTest {
     private final AwsSqsClient awsSqsClient = mock(AwsSqsClient.class);
     private final ConfigurationService configurationService = mock(ConfigurationService.class);
 
-    @Captor private ArgumentCaptor<String> messageCaptor;
     @Captor private ArgumentCaptor<String> txmaMessageCaptor;
 
     enum TestEvents implements AuditableEvent {
@@ -70,18 +48,13 @@ class AuditServiceTest {
 
     @BeforeEach
     void beforeEach() {
-        var signResponse =
-                SignResponse.builder()
-                        .signature(SdkBytes.fromByteArray("signature".getBytes()))
-                        .build();
-        when(kmsConnectionService.sign(any(SignRequest.class))).thenReturn(signResponse);
         when(configurationService.getOidcApiBaseURL()).thenReturn(Optional.of("oidc-base-url"));
         MockitoAnnotations.openMocks(this);
     }
 
     @AfterEach
     void afterEach() {
-        verifyNoMoreInteractions(snsService);
+        verifyNoInteractions(snsService, kmsConnectionService);
     }
 
     @Test
@@ -105,20 +78,6 @@ class AuditServiceTest {
                 "phone-number",
                 "persistent-session-id");
 
-        verify(snsService).publishAuditMessage(messageCaptor.capture());
-        var serialisedAuditMessage = messageCaptor.getValue();
-
-        assertThat(serialisedAuditMessage, hasTimestamp(FIXED_TIMESTAMP));
-        assertThat(serialisedAuditMessage, hasEventName(TEST_EVENT_ONE.toString()));
-        assertThat(serialisedAuditMessage, hasRequestId("request-id"));
-        assertThat(serialisedAuditMessage, hasSessionId("session-id"));
-        assertThat(serialisedAuditMessage, hasPersistentSessionId("persistent-session-id"));
-        assertThat(serialisedAuditMessage, hasClientId("client-id"));
-        assertThat(serialisedAuditMessage, hasSubjectId("subject-id"));
-        assertThat(serialisedAuditMessage, hasEmail("email"));
-        assertThat(serialisedAuditMessage, hasIpAddress("ip-address"));
-        assertThat(serialisedAuditMessage, hasPhoneNumber("phone-number"));
-
         verify(awsSqsClient).send(txmaMessageCaptor.capture());
 
         var txmaMessage = asJson(txmaMessageCaptor.getValue());
@@ -138,41 +97,6 @@ class AuditServiceTest {
         assertThat(userObject, hasFieldWithValue("email", equalTo("email")));
         assertThat(userObject, hasFieldWithValue("phone", equalTo("phone-number")));
         assertThat(userObject, hasFieldWithValue("ip_address", equalTo("ip-address")));
-    }
-
-    @Test
-    void shouldSignAuditEventPayload() throws InvalidProtocolBufferException {
-        var auditService =
-                new AuditService(
-                        FIXED_CLOCK,
-                        snsService,
-                        kmsConnectionService,
-                        configurationService,
-                        awsSqsClient);
-
-        var signingRequestCaptor = ArgumentCaptor.forClass(SignRequest.class);
-
-        auditService.submitAuditEvent(
-                TEST_EVENT_ONE,
-                "request-id",
-                "session-id",
-                "client-id",
-                "subject-id",
-                "email",
-                "ip-address",
-                "persistent-session-id",
-                "phone-number");
-
-        verify(kmsConnectionService).sign(signingRequestCaptor.capture());
-        verify(snsService).publishAuditMessage(messageCaptor.capture());
-
-        SignedAuditEvent event =
-                SignedAuditEvent.parseFrom(Base64.getDecoder().decode(messageCaptor.getValue()));
-
-        assertThat(
-                event.getPayload().toByteArray(),
-                is(signingRequestCaptor.getValue().message().asByteArray()));
-        assertThat(event.getSignature().toByteArray(), is("signature".getBytes()));
     }
 
     @Test
@@ -198,14 +122,6 @@ class AuditServiceTest {
                 pair("key", "value"),
                 pair("key2", "value2"));
 
-        verify(snsService).publishAuditMessage(messageCaptor.capture());
-        var serialisedAuditMessage = messageCaptor.getValue();
-
-        assertThat(serialisedAuditMessage, hasTimestamp(FIXED_TIMESTAMP));
-        assertThat(serialisedAuditMessage, hasEventName(TEST_EVENT_ONE.toString()));
-        assertThat(serialisedAuditMessage, hasMetadataPair(pair("key", "value")));
-        assertThat(serialisedAuditMessage, hasMetadataPair(pair("key2", "value2")));
-
         verify(awsSqsClient).send(txmaMessageCaptor.capture());
         var txmaMessage = asJson(txmaMessageCaptor.getValue());
 
@@ -216,21 +132,5 @@ class AuditServiceTest {
 
         assertThat(extensions, hasFieldWithValue("key", equalTo("value")));
         assertThat(extensions, hasFieldWithValue("key2", equalTo("value2")));
-        assertThat(extensions, hasFieldWithValue("legacy_audit_event_id", isUuid()));
-    }
-
-    private static CustomTypeSafeMatcher<String> isUuid() {
-        return new CustomTypeSafeMatcher<>("Should be a UUID") {
-            @Override
-            @SuppressWarnings("all")
-            protected boolean matchesSafely(String item) {
-                try {
-                    UUID.fromString(item);
-                    return true;
-                } catch (IllegalArgumentException e) {
-                    return false;
-                }
-            }
-        };
     }
 }
