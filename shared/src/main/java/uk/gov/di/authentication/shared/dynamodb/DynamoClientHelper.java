a473982ebd70891f075a89f8f358871afeac38e1
james.howes <james.howes@digital.cabinet-office.gov.uk>
2022-09-01 12:09:10 +0100

    AUT-398 - Add method in DynamoClientHelper to create a DynamoEnhancedClient
    - DynamoEnhancedClients will be created as part of the Dynamo SDK version2

--- shared/src/main/java/uk/gov/di/authentication/shared/dynamodb/DynamoClientHelper.java
+++ shared/src/main/java/uk/gov/di/authentication/shared/dynamodb/DynamoClientHelper.java
@@ -1,10 +1,15 @@
 package uk.gov.di.authentication.shared.dynamodb;
 
-import com.amazonaws.client.builder.AwsClientBuilder.EndpointConfiguration;
+import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapperConfig;
-import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapperConfig.ConsistentReads;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 import uk.gov.di.authentication.shared.services.ConfigurationService;
+
+import java.net.URI;
 
 import static com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder.standard;
 import static com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapperConfig.TableNameOverride.withTableNameReplacement;
@@ -14,7 +19,10 @@ public class DynamoClientHelper {
     public static AmazonDynamoDB createDynamoClient(ConfigurationService configurationService) {
         return configurationService
                 .getDynamoEndpointUri()
-                .map(uri -> new EndpointConfiguration(uri, configurationService.getAwsRegion()))
+                .map(
+                        uri ->
+                                new AwsClientBuilder.EndpointConfiguration(
+                                        uri, configurationService.getAwsRegion()))
                 .map(standard()::withEndpointConfiguration)
                 .orElse(standard().withRegion(configurationService.getAwsRegion()))
                 .build();
@@ -23,7 +31,21 @@ public class DynamoClientHelper {
     public static DynamoDBMapperConfig tableConfig(String tableName) {
         return new DynamoDBMapperConfig.Builder()
                 .withTableNameOverride(withTableNameReplacement(tableName))
-                .withConsistentReads(ConsistentReads.CONSISTENT)
+                .withConsistentReads(DynamoDBMapperConfig.ConsistentReads.CONSISTENT)
                 .build();
+    }
+
+    public static DynamoDbEnhancedClient createDynamoEnhancedClient(
+            ConfigurationService configurationService) {
+        var dynamoDbClientBuilder =
+                DynamoDbClient.builder()
+                        .credentialsProvider(DefaultCredentialsProvider.create())
+                        .region(Region.of(configurationService.getAwsRegion()));
+        configurationService
+                .getDynamoEndpointUri()
+                .ifPresent(
+                        endpoint -> dynamoDbClientBuilder.endpointOverride(URI.create(endpoint)));
+        var dynamoDbClient = dynamoDbClientBuilder.build();
+        return DynamoDbEnhancedClient.builder().dynamoDbClient(dynamoDbClient).build();
     }
 }
