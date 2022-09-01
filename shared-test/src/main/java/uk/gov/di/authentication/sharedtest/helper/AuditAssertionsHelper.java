0cf7831ad29b4157aeebc8392f84f30f498f0a47
Alex Wilson <alex.wilson@digital.cabinet-office.gov.uk>
2022-09-01 15:27:02 +0100

    AUT-584: Refactor waits to use awaitility

--- shared-test/src/main/java/uk/gov/di/authentication/sharedtest/helper/AuditAssertionsHelper.java
+++ shared-test/src/main/java/uk/gov/di/authentication/sharedtest/helper/AuditAssertionsHelper.java
@@ -5,11 +5,12 @@ import uk.gov.di.authentication.shared.domain.AuditableEvent;
 import uk.gov.di.authentication.sharedtest.extensions.SqsQueueExtension;
 import uk.gov.di.authentication.sharedtest.matchers.JsonMatcher;
 
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
 import java.util.Collection;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
-import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.awaitility.Awaitility.await;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
@@ -17,16 +18,12 @@ import static org.hamcrest.Matchers.hasItem;
 
 public class AuditAssertionsHelper {
 
-    private static final int SNS_TIMEOUT = 1;
-    public static final int SNS_TIMEOUT_MILLIS = SNS_TIMEOUT * 1000;
+    private static final Duration TIMEOUT = Duration.of(1, ChronoUnit.SECONDS);
 
     public static void assertNoTxmaAuditEventsReceived(SqsQueueExtension txmaAuditQueue) {
-        try {
-            Thread.sleep(SNS_TIMEOUT_MILLIS);
-        } catch (InterruptedException ex) {
-            throw new RuntimeException(ex);
-        }
-        assertThat(txmaAuditQueue.getApproximateMessageCount(), equalTo(0));
+        await().atMost(TIMEOUT)
+                .untilAsserted(
+                        () -> assertThat(txmaAuditQueue.getApproximateMessageCount(), equalTo(0)));
     }
 
     public static void assertTxmaAuditEventsReceived(
@@ -43,7 +40,7 @@ public class AuditAssertionsHelper {
                     "Do not call assertTxmaAuditEventsReceived() with an empty collection of event types; it won't wait to see if anything unexpected was received.  Instead, call Thread.sleep and then check the count of requests.");
         }
 
-        await().atMost(SNS_TIMEOUT, SECONDS)
+        await().atMost(TIMEOUT)
                 .untilAsserted(
                         () ->
                                 assertThat(
