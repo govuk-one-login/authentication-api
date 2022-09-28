ec5d1d879df93c50109cddc4750d6efb41cec94e
dbes-gds <daniel.besbrode@digital.cabinet-office.gov.uk>
2022-09-28 17:19:17 +0100

    AUT-692: Additional metrics for new and existing authentications
    Reduce number of dimensions in the metrics for easier reporting by client.

---
+++ shared/src/main/java/uk/gov/di/authentication/shared/domain/CloudwatchMetricDimensions.java
@@ -0,0 +1,21 @@
+package uk.gov.di.authentication.shared.domain;
+
+public enum CloudwatchMetricDimensions {
+    ACCOUNT("Account"),
+    ENVIRONMENT("Environment"),
+    CLIENT("Client"),
+    IS_TEST("IsTest"),
+    REQUESTED_LEVEL_OF_CONFIDENCE("RequestedLevelOfConfidence"),
+    MFA_REQUIRED("MfaRequired"),
+    CLIENT_NAME("ClientName");
+
+    private String value;
+
+    CloudwatchMetricDimensions(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
