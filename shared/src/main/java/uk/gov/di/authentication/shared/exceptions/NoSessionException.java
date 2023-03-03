cf625ab66d97b98a1c306166e6299ba0b4d89847
james.howes <james.howes@digital.cabinet-office.gov.uk>
2023-03-03 14:52:01 +0000

    AUT-1125 - Create shared class for no session orchestration logic
    - The logic currently in the DocAppCallback lambda will be similar for the IpvCallback lambda. Therefore it makes sense to pull this out into a shared class to reduce code duplication and have a single point for the logic.
    - The NoSessionOrchestrationService is called in the scenario where no session cookie is found for the user once the user returns from either DCMAW or IPV.
    - The main method in this class is the `generateNoSessionOrchestrationEntity` which checks if the query parameters it receives contains an `access_denied` error response along with a `state` parameter. It will also check that this feature is enabled as to start with it will not be enabled across all environments to allow testing.
    - If the access_denied error response and state parameter are both present, it will perform a lookup in Redis using the state to retrieve a client session id. It will use that client session id to perform an additional lookup in Redis to retrieve the client session of the user. The client session is required to be able to retrieve the RPs initial authentication request which will contain the RP redirect URI and RP state param, which are both required for sending the access_denied error response back to the RP.
    - The generateNoSessionOrchestrationEntity will finally return a NoSessionEntity object, which will contain everything required for the lambda to return the user back to the RP with the access_denied error response.

---
+++ shared/src/main/java/uk/gov/di/authentication/shared/exceptions/NoSessionException.java
@@ -0,0 +1,8 @@
+package uk.gov.di.authentication.shared.exceptions;
+
+public class NoSessionException extends Exception {
+
+    public NoSessionException(String message) {
+        super(message);
+    }
+}
