8fce5ef12ad73c135177a2e6f003ba7b04d3855b
james.howes <james.howes@digital.cabinet-office.gov.uk>
2023-04-11 10:55:37 +0100

    AUT-1199 - Add PhoneNumberCodeValidator class for use by the VerifyMfaCodeHandler
    - This class will allow the VerifyMfaCodeHandler lambda to validate registration requests for users with SMS as their MFA type
    - It can be extended to support sign ins later on

---
+++ shared/src/test/java/uk/gov/di/authentication/shared/validation/PhoneNumberCodeValidatorTest.java
@@ -0,0 +1,128 @@
+package uk.gov.di.authentication.shared.validation;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import uk.gov.di.authentication.shared.entity.ErrorResponse;
+import uk.gov.di.authentication.shared.entity.NotificationType;
+import uk.gov.di.authentication.shared.entity.Session;
+import uk.gov.di.authentication.shared.services.CodeStorageService;
+import uk.gov.di.authentication.shared.services.ConfigurationService;
+import uk.gov.di.authentication.shared.state.UserContext;
+
+import java.util.Optional;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static uk.gov.di.authentication.shared.services.CodeStorageService.CODE_BLOCKED_KEY_PREFIX;
+
+class PhoneNumberCodeValidatorTest {
+
+    private PhoneNumberCodeValidator phoneNumberCodeValidator;
+    private final Session session = mock(Session.class);
+    private final CodeStorageService codeStorageService = mock(CodeStorageService.class);
+    private final UserContext userContext = mock(UserContext.class);
+    private final ConfigurationService configurationService = mock(ConfigurationService.class);
+    private static final String TEST_EMAIL_ADDRESS = "joe.bloggs@example.com";
+    private static final String VALID_CODE = "123456";
+    private static final String INVALID_CODE = "826272";
+
+    @BeforeEach
+    void setup() {
+        when(configurationService.getCodeMaxRetries()).thenReturn(5);
+    }
+
+    @Test
+    void shouldReturnNoErrorForValidRegistrationPhoneNumberCode() {
+        setupPhoneNumberCode(true);
+
+        assertThat(phoneNumberCodeValidator.validateCode(VALID_CODE), equalTo(Optional.empty()));
+    }
+
+    @Test
+    void shouldReturnErrorForInvalidRegistrationPhoneNumberCode() {
+        setupPhoneNumberCode(true);
+
+        assertThat(
+                phoneNumberCodeValidator.validateCode(INVALID_CODE),
+                equalTo(Optional.of(ErrorResponse.ERROR_1037)));
+    }
+
+    @Test
+    void shouldReturnErrorWhenInvalidRegistrationPhoneNumberCodeUsedTooManyTimes() {
+        setUpPhoneNumberCodeRetryLimitExceeded();
+
+        assertThat(
+                phoneNumberCodeValidator.validateCode(INVALID_CODE),
+                equalTo(Optional.of(ErrorResponse.ERROR_1034)));
+    }
+
+    @Test
+    void shouldReturnErrorWhenUserIsBlockedFromEnteringRegistrationPhoneNumberCodes() {
+        setUpBlockedPhoneNumberCode();
+
+        assertThat(
+                phoneNumberCodeValidator.validateCode(INVALID_CODE),
+                equalTo(Optional.of(ErrorResponse.ERROR_1034)));
+    }
+
+    @Test
+    void shouldThrowExceptionForSignInPhoneNumberCode() {
+        setupPhoneNumberCode(false);
+
+        var expectedException =
+                assertThrows(
+                        RuntimeException.class,
+                        () -> phoneNumberCodeValidator.validateCode(INVALID_CODE),
+                        "Expected to throw exception");
+
+        assertThat(
+                expectedException.getMessage(),
+                equalTo("Sign In Phone number codes are not supported"));
+    }
+
+    public void setupPhoneNumberCode(boolean isRegistration) {
+        when(session.getEmailAddress()).thenReturn(TEST_EMAIL_ADDRESS);
+        when(userContext.getSession()).thenReturn(session);
+        when(configurationService.isTestClientsEnabled()).thenReturn(false);
+        when(codeStorageService.getOtpCode(
+                        TEST_EMAIL_ADDRESS, NotificationType.VERIFY_PHONE_NUMBER))
+                .thenReturn(Optional.of(VALID_CODE));
+        when(codeStorageService.isBlockedForEmail(TEST_EMAIL_ADDRESS, CODE_BLOCKED_KEY_PREFIX))
+                .thenReturn(false);
+        phoneNumberCodeValidator =
+                new PhoneNumberCodeValidator(
+                        codeStorageService, userContext, configurationService, isRegistration);
+    }
+
+    public void setUpPhoneNumberCodeRetryLimitExceeded() {
+        when(codeStorageService.getIncorrectMfaCodeAttemptsCount(TEST_EMAIL_ADDRESS)).thenReturn(6);
+        when(session.getEmailAddress()).thenReturn(TEST_EMAIL_ADDRESS);
+        when(userContext.getSession()).thenReturn(session);
+        when(configurationService.isTestClientsEnabled()).thenReturn(false);
+        when(codeStorageService.getOtpCode(
+                        TEST_EMAIL_ADDRESS, NotificationType.VERIFY_PHONE_NUMBER))
+                .thenReturn(Optional.of(VALID_CODE));
+        when(codeStorageService.isBlockedForEmail(TEST_EMAIL_ADDRESS, CODE_BLOCKED_KEY_PREFIX))
+                .thenReturn(false);
+        phoneNumberCodeValidator =
+                new PhoneNumberCodeValidator(
+                        codeStorageService, userContext, configurationService, true);
+    }
+
+    public void setUpBlockedPhoneNumberCode() {
+        when(session.getEmailAddress()).thenReturn(TEST_EMAIL_ADDRESS);
+        when(userContext.getSession()).thenReturn(session);
+        when(configurationService.isTestClientsEnabled()).thenReturn(false);
+        when(codeStorageService.getOtpCode(
+                        TEST_EMAIL_ADDRESS, NotificationType.VERIFY_PHONE_NUMBER))
+                .thenReturn(Optional.of(VALID_CODE));
+        when(codeStorageService.isBlockedForEmail(TEST_EMAIL_ADDRESS, CODE_BLOCKED_KEY_PREFIX))
+                .thenReturn(true);
+        phoneNumberCodeValidator =
+                new PhoneNumberCodeValidator(
+                        codeStorageService, userContext, configurationService, true);
+    }
+}
