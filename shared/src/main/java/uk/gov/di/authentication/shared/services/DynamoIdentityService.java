f28ec690c7bfba7bd6b2f50ece0c057a4b994028
Alex Wilson <alex.wilson@digital.cabinet-office.gov.uk>
2023-05-19 13:56:43 +0100

    BAU: Refactor identity dynamo to use new base class

--- shared/src/main/java/uk/gov/di/authentication/shared/services/DynamoIdentityService.java
+++ shared/src/main/java/uk/gov/di/authentication/shared/services/DynamoIdentityService.java
@@ -1,66 +1,42 @@
 package uk.gov.di.authentication.shared.services;
 
-import software.amazon.awssdk.enhanced.dynamodb.DynamoDbTable;
-import software.amazon.awssdk.enhanced.dynamodb.Key;
-import software.amazon.awssdk.enhanced.dynamodb.TableSchema;
 import uk.gov.di.authentication.shared.entity.IdentityCredentials;
 import uk.gov.di.authentication.shared.helpers.NowHelper;
 
 import java.time.temporal.ChronoUnit;
 import java.util.Map;
-import java.util.Objects;
 import java.util.Optional;
 
-import static uk.gov.di.authentication.shared.dynamodb.DynamoClientHelper.createDynamoEnhancedClient;
+public class DynamoIdentityService extends BaseDynamoService<IdentityCredentials> {
 
-public class DynamoIdentityService {
-
-    private static final String IDENTITY_CREDENTIALS_TABLE = "identity-credentials";
     private final long timeToExist;
-    private final DynamoDbTable<IdentityCredentials> dynamoIdentityCredentialsTable;
 
     public DynamoIdentityService(ConfigurationService configurationService) {
-        var tableName = configurationService.getEnvironment() + "-" + IDENTITY_CREDENTIALS_TABLE;
-
+        super(IdentityCredentials.class, "identity-credentials", configurationService);
         this.timeToExist = configurationService.getAccessTokenExpiry();
-        var dynamoDbEnhancedClient = createDynamoEnhancedClient(configurationService);
-        dynamoIdentityCredentialsTable =
-                dynamoDbEnhancedClient.table(
-                        tableName, TableSchema.fromBean(IdentityCredentials.class));
-
-        warmUp();
     }
 
     public void addCoreIdentityJWT(String subjectID, String coreIdentityJWT) {
         var identityCredentials =
-                Optional.ofNullable(
-                                dynamoIdentityCredentialsTable.getItem(
-                                        Key.builder().partitionValue(subjectID).build()))
-                        .orElse(new IdentityCredentials());
-        dynamoIdentityCredentialsTable.updateItem(
-                identityCredentials
+                get(subjectID)
+                        .orElse(new IdentityCredentials())
                         .withSubjectID(subjectID)
                         .withCoreIdentityJWT(coreIdentityJWT)
                         .withTimeToExist(
                                 NowHelper.nowPlus(timeToExist, ChronoUnit.SECONDS)
                                         .toInstant()
-                                        .getEpochSecond()));
+                                        .getEpochSecond());
+
+        update(identityCredentials);
     }
 
     public Optional<IdentityCredentials> getIdentityCredentials(String subjectID) {
-        return Optional.ofNullable(
-                        dynamoIdentityCredentialsTable.getItem(
-                                Key.builder().partitionValue(subjectID).build()))
+        return get(subjectID)
                 .filter(t -> t.getTimeToExist() > NowHelper.now().toInstant().getEpochSecond());
     }
 
     public void deleteIdentityCredentials(String subjectID) {
-        var identityCredentials =
-                dynamoIdentityCredentialsTable.getItem(
-                        Key.builder().partitionValue(subjectID).build());
-        if (Objects.nonNull(identityCredentials)) {
-            dynamoIdentityCredentialsTable.deleteItem(identityCredentials);
-        }
+        delete(subjectID);
     }
 
     public void saveIdentityClaims(
@@ -79,10 +55,6 @@ public class DynamoIdentityService {
                                         .toInstant()
                                         .getEpochSecond());
 
-        dynamoIdentityCredentialsTable.putItem(identityCredentials);
-    }
-
-    private void warmUp() {
-        dynamoIdentityCredentialsTable.describeTable();
+        put(identityCredentials);
     }
 }
