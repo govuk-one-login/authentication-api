22b9cff53c8100cb4e39218890d05b5ba2d458d9
Joe Roberts <joe.roberts@digital.cabinet-office.gov.uk>
2023-07-06 15:28:14 +0100

    Remove fix for test client email allow lists

--- ci/terraform/shared/read_secrets.sh
+++ ci/terraform/shared/read_secrets.sh
@@ -14,11 +14,3 @@ for i in $secrets; do
   VAR=(TF_VAR_$name=$value)
   export $VAR
 done
-
-TEST_CLIENT_EMAIL_ALLOWLIST=$(aws secretsmanager get-secret-value \
-	--secret-id test-client-email-allowlist \
-	--region eu-west-2 \
-	| jq --raw-output '.SecretString' | jq -r '."test-client-email-allowlist"'
-)
-
-export TF_VAR_test_client_email_allowlist=$TEST_CLIENT_EMAIL_ALLOWLIST
