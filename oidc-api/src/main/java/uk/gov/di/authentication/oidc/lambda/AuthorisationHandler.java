f053d867db3ef1e732ad799635259d307fdcf35f
james.howes <james.howes@digital.cabinet-office.gov.uk>
2023-07-28 14:51:16 +0100

    AUT-1251 - Set only the value of the State in the secure JAR

--- oidc-api/src/main/java/uk/gov/di/authentication/oidc/lambda/AuthorisationHandler.java
+++ oidc-api/src/main/java/uk/gov/di/authentication/oidc/lambda/AuthorisationHandler.java
@@ -357,7 +357,7 @@ public class AuthorisationHandler
                                             .getEffectiveVectorOfTrust(authenticationRequest)
                                             .getCredentialTrustLevel()
                                             .getValue())
-                            .claim("state", new State())
+                            .claim("state", new State().getValue())
                             .claim("client_id", configurationService.getOrchestrationClientId())
                             .claim("scope", authenticationRequest.getScope().toString())
                             .claim(
