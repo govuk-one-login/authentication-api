49b6a093126767223ef9f0bd6dd8d5d635d38821
Joe Roberts <joe.roberts@digital.cabinet-office.gov.uk>
2023-08-03 16:59:33 +0100

    ENCRYPT_DECRYPT instead of SIGN_VERIFY

--- ci/terraform/shared/kms.tf
+++ ci/terraform/shared/kms.tf
@@ -338,9 +338,9 @@ resource "aws_kms_alias" "orchestration_to_auth_signing_key_alias" {
 
 resource "aws_kms_key" "auth_code_store_signing_key" {
   description              = "KMS signing key for Authorization code store in DynamoDB"
-  deletion_window_in_days  = 30
-  key_usage                = "SIGN_VERIFY"
-  customer_master_key_spec = "ECC_NIST_P256"
+  deletion_window_in_days  = 30  
+  key_usage                = "ENCRYPT_DECRYPT"
+  customer_master_key_spec = "SYMMETRIC_DEFAULT"
   policy = jsonencode({
     Version = "2012-10-17"
     Id      = "key-policy-dynamodb",
