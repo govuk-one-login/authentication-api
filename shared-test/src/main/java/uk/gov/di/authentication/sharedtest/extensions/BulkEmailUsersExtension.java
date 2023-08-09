61e7c19026a71be89e05db59819924ff2ba8aa49
BeckaL <becka.lelew@digital.cabinet-office.gov.uk>
2023-08-09 15:55:37 +0100

    AUT-1441: Add service for Bulk Email Users
    Adds the basics for accessing and updating information in the bulk email users table, introduced temporarily to support sending a bulk email to update users on terms and conditions. Also adds test extension and integration test.
    
    The Base Dynamo Service has also been slightly modified to expose the non-enhanced client. This should not affect existing code, but means that we can run non-enhanced queries (queries that do not return the domain objects) since there are some known oddities with limiting the number of results with enhanced queries.
    
    Co-authored-by: Daniel Besbrode <daniel.besbrode@digital.cabinet-office.gov.uk>

---
+++ shared-test/src/main/java/uk/gov/di/authentication/sharedtest/extensions/BulkEmailUsersExtension.java
@@ -0,0 +1,105 @@
+package uk.gov.di.authentication.sharedtest.extensions;
+
+import org.junit.jupiter.api.extension.AfterEachCallback;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import software.amazon.awssdk.services.dynamodb.model.AttributeDefinition;
+import software.amazon.awssdk.services.dynamodb.model.AttributeValue;
+import software.amazon.awssdk.services.dynamodb.model.BillingMode;
+import software.amazon.awssdk.services.dynamodb.model.CreateTableRequest;
+import software.amazon.awssdk.services.dynamodb.model.GlobalSecondaryIndex;
+import software.amazon.awssdk.services.dynamodb.model.KeySchemaElement;
+import software.amazon.awssdk.services.dynamodb.model.KeyType;
+import software.amazon.awssdk.services.dynamodb.model.ProjectionType;
+import software.amazon.awssdk.services.dynamodb.model.PutItemRequest;
+import software.amazon.awssdk.services.dynamodb.model.ScalarAttributeType;
+import uk.gov.di.authentication.shared.entity.BulkEmailStatus;
+import uk.gov.di.authentication.shared.services.BulkEmailUsersService;
+import uk.gov.di.authentication.shared.services.ConfigurationService;
+import uk.gov.di.authentication.sharedtest.basetest.DynamoTestConfiguration;
+
+import java.util.Map;
+
+public class BulkEmailUsersExtension extends DynamoExtension implements AfterEachCallback {
+
+    public static final String SUBJECT_ID_FIELD = "SubjectID";
+    public static final String BULK_EMAIL_STATUS_FIELD = "BulkEmailStatus";
+    public static final String BULK_EMAIL_STATUS_INDEX = "BulkEmailStatusIndex";
+
+    public static final String BULK_EMAIL_USERS_TABLE = "local-bulk-email-users";
+
+    private BulkEmailUsersService dynamoService;
+    private final ConfigurationService configuration;
+
+    public BulkEmailUsersExtension() {
+        createInstance();
+        this.configuration = new DynamoTestConfiguration(REGION, ENVIRONMENT, DYNAMO_ENDPOINT) {};
+        dynamoService = new BulkEmailUsersService(configuration);
+    }
+
+    @Override
+    public void beforeAll(ExtensionContext context) throws Exception {
+        super.beforeAll(context);
+        dynamoService = new BulkEmailUsersService(configuration);
+    }
+
+    @Override
+    public void afterEach(ExtensionContext context) throws Exception {
+        clearDynamoTable(dynamoDB, BULK_EMAIL_USERS_TABLE, SUBJECT_ID_FIELD);
+    }
+
+    @Override
+    protected void createTables() {
+        if (!tableExists(BULK_EMAIL_USERS_TABLE)) {
+            createBulkEmailUsersTable();
+        }
+    }
+
+    private void createBulkEmailUsersTable() {
+        CreateTableRequest request =
+                CreateTableRequest.builder()
+                        .tableName(BULK_EMAIL_USERS_TABLE)
+                        .keySchema(
+                                KeySchemaElement.builder()
+                                        .keyType(KeyType.HASH)
+                                        .attributeName(SUBJECT_ID_FIELD)
+                                        .build())
+                        .billingMode(BillingMode.PAY_PER_REQUEST)
+                        .attributeDefinitions(
+                                AttributeDefinition.builder()
+                                        .attributeName(SUBJECT_ID_FIELD)
+                                        .attributeType(ScalarAttributeType.S)
+                                        .build(),
+                                AttributeDefinition.builder()
+                                        .attributeName(BULK_EMAIL_STATUS_FIELD)
+                                        .attributeType(ScalarAttributeType.S)
+                                        .build())
+                        .globalSecondaryIndexes(
+                                GlobalSecondaryIndex.builder()
+                                        .indexName(BULK_EMAIL_STATUS_INDEX)
+                                        .keySchema(
+                                                KeySchemaElement.builder()
+                                                        .attributeName(BULK_EMAIL_STATUS_FIELD)
+                                                        .keyType(KeyType.HASH)
+                                                        .build())
+                                        .projection(t -> t.projectionType(ProjectionType.ALL))
+                                        .build())
+                        .build();
+
+        dynamoDB.createTable(request);
+    }
+
+    public void addBulkEmailUser(String subjectID, BulkEmailStatus bulkEmailStatus) {
+        PutItemRequest request =
+                PutItemRequest.builder()
+                        .tableName(BULK_EMAIL_USERS_TABLE)
+                        .item(
+                                Map.of(
+                                        SUBJECT_ID_FIELD,
+                                        AttributeValue.fromS(subjectID),
+                                        BULK_EMAIL_STATUS_FIELD,
+                                        AttributeValue.fromS(bulkEmailStatus.toString())))
+                        .build();
+
+        dynamoDB.putItem(request);
+    }
+}
