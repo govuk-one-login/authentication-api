26948dcb0d077aff4ca3bc8969f82ac2829df72a
Bill Dunn <bill.dunn@digital.cabinet-office.gov.uk>
2023-08-21 09:38:28 +0100

    AUT-1604: Bump OIDC AWS provider versions to latest (5.13.1)

--- ci/terraform/oidc/site.tf
+++ ci/terraform/oidc/site.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "= 3.67.0"
+      version = "= 5.13.1"
     }
     time = {
       source  = "hashicorp/time"
--- ci/terraform/utils/site.tf
+++ ci/terraform/utils/site.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "= 4.19.0"
+      version = "= 5.13.1"
     }
     time = {
       source  = "hashicorp/time"
