d8c61205c75f906bb72de8d040a43edf60d01a86
Bill Dunn <bill.dunn@digital.cabinet-office.gov.uk>
2023-08-21 09:40:01 +0100

    AUT-1604: Replace 's3_force_path_style' with 's3_use_path_style'. 's3_force_path_style' was deprecated in Terraform AWS provider version 4.

--- ci/terraform/shared/site.tf
+++ ci/terraform/shared/site.tf
@@ -29,7 +29,7 @@ provider "aws" {
 
   insecure = var.use_localstack
 
-  s3_force_path_style         = var.use_localstack
+  s3_use_path_style           = var.use_localstack
   skip_credentials_validation = var.use_localstack
   skip_metadata_api_check     = var.use_localstack
   skip_requesting_account_id  = var.use_localstack
