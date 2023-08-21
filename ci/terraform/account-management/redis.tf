5622fac791ac2ee913445037a2592da22ac191b9
Bill Dunn <bill.dunn@digital.cabinet-office.gov.uk>
2023-08-21 15:39:27 +0100

    AUT-1604: Replace arguments in 'aws_elasticache_replication_group' which were deprecated in Terraform AWS Provider version 5.

--- ci/terraform/account-management/redis.tf
+++ ci/terraform/account-management/redis.tf
@@ -23,18 +23,18 @@ resource "random_password" "redis_password" {
 resource "aws_elasticache_replication_group" "account_management_sessions_store" {
   count = var.use_localstack ? 0 : 1
 
-  automatic_failover_enabled    = true
-  availability_zones            = data.aws_availability_zones.available.names
-  replication_group_id          = "${var.environment}-acct-mgmt-session-store"
-  replication_group_description = "A Redis cluster for storing user session data"
-  node_type                     = var.redis_node_size
-  number_cache_clusters         = length(data.aws_availability_zones.available.names)
-  engine                        = "redis"
-  engine_version                = "6.x"
-  parameter_group_name          = "default.redis6.x"
-  port                          = local.redis_port_number
-  maintenance_window            = "tue:22:00-tue:23:00"
-  notification_topic_arn        = data.aws_sns_topic.slack_events.arn
+  automatic_failover_enabled  = true
+  preferred_cache_cluster_azs = data.aws_availability_zones.available.names
+  replication_group_id        = "${var.environment}-acct-mgmt-session-store"
+  description                 = "A Redis cluster for storing user session data"
+  node_type                   = var.redis_node_size
+  num_cache_clusters          = length(data.aws_availability_zones.available.names)
+  engine                      = "redis"
+  engine_version              = "6.x"
+  parameter_group_name        = "default.redis6.x"
+  port                        = local.redis_port_number
+  maintenance_window          = "tue:22:00-tue:23:00"
+  notification_topic_arn      = data.aws_sns_topic.slack_events.arn
 
   multi_az_enabled = true
 
