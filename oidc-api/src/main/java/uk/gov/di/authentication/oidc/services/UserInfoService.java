910b4a5663552fae0654e4b283b08635ae8a5d7b
gn-GDS <george.nemeshanyi@digital.cabinet-office.gov.uk>
2023-09-01 14:26:53 +0100

    AUT-1253: user info service now returns data from Authentication user info table when orch split feature flag enabled.

--- oidc-api/src/main/java/uk/gov/di/authentication/oidc/services/UserInfoService.java
+++ oidc-api/src/main/java/uk/gov/di/authentication/oidc/services/UserInfoService.java
@@ -1,6 +1,8 @@
 package uk.gov.di.authentication.oidc.services;
 
 import com.nimbusds.oauth2.sdk.id.Subject;
+import com.nimbusds.oauth2.sdk.token.BearerTokenError;
+import com.nimbusds.oauth2.sdk.util.JSONObjectUtils;
 import com.nimbusds.openid.connect.sdk.OIDCScopeValue;
 import com.nimbusds.openid.connect.sdk.claims.UserInfo;
 import net.minidev.json.JSONArray;
@@ -11,6 +13,7 @@ import uk.gov.di.authentication.oidc.entity.AccessTokenInfo;
 import uk.gov.di.authentication.oidc.exceptions.UserInfoException;
 import uk.gov.di.authentication.shared.entity.CustomScopeValue;
 import uk.gov.di.authentication.shared.entity.ValidClaims;
+import uk.gov.di.authentication.shared.exceptions.AccessTokenException;
 import uk.gov.di.authentication.shared.helpers.ClientSubjectHelper;
 import uk.gov.di.authentication.shared.serialization.Json;
 import uk.gov.di.authentication.shared.services.AuthenticationService;
@@ -23,7 +26,7 @@ import java.util.Map;
 import java.util.Objects;
 
 public class UserInfoService {
-
+    private final AuthenticationUserInfoStorageService userInfoStorageService;
     private final AuthenticationService authenticationService;
     private final DynamoIdentityService identityService;
     private final DynamoDocAppService dynamoDocAppService;
@@ -38,12 +41,14 @@ public class UserInfoService {
             DynamoIdentityService identityService,
             DynamoDocAppService dynamoDocAppService,
             CloudwatchMetricsService cloudwatchMetricsService,
-            ConfigurationService configurationService) {
+            ConfigurationService configurationService,
+            AuthenticationUserInfoStorageService userInfoStorageService) {
         this.authenticationService = authenticationService;
         this.identityService = identityService;
         this.dynamoDocAppService = dynamoDocAppService;
         this.cloudwatchMetricsService = cloudwatchMetricsService;
         this.configurationService = configurationService;
+        this.userInfoStorageService = userInfoStorageService;
     }
 
     public String calculateSubjectForAudit(AccessTokenInfo accessTokenInfo) {
@@ -64,30 +69,71 @@ public class UserInfoService {
         }
     }
 
-    public UserInfo populateUserInfo(AccessTokenInfo accessTokenInfo) {
+    public UserInfo populateUserInfo(AccessTokenInfo accessTokenInfo) throws AccessTokenException {
         LOG.info("Populating UserInfo");
-        var userInfo = new UserInfo(new Subject(accessTokenInfo.getSubject()));
+        UserInfo userInfo = new UserInfo(new Subject(accessTokenInfo.getSubject()));
         if (accessTokenInfo.getScopes().contains(CustomScopeValue.DOC_CHECKING_APP.getValue())) {
             return populateDocAppUserInfo(accessTokenInfo, userInfo);
         }
-        var userProfile =
-                authenticationService.getUserProfileFromSubject(
-                        accessTokenInfo.getAccessTokenStore().getInternalSubjectId());
 
-        if (accessTokenInfo.getScopes().contains(OIDCScopeValue.EMAIL.getValue())) {
-            userInfo.setEmailAddress(userProfile.getEmail());
-            userInfo.setEmailVerified(userProfile.isEmailVerified());
+        if (configurationService.isAuthOrchSplitEnabled()) {
+            UserInfo tmpUserInfo;
+            try {
+                var authUserInfo =
+                        userInfoStorageService.getAuthenticationUserInfoData(
+                                accessTokenInfo.getAccessTokenStore().getInternalSubjectId());
+
+                if (authUserInfo.isPresent()) {
+                    tmpUserInfo =
+                            new UserInfo(JSONObjectUtils.parse(authUserInfo.get().getUserInfo()));
+                } else {
+                    throw new AccessTokenException(
+                            "Unable to find subject", BearerTokenError.INVALID_TOKEN);
+                }
+            } catch (Exception e) {
+                throw new AccessTokenException(
+                        "Unable to get user info for Subject", BearerTokenError.INVALID_TOKEN);
+            }
+
+            if (accessTokenInfo.getScopes().contains(OIDCScopeValue.EMAIL.getValue())) {
+                userInfo.setEmailAddress(tmpUserInfo.getEmailAddress());
+                userInfo.setEmailVerified(tmpUserInfo.getEmailVerified());
+            }
+            if (accessTokenInfo.getScopes().contains(OIDCScopeValue.PHONE.getValue())) {
+                userInfo.setPhoneNumber(tmpUserInfo.getPhoneNumber());
+                userInfo.setPhoneNumberVerified(tmpUserInfo.getPhoneNumberVerified());
+            }
+            if (accessTokenInfo.getScopes().contains(CustomScopeValue.GOVUK_ACCOUNT.getValue())) {
+                userInfo.setClaim("legacy_subject_id", tmpUserInfo.getClaim("legacy_subject_id"));
+            }
+            if (accessTokenInfo
+                    .getScopes()
+                    .contains(CustomScopeValue.ACCOUNT_MANAGEMENT.getValue())) {
+                userInfo.setClaim("public_subject_id", tmpUserInfo.getClaim("public_subject_id"));
+            }
+        } else {
+            var userProfile =
+                    authenticationService.getUserProfileFromSubject(
+                            accessTokenInfo.getAccessTokenStore().getInternalSubjectId());
+
+            if (accessTokenInfo.getScopes().contains(OIDCScopeValue.EMAIL.getValue())) {
+                userInfo.setEmailAddress(userProfile.getEmail());
+                userInfo.setEmailVerified(userProfile.isEmailVerified());
+            }
+            if (accessTokenInfo.getScopes().contains(OIDCScopeValue.PHONE.getValue())) {
+                userInfo.setPhoneNumber(userProfile.getPhoneNumber());
+                userInfo.setPhoneNumberVerified(userProfile.isPhoneNumberVerified());
+            }
+            if (accessTokenInfo.getScopes().contains(CustomScopeValue.GOVUK_ACCOUNT.getValue())) {
+                userInfo.setClaim("legacy_subject_id", userProfile.getLegacySubjectID());
+            }
+            if (accessTokenInfo
+                    .getScopes()
+                    .contains(CustomScopeValue.ACCOUNT_MANAGEMENT.getValue())) {
+                userInfo.setClaim("public_subject_id", userProfile.getPublicSubjectID());
+            }
         }
-        if (accessTokenInfo.getScopes().contains(OIDCScopeValue.PHONE.getValue())) {
-            userInfo.setPhoneNumber(userProfile.getPhoneNumber());
-            userInfo.setPhoneNumberVerified(userProfile.isPhoneNumberVerified());
-        }
-        if (accessTokenInfo.getScopes().contains(CustomScopeValue.GOVUK_ACCOUNT.getValue())) {
-            userInfo.setClaim("legacy_subject_id", userProfile.getLegacySubjectID());
-        }
-        if (accessTokenInfo.getScopes().contains(CustomScopeValue.ACCOUNT_MANAGEMENT.getValue())) {
-            userInfo.setClaim("public_subject_id", userProfile.getPublicSubjectID());
-        }
+
         if (configurationService.isIdentityEnabled()
                 && Objects.nonNull(accessTokenInfo.getIdentityClaims())) {
             return populateIdentityInfo(accessTokenInfo, userInfo);
