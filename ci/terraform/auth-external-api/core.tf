93560ae8433dfbea1c2f3fcf548e5d4fdeadcec2
james.howes <james.howes@digital.cabinet-office.gov.uk>
2023-09-06 11:20:41 +0100

    AUT-1645 - Add core and site.tf files

---
+++ ci/terraform/auth-external-api/core.tf
@@ -0,0 +1,26 @@
+data "terraform_remote_state" "core" {
+  backend = "s3"
+  config = {
+    bucket                      = var.common_state_bucket
+    key                         = "${var.environment}-core-terraform.tfstate"
+    role_arn                    = var.deployer_role_arn
+    region                      = var.aws_region
+    endpoint                    = null
+    iam_endpoint                = null
+    sts_endpoint                = null
+    skip_credentials_validation = false
+    skip_metadata_api_check     = false
+    force_path_style            = false
+  }
+}
+
+locals {
+  vpc_arn                                    = data.terraform_remote_state.core.outputs.vpc_arn
+  vpc_id                                     = data.terraform_remote_state.core.outputs.vpc_id
+  allow_aws_service_access_security_group_id = data.terraform_remote_state.core.outputs.allow_aws_service_access_security_group_id
+  allow_egress_security_group_id             = data.terraform_remote_state.core.outputs.allow_egress_security_group_id
+  private_subnet_ids                         = data.terraform_remote_state.core.outputs.private_subnet_ids
+  private_subnet_cidr_blocks                 = data.terraform_remote_state.core.outputs.private_subnet_cidr_blocks
+  public_subnet_ids                          = data.terraform_remote_state.core.outputs.public_subnet_ids
+  public_subnet_cidr_blocks                  = data.terraform_remote_state.core.outputs.private_subnet_cidr_blocks
+}
