661438876ecd3a3cfb99098d555d57edeb09c968
Tom Whitwell <tom.whitwell@digital.cabinet-office.gov.uk>
2023-09-27 10:24:07 +0100

    AUT-1678: If no secrets are found, exit with error

--- ci/terraform/shared/read_secrets.sh
+++ ci/terraform/shared/read_secrets.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -euo pipefail
 
+[[ "${BASH_SOURCE[0]}" != "${0}" ]] || {
+  echo "Error: Script must be sourced, not executed"
+  exit 1
+}
+
 ENVIRONMENT="${1}"
 
 if [ "$ENVIRONMENT" = "dev" ]; then
@@ -12,6 +17,11 @@ secrets="$(
     --filter "Key=\"name\",Values=\"/deploy/${ENVIRONMENT}/\"" --region eu-west-2 |
     jq -r '.SecretList[]|[.ARN,(.Name|split("/")|last)]|@tsv'
 )"
+
+if [ -z "${secrets}" ]; then
+  printf '!! ERROR: No secrets found for environment %s. Exiting.\n' "${ENVIRONMENT}" >&2
+  exit 1
+fi
 
 while IFS=$'\t' read -r arn name; do
   value=$(aws secretsmanager get-secret-value --secret-id "${arn}" | jq -r '.SecretString')
