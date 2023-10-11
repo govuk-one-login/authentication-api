31b3da5da306089d51c1a5505d50fe4f385a8147
Joe Roberts <joe.roberts@digital.cabinet-office.gov.uk>
2023-10-11 10:07:52 +0100

    Update Dynatrace integration to reflect changes upstream

--- ci/terraform/modules/endpoint-module/dynatrace.tf
+++ ci/terraform/modules/endpoint-module/dynatrace.tf
@@ -1,41 +1,26 @@
-data "aws_secretsmanager_secret" "dynatrace_auth_token" {
-  name = "DynatraceConnectionAuthToken"
+data "aws_secretsmanager_secret" "dynatrace_secret" {
+  arn = var.environment == "production" ? local.dynatrace_production_secret : local.dynatrace_nonproduction_secret
 }
-data "aws_secretsmanager_secret_version" "dynatrace_auth_token" {
-  secret_id = data.aws_secretsmanager_secret.dynatrace_auth_token.id
-}
-
-data "aws_secretsmanager_secret" "dynatrace_base_url" {
-  name = "DynatraceConnectionBaseURL"
-}
-data "aws_secretsmanager_secret_version" "dynatrace_base_url" {
-  secret_id = data.aws_secretsmanager_secret.dynatrace_base_url.id
-}
-
-data "aws_secretsmanager_secret" "dynatrace_cluster_id" {
-  name = "DynatraceClusterID"
-}
-data "aws_secretsmanager_secret_version" "dynatrace_cluster_id" {
-  secret_id = data.aws_secretsmanager_secret.dynatrace_cluster_id.id
-}
-
-data "aws_secretsmanager_secret" "dynatrace_tenant" {
-  name = "DynatraceTenant"
-}
-data "aws_secretsmanager_secret_version" "dynatrace_tenant" {
-  secret_id = data.aws_secretsmanager_secret.dynatrace_tenant.id
+data "aws_secretsmanager_secret_version" "dynatrace_secret" {
+  secret_id = data.aws_secretsmanager_secret.dynatrace_secret.id
 }
 
 locals {
-  dynatrace_layer_arn = "arn:aws:lambda:eu-west-2:985486846182:layer:DTOneAgentJavaLayer:8"
+  dynatrace_layer_arn = local.dynatrace_secret["JAVA_LAYER"]
   dynatrace_environment_variables = {
     AWS_LAMBDA_EXEC_WRAPPER = "/opt/dynatrace"
 
-    DT_CONNECTION_AUTH_TOKEN = data.aws_secretsmanager_secret_version.dynatrace_auth_token.secret_string
-    DT_CONNECTION_BASE_URL   = data.aws_secretsmanager_secret_version.dynatrace_base_url.secret_string
-    DT_CLUSTER_ID            = data.aws_secretsmanager_secret_version.dynatrace_cluster_id.secret_string
-    DT_TENANT                = data.aws_secretsmanager_secret_version.dynatrace_tenant.secret_string
+    DT_CONNECTION_AUTH_TOKEN     = local.dynatrace_secret["DT_CONNECTION_AUTH_TOKEN"]
+    DT_CONNECTION_BASE_URL       = local.dynatrace_secret["DT_CONNECTION_BASE_URL"]
+    DT_CLUSTER_ID                = local.dynatrace_secret["DT_CLUSTER_ID"]
+    DT_TENANT                    = local.dynatrace_secret["DT_TENANT"]
+    DT_LOG_COLLECTION_AUTH_TOKEN = local.dynatrace_secret["DT_LOG_COLLECTION_AUTH_TOKEN"]
 
     DT_OPEN_TELEMETRY_ENABLE_INTEGRATION = "true"
   }
+
+  dynatrace_production_secret    = "arn:aws:secretsmanager:eu-west-2:216552277552:secret:DynatraceProductionVariables"
+  dynatrace_nonproduction_secret = "arn:aws:secretsmanager:eu-west-2:216552277552:secret:DynatraceNonProductionVariables"
+
+  dynatrace_secret = jsondecode(data.aws_secretsmanager_secret_version.dynatrace_secret.secret_string)
 }
