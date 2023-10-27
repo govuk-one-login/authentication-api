26bb4bc0fcbfb246bdb776bfb8fb8cf9d2a98df0
Alex Wilson <alex.wilson@digital.cabinet-office.gov.uk>
2023-10-27 12:34:08 +0100

    BAU: Refactor test with static imports and making test class a field

--- shared/src/test/java/uk/gov/di/authentication/shared/jwt/KmsJwsSignerTest.java
+++ shared/src/test/java/uk/gov/di/authentication/shared/jwt/KmsJwsSignerTest.java
@@ -4,24 +4,27 @@ import com.amazonaws.util.Base64;
 import com.nimbusds.jose.JOSEException;
 import com.nimbusds.jose.JWSAlgorithm;
 import com.nimbusds.jose.JWSHeader;
-import com.nimbusds.jose.crypto.impl.ECDSA;
 import com.nimbusds.jose.util.Base64URL;
 import org.junit.jupiter.api.Test;
-import software.amazon.awssdk.services.kms.model.SigningAlgorithmSpec;
 import uk.gov.di.authentication.shared.services.KmsConnectionService;
 
+import static com.nimbusds.jose.JWSAlgorithm.ES256;
+import static com.nimbusds.jose.crypto.impl.ECDSA.getSignatureByteArrayLength;
+import static com.nimbusds.jose.crypto.impl.ECDSA.transcodeSignatureToConcat;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+import static software.amazon.awssdk.services.kms.model.SigningAlgorithmSpec.ECDSA_SHA_256;
+import static software.amazon.awssdk.services.kms.model.SigningAlgorithmSpec.RSASSA_PKCS1_V1_5_SHA_256;
 
 class KmsJwsSignerTest {
 
     private final KmsConnectionService kms = mock(KmsConnectionService.class);
+    private final KmsJwsSigner signer = new KmsJwsSigner(kms, "rsa-signing-key", "ec-signing-key");
 
     @Test
     void shouldReturnEmptyBaseUrlWithNoData() throws JOSEException {
-        var signer = new KmsJwsSigner(kms, "rsa-signing-key", "ec-signing-key");
         var header = new JWSHeader.Builder(JWSAlgorithm.RS256).build();
         var emptyData = new byte[0];
 
@@ -30,11 +33,10 @@ class KmsJwsSignerTest {
 
     @Test
     void shouldSignDataWithRS256IfNonEmpty() throws JOSEException {
-        var signer = new KmsJwsSigner(kms, "rsa-signing-key", "ec-signing-key");
         var header = new JWSHeader.Builder(JWSAlgorithm.RS256).build();
         var data = "input".getBytes();
 
-        when(kms.sign("rsa-signing-key", SigningAlgorithmSpec.RSASSA_PKCS1_V1_5_SHA_256, data))
+        when(kms.sign("rsa-signing-key", RSASSA_PKCS1_V1_5_SHA_256, data))
                 .thenReturn("output".getBytes());
 
         assertThat(signer.sign(header, data), is(Base64URL.encode("output")));
@@ -42,8 +44,7 @@ class KmsJwsSignerTest {
 
     @Test
     void shouldSignDataWithES256IfNonEmpty() throws JOSEException {
-        var signer = new KmsJwsSigner(kms, "rsa-signing-key", "ec-signing-key");
-        var header = new JWSHeader.Builder(JWSAlgorithm.ES256).build();
+        var header = new JWSHeader.Builder(ES256).build();
         var data = "input".getBytes();
 
         // "output" signed with disposable key
@@ -51,13 +52,11 @@ class KmsJwsSignerTest {
                 Base64.decode(
                         "MEUCICPb0OfprJaOLMB8mTsZniBwShawFyxujrtriH4gykAXAiEAjHJfq/lM2XgbvzjzBn1Lw9nMdLjzbCserDIw7MfzfPs=");
 
-        when(kms.sign("ec-signing-key", SigningAlgorithmSpec.ECDSA_SHA_256, data))
-                .thenReturn(output);
+        when(kms.sign("ec-signing-key", ECDSA_SHA_256, data)).thenReturn(output);
 
         var expectedOutput =
                 Base64URL.encode(
-                        ECDSA.transcodeSignatureToConcat(
-                                output, ECDSA.getSignatureByteArrayLength(JWSAlgorithm.ES256)));
+                        transcodeSignatureToConcat(output, getSignatureByteArrayLength(ES256)));
 
         assertThat(signer.sign(header, data), is(expectedOutput));
     }
