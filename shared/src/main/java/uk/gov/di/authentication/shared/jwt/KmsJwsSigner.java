8a735512f0a48135ab0022e65cdbd13435a5db40
Alex Wilson <alex.wilson@digital.cabinet-office.gov.uk>
2023-10-27 11:20:27 +0100

    BAU: Add `KmsConnectionService` as constructor parameter, as we will need it

--- shared/src/main/java/uk/gov/di/authentication/shared/jwt/KmsJwsSigner.java
+++ shared/src/main/java/uk/gov/di/authentication/shared/jwt/KmsJwsSigner.java
@@ -6,11 +6,19 @@ import com.nimbusds.jose.JWSHeader;
 import com.nimbusds.jose.JWSSigner;
 import com.nimbusds.jose.jca.JCAContext;
 import com.nimbusds.jose.util.Base64URL;
+import uk.gov.di.authentication.shared.services.KmsConnectionService;
 
 import java.util.Collections;
 import java.util.Set;
 
 public class KmsJwsSigner implements JWSSigner {
+
+    private final KmsConnectionService kms;
+
+    public KmsJwsSigner(KmsConnectionService kms) {
+        this.kms = kms;
+    }
+
     @Override
     public Base64URL sign(JWSHeader header, byte[] signingInput) throws JOSEException {
         return new Base64URL("");
--- shared/src/test/java/uk/gov/di/authentication/shared/jwt/KmsJwsSignerTest.java
+++ shared/src/test/java/uk/gov/di/authentication/shared/jwt/KmsJwsSignerTest.java
@@ -5,15 +5,19 @@ import com.nimbusds.jose.JWSAlgorithm;
 import com.nimbusds.jose.JWSHeader;
 import com.nimbusds.jose.util.Base64URL;
 import org.junit.jupiter.api.Test;
+import uk.gov.di.authentication.shared.services.KmsConnectionService;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.Mockito.mock;
 
 class KmsJwsSignerTest {
 
+    private final KmsConnectionService kms = mock(KmsConnectionService.class);
+
     @Test
     void shouldReturnEmptyBaseUrlWithNoData() throws JOSEException {
-        var signer = new KmsJwsSigner();
+        var signer = new KmsJwsSigner(kms);
         var header = new JWSHeader.Builder(JWSAlgorithm.RS256).build();
         var emptyData = new byte[0];
 
