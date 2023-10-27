e4254a60d2a904a2b901450fae68a646f7f832e0
Alex Wilson <alex.wilson@digital.cabinet-office.gov.uk>
2023-10-27 12:31:43 +0100

    BAU: Sign input with ES256 using KMS

--- shared/src/main/java/uk/gov/di/authentication/shared/jwt/KmsJwsSigner.java
+++ shared/src/main/java/uk/gov/di/authentication/shared/jwt/KmsJwsSigner.java
@@ -4,6 +4,7 @@ import com.nimbusds.jose.JOSEException;
 import com.nimbusds.jose.JWSAlgorithm;
 import com.nimbusds.jose.JWSHeader;
 import com.nimbusds.jose.JWSSigner;
+import com.nimbusds.jose.crypto.impl.ECDSA;
 import com.nimbusds.jose.jca.JCAContext;
 import com.nimbusds.jose.util.Base64URL;
 import uk.gov.di.authentication.shared.services.KmsConnectionService;
@@ -11,16 +12,19 @@ import uk.gov.di.authentication.shared.services.KmsConnectionService;
 import java.util.Collections;
 import java.util.Set;
 
+import static software.amazon.awssdk.services.kms.model.SigningAlgorithmSpec.ECDSA_SHA_256;
 import static software.amazon.awssdk.services.kms.model.SigningAlgorithmSpec.RSASSA_PKCS1_V1_5_SHA_256;
 
 public class KmsJwsSigner implements JWSSigner {
 
     private final KmsConnectionService kms;
     private final String rsaSigningKeyId;
+    private final String ecSigningKeyId;
 
-    public KmsJwsSigner(KmsConnectionService kms, String rsaSigningKeyId) {
+    public KmsJwsSigner(KmsConnectionService kms, String rsaSigningKeyId, String ecSigningKeyId) {
         this.kms = kms;
         this.rsaSigningKeyId = rsaSigningKeyId;
+        this.ecSigningKeyId = ecSigningKeyId;
     }
 
     @Override
@@ -29,7 +33,23 @@ public class KmsJwsSigner implements JWSSigner {
             return new Base64URL("");
         }
 
-        return Base64URL.encode(kms.sign(rsaSigningKeyId, RSASSA_PKCS1_V1_5_SHA_256, signingInput));
+        var signingKey =
+                header.getAlgorithm() == JWSAlgorithm.RS256 ? rsaSigningKeyId : ecSigningKeyId;
+
+        var signingAlgorithm =
+                header.getAlgorithm() == JWSAlgorithm.RS256
+                        ? RSASSA_PKCS1_V1_5_SHA_256
+                        : ECDSA_SHA_256;
+
+        var signedOutput = kms.sign(signingKey, signingAlgorithm, signingInput);
+
+        if (header.getAlgorithm() == JWSAlgorithm.RS256) {
+            return Base64URL.encode(signedOutput);
+        } else {
+            return Base64URL.encode(
+                    ECDSA.transcodeSignatureToConcat(
+                            signedOutput, ECDSA.getSignatureByteArrayLength(JWSAlgorithm.ES256)));
+        }
     }
 
     @Override
--- shared/src/test/java/uk/gov/di/authentication/shared/jwt/KmsJwsSignerTest.java
+++ shared/src/test/java/uk/gov/di/authentication/shared/jwt/KmsJwsSignerTest.java
@@ -1,8 +1,10 @@
 package uk.gov.di.authentication.shared.jwt;
 
+import com.amazonaws.util.Base64;
 import com.nimbusds.jose.JOSEException;
 import com.nimbusds.jose.JWSAlgorithm;
 import com.nimbusds.jose.JWSHeader;
+import com.nimbusds.jose.crypto.impl.ECDSA;
 import com.nimbusds.jose.util.Base64URL;
 import org.junit.jupiter.api.Test;
 import software.amazon.awssdk.services.kms.model.SigningAlgorithmSpec;
@@ -19,7 +21,7 @@ class KmsJwsSignerTest {
 
     @Test
     void shouldReturnEmptyBaseUrlWithNoData() throws JOSEException {
-        var signer = new KmsJwsSigner(kms, "rsa-signing-key");
+        var signer = new KmsJwsSigner(kms, "rsa-signing-key", "ec-signing-key");
         var header = new JWSHeader.Builder(JWSAlgorithm.RS256).build();
         var emptyData = new byte[0];
 
@@ -28,7 +30,7 @@ class KmsJwsSignerTest {
 
     @Test
     void shouldSignDataWithRS256IfNonEmpty() throws JOSEException {
-        var signer = new KmsJwsSigner(kms, "rsa-signing-key");
+        var signer = new KmsJwsSigner(kms, "rsa-signing-key", "ec-signing-key");
         var header = new JWSHeader.Builder(JWSAlgorithm.RS256).build();
         var data = "input".getBytes();
 
@@ -36,5 +38,27 @@ class KmsJwsSignerTest {
                 .thenReturn("output".getBytes());
 
         assertThat(signer.sign(header, data), is(Base64URL.encode("output")));
+    }
+
+    @Test
+    void shouldSignDataWithES256IfNonEmpty() throws JOSEException {
+        var signer = new KmsJwsSigner(kms, "rsa-signing-key", "ec-signing-key");
+        var header = new JWSHeader.Builder(JWSAlgorithm.ES256).build();
+        var data = "input".getBytes();
+
+        // "output" signed with disposable key
+        var output =
+                Base64.decode(
+                        "MEUCICPb0OfprJaOLMB8mTsZniBwShawFyxujrtriH4gykAXAiEAjHJfq/lM2XgbvzjzBn1Lw9nMdLjzbCserDIw7MfzfPs=");
+
+        when(kms.sign("ec-signing-key", SigningAlgorithmSpec.ECDSA_SHA_256, data))
+                .thenReturn(output);
+
+        var expectedOutput =
+                Base64URL.encode(
+                        ECDSA.transcodeSignatureToConcat(
+                                output, ECDSA.getSignatureByteArrayLength(JWSAlgorithm.ES256)));
+
+        assertThat(signer.sign(header, data), is(expectedOutput));
     }
 }
