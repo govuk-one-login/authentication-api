489735d0d32221f0b466fe00c6d6b79345f819f0
Aidan Comer <alhcomer@hotmail.co.uk>
2023-11-06 14:38:57 +0000

    AUT-1826: added CMK encryption to client registry table
    updated dynamodb encryption

--- ci/terraform/oidc/send_notification.tf
+++ ci/terraform/oidc/send_notification.tf
@@ -11,7 +11,8 @@ module "frontend_api_send_notification_role" {
     aws_iam_policy.lambda_sns_policy.arn,
     aws_iam_policy.redis_parameter_policy.arn,
     module.oidc_txma_audit.access_policy_arn,
-    local.account_modifiers_encryption_policy_arn
+    local.account_modifiers_encryption_policy_arn,
+    local.client_registry_encryption_policy_arn
   ]
 }
 
