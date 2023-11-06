daa1a1687c8514d4eeaf5c272dcc09d14a73b4e2
Aidan Comer <alhcomer@hotmail.co.uk>
2023-11-06 14:38:57 +0000

    AUT-1826: added CMK encryption to client registry table
    updated dynamodb encryption
    
    AUT-1826: gave lambdas client registry decryption capability

--- ci/terraform/oidc/reset_password.tf
+++ ci/terraform/oidc/reset_password.tf
@@ -15,7 +15,8 @@ module "frontend_api_reset_password_role" {
     aws_iam_policy.redis_parameter_policy.arn,
     aws_iam_policy.dynamo_common_passwords_read_access_policy.arn,
     module.oidc_txma_audit.access_policy_arn,
-    local.account_modifiers_encryption_policy_arn
+    local.account_modifiers_encryption_policy_arn,
+    local.client_registry_encryption_policy_arn
   ]
 }
 
