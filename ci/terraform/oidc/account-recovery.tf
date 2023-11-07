d171296be3d51f1b69b53c76b1fa296ecbd4fd03
Aidan Comer <alhcomer@hotmail.co.uk>
2023-11-07 09:37:39 +0000

    AUT-1826: applied CMK encryption to user creds table
    turned on encryption
    
    fmt

--- ci/terraform/oidc/account-recovery.tf
+++ ci/terraform/oidc/account-recovery.tf
@@ -13,7 +13,8 @@ module "frontend_api_account_recovery_role" {
     aws_iam_policy.redis_parameter_policy.arn,
     aws_iam_policy.dynamo_account_modifiers_read_access_policy.arn,
     module.oidc_txma_audit.access_policy_arn,
-    local.account_modifiers_encryption_policy_arn
+    local.account_modifiers_encryption_policy_arn,
+    local.user_credentials_encryption_policy_arn
   ]
 }
 
