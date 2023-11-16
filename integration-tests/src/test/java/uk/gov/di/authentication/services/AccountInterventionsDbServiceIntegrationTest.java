058f8e233c6857c5355c59e70270fa4cc41fb80d
BeckaL <becka.lelew@digital.cabinet-office.gov.uk>
2023-11-16 16:44:42 +0000

    AUT-1865: Add AccountInterventionsStubService
    Co-authored-by: Dominic Taylor <dominic.taylor@digital.cabinet-office.gov.uk>

---
+++ integration-tests/src/test/java/uk/gov/di/authentication/services/AccountInterventionsDbServiceIntegrationTest.java
@@ -0,0 +1,47 @@
+package uk.gov.di.authentication.services;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+import uk.gov.di.authentication.interventions.api.stub.entity.AccountInterventionsStore;
+import uk.gov.di.authentication.interventions.api.stub.services.AccountInterventionsDbService;
+import uk.gov.di.authentication.shared.services.ConfigurationService;
+import uk.gov.di.authentication.sharedtest.extensions.AccountInterventionsStubStoreExtension;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class AccountInterventionsDbServiceIntegrationTest {
+
+    private static final String INTERNAL_PAIRWISE_ID = "test-pairwise-id";
+
+    @RegisterExtension
+    protected static final AccountInterventionsStubStoreExtension dbExtension =
+            new AccountInterventionsStubStoreExtension();
+
+    AccountInterventionsDbService dynamoAuthCodeService =
+            new AccountInterventionsDbService(ConfigurationService.getInstance());
+
+    @Test
+    void shouldReturnAccountInterventionsStoreWhereItExists() {
+        dbExtension.addAccountInterventions(INTERNAL_PAIRWISE_ID, true, true, true, true);
+        Optional<AccountInterventionsStore> accountInterventionsStore;
+        accountInterventionsStore =
+                dynamoAuthCodeService.getAccountInterventions(INTERNAL_PAIRWISE_ID);
+
+        assertTrue(accountInterventionsStore.isPresent());
+        assertTrue(accountInterventionsStore.get().isBlocked());
+        assertTrue(accountInterventionsStore.get().isSuspended());
+        assertTrue(accountInterventionsStore.get().isReproveIdentity());
+        assertTrue(accountInterventionsStore.get().isResetPassword());
+    }
+
+    @Test
+    void shouldReturnNoneWhereThePairwiseIdDoesNotExist() {
+        dbExtension.addAccountInterventions("anotherPairwiseId", true, true, true, true);
+        var accountInterventionsStore =
+                dynamoAuthCodeService.getAccountInterventions(INTERNAL_PAIRWISE_ID);
+
+        assertTrue(accountInterventionsStore.isEmpty());
+    }
+}
---
+++ shared-test/src/main/java/uk/gov/di/authentication/sharedtest/extensions/AccountInterventionsStubStoreExtension.java
@@ -0,0 +1,78 @@
+package uk.gov.di.authentication.sharedtest.extensions;
+
+import org.junit.jupiter.api.extension.AfterEachCallback;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import software.amazon.awssdk.services.dynamodb.model.AttributeDefinition;
+import software.amazon.awssdk.services.dynamodb.model.BillingMode;
+import software.amazon.awssdk.services.dynamodb.model.CreateTableRequest;
+import software.amazon.awssdk.services.dynamodb.model.KeySchemaElement;
+import software.amazon.awssdk.services.dynamodb.model.KeyType;
+import software.amazon.awssdk.services.dynamodb.model.ScalarAttributeType;
+import uk.gov.di.authentication.interventions.api.stub.services.AccountInterventionsDbService;
+import uk.gov.di.authentication.shared.services.ConfigurationService;
+import uk.gov.di.authentication.sharedtest.basetest.DynamoTestConfiguration;
+
+public class AccountInterventionsStubStoreExtension extends DynamoExtension
+        implements AfterEachCallback {
+
+    public static final String PAIRWISE_ID_FIELD = "InternalPairwiseId";
+    public static final String ACCOUNT_INTERVENTIONS_STORE_TABLE =
+            "local-stub-account-interventions";
+
+    private AccountInterventionsDbService db;
+    private final ConfigurationService configuration;
+
+    public AccountInterventionsStubStoreExtension() {
+        createInstance();
+        this.configuration = new DynamoTestConfiguration(REGION, ENVIRONMENT, DYNAMO_ENDPOINT);
+    }
+
+    @Override
+    public void beforeAll(ExtensionContext context) throws Exception {
+        super.beforeAll(context);
+
+        db = new AccountInterventionsDbService(configuration);
+    }
+
+    @Override
+    public void afterEach(ExtensionContext context) throws Exception {
+        clearDynamoTable(dynamoDB, ACCOUNT_INTERVENTIONS_STORE_TABLE, PAIRWISE_ID_FIELD);
+    }
+
+    @Override
+    protected void createTables() {
+        if (!tableExists(ACCOUNT_INTERVENTIONS_STORE_TABLE)) {
+            createAccountInterventionsStoreTable();
+        }
+    }
+
+    public void addAccountInterventions(
+            String internalPairwiseId,
+            boolean blocked,
+            boolean suspended,
+            boolean reproveIdentity,
+            boolean resetPassword) {
+        db.addAccountInterventions(
+                internalPairwiseId, blocked, suspended, reproveIdentity, resetPassword);
+    }
+
+    private void createAccountInterventionsStoreTable() {
+        CreateTableRequest request =
+                CreateTableRequest.builder()
+                        .tableName(ACCOUNT_INTERVENTIONS_STORE_TABLE)
+                        .keySchema(
+                                KeySchemaElement.builder()
+                                        .keyType(KeyType.HASH)
+                                        .attributeName(PAIRWISE_ID_FIELD)
+                                        .build())
+                        .billingMode(BillingMode.PAY_PER_REQUEST)
+                        .attributeDefinitions(
+                                AttributeDefinition.builder()
+                                        .attributeName(PAIRWISE_ID_FIELD)
+                                        .attributeType(ScalarAttributeType.S)
+                                        .build())
+                        .build();
+
+        dynamoDB.createTable(request);
+    }
+}
