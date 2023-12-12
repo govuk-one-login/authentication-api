c20e1bea2f2f2e78150766523cd21a237de00230
Ethan Mills <ethan.mills@digital.cabinet-office.gov.uk>
2023-12-12 16:57:57 +0000

    Revert "ATO-151: Add permissions for the protected subnet to Redis"
    This reverts commit fc44b61a772d2291ac644f42a6c62e860f3c8970.

--- ci/terraform/shared/security-groups.tf
+++ ci/terraform/shared/security-groups.tf
@@ -28,16 +28,6 @@ resource "aws_security_group_rule" "allow_incoming_redis_from_private_subnet" {
   type        = "ingress"
 }
 
-resource "aws_security_group_rule" "allow_incoming_redis_from_protected_subnet" {
-  security_group_id = aws_security_group.redis_security_group.id
-
-  from_port   = local.redis_port_number
-  protocol    = "tcp"
-  cidr_blocks = local.protected_subnet_cidr_blocks
-  to_port     = local.redis_port_number
-  type        = "ingress"
-}
-
 resource "aws_security_group_rule" "allow_connection_to_oidc_redis" {
   security_group_id = aws_security_group.allow_access_to_oidc_redis.id
 
