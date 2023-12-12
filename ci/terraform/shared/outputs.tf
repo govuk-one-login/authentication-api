43b0946e903ddd94ad0dc1a4c06d28cba9039efa
Ethan Mills <ethan.mills@digital.cabinet-office.gov.uk>
2023-12-12 16:06:25 +0000

    ATO-151: re-add exported subnet value
    This is used by the experian check

--- ci/terraform/shared/outputs.tf
+++ ci/terraform/shared/outputs.tf
@@ -27,6 +27,10 @@ output "authentication_oidc_redis_security_group_id" {
   value = aws_security_group.allow_access_to_oidc_redis.id
 }
 
+output "authentication_subnet_ids" {
+  value = local.private_subnet_ids
+}
+
 output "authentication_private_subnet_ids" {
   value = local.private_subnet_ids
 }
