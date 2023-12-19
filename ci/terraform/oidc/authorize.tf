43d037c1483f700cdb8e8a114444de25a82451bf
cargil <carly.gilson@digital.cabinet-office.gov.uk>
2023-12-19 15:53:48 +0000

    ATO-208: Add kms permissions and token signing key env variable

--- ci/terraform/oidc/authorize.tf
+++ ci/terraform/oidc/authorize.tf
@@ -17,7 +17,8 @@ module "oidc_authorize_role" {
     aws_iam_policy.auth_public_encryption_key_parameter_policy.arn,
     local.client_registry_encryption_policy_arn,
     local.doc_app_credential_encryption_policy_arn,
-    local.user_credentials_encryption_policy_arn
+    local.user_credentials_encryption_policy_arn,
+    aws_iam_policy.oidc_token_kms_signing_policy.arn
   ]
 }
 
@@ -56,6 +57,7 @@ module "authorize" {
     DYNAMO_ENDPOINT                      = var.use_localstack ? var.lambda_dynamo_endpoint : null
     CUSTOM_DOC_APP_CLAIM_ENABLED         = var.custom_doc_app_claim_enabled
     ORCH_REDIRECT_URI                    = var.orch_redirect_uri
+    TOKEN_SIGNING_KEY_ALIAS              = local.id_token_signing_key_alias_name
   }
   handler_function_name = "uk.gov.di.authentication.oidc.lambda.AuthorisationHandler::handleRequest"
   rest_api_id           = aws_api_gateway_rest_api.di_authentication_api.id
