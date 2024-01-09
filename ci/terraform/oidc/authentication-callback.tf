b673f5efd111c7f8e5d12e23af85a7be55b5ba85
Bill Dunn <bill.dunn@digital.cabinet-office.gov.uk>
2024-01-09 15:32:07 +0000

    ATO-171: Attach policy to give orchestration-redirect lambda read access to user profile dynamo tables
    - Fixes deployment error: `.../staging-orchestration-redirect-lambda is not authorized to perform: dynamodb:DescribeTable on resource: .../staging-user-profile`

--- ci/terraform/oidc/authentication-callback.tf
+++ ci/terraform/oidc/authentication-callback.tf
@@ -14,7 +14,8 @@ module "oidc_api_authentication_callback_role" {
     aws_iam_policy.orch_to_auth_kms_policy.arn,
     aws_iam_policy.authentication_callback_userinfo_encryption_key_kms_policy.arn,
     module.oidc_txma_audit.access_policy_arn,
-    local.client_registry_encryption_policy_arn
+    local.client_registry_encryption_policy_arn,
+    aws_iam_policy.dynamo_user_read_access_policy.arn
   ]
 }
 
