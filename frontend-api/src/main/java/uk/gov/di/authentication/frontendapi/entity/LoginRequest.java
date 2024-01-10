06716054ea3a906a2f839d39834c827e4aa8be0b
ayoshebby <ayo.sebiotimo@digital.cabinet-office.gov.uk>
2024-01-10 12:31:00 +0000

    AUT-2117: Create new REAUTHENTICATION journey type for incorrect password count in Login Handler - Create new Journey Type REAUTHENTICATE - Implement new journey type in the Login handler - Refactor CodeStorageService for reading and updating incorrect password count for REAUTHENTICATE journey type by creating a new prefix

--- frontend-api/src/main/java/uk/gov/di/authentication/frontendapi/entity/LoginRequest.java
+++ frontend-api/src/main/java/uk/gov/di/authentication/frontendapi/entity/LoginRequest.java
@@ -1,21 +1,32 @@
 package uk.gov.di.authentication.frontendapi.entity;
 
 import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
 import uk.gov.di.authentication.shared.entity.BaseFrontendRequest;
+import uk.gov.di.authentication.shared.entity.JourneyType;
 import uk.gov.di.authentication.shared.validation.Required;
 
 public class LoginRequest extends BaseFrontendRequest {
 
     @Expose @Required private String password;
 
+    @SerializedName("journeyType")
+    @Expose
+    protected JourneyType journeyType;
+
     public LoginRequest() {}
 
-    public LoginRequest(String email, String password) {
+    public LoginRequest(String email, String password, JourneyType journeyType) {
         this.email = email;
         this.password = password;
+        this.journeyType = journeyType;
     }
 
     public String getPassword() {
         return password;
+    }
+
+    public JourneyType getJourneyType() {
+        return journeyType;
     }
 }
