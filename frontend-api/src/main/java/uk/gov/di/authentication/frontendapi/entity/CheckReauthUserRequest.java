e80620e27cba6f10ed48cf2c08feb2c9325dec79
ayoshebby <ayo.sebiotimo@digital.cabinet-office.gov.uk>
2024-01-30 15:59:57 +0000

    AUT-2127: Create CheckReAuthUser handler - Do a look up the user's profile via email in the input request - Recalulate the RP pairwise ID, and compare pairwaise IDs of the signed-in user and user requesting reauthentication - Return 200 http status code if there is a match, 400 http status code if no match or user not found

---
+++ frontend-api/src/main/java/uk/gov/di/authentication/frontendapi/entity/CheckReauthUserRequest.java
@@ -0,0 +1,7 @@
+package uk.gov.di.authentication.frontendapi.entity;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+import org.apache.logging.log4j.core.config.plugins.validation.constraints.Required;
+
+public record CheckReauthUserRequest(@SerializedName("email") @Expose @Required String email) {}
