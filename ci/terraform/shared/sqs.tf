952b78b410465421cd991f2184bd3a288c9c0c0e
BeckaL <becka.lelew@digital.cabinet-office.gov.uk>
2024-01-31 16:17:37 +0000

    AUT-2146: Add pending email check dead letter queue
    This is configured with a maxReceiveCount of 1, meaning that messages should go to this queue after zero retries.

--- ci/terraform/shared/sqs.tf
+++ ci/terraform/shared/sqs.tf
@@ -8,5 +8,21 @@ resource "aws_sqs_queue" "pending_email_check_queue" {
   kms_master_key_id                 = var.use_localstack ? null : "alias/aws/sqs"
   kms_data_key_reuse_period_seconds = var.use_localstack ? null : 300
 
+  redrive_policy = jsonencode({
+    deadLetterTargetArn = aws_sqs_queue.pending_email_check_dead_letter_queue.arn
+    maxReceiveCount     = 1
+  })
+
+  tags = local.default_tags
+}
+
+resource "aws_sqs_queue" "pending_email_check_dead_letter_queue" {
+  name = "${var.environment}-pending-email-check-dlq"
+
+  kms_master_key_id                 = var.use_localstack ? null : "alias/aws/sqs"
+  kms_data_key_reuse_period_seconds = var.use_localstack ? null : 300
+
+  message_retention_seconds = 1209600
+
   tags = local.default_tags
 }
