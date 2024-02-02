98452651fd228df3ba848a705a581fb86e51baa6
BeckaL <becka.lelew@digital.cabinet-office.gov.uk>
2024-02-02 14:10:38 +0000

    AUT-2146: Add kms key for encrypting pending email queue
    Note that this needs us to create the relevant secrets in aws for the auth_check_account_id in each environment
    
    Co-authored-by: Tom Whitwell <tom.whitwell@digital.cabinet-office.gov.uk>

--- ci/terraform/shared/outputs.tf
+++ ci/terraform/shared/outputs.tf
@@ -67,6 +67,11 @@ output "pending_email_check_queue_id" {
   value = aws_sqs_queue.pending_email_check_queue.id
 }
 
+output "pending_email_check_queue_encryption_key_arn" {
+  description = "the ARN of the KMS key used to encrypt payloads in the pending email check queue"
+  value       = aws_kms_key.pending_email_check_queue_encryption_key.arn
+}
+
 output "email_lambda_iam_role_arn" {
   value = aws_iam_role.email_lambda_iam_role.arn
 }
--- ci/terraform/shared/sqs.tf
+++ ci/terraform/shared/sqs.tf
@@ -5,7 +5,7 @@ resource "aws_sqs_queue" "pending_email_check_queue" {
   message_retention_seconds = 1209600
   receive_wait_time_seconds = 10
 
-  kms_master_key_id                 = var.use_localstack ? null : "alias/aws/sqs"
+  kms_master_key_id                 = var.use_localstack ? null : aws_kms_key.pending_email_check_queue_encryption_key.arn
   kms_data_key_reuse_period_seconds = var.use_localstack ? null : 300
 
   redrive_policy = jsonencode({
@@ -19,10 +19,11 @@ resource "aws_sqs_queue" "pending_email_check_queue" {
 resource "aws_sqs_queue" "pending_email_check_dead_letter_queue" {
   name = "${var.environment}-pending-email-check-dlq"
 
-  kms_master_key_id                 = var.use_localstack ? null : "alias/aws/sqs"
+  kms_master_key_id                 = var.use_localstack ? null : aws_kms_key.pending_email_check_queue_encryption_key.arn
   kms_data_key_reuse_period_seconds = var.use_localstack ? null : 300
 
   message_retention_seconds = 1209600
 
   tags = local.default_tags
 }
+
