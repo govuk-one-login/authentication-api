0e550a8107aff52a976a1b6fedb8544039f58eae
Bill Dunn <bill.dunn@digital.cabinet-office.gov.uk>
2024-02-08 11:23:52 +0000

    ATO-347: Add new `Account InterventionStatus` enum which `AccountInterventionState` makes use of.

--- orchestration-shared/src/main/java/uk/gov/di/orchestration/shared/entity/AccountInterventionState.java
+++ orchestration-shared/src/main/java/uk/gov/di/orchestration/shared/entity/AccountInterventionState.java
@@ -7,4 +7,24 @@ public record AccountInterventionState(
         @Expose boolean blocked,
         @Expose boolean suspended,
         @Expose @SerializedName("reproveIdentity") boolean reproveIdentity,
-        @Expose @SerializedName("resetPassword") boolean resetPassword) {}
+        @Expose @SerializedName("resetPassword") boolean resetPassword) {
+
+    public AccountInterventionStatus getStatus() {
+        if (blocked) return AccountInterventionStatus.BLOCKED;
+        if (suspended) {
+            if (!reproveIdentity && !resetPassword) {
+                return AccountInterventionStatus.SUSPENDED_NO_ACTION;
+            }
+            if (reproveIdentity && !resetPassword) {
+                return AccountInterventionStatus.SUSPENDED_REPROVE_ID;
+            }
+            if (!reproveIdentity && resetPassword) {
+                return AccountInterventionStatus.SUSPENDED_RESET_PASSWORD;
+            }
+            if (reproveIdentity && resetPassword) {
+                return AccountInterventionStatus.SUSPENDED_RESET_PASSWORD_REPROVE_ID;
+            }
+        }
+        return AccountInterventionStatus.NO_INTERVENTION;
+    }
+}
---
+++ orchestration-shared/src/main/java/uk/gov/di/orchestration/shared/entity/AccountInterventionStatus.java
@@ -0,0 +1,10 @@
+package uk.gov.di.orchestration.shared.entity;
+
+public enum AccountInterventionStatus {
+    NO_INTERVENTION,
+    BLOCKED,
+    SUSPENDED_NO_ACTION,
+    SUSPENDED_REPROVE_ID,
+    SUSPENDED_RESET_PASSWORD,
+    SUSPENDED_RESET_PASSWORD_REPROVE_ID,
+}
