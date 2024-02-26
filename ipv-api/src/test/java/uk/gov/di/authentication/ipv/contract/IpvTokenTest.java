39568dd6b1103867b1a261234e017bdf96dbd455
Isaac Au <isaac.au@digital.cabinet-office.gov.uk>
2024-02-26 13:15:14 +0000

    ATO-384: formatting

--- ipv-api/src/test/java/uk/gov/di/authentication/ipv/contract/IpvTokenTest.java
+++ ipv-api/src/test/java/uk/gov/di/authentication/ipv/contract/IpvTokenTest.java
@@ -58,7 +58,7 @@ import static uk.gov.di.orchestration.sharedtest.helper.IdentityTestData.*;
 import static uk.gov.di.orchestration.sharedtest.helper.IdentityTestData.PASSPORT_CLAIM;
 
 @PactConsumerTest
-@MockServerConfig(hostInterface = "localHost", port="1234")
+@MockServerConfig(hostInterface = "localHost", port = "1234")
 public class IpvTokenTest {
     private static final Subject SUBJECT = new Subject();
     private final Context context = mock(Context.class);
@@ -77,7 +77,8 @@ public class IpvTokenTest {
             mock(AccountInterventionService.class);
     private final IPVCallbackHelper ipvCallbackHelper = mock(IPVCallbackHelper.class);
     private final AuditService auditService = mock(AuditService.class);
-    private final IPVTokenService ipvTokenService = new IPVTokenService(configService, kmsConnectionService);
+    private final IPVTokenService ipvTokenService =
+            new IPVTokenService(configService, kmsConnectionService);
 
     private final Json objectMapper = SerializationService.getInstance();
 
@@ -144,14 +145,88 @@ public class IpvTokenTest {
     private final String URI_VALUE = "https://localhost";
     private static final String KEY_ID = "14342354354353";
 
-    private static final String PRIVATE_JWT_KEY = "{\"kty\":\"EC\",\"d\":\"A2cfN3vYKgOQ_r1S6PhGHCLLiVEqUshFYExrxMwkq_A\",\"crv\":\"P-256\",\"kid\":\"14342354354353\",\"x\":\"BMyQQqr3NEFYgb9sEo4hRBje_HHEsy87PbNIBGL4Uiw\",\"y\":\"qoXdkYVomy6HWT6yNLqjHSmYoICs6ioUF565Btx0apw\",\"alg\":\"ES256\"}";
+    private static final String PRIVATE_JWT_KEY =
+            "{\"kty\":\"EC\",\"d\":\"A2cfN3vYKgOQ_r1S6PhGHCLLiVEqUshFYExrxMwkq_A\",\"crv\":\"P-256\",\"kid\":\"14342354354353\",\"x\":\"BMyQQqr3NEFYgb9sEo4hRBje_HHEsy87PbNIBGL4Uiw\",\"y\":\"qoXdkYVomy6HWT6yNLqjHSmYoICs6ioUF565Btx0apw\",\"alg\":\"ES256\"}";
 
-    private static final String CLIENT_ASSERTION_HEADER = "eyJraWQiOiIxNDM0MjM1NDM1NDM1MyIsImFsZyI6IkVTMjU2In0";
+    private static final String CLIENT_ASSERTION_HEADER =
+            "eyJraWQiOiIxNDM0MjM1NDM1NDM1MyIsImFsZyI6IkVTMjU2In0";
     private static final String CLIENT_ASSERTION_BODY =
             "eyJzdWIiOiJhdXRoT3JjaGVzdHJhdG9yIiwiYXVkIjoiaHR0cDovL2lwdi8iLCJuYmYiOjk0NjY4NDgwMCwiaXNzIjoiYXV0aE9yY2hlc3RyYXRvciIsImV4cCI6NDA3MDkwODgwMCwiaWF0Ijo5NDY2ODQ4MDAsImp0aSI6IjEifQ";
     private static final String CLIENT_ASSERTION_SIGNATURE =
             "L3h9FCeYLIUCpjMGjnBm6Ca8GmKqICHSGY5Aq0svbMNTmLP04dzh5V8E6N2InzbXC9_4Q7u6mAo3yubbYsVSdA";
-    private static final byte[] SIGNATURE_BYTES = {(byte) 48, (byte)68, (byte)2, (byte)32, (byte)47, (byte)120, (byte)125, (byte)20, (byte)39, (byte)-104, (byte)44, (byte)-123, (byte)2, (byte)-90, (byte)51, (byte)6, (byte)-114, (byte)112, (byte)102, (byte)-24, (byte)38, (byte)-68, (byte)26, (byte)98, (byte)-86, (byte)32, (byte)33, (byte)-46, (byte)25, (byte)-114, (byte)64, (byte)-85, (byte)75, (byte)47, (byte)108, (byte)-61, (byte)2, (byte)32, (byte)83, (byte)-104, (byte)-77, (byte)-12, (byte)-31, (byte)-36, (byte)-31, (byte)-27, (byte)95, (byte)4, (byte)-24, (byte)-35, (byte)-120, (byte)-97, (byte)54, (byte)-41, (byte)11, (byte)-33, (byte)-8, (byte)67, (byte)-69, (byte)-70, (byte)-104, (byte)10, (byte)55, (byte)-54, (byte)-26, (byte)-37, (byte)98, (byte)-59, (byte)82, (byte)116};
+    private static final byte[] SIGNATURE_BYTES = {
+        (byte) 48,
+        (byte) 68,
+        (byte) 2,
+        (byte) 32,
+        (byte) 47,
+        (byte) 120,
+        (byte) 125,
+        (byte) 20,
+        (byte) 39,
+        (byte) -104,
+        (byte) 44,
+        (byte) -123,
+        (byte) 2,
+        (byte) -90,
+        (byte) 51,
+        (byte) 6,
+        (byte) -114,
+        (byte) 112,
+        (byte) 102,
+        (byte) -24,
+        (byte) 38,
+        (byte) -68,
+        (byte) 26,
+        (byte) 98,
+        (byte) -86,
+        (byte) 32,
+        (byte) 33,
+        (byte) -46,
+        (byte) 25,
+        (byte) -114,
+        (byte) 64,
+        (byte) -85,
+        (byte) 75,
+        (byte) 47,
+        (byte) 108,
+        (byte) -61,
+        (byte) 2,
+        (byte) 32,
+        (byte) 83,
+        (byte) -104,
+        (byte) -77,
+        (byte) -12,
+        (byte) -31,
+        (byte) -36,
+        (byte) -31,
+        (byte) -27,
+        (byte) 95,
+        (byte) 4,
+        (byte) -24,
+        (byte) -35,
+        (byte) -120,
+        (byte) -97,
+        (byte) 54,
+        (byte) -41,
+        (byte) 11,
+        (byte) -33,
+        (byte) -8,
+        (byte) 67,
+        (byte) -69,
+        (byte) -70,
+        (byte) -104,
+        (byte) 10,
+        (byte) 55,
+        (byte) -54,
+        (byte) -26,
+        (byte) -37,
+        (byte) 98,
+        (byte) -59,
+        (byte) 82,
+        (byte) 116
+    };
+
     @BeforeEach
     void setUp() {
         handler =
@@ -200,10 +275,9 @@ public class IpvTokenTest {
                                 + "."
                                 + CLIENT_ASSERTION_SIGNATURE
                                 + "&"
-                                + "client_id=" + CLIENT_ID.getValue())
-                .headers(
-                        "Content-Type",
-                        "application/x-www-form-urlencoded; charset=UTF-8")
+                                + "client_id="
+                                + CLIENT_ID.getValue())
+                .headers("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8")
                 .willRespondWith()
                 .status(200)
                 .body(
@@ -220,8 +294,7 @@ public class IpvTokenTest {
             providerName = "IPV-orch-token-provider",
             pactMethod = "success",
             pactVersion = PactSpecVersion.V3)
-    void getIPVResponse(MockServer mockServer)
-            throws Json.JsonException {
+    void getIPVResponse(MockServer mockServer) throws Json.JsonException {
         when(configService.getIPVAuthorisationClientId()).thenReturn(CLIENT_ID.getValue());
         when(configService.getIPVBackendURI()).thenReturn(URI.create(mockServer.getUrl()));
         when(configService.getIPVAudience()).thenReturn(IPV_URI.toString());
@@ -252,12 +325,23 @@ public class IpvTokenTest {
         claims.putAll(userIdentityAdditionalClaims);
 
         try (MockedStatic<NowHelper> mockedNowHelperClass = Mockito.mockStatic(NowHelper.class)) {
-            mockedNowHelperClass.when(NowHelper::now).thenReturn(Date.from(Instant.parse("2000-01-01T00:00:00.00Z")));
-            mockedNowHelperClass.when(() -> NowHelper.nowPlus(5L, ChronoUnit.MINUTES)).thenReturn(Date.from(Instant.parse("2099-01-01T00:00:00.00Z")));
-            try (var mockJwtId = mockConstruction(JWTID.class, (mock, context) -> {when(mock.getValue()).thenReturn("1");})) {
+            mockedNowHelperClass
+                    .when(NowHelper::now)
+                    .thenReturn(Date.from(Instant.parse("2000-01-01T00:00:00.00Z")));
+            mockedNowHelperClass
+                    .when(() -> NowHelper.nowPlus(5L, ChronoUnit.MINUTES))
+                    .thenReturn(Date.from(Instant.parse("2099-01-01T00:00:00.00Z")));
+            try (var mockJwtId =
+                    mockConstruction(
+                            JWTID.class,
+                            (mock, context) -> {
+                                when(mock.getValue()).thenReturn("1");
+                            })) {
                 handler.handleRequest(
-                        getApiGatewayProxyRequestEvent(new UserInfo(new JSONObject(claims))), context);
-            };
+                        getApiGatewayProxyRequestEvent(new UserInfo(new JSONObject(claims))),
+                        context);
+            }
+            ;
         }
 
         verifyAuditEvent(IPVAuditableEvent.IPV_AUTHORISATION_RESPONSE_RECEIVED);
@@ -355,10 +439,10 @@ public class IpvTokenTest {
 
     private SignResponse mockKmsReturn() {
         return SignResponse.builder()
-                        .signature(SdkBytes.fromByteArray(SIGNATURE_BYTES))
-                        .signingAlgorithm(SigningAlgorithmSpec.ECDSA_SHA_256)
-                        .keyId(KEY_ID)
-                        .build();
+                .signature(SdkBytes.fromByteArray(SIGNATURE_BYTES))
+                .signingAlgorithm(SigningAlgorithmSpec.ECDSA_SHA_256)
+                .keyId(KEY_ID)
+                .build();
     }
 
     public HTTPResponse getSuccessfulTokenHttpResponse() throws ParseException {
