bd05b31e23b9306b25c67b91d786b107fcda5702
Isaac Au <isaac.au@digital.cabinet-office.gov.uk>
2024-02-26 09:59:42 +0000

    ATO-384: update token test to use lambda to invoke api call
    - update the token test to call the api through the lambda instead of setting it within the test
    - hard code the JWT for the test
    - hard code the signature and signed JWT for the test
    - hard coded values came from running test and using the debugger

--- ipv-api/src/test/java/uk/gov/di/authentication/ipv/contract/IpvTokenTest.java
+++ ipv-api/src/test/java/uk/gov/di/authentication/ipv/contract/IpvTokenTest.java
@@ -3,6 +3,7 @@ package uk.gov.di.authentication.ipv.contract;
 import au.com.dius.pact.consumer.MockServer;
 import au.com.dius.pact.consumer.dsl.PactDslJsonBody;
 import au.com.dius.pact.consumer.dsl.PactDslWithProvider;
+import au.com.dius.pact.consumer.junit.MockServerConfig;
 import au.com.dius.pact.consumer.junit5.PactConsumerTest;
 import au.com.dius.pact.consumer.junit5.PactTestFor;
 import au.com.dius.pact.core.model.PactSpecVersion;
@@ -20,24 +21,28 @@ import com.nimbusds.openid.connect.sdk.OIDCScopeValue;
 import com.nimbusds.openid.connect.sdk.claims.UserInfo;
 import net.minidev.json.JSONArray;
 import net.minidev.json.JSONObject;
-import org.apache.hc.client5.http.fluent.Request;
-import org.apache.hc.core5.net.URIBuilder;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+import software.amazon.awssdk.core.SdkBytes;
+import software.amazon.awssdk.services.kms.model.SignRequest;
+import software.amazon.awssdk.services.kms.model.SignResponse;
+import software.amazon.awssdk.services.kms.model.SigningAlgorithmSpec;
 import uk.gov.di.authentication.ipv.domain.IPVAuditableEvent;
 import uk.gov.di.authentication.ipv.helpers.IPVCallbackHelper;
 import uk.gov.di.authentication.ipv.lambda.IPVCallbackHandler;
 import uk.gov.di.authentication.ipv.services.IPVAuthorisationService;
 import uk.gov.di.authentication.ipv.services.IPVTokenService;
 import uk.gov.di.orchestration.shared.entity.*;
-import uk.gov.di.orchestration.shared.exceptions.UnsuccessfulCredentialResponseException;
 import uk.gov.di.orchestration.shared.helpers.*;
 import uk.gov.di.orchestration.shared.serialization.Json;
 import uk.gov.di.orchestration.shared.services.*;
 
-import java.io.IOException;
 import java.net.URI;
-import java.net.URISyntaxException;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -53,12 +58,13 @@ import static uk.gov.di.orchestration.sharedtest.helper.IdentityTestData.*;
 import static uk.gov.di.orchestration.sharedtest.helper.IdentityTestData.PASSPORT_CLAIM;
 
 @PactConsumerTest
+@MockServerConfig(hostInterface = "localHost", port="1234")
 public class IpvTokenTest {
     private static final Subject SUBJECT = new Subject();
     private final Context context = mock(Context.class);
     private final ConfigurationService configService = mock(ConfigurationService.class);
+    private final KmsConnectionService kmsConnectionService = mock(KmsConnectionService.class);
     private final IPVAuthorisationService responseService = mock(IPVAuthorisationService.class);
-    private final IPVTokenService ipvTokenService = mock(IPVTokenService.class);
     private final SessionService sessionService = mock(SessionService.class);
     private final DynamoService dynamoService = mock(DynamoService.class);
     private final CookieHelper cookieHelper = mock(CookieHelper.class);
@@ -71,6 +77,7 @@ public class IpvTokenTest {
             mock(AccountInterventionService.class);
     private final IPVCallbackHelper ipvCallbackHelper = mock(IPVCallbackHelper.class);
     private final AuditService auditService = mock(AuditService.class);
+    private final IPVTokenService ipvTokenService = new IPVTokenService(configService, kmsConnectionService);
 
     private final Json objectMapper = SerializationService.getInstance();
 
@@ -88,7 +95,7 @@ public class IpvTokenTest {
     private static final URI REDIRECT_URI = URI.create("test-uri");
     private static final State RP_STATE = new State();
     private static final URI IPV_URI = URI.create("http://ipv/");
-    private static final ClientID CLIENT_ID = new ClientID();
+    private static final ClientID CLIENT_ID = new ClientID("authOrchestrator");
     private static final String CLIENT_NAME = "client-name";
 
     private static final Subject PUBLIC_SUBJECT =
@@ -135,7 +142,16 @@ public class IpvTokenTest {
     private final String TOKEN_TYPE_VALUE = "bearer";
     private final String EXPIRES_IN_VALUE = "3600";
     private final String URI_VALUE = "https://localhost";
+    private static final String KEY_ID = "14342354354353";
 
+    private static final String PRIVATE_JWT_KEY = "{\"kty\":\"EC\",\"d\":\"A2cfN3vYKgOQ_r1S6PhGHCLLiVEqUshFYExrxMwkq_A\",\"crv\":\"P-256\",\"kid\":\"14342354354353\",\"x\":\"BMyQQqr3NEFYgb9sEo4hRBje_HHEsy87PbNIBGL4Uiw\",\"y\":\"qoXdkYVomy6HWT6yNLqjHSmYoICs6ioUF565Btx0apw\",\"alg\":\"ES256\"}";
+
+    private static final String CLIENT_ASSERTION_HEADER = "eyJraWQiOiIxNDM0MjM1NDM1NDM1MyIsImFsZyI6IkVTMjU2In0";
+    private static final String CLIENT_ASSERTION_BODY =
+            "eyJzdWIiOiJhdXRoT3JjaGVzdHJhdG9yIiwiYXVkIjoiaHR0cDovL2lwdi8iLCJuYmYiOjk0NjY4NDgwMCwiaXNzIjoiYXV0aE9yY2hlc3RyYXRvciIsImV4cCI6NDA3MDkwODgwMCwiaWF0Ijo5NDY2ODQ4MDAsImp0aSI6IjEifQ";
+    private static final String CLIENT_ASSERTION_SIGNATURE =
+            "L3h9FCeYLIUCpjMGjnBm6Ca8GmKqICHSGY5Aq0svbMNTmLP04dzh5V8E6N2InzbXC9_4Q7u6mAo3yubbYsVSdA";
+    private static final byte[] SIGNATURE_BYTES = {(byte) 48, (byte)68, (byte)2, (byte)32, (byte)47, (byte)120, (byte)125, (byte)20, (byte)39, (byte)-104, (byte)44, (byte)-123, (byte)2, (byte)-90, (byte)51, (byte)6, (byte)-114, (byte)112, (byte)102, (byte)-24, (byte)38, (byte)-68, (byte)26, (byte)98, (byte)-86, (byte)32, (byte)33, (byte)-46, (byte)25, (byte)-114, (byte)64, (byte)-85, (byte)75, (byte)47, (byte)108, (byte)-61, (byte)2, (byte)32, (byte)83, (byte)-104, (byte)-77, (byte)-12, (byte)-31, (byte)-36, (byte)-31, (byte)-27, (byte)95, (byte)4, (byte)-24, (byte)-35, (byte)-120, (byte)-97, (byte)54, (byte)-41, (byte)11, (byte)-33, (byte)-8, (byte)67, (byte)-69, (byte)-70, (byte)-104, (byte)10, (byte)55, (byte)-54, (byte)-26, (byte)-37, (byte)98, (byte)-59, (byte)82, (byte)116};
     @BeforeEach
     void setUp() {
         handler =
@@ -155,7 +171,6 @@ public class IpvTokenTest {
                         ipvCallbackHelper);
         when(configService.getLoginURI()).thenReturn(LOGIN_URL);
         when(configService.getOidcApiBaseURL()).thenReturn(Optional.of(OIDC_BASE_URL));
-        when(configService.getIPVBackendURI()).thenReturn(IPV_URI);
         when(configService.isIdentityEnabled()).thenReturn(true);
         when(configService.getInternalSectorUri()).thenReturn("https://test.account.gov.uk");
         when(context.getAwsRequestId()).thenReturn(REQUEST_ID);
@@ -165,12 +180,30 @@ public class IpvTokenTest {
 
     @Pact(consumer = "IPV-orch-token-consumer")
     RequestResponsePact success(PactDslWithProvider builder) {
-        return builder.given("send token request to IPV")
+        return builder.given("dummyAuthCode is a valid authorization code")
+                .given("localHost is a valid resource URI")
+                .given("the JWT is signed with " + PRIVATE_JWT_KEY)
                 .uponReceiving("token request")
                 .path("/" + IPV_TOKEN_PATH)
                 .method("POST")
-                .matchQuery("client_id", "^[a-zA-Z0-9-_]*$", CLIENT_ID.getValue())
-                .matchQuery("resource", IPV_TOKEN_URI.toString())
+                .body(
+                        "client_assertion_type=urn%3Aietf%3Aparams%3Aoauth%3Aclient-assertion-type%3Ajwt-bearer&"
+                                + "code=dummyAuthCode&"
+                                + "grant_type=authorization_code&"
+                                + "resource="
+                                + "http://localhost:1234/token"
+                                + "&"
+                                + "client_assertion="
+                                + CLIENT_ASSERTION_HEADER
+                                + "."
+                                + CLIENT_ASSERTION_BODY
+                                + "."
+                                + CLIENT_ASSERTION_SIGNATURE
+                                + "&"
+                                + "client_id=" + CLIENT_ID.getValue())
+                .headers(
+                        "Content-Type",
+                        "application/x-www-form-urlencoded; charset=UTF-8")
                 .willRespondWith()
                 .status(200)
                 .body(
@@ -188,15 +221,11 @@ public class IpvTokenTest {
             pactMethod = "success",
             pactVersion = PactSpecVersion.V3)
     void getIPVResponse(MockServer mockServer)
-            throws IOException,
-                    Json.JsonException,
-                    UnsuccessfulCredentialResponseException,
-                    URISyntaxException,
-                    ParseException {
-        URIBuilder builder = new URIBuilder(mockServer.getUrl() + "/" + IPV_TOKEN_PATH);
-        builder.setParameter("client_id", CLIENT_ID.getValue())
-                .setParameter("resource", IPV_TOKEN_URI.toString());
-        Request.post(builder.build()).execute();
+            throws Json.JsonException {
+        when(configService.getIPVAuthorisationClientId()).thenReturn(CLIENT_ID.getValue());
+        when(configService.getIPVBackendURI()).thenReturn(URI.create(mockServer.getUrl()));
+        when(configService.getIPVAudience()).thenReturn(IPV_URI.toString());
+        when(configService.getIPVTokenSigningKeyAlias()).thenReturn(KEY_ID);
 
         usingValidSession();
         usingValidClientSession();
@@ -222,8 +251,14 @@ public class IpvTokenTest {
                                 CREDENTIAL_JWT_CLAIM));
         claims.putAll(userIdentityAdditionalClaims);
 
-        handler.handleRequest(
-                getApiGatewayProxyRequestEvent(new UserInfo(new JSONObject(claims))), context);
+        try (MockedStatic<NowHelper> mockedNowHelperClass = Mockito.mockStatic(NowHelper.class)) {
+            mockedNowHelperClass.when(NowHelper::now).thenReturn(Date.from(Instant.parse("2000-01-01T00:00:00.00Z")));
+            mockedNowHelperClass.when(() -> NowHelper.nowPlus(5L, ChronoUnit.MINUTES)).thenReturn(Date.from(Instant.parse("2099-01-01T00:00:00.00Z")));
+            try (var mockJwtId = mockConstruction(JWTID.class, (mock, context) -> {when(mock.getValue()).thenReturn("1");})) {
+                handler.handleRequest(
+                        getApiGatewayProxyRequestEvent(new UserInfo(new JSONObject(claims))), context);
+            };
+        }
 
         verifyAuditEvent(IPVAuditableEvent.IPV_AUTHORISATION_RESPONSE_RECEIVED);
         verifyAuditEvent(IPVAuditableEvent.IPV_SUCCESSFUL_TOKEN_RESPONSE_RECEIVED);
@@ -288,11 +323,9 @@ public class IpvTokenTest {
     }
 
     private APIGatewayProxyRequestEvent getApiGatewayProxyRequestEvent(
-            UserInfo userIdentityUserInfo)
-            throws UnsuccessfulCredentialResponseException, ParseException {
-        var tokenRequest = mock(TokenRequest.class);
+            UserInfo userIdentityUserInfo) {
         Map<String, String> responseHeaders = new HashMap<>();
-        responseHeaders.put("code", AUTH_CODE.getValue());
+        responseHeaders.put("code", "dummyAuthCode");
         responseHeaders.put("state", STATE.getValue());
         when(dynamoClientService.getClient(CLIENT_ID.getValue()))
                 .thenReturn(Optional.of(clientRegistry));
@@ -301,10 +334,7 @@ public class IpvTokenTest {
         when(dynamoService.getUserProfileFromEmail(TEST_EMAIL_ADDRESS))
                 .thenReturn(Optional.of(userProfile));
         when(dynamoService.getOrGenerateSalt(userProfile)).thenReturn(salt);
-        when(ipvTokenService.constructTokenRequest(AUTH_CODE.getValue())).thenReturn(tokenRequest);
-        when(ipvTokenService.sendTokenRequest(tokenRequest))
-                .thenReturn(getSuccessfulTokenHttpResponse());
-        when(ipvTokenService.sendIpvUserIdentityRequest(any())).thenReturn(userIdentityUserInfo);
+        when(kmsConnectionService.sign(any(SignRequest.class))).thenReturn(mockKmsReturn());
 
         var event = new APIGatewayProxyRequestEvent();
         event.setQueryStringParameters(responseHeaders);
@@ -323,7 +353,15 @@ public class IpvTokenTest {
                 PERSISTENT_SESSION_ID);
     }
 
-    public TokenResponse getSuccessfulTokenHttpResponse() throws ParseException {
+    private SignResponse mockKmsReturn() {
+        return SignResponse.builder()
+                        .signature(SdkBytes.fromByteArray(SIGNATURE_BYTES))
+                        .signingAlgorithm(SigningAlgorithmSpec.ECDSA_SHA_256)
+                        .keyId(KEY_ID)
+                        .build();
+    }
+
+    public HTTPResponse getSuccessfulTokenHttpResponse() throws ParseException {
         var tokenResponseContent =
                 "{"
                         + "  \""
@@ -351,6 +389,6 @@ public class IpvTokenTest {
         tokenHTTPResponse.setEntityContentType(APPLICATION_JSON);
         tokenHTTPResponse.setContent(tokenResponseContent);
 
-        return TokenResponse.parse(tokenHTTPResponse);
+        return tokenHTTPResponse;
     }
 }
