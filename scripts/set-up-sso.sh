b95fa3a2b6b8fde4db9f4a991b3132c49acc8b5d
Tom Whitwell <tom.whitwell@digital.cabinet-office.gov.uk>
2024-03-07 11:27:05 +0000

    BAU: set-up-sso.sh: enforce python version >= 3.10

--- scripts/set-up-sso.sh
+++ scripts/set-up-sso.sh
@@ -3,6 +3,15 @@ set -euo pipefail
 
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
+# Ensure python is at least 3.10
+min_python_version=3.10
+python_version="$(python3 --version | cut -d' ' -f2)"
+if [ "$(printf '%s\n' "${min_python_version}" "${python_version}" | sort -V | head -n1)" != "${min_python_version}" ]; then
+    # shellcheck disable=SC2016
+    printf 'Please install python %s or later (found: %s). You could probably use `brew install python@3.12`' "${min_python_version}" "${python_version}"
+    exit 1
+fi
+
 # test if a python virtualenv already exists
 if [ -d "${DIR}/.venv" ]; then
     echo "! Using existing virtualenv"
