dca4132d25302386a0131380e016e1d4c4e343ca
Bill Dunn <bill.dunn@digital.cabinet-office.gov.uk>
2024-03-07 10:08:48 +0000

    ATO-96: Create new well-known endpoint for storage token signing public key
    - Move storage token kms signing policy to kms-policies so it is accessible to storage-token-jwk and authentication-callback

---
+++ oidc-api/src/main/java/uk/gov/di/authentication/oidc/lambda/StorageTokenJwkHandler.java
@@ -0,0 +1,74 @@
+package uk.gov.di.authentication.oidc.lambda;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.JWKSet;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import uk.gov.di.orchestration.shared.services.ConfigurationService;
+import uk.gov.di.orchestration.shared.services.JwksService;
+import uk.gov.di.orchestration.shared.services.KmsConnectionService;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import static uk.gov.di.orchestration.shared.helpers.ApiGatewayResponseHelper.generateApiGatewayProxyResponse;
+import static uk.gov.di.orchestration.shared.helpers.InstrumentationHelper.segmentedFunctionCall;
+
+public class StorageTokenJwkHandler
+        implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+
+    private final JwksService jwksService;
+    private static final Logger LOG = LogManager.getLogger(JwksHandler.class);
+
+    public StorageTokenJwkHandler(
+            ConfigurationService configurationService, JwksService jwksService) {
+        this.jwksService = jwksService;
+    }
+
+    public StorageTokenJwkHandler(ConfigurationService configurationService) {
+        this.jwksService =
+                new JwksService(
+                        configurationService, new KmsConnectionService(configurationService));
+    }
+
+    public StorageTokenJwkHandler() {
+        this(ConfigurationService.getInstance());
+    }
+
+    @Override
+    public APIGatewayProxyResponseEvent handleRequest(
+            APIGatewayProxyRequestEvent input, Context context) {
+        return segmentedFunctionCall(
+                "oidc-api::" + getClass().getSimpleName(),
+                () -> storageTokenJwkRequestHandler(input, context));
+    }
+
+    public APIGatewayProxyResponseEvent storageTokenJwkRequestHandler(
+            APIGatewayProxyRequestEvent input, Context context) {
+        try {
+            LOG.info("StorageTokenJwk request received");
+
+            List<JWK> signingKeys = new ArrayList<>();
+
+            signingKeys.add(jwksService.getPublicStorageTokenJwkWithOpaqueId());
+
+            JWKSet jwkSet = new JWKSet(signingKeys);
+
+            LOG.info("Generating StorageTokenJwk successful response");
+
+            return generateApiGatewayProxyResponse(
+                    200,
+                    segmentedFunctionCall("serialiseJWKSet", () -> jwkSet.toString(true)),
+                    Map.of("Cache-Control", "max-age=86400"),
+                    null);
+        } catch (Exception e) {
+            LOG.error("Error in StorageTokenJwk lambda", e);
+            return generateApiGatewayProxyResponse(500, "Error providing StorageTokenJwk data");
+        }
+    }
+}
