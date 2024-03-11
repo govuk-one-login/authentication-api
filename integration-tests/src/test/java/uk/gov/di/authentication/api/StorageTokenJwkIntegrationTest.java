513937c11cb53cdbefe574a7ff32c8b792468acc
Bill Dunn <bill.dunn@digital.cabinet-office.gov.uk>
2024-03-11 17:08:18 +0000

    ATO-96: Add integration test for new StorageTokenJwkHandler lambda

---
+++ integration-tests/src/test/java/uk/gov/di/authentication/api/StorageTokenJwkIntegrationTest.java
@@ -0,0 +1,37 @@
+package uk.gov.di.authentication.api;
+
+import com.nimbusds.jose.jwk.JWKSet;
+import org.junit.jupiter.api.Test;
+import uk.gov.di.authentication.oidc.lambda.StorageTokenJwkHandler;
+import uk.gov.di.orchestration.sharedtest.basetest.ApiGatewayHandlerIntegrationTest;
+
+import java.text.ParseException;
+import java.util.Map;
+import java.util.Optional;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasSize;
+import static uk.gov.di.orchestration.sharedtest.helper.AuditAssertionsHelper.assertNoTxmaAuditEventsReceived;
+import static uk.gov.di.orchestration.sharedtest.matchers.APIGatewayProxyResponseEventMatcher.hasStatus;
+
+public class StorageTokenJwkIntegrationTest extends ApiGatewayHandlerIntegrationTest {
+
+    @Test
+    void shouldReturn200AndClientInfoResponseForValidClient() throws ParseException {
+        var configurationService =
+                new IntegrationTestConfigurationService(
+                        externalTokenSigner,
+                        storageTokenSigner,
+                        ipvPrivateKeyJwtSigner,
+                        spotQueue,
+                        docAppPrivateKeyJwtSigner,
+                        configurationParameters);
+        handler = new StorageTokenJwkHandler(configurationService);
+        var response = makeRequest(Optional.empty(), Map.of(), Map.of());
+
+        assertThat(response, hasStatus(200));
+        assertThat(JWKSet.parse(response.getBody()).getKeys(), hasSize(1));
+
+        assertNoTxmaAuditEventsReceived(txmaAuditQueue);
+    }
+}
