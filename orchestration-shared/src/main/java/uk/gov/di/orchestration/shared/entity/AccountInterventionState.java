090279d95386c35a28a6d96f8fdbdb38d09755ad
Bill Dunn <bill.dunn@digital.cabinet-office.gov.uk>
2024-03-12 15:45:09 +0000

    ATO-347: Change AccountInterventionState from `record` to `class` so that status does not need to be recalculated every time `getStatus` is called.

--- orchestration-shared/src/main/java/uk/gov/di/orchestration/shared/entity/AccountInterventionState.java
+++ orchestration-shared/src/main/java/uk/gov/di/orchestration/shared/entity/AccountInterventionState.java
@@ -3,13 +3,55 @@ package uk.gov.di.orchestration.shared.entity;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
-public record AccountInterventionState(
-        @Expose boolean blocked,
-        @Expose boolean suspended,
-        @Expose @SerializedName("reproveIdentity") boolean reproveIdentity,
-        @Expose @SerializedName("resetPassword") boolean resetPassword) {
+public class AccountInterventionState {
+
+    @Expose private final boolean blocked;
+
+    @Expose private final boolean suspended;
+
+    @Expose
+    @SerializedName("reproveIdentity")
+    private final boolean reproveIdentity;
+
+    @Expose
+    @SerializedName("resetPassword")
+    private final boolean resetPassword;
+
+    private AccountInterventionStatus status;
+
+    public AccountInterventionState(
+            boolean blocked, boolean suspended, boolean reproveIdentity, boolean resetPassword) {
+        this.blocked = blocked;
+        this.suspended = suspended;
+        this.reproveIdentity = reproveIdentity;
+        this.resetPassword = resetPassword;
+        this.status = calculateStatus();
+    }
+
+    public boolean blocked() {
+        return blocked;
+    }
+
+    public boolean suspended() {
+        return suspended;
+    }
+
+    public boolean resetPassword() {
+        return resetPassword;
+    }
+
+    public boolean reproveIdentity() {
+        return reproveIdentity;
+    }
 
     public AccountInterventionStatus getStatus() {
+        if (status == null) {
+            this.status = calculateStatus();
+        }
+        return status;
+    }
+
+    private AccountInterventionStatus calculateStatus() {
         if (blocked) return AccountInterventionStatus.BLOCKED;
         if (suspended) {
             if (!reproveIdentity && !resetPassword) {
