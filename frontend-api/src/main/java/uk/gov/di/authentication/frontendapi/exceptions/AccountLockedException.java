462bbaa2ff5924c8b337c7bf47049f692c78a9fb
ayoshebby <ayo.sebiotimo@digital.cabinet-office.gov.uk>
2024-03-21 13:57:55 +0000

    AUT-2613: Check if account has been locked for entering max password attempts in re-auth journey - During re-auth, when a user has been blocked for entering incorrect password too many times, the user is still able to proceed to complete the re-auth journey. - The Reauth handler will now validate to check whether the user has been blocked from entering too many incorrect password

--- frontend-api/src/main/java/uk/gov/di/authentication/frontendapi/exceptions/AccountLockedException.java
+++ frontend-api/src/main/java/uk/gov/di/authentication/frontendapi/exceptions/AccountLockedException.java
@@ -1,7 +1,16 @@
 package uk.gov.di.authentication.frontendapi.exceptions;
 
+import uk.gov.di.authentication.shared.entity.ErrorResponse;
+
 public class AccountLockedException extends RuntimeException {
-    public AccountLockedException(String message) {
+    private final ErrorResponse errorResponse;
+
+    public AccountLockedException(String message, ErrorResponse errorResponse) {
         super(message);
+        this.errorResponse = errorResponse;
+    }
+
+    public ErrorResponse getErrorResponse() {
+        return this.errorResponse;
     }
 }
