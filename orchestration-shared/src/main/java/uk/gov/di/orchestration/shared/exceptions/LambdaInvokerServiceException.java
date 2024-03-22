698333f603f7c08d88a3e34f360476dc7008541a
Alex Wilson <alex.wilson@digital.cabinet-office.gov.uk>
2024-03-22 14:56:03 +0000

    BAU: Remove classes copied from authentication-shared that are no longer needed

--- orchestration-shared/src/main/java/uk/gov/di/orchestration/shared/exceptions/LambdaInvokerServiceException.java
+++
@@ -1,7 +0,0 @@
-package uk.gov.di.orchestration.shared.exceptions;
-
-public class LambdaInvokerServiceException extends RuntimeException {
-    public LambdaInvokerServiceException(String message) {
-        super(message);
-    }
-}
