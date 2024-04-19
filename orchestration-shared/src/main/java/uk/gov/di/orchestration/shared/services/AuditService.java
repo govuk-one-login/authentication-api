68d85529e4708c6157d31d1b5cdc6ccc5fd27c64
Alex Wilson <alex.wilson@digital.cabinet-office.gov.uk>
2024-04-19 14:18:55 +0100

    RFF-2: Extract `submitAuditEvent` which wraps user data in a `TxmaAuditUser`

--- orchestration-shared/src/main/java/uk/gov/di/orchestration/shared/services/AuditService.java
+++ orchestration-shared/src/main/java/uk/gov/di/orchestration/shared/services/AuditService.java
@@ -76,6 +76,14 @@ public class AuditService {
                         .withPersistentSessionId(persistentSessionId)
                         .withGovukSigninJourneyId(clientSessionId);
 
+        submitAuditEvent(event, clientId, user, metadataPairs);
+    }
+
+    public void submitAuditEvent(
+            AuditableEvent event,
+            String clientId,
+            TxmaAuditUser user,
+            MetadataPair... metadataPairs) {
         var txmaAuditEvent =
                 auditEventWithTime(event, () -> Date.from(clock.instant()))
                         .withClientId(clientId)
@@ -89,7 +97,7 @@ public class AuditService {
         Arrays.stream(metadataPairs)
                 .forEach(pair -> txmaAuditEvent.addExtension(pair.getKey(), pair.getValue()));
 
-        Optional.ofNullable(phoneNumber)
+        Optional.ofNullable(user.getPhone())
                 .filter(not(String::isBlank))
                 .flatMap(PhoneNumberHelper::maybeGetCountry)
                 .ifPresent(
