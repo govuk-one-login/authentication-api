a917d1d3f4f61e7bc33fa3aea1dec294d9ec6ae4
cargil <carly.gilson@digital.cabinet-office.gov.uk>
2024-04-22 17:41:48 +0100

    ATO-554: Remove read permission from spot queue from resource policy for ipv callback lambda in staging - this is due to the fact we have turned identity policies for the ipv callback lambda role

--- ci/terraform/oidc/variables.tf
+++ ci/terraform/oidc/variables.tf
@@ -593,6 +593,12 @@ variable "oidc_origin_domain_enabled" {
   description = "Feature flag to control the creation of DNS records for the origin.oidc domain"
 }
 
+variable "remove_ipv_callback_from_spot_queue_resource_policy" {
+  type        = bool
+  default     = false
+  description = "Feature flag to control the removal of read permissions to the IPV callback lambda through a resource policy"
+}
+
 locals {
   default_performance_parameters = {
     memory          = var.endpoint_memory_size
