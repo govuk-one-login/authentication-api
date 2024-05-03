49b7d38b1273ad92fda7511fe4a90dec431d7336
BeckaL <becka.lelew@digital.cabinet-office.gov.uk>
2024-05-03 14:23:15 +0100

    RFF-3: Introduce new method to return headers
    This is a step in a refactoring to simplify how we get headers. This introduces a new method which returns an optional, and uses this in other places in the request header helper. Eventually we will replace all calls to other methods in the request header helper with this new method.

--- shared/src/main/java/uk/gov/di/authentication/shared/helpers/RequestHeaderHelper.java
+++ shared/src/main/java/uk/gov/di/authentication/shared/helpers/RequestHeaderHelper.java
@@ -4,6 +4,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.util.Map;
+import java.util.Optional;
 
 public final class RequestHeaderHelper {
 
@@ -13,25 +14,29 @@ public final class RequestHeaderHelper {
 
     public static boolean headersContainValidHeader(
             Map<String, String> headers, String headerName, boolean matchLowerCase) {
-        return getHeaderValueFromHeaders(headers, headerName, matchLowerCase) != null;
+        return getOptionalHeaderValueFromHeaders(headers, headerName, matchLowerCase).isPresent();
     }
 
     public static String getHeaderValueFromHeaders(
             Map<String, String> headers, String headerName, boolean matchLowerCase) {
+        return getOptionalHeaderValueFromHeaders(headers, headerName, matchLowerCase).orElse(null);
+    }
+
+    public static Optional<String> getOptionalHeaderValueFromHeaders(
+            Map<String, String> headers, String headerName, boolean matchLowerCase) {
         if (headers == null || headers.isEmpty()) {
-            return null;
+            return Optional.empty();
         } else if (headers.containsKey(headerName)) {
-            return headers.get(headerName);
+            return Optional.of(headers.get(headerName));
         } else if (matchLowerCase && headers.containsKey(headerName.toLowerCase())) {
-            return headers.get(headerName.toLowerCase());
+            return Optional.of(headers.get(headerName.toLowerCase()));
         } else {
-            return null;
+            return Optional.empty();
         }
     }
 
     public static String getHeaderValueOrElse(
             Map<String, String> headers, String headerName, String orElse) {
-        String headerValue = getHeaderValueFromHeaders(headers, headerName, false);
-        return headerValue != null ? headerValue : orElse;
+        return getOptionalHeaderValueFromHeaders(headers, headerName, false).orElse(orElse);
     }
 }
