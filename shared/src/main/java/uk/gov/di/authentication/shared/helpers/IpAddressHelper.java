c52ff4b9d689301be55dcf7dadd306b6ce7a7084
BeckaL <becka.lelew@digital.cabinet-office.gov.uk>
2024-05-07 17:37:34 +0100

    BAU: Extract IP address header name

--- shared/src/main/java/uk/gov/di/authentication/shared/helpers/IpAddressHelper.java
+++ shared/src/main/java/uk/gov/di/authentication/shared/helpers/IpAddressHelper.java
@@ -12,13 +12,15 @@ import static uk.gov.di.authentication.shared.helpers.RequestHeaderHelper.getOpt
 
 public class IpAddressHelper {
 
+    private static String IP_HEADER_NAME = "X-Forwarded-For";
+
     public static String extractIpAddress(APIGatewayProxyRequestEvent input) {
         var headers =
                 Optional.ofNullable(input)
                         .map(APIGatewayProxyRequestEvent::getHeaders)
                         .orElse(emptyMap());
 
-        return getOptionalHeaderValueFromHeaders(headers, "X-Forwarded-For", true)
+        return getOptionalHeaderValueFromHeaders(headers, IP_HEADER_NAME, true)
                 .map(forwardedValue -> forwardedValue.split(",")[0].trim())
                 .orElse(
                         Optional.ofNullable(input)
