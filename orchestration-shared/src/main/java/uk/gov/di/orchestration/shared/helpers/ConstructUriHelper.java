451a1da168a3197cf45e7989ae1753fa43e09639
BenjaminWCO <benjamin.woolley@digital.cabinet-office.gov.uk>
2024-05-08 16:52:36 +0100

    ConstructUriHelper Improvement

--- orchestration-shared/src/main/java/uk/gov/di/orchestration/shared/helpers/ConstructUriHelper.java
+++ orchestration-shared/src/main/java/uk/gov/di/orchestration/shared/helpers/ConstructUriHelper.java
@@ -1,40 +1,56 @@
 package uk.gov.di.orchestration.shared.helpers;
 
-import org.apache.http.client.utils.URIBuilder;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.hc.core5.net.URIBuilder;
 
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.Map;
-import java.util.Objects;
+import java.util.Optional;
 
 public class ConstructUriHelper {
 
+    public static URI buildURI(URI baseUrl, String path, Map<String, String> queryParams) {
+        return buildURI(baseUrl.toString(), path, queryParams);
+    }
+
     public static URI buildURI(String baseUrl, String path, Map<String, String> queryParams) {
-        if (!baseUrl.endsWith("/")) {
-            baseUrl = baseUrl + "/";
-        }
-        var uri =
-                Objects.isNull(path)
-                        ? URI.create(baseUrl)
-                        : URI.create(baseUrl + path.replaceAll("^/+", ""));
+        return buildURI(baseUrl, Optional.ofNullable(path), Optional.ofNullable(queryParams));
+    }
+
+    public static URI buildURI(URI baseUri, Map<String, String> queryParams) {
+        return buildURI(baseUri.toString(), queryParams);
+    }
+
+    public static URI buildURI(String baseUri, Map<String, String> queryParams) {
+        return buildURI(baseUri, Optional.empty(), Optional.ofNullable(queryParams));
+    }
+
+    public static URI buildURI(URI baseUri, String path) {
+        return buildURI(baseUri.toString(), path);
+    }
+
+    public static URI buildURI(String baseUri, String path) {
+        return buildURI(baseUri, Optional.ofNullable(path), Optional.empty());
+    }
+
+    private static URI buildURI(
+            String baseUri, Optional<String> path, Optional<Map<String, String>> queryParams) {
         try {
-            var uriBuilder = new URIBuilder(uri);
-            if (Objects.nonNull(queryParams)) {
-                for (Map.Entry<String, String> entry : queryParams.entrySet()) {
-                    uriBuilder.addParameter(entry.getKey(), entry.getValue());
-                }
-            }
+            var uriBuilder = new URIBuilder(StringUtils.removeEnd(baseUri, "/"));
+            path.ifPresent(
+                    p ->
+                            uriBuilder.appendPath(
+                                    StringUtils.removeEnd(StringUtils.removeStart(p, "/"), "/")));
+            queryParams.ifPresent(
+                    qp -> {
+                        for (var entry : qp.entrySet()) {
+                            uriBuilder.addParameter(entry.getKey(), entry.getValue());
+                        }
+                    });
             return uriBuilder.build();
         } catch (URISyntaxException e) {
             throw new RuntimeException("Unable to build URI", e);
         }
-    }
-
-    public static URI buildURI(String baseUrl, String path) {
-        return buildURI(baseUrl, path, null);
-    }
-
-    public static URI buildURI(String baseUrl) {
-        return buildURI(baseUrl, null, null);
     }
 }
