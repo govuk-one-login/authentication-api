2dfe29326ce0f3bb1597823d433f5407784cfda7
Mark Thomas <mark.thomas@digital.cabinet-office.gov.uk>
2024-05-16 13:28:22 +0100

    AUT-2663: new audit helper class to help extract CF encoded txma header into a restrictedsection record

---
+++ shared/src/main/java/uk/gov/di/authentication/shared/helpers/AuditHelper.java
@@ -0,0 +1,27 @@
+package uk.gov.di.authentication.shared.helpers;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import uk.gov.di.authentication.shared.services.AuditService;
+
+import java.util.Map;
+import java.util.Optional;
+
+public class AuditHelper {
+
+    private static final Logger LOG = LogManager.getLogger(AuditHelper.class);
+    public static final String TXMA_ENCODED_HEADER_NAME = "txma-audit-encoded";
+
+    public static AuditService.RestrictedSection buildRestrictedSection(
+            Map<String, String> headers) {
+        String txmaEncodedValue =
+                RequestHeaderHelper.getHeaderValueFromHeaders(
+                        headers, TXMA_ENCODED_HEADER_NAME, false);
+        if (txmaEncodedValue != null && !txmaEncodedValue.isEmpty()) {
+            return new AuditService.RestrictedSection(Optional.of(txmaEncodedValue));
+        } else {
+            LOG.warn("Audit header field value cannot be empty");
+            return AuditService.RestrictedSection.empty;
+        }
+    }
+}
