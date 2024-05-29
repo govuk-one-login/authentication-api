d5768574a4e183d0471d5bcc54a79b1beeaeda02
Matt Votsikas McLean <matt.votsikas-mclean@digital.cabinet-office.gov.uk>
2024-05-29 15:27:41 +0100

    BAU: Add missing `mfaRequired` test for when VTR is medium
    Part of a larger refactor of how we are storing MFA configuration, this adds a missing test on the existing implementation MFA

--- shared/src/test/java/uk/gov/di/authentication/shared/conditions/MfaHelperTest.java
+++ shared/src/test/java/uk/gov/di/authentication/shared/conditions/MfaHelperTest.java
@@ -2,6 +2,7 @@ package uk.gov.di.authentication.shared.conditions;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -14,5 +15,16 @@ class MfaHelperTest {
     void shouldThrowRuntimeExceptionWhenInvalidAuthRequestParamsPassed() {
         Map<String, List<String>> authRequestParams = new HashMap<String, List<String>>();
         assertThrows(RuntimeException.class, () -> MfaHelper.mfaRequired(authRequestParams));
+    }
+
+    @Test
+    void shouldReturnTrueWhenVtrParameterIsMediumLevel() {
+        Map<String, List<String>> authRequestParams = new HashMap<String, List<String>>();
+        authRequestParams.put("vtr", List.of("[\"Cl.Cm\"]"));
+        authRequestParams.put("client_id", Collections.singletonList("CLIENT_ID"));
+        authRequestParams.put("response_type", Collections.singletonList("code"));
+        authRequestParams.put("scope", Collections.singletonList("openid"));
+        authRequestParams.put("redirect_uri", Collections.singletonList("REDIRECT_URI"));
+        assertTrue(MfaHelper.mfaRequired(authRequestParams));
     }
 }
