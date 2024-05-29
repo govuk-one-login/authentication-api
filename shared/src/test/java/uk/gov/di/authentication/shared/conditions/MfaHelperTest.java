394c9dfe1766382ff38e5cd18bd8e0f6a15b8ca8
dbes-gds <daniel.besbrode@digital.cabinet-office.gov.uk>
2024-05-29 15:43:46 +0100

    BAU: Test case for low level of confidence
    Extract buildAuthRequest method for reuse.

--- shared/src/test/java/uk/gov/di/authentication/shared/conditions/MfaHelperTest.java
+++ shared/src/test/java/uk/gov/di/authentication/shared/conditions/MfaHelperTest.java
@@ -1,8 +1,13 @@
 package uk.gov.di.authentication.shared.conditions;
 
+import com.nimbusds.oauth2.sdk.ResponseType;
+import com.nimbusds.oauth2.sdk.Scope;
+import com.nimbusds.oauth2.sdk.id.ClientID;
+import com.nimbusds.openid.connect.sdk.AuthenticationRequest;
+import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.Test;
 
-import java.util.Collections;
+import java.net.URI;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -19,12 +24,25 @@ class MfaHelperTest {
 
     @Test
     void shouldReturnTrueWhenVtrParameterIsMediumLevel() {
-        Map<String, List<String>> authRequestParams = new HashMap<String, List<String>>();
-        authRequestParams.put("vtr", List.of("[\"Cl.Cm\"]"));
-        authRequestParams.put("client_id", Collections.singletonList("CLIENT_ID"));
-        authRequestParams.put("response_type", Collections.singletonList("code"));
-        authRequestParams.put("scope", Collections.singletonList("openid"));
-        authRequestParams.put("redirect_uri", Collections.singletonList("REDIRECT_URI"));
+        Map<String, List<String>> authRequestParams = buildAuthRequest("[\"Cl.Cm\"]");
         assertTrue(MfaHelper.mfaRequired(authRequestParams));
+    }
+
+    @Test
+    void shouldReturnFalseWhenVtrParameterIsLowLevel() {
+        Map<String, List<String>> authRequestParams = buildAuthRequest("[\"Cl\"]");
+        assertFalse(MfaHelper.mfaRequired(authRequestParams));
+    }
+
+    @NotNull
+    private static Map<String, List<String>> buildAuthRequest(String vtr) {
+        return new AuthenticationRequest.Builder(
+                        ResponseType.CODE,
+                        new Scope("openid"),
+                        new ClientID("CLIENT_ID"),
+                        URI.create("REDIRECT_URI"))
+                .customParameter("vtr", vtr)
+                .build()
+                .toParameters();
     }
 }
