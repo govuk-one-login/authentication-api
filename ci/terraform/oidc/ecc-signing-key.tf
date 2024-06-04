7b72ebb93cc5594469a4acae6ef3dccf1c7339ef
gn-GDS <george.nemeshanyi@digital.cabinet-office.gov.uk>
2024-06-04 17:43:03 +0100

    ATO-658: Create kms signing key

--- ci/terraform/oidc/ecc-signing-key.tf
+++ ci/terraform/oidc/ecc-signing-key.tf
@@ -63,3 +63,36 @@ data "aws_iam_policy_document" "storage_token_signing_key_access_policy" {
     }
   }
 }
+
+resource "aws_kms_key" "mfa_reset_signing_key_ecc" {
+  description              = "KMS signing key (ECC) for MFA reset token"
+  deletion_window_in_days  = 30
+  key_usage                = "SIGN_VERIFY"
+  customer_master_key_spec = "ECC_NIST_P256"
+
+  policy = data.aws_iam_policy_document.mfa_reset_signing_key_access_policy.json
+
+  tags = local.default_tags
+}
+
+resource "aws_kms_alias" "mfa_reset_signing_key_alias" {
+  name          = "alias/${var.environment}-mfa-reset-signing-key-ecc-alias"
+  target_key_id = aws_kms_key.mfa_reset_signing_key_ecc.key_id
+}
+
+data "aws_iam_policy_document" "mfa_reset_signing_key_access_policy" {
+  statement {
+    sid    = "DefaultAccessPolicy"
+    effect = "Allow"
+
+    actions = [
+      "kms:*"
+    ]
+    resources = ["*"]
+
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"]
+    }
+  }
+}
