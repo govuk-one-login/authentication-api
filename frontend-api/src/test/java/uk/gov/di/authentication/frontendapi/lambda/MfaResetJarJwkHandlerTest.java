51b44e8fecc0ea6ecdb6ab8ce8f9caf45da49539
Ryan Andrews <ryan.andrews@digital.cabinet-office.gov.uk>
2024-06-24 10:33:28 +0100

    ATO-737: Adds new lambda handler for reauth JWK

---
+++ frontend-api/src/test/java/uk/gov/di/authentication/frontendapi/lambda/MfaResetJarJwkHandlerTest.java
@@ -0,0 +1,62 @@
+package uk.gov.di.authentication.frontendapi.lambda;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.nimbusds.jose.JOSEException;
+import com.nimbusds.jose.jwk.Curve;
+import com.nimbusds.jose.jwk.ECKey;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.gen.ECKeyGenerator;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import uk.gov.di.authentication.shared.services.JwksService;
+
+import java.util.List;
+import java.util.UUID;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static uk.gov.di.authentication.sharedtest.matchers.APIGatewayProxyResponseEventMatcher.hasBody;
+import static uk.gov.di.authentication.sharedtest.matchers.APIGatewayProxyResponseEventMatcher.hasHeader;
+import static uk.gov.di.authentication.sharedtest.matchers.APIGatewayProxyResponseEventMatcher.hasStatus;
+
+class MfaResetJarJwkHandlerTest {
+
+    private final Context context = mock(Context.class);
+    private final JwksService jwksService = mock(JwksService.class);
+    private MfaResetJarJwkHandler handler;
+    private final ECKey jarPublicSigningKey =
+            new ECKeyGenerator(Curve.P_256).keyID(UUID.randomUUID().toString()).generate();
+
+    MfaResetJarJwkHandlerTest() throws JOSEException {}
+
+    @BeforeEach
+    public void setUp() {
+        handler = new MfaResetJarJwkHandler(jwksService);
+        when(jwksService.getPublicMfaResetJarJwkWithOpaqueId()).thenReturn(jarPublicSigningKey);
+    }
+
+    @Test
+    void shouldReturnMfaResetStorageTokenJwk() {
+        var event = new APIGatewayProxyRequestEvent();
+        var result = handler.handleRequest(event, context);
+
+        var expectedJWKSet = new JWKSet(List.of(jarPublicSigningKey));
+
+        assertThat(result, hasStatus(200));
+        assertThat(result, hasBody(expectedJWKSet.toString(true)));
+        assertThat(result, hasHeader("Cache-Control", "max-age=86400"));
+    }
+
+    @Test
+    void shouldReturn500WhenSigningKeyIsNotPresent() {
+        when(jwksService.getPublicMfaResetJarJwkWithOpaqueId()).thenReturn(null);
+
+        var event = new APIGatewayProxyRequestEvent();
+        var result = handler.handleRequest(event, context);
+
+        assertThat(result, hasStatus(500));
+        assertThat(result, hasBody("Error providing MFA Reset JAR JWK data"));
+    }
+}
