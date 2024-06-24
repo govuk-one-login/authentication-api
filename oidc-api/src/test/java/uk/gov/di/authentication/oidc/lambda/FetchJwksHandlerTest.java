8a073c3b3a9f845cf299048cd5078efb35527ab1
Bill Dunn <bill.dunn@digital.cabinet-office.gov.uk>
2024-06-24 12:18:43 +0100

    ATO-689: Add unit tests for FetchJwksHandler

---
+++ oidc-api/src/test/java/uk/gov/di/authentication/oidc/lambda/FetchJwksHandlerTest.java
@@ -0,0 +1,68 @@
+package uk.gov.di.authentication.oidc.lambda;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.nimbusds.jose.KeySourceException;
+import org.junit.jupiter.api.Test;
+import uk.gov.di.authentication.oidc.entity.FetchJwksResponse;
+import uk.gov.di.orchestration.shared.services.JwksService;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Map;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class FetchJwksHandlerTest {
+
+    private static final Context CONTEXT = mock(Context.class);
+    JwksService jwksService = mock(JwksService.class);
+    private final FetchJwksHandler handler = new FetchJwksHandler(jwksService);
+    private final String keyId = "some-key-id";
+    private final String url =
+            "https://oidc.test.account.gov.uk/.well-known/storage-token-jwk.json";
+
+    @Test
+    void returnsErrorWhenServiceThrowsKeySourceException()
+            throws MalformedURLException, KeySourceException {
+        // given
+        Map<String, String> event = Map.of("url", url, "keyId", keyId);
+        when(jwksService.retrieveJwkFromURLWithKeyId(new URL(url), keyId))
+                .thenThrow(new KeySourceException());
+
+        // when
+        FetchJwksResponse response = handler.handleRequest(event, CONTEXT);
+
+        // then
+        assertThat(response.getError().getCode(), equalTo("server_error"));
+        assertThat(response.getJwk(), equalTo(null));
+    }
+
+    @Test
+    void returnsErrorWhenUrlIsMissing() {
+        // given
+        Map<String, String> event = Map.of("keyId", keyId);
+
+        // when
+        FetchJwksResponse response = handler.handleRequest(event, CONTEXT);
+
+        // then
+        assertThat(response.getError().getCode(), equalTo("server_error"));
+        assertThat(response.getJwk(), equalTo(null));
+    }
+
+    @Test
+    void returnsErrorWhenKeyIdIsMissing() {
+        // given
+        Map<String, String> event = Map.of("url", url);
+
+        // when
+        FetchJwksResponse response = handler.handleRequest(event, CONTEXT);
+
+        // then
+        assertThat(response.getError().getCode(), equalTo("server_error"));
+        assertThat(response.getJwk(), equalTo(null));
+    }
+}
