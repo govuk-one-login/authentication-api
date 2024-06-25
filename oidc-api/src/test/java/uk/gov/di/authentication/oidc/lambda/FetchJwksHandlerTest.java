6ea853e7c76fe6a7eb99732bffd303eff9292a42
Bill Dunn <bill.dunn@digital.cabinet-office.gov.uk>
2024-06-25 07:58:47 +0100

    ATO-689: Add test for the FetchJwksHandler happy path

--- oidc-api/src/test/java/uk/gov/di/authentication/oidc/lambda/FetchJwksHandlerTest.java
+++ oidc-api/src/test/java/uk/gov/di/authentication/oidc/lambda/FetchJwksHandlerTest.java
@@ -2,12 +2,14 @@ package uk.gov.di.authentication.oidc.lambda;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.nimbusds.jose.KeySourceException;
+import com.nimbusds.jose.jwk.JWK;
 import org.junit.jupiter.api.Test;
 import uk.gov.di.authentication.oidc.entity.JwksResponse;
 import uk.gov.di.orchestration.shared.services.JwksService;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.text.ParseException;
 import java.util.Map;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -22,6 +24,24 @@ class FetchJwksHandlerTest {
     private final FetchJwksHandler handler = new FetchJwksHandler(jwksService);
     private final String keyId = "some-key-id";
     private final String url = "https://oidc.test.account.gov.uk/.well-known/jwk.json";
+
+    @Test
+    void returnsAJwkWhenUrlAndKeyIdAreValid()
+            throws MalformedURLException, KeySourceException, ParseException {
+        // given
+        Map<String, String> event = Map.of("url", url, "keyId", keyId);
+        String jwkJson =
+                "{\"kty\":\"EC\",\"use\":\"sig\",\"crv\":\"P-256\",\"kid\":\"f27ff20940cdc6c8b34f97f44c24c8601ded9465c0713dd190ed152272d07ddb\",\"x\":\"sSdmBkED2EfjTdX-K2_cT6CfBwXQFt-DJ6v8-6tr_n8\",\"y\":\"WTXmQdqLwrmHN5tiFsTFUtNAvDYhhTQB4zyfteCrWIE\",\"alg\":\"ES256\"}";
+        JWK jwk = JWK.parse(jwkJson);
+        when(jwksService.retrieveJwkFromURLWithKeyId(new URL(url), keyId)).thenReturn(jwk);
+
+        // when
+        JwksResponse response = handler.handleRequest(event, CONTEXT);
+
+        // then
+        assertThat(response.jwk().toJSONString(), equalTo(jwkJson));
+        assertThat(response.error(), equalTo(null));
+    }
 
     @Test
     void returnsErrorWhenServiceThrowsKeySourceException()
