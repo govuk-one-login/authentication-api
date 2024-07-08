5e3eb642241bdf9b7a717ac899f82a05ef48f8f3
cargil <carly.gilson@digital.cabinet-office.gov.uk>
2024-07-08 17:17:21 +0100

    ATO-887: Add test for invalid redirect uri

--- shared/src/main/java/uk/gov/di/authentication/shared/exceptions/ClientRedirectUriValidationException.java
+++
@@ -1,7 +0,0 @@
-package uk.gov.di.authentication.shared.exceptions;
-
-public class ClientRedirectUriValidationException extends RuntimeException {
-    public ClientRedirectUriValidationException(String message) {
-        super(message);
-    }
-}
