67987d5e15fe06c6771b2215e213eb50234d9b65
BeckaL <becka.lelew@digital.cabinet-office.gov.uk>
2024-07-11 15:40:29 +0100

    BAU: Convert user exists response request to a record
    This removes some of the class boilerplate

--- frontend-api/src/main/java/uk/gov/di/authentication/frontendapi/entity/CheckUserExistsResponse.java
+++ frontend-api/src/main/java/uk/gov/di/authentication/frontendapi/entity/CheckUserExistsResponse.java
@@ -6,58 +6,10 @@ import uk.gov.di.authentication.shared.entity.MFAMethodType;
 
 import java.util.List;
 
-public class CheckUserExistsResponse {
-
-    @SerializedName("email")
-    @Expose
-    private String email;
-
-    @SerializedName("doesUserExist")
-    @Expose
-    private boolean doesUserExist;
-
-    @SerializedName("mfaMethodType")
-    @Expose
-    private MFAMethodType mfaMethodType;
-
-    @SerializedName("phoneNumberLastThree")
-    @Expose
-    private String phoneNumberLastThree;
-
-    @SerializedName("lockoutInformation")
-    @Expose
-    private List<LockoutInformation> lockoutInformation;
-
-    public CheckUserExistsResponse(
-            String email,
-            boolean doesUserExist,
-            MFAMethodType mfaMethodType,
-            String phoneNumberLastThree,
-            List<LockoutInformation> lockoutInformation) {
-        this.email = email;
-        this.doesUserExist = doesUserExist;
-        this.mfaMethodType = mfaMethodType;
-        this.phoneNumberLastThree = phoneNumberLastThree;
-        this.lockoutInformation = lockoutInformation;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public boolean doesUserExist() {
-        return doesUserExist;
-    }
-
-    public MFAMethodType getMfaMethodType() {
-        return mfaMethodType;
-    }
-
-    public String getPhoneNumberLastThree() {
-        return phoneNumberLastThree;
-    }
-
-    public List<LockoutInformation> getLockoutInformation() {
-        return lockoutInformation;
-    }
-}
+public record CheckUserExistsResponse(
+        @SerializedName("email") @Expose String email,
+        @SerializedName("doesUserExist") @Expose boolean doesUserExist,
+        @SerializedName("mfaMethodType") @Expose MFAMethodType mfaMethodType,
+        @SerializedName("phoneNumberLastThree") @Expose String phoneNumberLastThree,
+        @SerializedName("lockoutInformation") @Expose
+                List<LockoutInformation> lockoutInformation) {}
