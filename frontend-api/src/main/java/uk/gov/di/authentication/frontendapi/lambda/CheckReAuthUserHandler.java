90a7c61264b6f4acf01c2b8f623c89791d3ef432
VladGavrilet <vlad-andrei.gavrilet@digital.cabinet-office.gov.uk>
2024-07-12 16:59:21 +0300

    AUT-2790: Update logic to not lock the user in the reauth journey when introducing a wrong email 6 times.

--- frontend-api/src/main/java/uk/gov/di/authentication/frontendapi/lambda/CheckReAuthUserHandler.java
+++ frontend-api/src/main/java/uk/gov/di/authentication/frontendapi/lambda/CheckReAuthUserHandler.java
@@ -108,6 +108,7 @@ public class CheckReAuthUserHandler extends BaseFrontendHandler<CheckReauthUserR
                     .flatMap(
                             userProfile -> {
                                 if (hasEnteredIncorrectEmailTooManyTimes(userProfile.getEmail())) {
+                                    removeEmailCountLock(userProfile.getEmail());
                                     throw new AccountLockedException(
                                             "Account is locked due to too many failed attempts.",
                                             ErrorResponse.ERROR_1057);
@@ -181,6 +182,7 @@ public class CheckReAuthUserHandler extends BaseFrontendHandler<CheckReauthUserR
     private APIGatewayProxyResponseEvent generateErrorResponse(
             String email, AuditContext auditContext) {
         if (hasEnteredIncorrectEmailTooManyTimes(email)) {
+            removeEmailCountLock(email);
             throw new AccountLockedException(
                     "Account is locked due to too many failed attempts.", ErrorResponse.ERROR_1057);
         }
