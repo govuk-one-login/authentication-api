5a38ac923b3a6b97ad40d3aca4c2586a02a8b79b
Ethan Mills <ethan.mills@digital.cabinet-office.gov.uk>
2024-07-17 15:45:06 +0100

    ATO-906: Fix casing of cloudfront header name
    This is causing lots of log noise. Really these should be matched entirely case insensitively, but that's a bigger change because of the way the headers are structured in the request

--- orchestration-shared/src/main/java/uk/gov/di/orchestration/shared/helpers/IpAddressHelper.java
+++ orchestration-shared/src/main/java/uk/gov/di/orchestration/shared/helpers/IpAddressHelper.java
@@ -14,7 +14,8 @@ import static uk.gov.di.orchestration.shared.helpers.RequestHeaderHelper.getHead
 import static uk.gov.di.orchestration.shared.helpers.RequestHeaderHelper.headersContainValidHeader;
 
 public class IpAddressHelper {
-    private static final String cloudFrontViewerAddressHeaderName = "Cloudfront-Viewer-Address";
+    private static final String CLOUD_FRONT_VIEWER_ADDRESS_HEADER_NAME =
+            "Cloudfront-Viewer-Address";
 
     private static final Logger LOG = LogManager.getLogger(IpAddressHelper.class);
 
@@ -24,8 +25,8 @@ public class IpAddressHelper {
                         .map(APIGatewayProxyRequestEvent::getHeaders)
                         .orElse(emptyMap());
 
-        if (headersContainValidHeader(headers, cloudFrontViewerAddressHeaderName, true)) {
-            return getHeaderValueFromHeaders(headers, cloudFrontViewerAddressHeaderName, true)
+        if (headersContainValidHeader(headers, CLOUD_FRONT_VIEWER_ADDRESS_HEADER_NAME, true)) {
+            return getHeaderValueFromHeaders(headers, CLOUD_FRONT_VIEWER_ADDRESS_HEADER_NAME, true)
                     .split(":")[0]
                     .trim();
         } else if (headersContainValidHeader(headers, "X-Forwarded-For", true)) {
