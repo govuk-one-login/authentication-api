ec04d99176c3572f85abab32d26d189a5313759a
Tom Whitwell <tom.whitwell@digital.cabinet-office.gov.uk>
2024-07-19 16:37:34 +0100

    wip: better deploy script

--- deploy-sandpit.sh
+++ deploy-sandpit.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null 2>&1 && pwd)"
 
-export DEPLOY_ENV="sandpit"
-export AWS_PROFILE="gds-di-development-admin"
-
-# shellcheck source=scripts/dev_deploy_common.sh
-source "${DIR}/scripts/dev_deploy_common.sh"
+exec "${DIR}/deploy-dev.sh" "sandpit" "${@}"
