c39821b46c9195468bb870dbf5fd3a1596968748
cargil <carly.gilson@digital.cabinet-office.gov.uk>
2024-07-25 09:52:40 +0100

    ATO-917: Create AuthorisationService to classify parse exceptions

---
+++ oidc-api/src/main/java/uk/gov/di/authentication/oidc/services/AuthorisationService.java
@@ -0,0 +1,58 @@
+package uk.gov.di.authentication.oidc.services;
+
+import com.nimbusds.oauth2.sdk.ParseException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import uk.gov.di.authentication.oidc.exceptions.IncorrectRedirectUriException;
+import uk.gov.di.authentication.oidc.exceptions.InvalidAuthenticationRequestException;
+import uk.gov.di.authentication.oidc.exceptions.MissingClientIDException;
+import uk.gov.di.authentication.oidc.exceptions.MissingRedirectUriException;
+import uk.gov.di.authentication.oidc.lambda.AuthorisationHandler;
+import uk.gov.di.orchestration.shared.exceptions.ClientNotFoundException;
+import uk.gov.di.orchestration.shared.services.ClientService;
+import uk.gov.di.orchestration.shared.services.ConfigurationService;
+import uk.gov.di.orchestration.shared.services.DynamoClientService;
+
+public class AuthorisationService {
+    private final ClientService clientService;
+    private static final Logger LOG = LogManager.getLogger(AuthorisationHandler.class);
+
+    public AuthorisationService(ClientService clientService) {
+        this.clientService = clientService;
+    }
+
+    public AuthorisationService(ConfigurationService configurationService) {
+        this.clientService = new DynamoClientService(configurationService);
+    }
+
+    public void classifyParseException(ParseException error)
+            throws MissingClientIDException,
+                    IncorrectRedirectUriException,
+                    ClientNotFoundException,
+                    InvalidAuthenticationRequestException,
+                    MissingRedirectUriException {
+        if (error.getClientID() == null) {
+            throw new MissingClientIDException(error.getErrorObject());
+        }
+
+        if (error.getRedirectionURI() == null) {
+            throw new MissingRedirectUriException(error.getErrorObject());
+        }
+
+        var client =
+                clientService
+                        .getClient(error.getClientID().getValue())
+                        .orElseThrow(
+                                () -> new ClientNotFoundException(error.getClientID().getValue()));
+
+        if (client.getRedirectUrls().contains(error.getRedirectionURI().toString())) {
+            throw new InvalidAuthenticationRequestException(error.getErrorObject());
+        }
+
+        LOG.warn(
+                "Redirect URI {} is invalid for client {}",
+                error.getRedirectionURI(),
+                client.getClientID());
+        throw new IncorrectRedirectUriException(error.getErrorObject());
+    }
+}
