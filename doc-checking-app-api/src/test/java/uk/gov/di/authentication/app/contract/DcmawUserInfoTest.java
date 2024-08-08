a362b4ea707e785faa9862d403d514263d49a998
Isaac Au <isaac.au@digital.cabinet-office.gov.uk>
2024-08-08 10:59:19 +0100

    ATO-958: remove 404 test for DCMAW userinfo contract

--- doc-checking-app-api/src/test/java/uk/gov/di/authentication/app/contract/DcmawUserInfoTest.java
+++ doc-checking-app-api/src/test/java/uk/gov/di/authentication/app/contract/DcmawUserInfoTest.java
@@ -111,51 +111,9 @@ public class DcmawUserInfoTest {
     }
 
     @Pact(consumer = "OrchUserInfoConsumer")
-    RequestResponsePact validRequestReturnsAccessDenied(PactDslWithProvider builder) {
-        return builder.given("accessToken is a valid access token")
-                .uponReceiving("Valid access token")
-                .path("/" + DOC_APP_USER_INFO_PATH)
-                .method("POST")
-                .matchHeader(
-                        "Authorization",
-                        "^(?i)Bearer (.*)(?-i)",
-                        tokens.getAccessToken().toAuthorizationHeader())
-                .willRespondWith()
-                .status(404)
-                .body(ERROR_MESSAGE)
-                .toPact();
-    }
-
-    @Test
-    @PactTestFor(
-            providerName = "DcmawUserInfoProvider",
-            pactMethod = "validRequestReturnsAccessDenied",
-            pactVersion = PactSpecVersion.V3)
-    void getDocAppUserInfoErrorDeniedResponse(MockServer mockServer) {
-
-        var request =
-                new HTTPRequest(
-                        POST,
-                        ConstructUriHelper.buildURI(mockServer.getUrl(), DOC_APP_USER_INFO_PATH));
-        request.setAuthorization(tokens.getAccessToken().toAuthorizationHeader());
-
-        UnsuccessfulCredentialResponseException exception =
-                assertThrows(
-                        UnsuccessfulCredentialResponseException.class,
-                        () -> docAppCriService.sendCriDataRequest(request, DOC_APP_SUBJECT_ID));
-
-        assertThat(exception.getHttpCode(), equalTo(404));
-        assertThat(
-                exception.getMessage(),
-                equalTo(
-                        "Error 404 when attempting to call CRI data endpoint: "
-                                + ERROR_MESSAGE
-                                + "\n"));
-    }
-
-    @Pact(consumer = "OrchUserInfoConsumer")
     RequestResponsePact invalidAccessTokenReturnsError(PactDslWithProvider builder) {
         return builder.given("accessToken is an invalid access token")
+                .given("dummy-doc-app-subject-id is a valid subject")
                 .uponReceiving("Invalid access token")
                 .path("/" + DOC_APP_USER_INFO_PATH)
                 .method("POST")
