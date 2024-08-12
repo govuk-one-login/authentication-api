3b6bf42c14653241cb97ddbfc39bd7e904a7b6c6
BeckaL <becka.lelew@digital.cabinet-office.gov.uk>
2024-08-12 13:25:16 +0100

    BAU: Remove unused auth Doc App Authorisation Service
    The only place this was now called was in tests. This functionality has now moved to orchestration, and so can safely be removed from authentication

--- shared/src/main/java/uk/gov/di/authentication/shared/exceptions/DocAppAuthorisationServiceException.java
+++
@@ -1,7 +0,0 @@
-package uk.gov.di.authentication.shared.exceptions;
-
-public class DocAppAuthorisationServiceException extends RuntimeException {
-    public DocAppAuthorisationServiceException(Exception e) {
-        super(e.getMessage());
-    }
-}
