918b4546a3d74e25e85b5dfd0a542fe2dead75d3
Tom Whitwell <tom.whitwell@digital.cabinet-office.gov.uk>
2024-08-15 16:48:15 +0100

    wip: test again

--- ci/stack-orchestration/provision_all.sh
+++ ci/stack-orchestration/provision_all.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
+set -euo pipefail
+
+cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null || usage "Error: Unable to change to script directory."
+
 ENVIRONMENT=${1}
 
-PROVISION_COMMAND="./.devplatform-deploy/stack-orchestration-tool/provisioner.sh"
+PROVISIONER_SCRIPT="./.devplatform-deploy/stack-orchestration-tool/provisioner.sh"
+if [[ ! -f ${PROVISIONER_SCRIPT} ]]; then
+  echo 'Provisioner script not found. You probably need to pull the submodule: git submodule update --init --recursive'
+  exit 1
+fi
+cp "${PROVISIONER_SCRIPT}" .
 
 export AUTO_APPLY_CHANGESET=true
 export SKIP_AWS_AUTHENTICATION=true
@@ -11,9 +20,9 @@ export AWS_PAGER=""
 for directory in configuration/"${ENVIRONMENT}"/*/; do
   STACK=$(basename "${directory}")
   if [[ ${STACK} != "${ENVIRONMENT}-orch-be-pipeline" && -f "configuration/${ENVIRONMENT}/${STACK}/parameters.json" ]]; then
-    ${PROVISION_COMMAND} "${ENVIRONMENT}" "${STACK}" "${STACK}" LATEST
+    ./provisioner.sh "${ENVIRONMENT}" "${STACK}" "${STACK}" LATEST
   fi
 done
 
 ### Provision secure pipelines
-${PROVISION_COMMAND} "${ENVIRONMENT}" "${ENVIRONMENT}-orch-be-pipeline" sam-deploy-pipeline LATEST
+./provisioner.sh "${ENVIRONMENT}" "${ENVIRONMENT}-orch-be-pipeline" sam-deploy-pipeline LATEST
