c74f81118fc075445ad2c976ec2942b7676897c3
BeckaL <becka.lelew@digital.cabinet-office.gov.uk>
2024-08-16 16:41:37 +0100

    BAU: Remove no session service and related code in auth
    This was only used in test, and has been redundant as of the orch/auth split. This was used in the doc app and ipv code, which has all now moved to orchestration, who have their own copy of this code. Therefore, this can be safely removed from here

--- shared/src/main/java/uk/gov/di/authentication/shared/exceptions/NoSessionException.java
+++
@@ -1,8 +0,0 @@
-package uk.gov.di.authentication.shared.exceptions;
-
-public class NoSessionException extends Exception {
-
-    public NoSessionException(String message) {
-        super(message);
-    }
-}
