34e66c4c81723d613b517e2fdec80e1104a989ad
Ryan Andrews <ryan.andrews@digital.cabinet-office.gov.uk>
2024-09-11 11:25:20 +0100

    ATO-981: Adds redis locals to auth-external shared
    The UserInfo Lambda will now need to access the shared session store to,
    be able to set the value of isNewAccount. This adds the required policy
    and redis_key for us to be able to give the lambda permission to access
    and update the session store.

--- ci/terraform/auth-external-api/shared.tf
+++ ci/terraform/auth-external-api/shared.tf
@@ -27,4 +27,6 @@ locals {
   access_token_store_signing_key_arn        = data.terraform_remote_state.shared.outputs.access_token_store_signing_key_arn
   user_credentials_encryption_policy_arn    = data.terraform_remote_state.shared.outputs.user_credentials_encryption_policy_arn
   user_profile_kms_key_arn                  = data.terraform_remote_state.shared.outputs.user_profile_kms_key_arn
+  redis_ssm_parameter_policy                = data.terraform_remote_state.shared.outputs.redis_ssm_parameter_policy
+  redis_key                                 = "session"
 }
