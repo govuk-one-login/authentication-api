0c610ff230a187f2336b5e7d30a115903f73d4e0
dbes-gds <daniel.besbrode@digital.cabinet-office.gov.uk>
2024-09-16 16:54:29 +0100

    INCIDEN-927: Set treat missing to not breaching for ipv_handback alarm
    Having triggered the alarm has not resolved automatically even though there are no longer any errors.

--- ci/terraform/oidc/ipv-handback-alerts.tf
+++ ci/terraform/oidc/ipv-handback-alerts.tf
@@ -75,4 +75,5 @@ resource "aws_cloudwatch_metric_alarm" "ipv_handback_p1_cloudwatch_alarm" {
   threshold           = var.ipv_p1_alarm_error_threshold
   alarm_description   = "${var.ipv_p1_alarm_error_threshold} or more IPV handback errors have occurred in ${var.environment}.ACCOUNT: ${data.aws_iam_account_alias.current.account_alias}"
   alarm_actions       = [var.environment == "production" ? data.aws_sns_topic.pagerduty_p1_alerts[0].arn : data.aws_sns_topic.slack_events.arn]
+  treat_missing_data  = "notBreaching"
 }
