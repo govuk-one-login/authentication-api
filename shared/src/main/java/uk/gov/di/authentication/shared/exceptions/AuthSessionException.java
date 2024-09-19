ab80473998421dc8dfdd57c9c08cc18bc1bbb0ba
Bill Dunn <bill.dunn@digital.cabinet-office.gov.uk>
2024-09-19 12:12:56 +0100

    ATO-979: Define AuthSessionException which is thrown when there are exceptions relating to the Auth session table

---
+++ shared/src/main/java/uk/gov/di/authentication/shared/exceptions/AuthSessionException.java
@@ -0,0 +1,8 @@
+package uk.gov.di.authentication.shared.exceptions;
+
+public class AuthSessionException extends RuntimeException {
+
+    public AuthSessionException(String message) {
+        super(message);
+    }
+}
