b924bfd1f5bb9be315a1e35fa15b818a905c8b32
Bill Dunn <bill.dunn@digital.cabinet-office.gov.uk>
2024-10-01 16:05:48 +0100

    ATO-1075: Create OrchSessionItem entity
    This class will map to the Orch session DynamoDB table. For now it just contains sessionId and ttl fields, but more will be added as more data is split from the shared session.

---
+++ orchestration-shared/src/main/java/uk/gov/di/orchestration/shared/entity/OrchSessionItem.java
@@ -0,0 +1,45 @@
+package uk.gov.di.orchestration.shared.entity;
+
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbAttribute;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbBean;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbPartitionKey;
+
+@DynamoDbBean
+public class OrchSessionItem {
+
+    public static final String ATTRIBUTE_SESSION_ID = "SessionId";
+
+    private String sessionId;
+    private long timeToLive;
+
+    public OrchSessionItem() {}
+
+    @DynamoDbPartitionKey
+    @DynamoDbAttribute(ATTRIBUTE_SESSION_ID)
+    public String getSessionId() {
+        return sessionId;
+    }
+
+    public void setSessionId(String sessionId) {
+        this.sessionId = sessionId;
+    }
+
+    public OrchSessionItem withSessionId(String sessionId) {
+        this.sessionId = sessionId;
+        return this;
+    }
+
+    @DynamoDbAttribute("ttl")
+    public long getTimeToLive() {
+        return timeToLive;
+    }
+
+    public void setTimeToLive(long timeToLive) {
+        this.timeToLive = timeToLive;
+    }
+
+    public OrchSessionItem withTimeToLive(long timeToLive) {
+        this.timeToLive = timeToLive;
+        return this;
+    }
+}
