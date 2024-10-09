a6c0230841e58baef0873a12e672333cc758b11e
Aidan Comer <alhcomer@hotmail.co.uk>
2024-10-09 15:03:51 +0100

    BAU: refactor RemoveAccountRequest to a record

--- account-management-api/src/main/java/uk/gov/di/accountmanagement/entity/RemoveAccountRequest.java
+++ account-management-api/src/main/java/uk/gov/di/accountmanagement/entity/RemoveAccountRequest.java
@@ -3,16 +3,4 @@ package uk.gov.di.accountmanagement.entity;
 import com.google.gson.annotations.Expose;
 import uk.gov.di.authentication.shared.validation.Required;
 
-public class RemoveAccountRequest {
-    @Expose @Required private String email;
-
-    public RemoveAccountRequest() {}
-
-    public RemoveAccountRequest(String email) {
-        this.email = email;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-}
+public record RemoveAccountRequest(@Expose @Required String email) {}
