c276882a7d3d7f9382bb05303bcb78c32e58abd4
Bill Dunn <bill.dunn@digital.cabinet-office.gov.uk>
2024-10-15 13:15:52 +0100

    ATO-1049: Define a new metric filter and alarm for alerting on AIS_FAIL_OPEN errors.
    When AIS went down previously, a pager duty alarm was triggered even though we were failing open. This new alarm will alert on slack only when AIS is failing open. The original P1 alarm is unaffected.

--- ci/terraform/oidc/ipv-handback-alerts.tf
+++ ci/terraform/oidc/ipv-handback-alerts.tf
@@ -63,6 +63,19 @@ resource "aws_cloudwatch_log_metric_filter" "processing_identity_metric_filter" 
   }
 }
 
+resource "aws_cloudwatch_log_metric_filter" "processing_identity_ais_fail_open_metric_filter" {
+  count          = var.use_localstack ? 0 : 1
+  name           = replace("${var.environment}-processing-identity-ais-fail-open-errors", ".", "")
+  pattern        = "{($.level = \"AIS_FAIL_OPEN\")}"
+  log_group_name = data.aws_cloudwatch_log_group.processing_identity_lambda_log_group[0].name
+
+  metric_transformation {
+    name      = replace("${var.environment}-ipv-handback-error-count", ".", "")
+    namespace = "LambdaErrorsNamespace"
+    value     = "1"
+  }
+}
+
 resource "aws_cloudwatch_metric_alarm" "ipv_handback_p1_cloudwatch_alarm" {
   count               = var.use_localstack ? 0 : 1
   alarm_name          = replace("${var.environment}-P1-ipv-handback-alarm", ".", "")
@@ -75,5 +88,20 @@ resource "aws_cloudwatch_metric_alarm" "ipv_handback_p1_cloudwatch_alarm" {
   threshold           = var.ipv_p1_alarm_error_threshold
   alarm_description   = "${var.ipv_p1_alarm_error_threshold} or more IPV handback errors have occurred in ${var.environment}.ACCOUNT: ${data.aws_iam_account_alias.current.account_alias}"
   alarm_actions       = [var.environment == "production" ? data.aws_sns_topic.pagerduty_p1_alerts[0].arn : data.aws_sns_topic.slack_events.arn]
+  treat_missing_data  = "notBreaching"
+}
+
+resource "aws_cloudwatch_metric_alarm" "ipv_handback_ais_fail_open_cloudwatch_alarm" {
+  count               = var.use_localstack ? 0 : 1
+  alarm_name          = replace("${var.environment}-ais-fail-open-ipv-handback-alarm", ".", "")
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = "1"
+  metric_name         = aws_cloudwatch_log_metric_filter.processing_identity_ais_fail_open_metric_filter[0].metric_transformation[0].name
+  namespace           = aws_cloudwatch_log_metric_filter.processing_identity_ais_fail_open_metric_filter[0].metric_transformation[0].namespace
+  period              = var.ipv_p1_alarm_error_time_period
+  statistic           = "Sum"
+  threshold           = var.ipv_p1_alarm_error_threshold
+  alarm_description   = "${var.ipv_p1_alarm_error_threshold} or more errors have occured while communicating with Account Intervention Service in ${var.environment}. Currently failing open and assuming no intervention. ACCOUNT: ${data.aws_iam_account_alias.current.account_alias}"
+  alarm_actions       = data.aws_sns_topic.slack_events.arn
   treat_missing_data  = "notBreaching"
 }
