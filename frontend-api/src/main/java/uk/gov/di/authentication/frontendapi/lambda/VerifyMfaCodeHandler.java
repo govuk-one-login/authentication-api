e2f16095d56e19362f69812657be645adfa20a28
Aidan Comer <alhcomer@hotmail.co.uk>
2024-10-17 12:56:39 +0100

    BAU: throw and handle exception if client is not present
    The client should always be present. An exception is now thrown and caught early in the lambda handler
    if a client is not present, returning an error response.

--- frontend-api/src/main/java/uk/gov/di/authentication/frontendapi/lambda/VerifyMfaCodeHandler.java
+++ frontend-api/src/main/java/uk/gov/di/authentication/frontendapi/lambda/VerifyMfaCodeHandler.java
@@ -25,6 +25,7 @@ import uk.gov.di.authentication.shared.entity.JourneyType;
 import uk.gov.di.authentication.shared.entity.MFAMethodType;
 import uk.gov.di.authentication.shared.entity.Session;
 import uk.gov.di.authentication.shared.entity.UserProfile;
+import uk.gov.di.authentication.shared.exceptions.ClientNotFoundException;
 import uk.gov.di.authentication.shared.helpers.ApiGatewayResponseHelper;
 import uk.gov.di.authentication.shared.helpers.ClientSubjectHelper;
 import uk.gov.di.authentication.shared.helpers.IpAddressHelper;
@@ -171,7 +172,18 @@ public class VerifyMfaCodeHandler extends BaseFrontendHandler<VerifyMfaCodeReque
         var journeyType = codeRequest.getJourneyType();
         Optional<UserProfile> userProfileMaybe = userContext.getUserProfile();
         UserProfile userProfile = userProfileMaybe.orElse(null);
-        var maybeRpPairwiseId = getRpPairwiseId(userProfile, userContext.getClient());
+        ClientRegistry client;
+        try {
+            client =
+                    userContext
+                            .getClient()
+                            .orElseThrow(
+                                    () -> new ClientNotFoundException(userContext.getSession()));
+        } catch (ClientNotFoundException e) {
+            LOG.warn("Client not found");
+            return generateApiGatewayProxyErrorResponse(400, ErrorResponse.ERROR_1015);
+        }
+        Optional<String> maybeRpPairwiseId = getRpPairwiseId(userProfile, client);
 
         var auditContext =
                 auditContextFromUserContext(
@@ -191,7 +203,7 @@ public class VerifyMfaCodeHandler extends BaseFrontendHandler<VerifyMfaCodeReque
             return generateApiGatewayProxyErrorResponse(400, ErrorResponse.ERROR_1049);
 
         if (checkErrorCountsForReauthAndEmitFailedAuditEventIfBlocked(
-                journeyType, userProfile, auditContext, userContext, maybeRpPairwiseId))
+                journeyType, userProfile, auditContext, maybeRpPairwiseId, client))
             return generateApiGatewayProxyErrorResponse(400, ErrorResponse.ERROR_1057);
 
         try {
@@ -202,7 +214,8 @@ public class VerifyMfaCodeHandler extends BaseFrontendHandler<VerifyMfaCodeReque
                     userContext,
                     subjectID,
                     authSession.get(),
-                    maybeRpPairwiseId);
+                    maybeRpPairwiseId,
+                    client);
         } catch (Exception e) {
             LOG.error("Unexpected exception thrown");
             return generateApiGatewayProxyErrorResponse(400, ErrorResponse.ERROR_1001);
@@ -230,8 +243,8 @@ public class VerifyMfaCodeHandler extends BaseFrontendHandler<VerifyMfaCodeReque
             JourneyType journeyType,
             UserProfile userProfile,
             AuditContext auditContext,
-            UserContext userContext,
-            Optional<String> maybeRpPairwiseId) {
+            Optional<String> maybeRpPairwiseId,
+            ClientRegistry client) {
         if (configurationService.isAuthenticationAttemptsServiceEnabled()
                 && JourneyType.REAUTHENTICATION.equals(journeyType)
                 && userProfile != null) {
@@ -248,7 +261,6 @@ public class VerifyMfaCodeHandler extends BaseFrontendHandler<VerifyMfaCodeReque
                     ReauthAuthenticationAttemptsHelper.countTypesWhereUserIsBlockedForReauth(
                             counts, configurationService);
 
-            ClientRegistry client = userContext.getClient().orElse(null);
             if (!countTypesWhereLimitExceeded.isEmpty() && client != null) {
                 ReauthFailureReasons failureReason =
                         getReauthFailureReasonFromCountTypes(countTypesWhereLimitExceeded);
@@ -283,7 +295,8 @@ public class VerifyMfaCodeHandler extends BaseFrontendHandler<VerifyMfaCodeReque
             UserContext userContext,
             String subjectId,
             AuthSessionItem authSession,
-            Optional<String> maybeRpPairwiseId) {
+            Optional<String> maybeRpPairwiseId,
+            ClientRegistry client) {
 
         var session = userContext.getSession();
 
@@ -366,8 +379,8 @@ public class VerifyMfaCodeHandler extends BaseFrontendHandler<VerifyMfaCodeReque
                 codeRequest.getJourneyType(),
                 userContext.getUserProfile().orElse(null),
                 auditContext,
-                userContext,
-                maybeRpPairwiseId)) {
+                maybeRpPairwiseId,
+                client)) {
             return generateApiGatewayProxyErrorResponse(400, ErrorResponse.ERROR_1057);
         }
 
@@ -555,17 +568,15 @@ public class VerifyMfaCodeHandler extends BaseFrontendHandler<VerifyMfaCodeReque
                 .toArray(AuditService.MetadataPair[]::new);
     }
 
-    private Optional<String> getRpPairwiseId(
-            UserProfile userProfile, Optional<ClientRegistry> maybeClient) {
+    private Optional<String> getRpPairwiseId(UserProfile userProfile, ClientRegistry client) {
         try {
-            return maybeClient.map(
-                    client ->
-                            ClientSubjectHelper.getSubject(
-                                            userProfile,
-                                            client,
-                                            authenticationService,
-                                            configurationService.getInternalSectorUri())
-                                    .getValue());
+            return Optional.of(
+                    ClientSubjectHelper.getSubject(
+                                    userProfile,
+                                    client,
+                                    authenticationService,
+                                    configurationService.getInternalSectorUri())
+                            .getValue());
         } catch (RuntimeException e) {
             LOG.warn("Failed to derive Internal Common Subject Identifier. Defaulting to UNKNOWN.");
             return Optional.empty();
