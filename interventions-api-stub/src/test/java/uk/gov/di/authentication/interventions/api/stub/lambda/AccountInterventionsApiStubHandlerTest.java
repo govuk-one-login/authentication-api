bf52ccd4a910279476724902c4b9ac3687692991
Matt Votsikas McLean <matthew@votsikas.com>
2024-11-13 10:55:56 +0000

    Revert "AUT-3845: Temporary logging of `internalPairwiseId` in lower envs"

--- interventions-api-stub/src/test/java/uk/gov/di/authentication/interventions/api/stub/lambda/AccountInterventionsApiStubHandlerTest.java
+++ interventions-api-stub/src/test/java/uk/gov/di/authentication/interventions/api/stub/lambda/AccountInterventionsApiStubHandlerTest.java
@@ -5,7 +5,6 @@ import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import org.junit.jupiter.api.Test;
 import uk.gov.di.authentication.interventions.api.stub.entity.AccountInterventionsStore;
 import uk.gov.di.authentication.interventions.api.stub.services.AccountInterventionsDbService;
-import uk.gov.di.authentication.shared.services.ConfigurationService;
 
 import java.util.Map;
 import java.util.Optional;
@@ -35,9 +34,7 @@ class AccountInterventionsApiStubHandlerTest {
 
     @Test
     void shouldReturn200ForSuccessfulRequest() {
-        var handler =
-                new AccountInterventionsApiStubHandler(
-                        accountInterventionsDbService, new ConfigurationService());
+        var handler = new AccountInterventionsApiStubHandler(accountInterventionsDbService);
         when(accountInterventionsDbService.getAccountInterventions(PAIRWISE_ID))
                 .thenReturn(Optional.of(accountInterventionsStore));
 
@@ -57,9 +54,7 @@ class AccountInterventionsApiStubHandlerTest {
 
     @Test
     void shouldReturn200WhenThePairwiseIdDoesNotExistInTheDatabase() {
-        var handler =
-                new AccountInterventionsApiStubHandler(
-                        accountInterventionsDbService, new ConfigurationService());
+        var handler = new AccountInterventionsApiStubHandler(accountInterventionsDbService);
         when(accountInterventionsDbService.getAccountInterventions(PAIRWISE_ID))
                 .thenReturn(Optional.empty());
 
