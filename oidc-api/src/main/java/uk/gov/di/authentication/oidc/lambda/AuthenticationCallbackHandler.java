a33b12633cb7574e5c80376db3aa120756017ad4
Ryan Andrews <ryan.andrews@digital.cabinet-office.gov.uk>
2024-11-25 17:06:59 +0000

    BAU: Use orch account state for metric
    This one didn't directly use session.isNewAccount so was missed

--- oidc-api/src/main/java/uk/gov/di/authentication/oidc/lambda/AuthenticationCallbackHandler.java
+++ oidc-api/src/main/java/uk/gov/di/authentication/oidc/lambda/AuthenticationCallbackHandler.java
@@ -514,7 +514,7 @@ public class AuthenticationCallbackHandler
                 sessionService.storeOrUpdateSession(userSession);
                 cloudwatchMetricsService.incrementCounter("SignIn", dimensions);
                 cloudwatchMetricsService.incrementSignInByClient(
-                        accountState, clientId, clientSession.getClientName(), isTestJourney);
+                        orchAccountState, clientId, clientSession.getClientName(), isTestJourney);
 
                 LOG.info("Successfully processed request");
 
