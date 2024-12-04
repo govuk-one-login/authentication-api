9fd7190eade3938c0a7ac1513e251346536938a4
BeckaL <becka.lelew@digital.cabinet-office.gov.uk>
2024-12-04 17:17:48 +0000

    AUT-3902: Add provisioned concurrency and scaling policy for ticf lambda
    For lambdas in oidc, we generally use the endpoint module which sets this up for us. I haven't used this here for a few reasons:
    a) This lambda is not an endpoint lambda
    b) We're unlikely to keep it in this form - in phase 2 of delivering ticf we will call it asynchronously, which will likely mean rewriting it as an endpoint lambda
    c) We're eventually migrating away from terraform, so the effort to switch over now may not be worth it

--- ci/terraform/oidc/ticf-cri.tf
+++ ci/terraform/oidc/ticf-cri.tf
@@ -9,6 +9,10 @@ module "frontend_api_ticf_cri_role" {
   }
 }
 
+locals {
+  provisioned_concurrency = lookup(var.performance_tuning, "ticf-cri", local.default_performance_parameters).concurrency
+}
+
 resource "aws_lambda_function" "ticf_cri_lambda" {
   count         = local.deploy_ticf_cri_count
   function_name = "${var.environment}-ticf-cri-lambda"
@@ -18,7 +22,6 @@ resource "aws_lambda_function" "ticf_cri_lambda" {
   publish       = true
   timeout       = 60
 
-  memory_size                    = lookup(var.performance_tuning, "ticf-cri", local.default_performance_parameters).memory
   reserved_concurrent_executions = 1
 
   tracing_config {
@@ -28,6 +31,8 @@ resource "aws_lambda_function" "ticf_cri_lambda" {
   s3_bucket         = aws_s3_bucket.source_bucket.bucket
   s3_key            = aws_s3_object.frontend_api_release_zip.key
   s3_object_version = aws_s3_object.frontend_api_release_zip.version_id
+
+  memory_size = lookup(var.performance_tuning, "ticf", local.default_performance_parameters).memory
 
   kms_key_arn             = local.lambda_env_vars_encryption_kms_key_arn
   code_signing_config_arn = local.lambda_code_signing_configuration_arn
@@ -47,6 +52,46 @@ resource "aws_lambda_function" "ticf_cri_lambda" {
   # checkov:skip=CKV_AWS_116:Adding a DLQ would not be useful as we're not adding a retry policy.
   tags = {
     Service = "ticf-cri"
+  }
+}
+
+resource "aws_lambda_provisioned_concurrency_config" "ticf_lambda_concurrency_config" {
+  count = local.deploy_ticf_cri_count == 0 || local.provisioned_concurrency == 0 ? 0 : 1
+
+  function_name = aws_lambda_function.ticf_cri_lambda[0].function_name
+  qualifier     = aws_lambda_alias.ticf_cri_lambda_alias[0].name
+
+  provisioned_concurrent_executions = local.provisioned_concurrency
+
+  lifecycle {
+    ignore_changes = [provisioned_concurrent_executions]
+  }
+}
+
+resource "aws_appautoscaling_target" "ticf_lambda_target" {
+  count = local.deploy_ticf_cri_count
+
+  max_capacity       = lookup(var.performance_tuning, "ticf-cri", local.default_performance_parameters).max_concurrency
+  min_capacity       = lookup(var.performance_tuning, "ticf-cri", local.default_performance_parameters).concurrency
+  resource_id        = "function:${aws_lambda_function.ticf_cri_lambda[0].function_name}:${aws_lambda_alias.ticf_cri_lambda_alias[0].name}"
+  scalable_dimension = "lambda:function:ProvisionedConcurrency"
+  service_namespace  = "lambda"
+}
+
+resource "aws_appautoscaling_policy" "ticf_provisioned_concurrency_policy" {
+  count = local.deploy_ticf_cri_count
+
+  name               = "LambdaProvisonedConcurrency:${aws_lambda_function.ticf_cri_lambda[0].function_name}"
+  resource_id        = aws_appautoscaling_target.ticf_lambda_target[0].resource_id
+  scalable_dimension = aws_appautoscaling_target.ticf_lambda_target[0].scalable_dimension
+  service_namespace  = aws_appautoscaling_target.ticf_lambda_target[0].service_namespace
+  policy_type        = "TargetTrackingScaling"
+
+  target_tracking_scaling_policy_configuration {
+    target_value = lookup(var.performance_tuning, "ticf-cri", local.default_performance_parameters).scaling_trigger
+    predefined_metric_specification {
+      predefined_metric_type = "LambdaProvisionedConcurrencyUtilization"
+    }
   }
 }
 
