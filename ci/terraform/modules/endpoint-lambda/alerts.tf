66a971507f1a528ff1f146174c2f8daa229f7ca2
Matt Votsikas McLean <matt.votsikas-mclean@digital.cabinet-office.gov.uk>
2024-12-09 15:58:39 +0000

    BAU: Add runbook link support to `endpoint-lambda`
    Like 1e50cc8f75ab5e7f22bddcbb45bedae6e8eb4a60, add support for appending runbook links to CloudWatch Alarm descriptions.

--- ci/terraform/modules/endpoint-lambda/alerts.tf
+++ ci/terraform/modules/endpoint-lambda/alerts.tf
@@ -1,3 +1,10 @@
+locals {
+  base_error_alarm_description      = "${var.lambda_log_alarm_threshold} or more errors have occurred in the ${var.environment} ${var.endpoint_name} lambda.ACCOUNT: ${var.account_alias}"
+  error_alarm_description           = var.runbook_link == "" ? local.base_error_alarm_description : "${local.base_error_alarm_description}. Runbook: ${var.runbook_link}"
+  base_error_rate_alarm_description = "Lambda error rate of ${var.lambda_log_alarm_error_rate_threshold} has been reached in the ${var.environment} ${var.endpoint_name} lambda.ACCOUNT: ${var.account_alias}"
+  error_rate_alarm_description      = var.runbook_link == "" ? local.base_error_rate_alarm_description : "${local.base_error_rate_alarm_description}. Runbook: ${var.runbook_link}"
+}
+
 resource "aws_cloudwatch_log_metric_filter" "lambda_error_metric_filter" {
   name           = replace("${var.environment}-${var.endpoint_name}-errors", ".", "")
   pattern        = "{($.level = \"ERROR\")}"
@@ -19,7 +26,7 @@ resource "aws_cloudwatch_metric_alarm" "lambda_error_cloudwatch_alarm" {
   period              = "3600"
   statistic           = "Sum"
   threshold           = var.lambda_log_alarm_threshold
-  alarm_description   = "${var.lambda_log_alarm_threshold} or more errors have occurred in the ${var.environment} ${var.endpoint_name} lambda.ACCOUNT: ${var.account_alias}"
+  alarm_description   = local.error_alarm_description
   alarm_actions       = [var.slack_event_topic_arn]
 
   tags = local.extra_tags
@@ -31,7 +38,8 @@ resource "aws_cloudwatch_metric_alarm" "lambda_error_rate_cloudwatch_alarm" {
   comparison_operator = "GreaterThanOrEqualToThreshold"
   evaluation_periods  = "1"
   threshold           = var.lambda_log_alarm_error_rate_threshold
-  alarm_description   = "Lambda error rate of ${var.lambda_log_alarm_error_rate_threshold} has been reached in the ${var.environment} ${var.endpoint_name} lambda.ACCOUNT: ${var.account_alias}"
+
+  alarm_description = local.error_rate_alarm_description
 
   metric_query {
     id          = "e1"
