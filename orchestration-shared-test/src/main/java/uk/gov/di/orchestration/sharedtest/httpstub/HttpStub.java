d3e661b4cf1fa6d16f2c66b97ce9015eb4b1cc27
Matt Votsikas McLean <matt.votsikas-mclean@digital.cabinet-office.gov.uk>
2024-12-12 08:44:51 +0000

    BAU: Upgrade `jetty` to 12.0.15
    This involves changing some implementation details for methods that have been removed with the bump.
    
    Also took the opportunity to simplify `RecordedRequest` as it contained methods and properties that were not used.
    
    This change is duplicated across auth and orch `shared-test` packages.

--- orchestration-shared-test/src/main/java/uk/gov/di/orchestration/sharedtest/httpstub/HttpStub.java
+++ orchestration-shared-test/src/main/java/uk/gov/di/orchestration/sharedtest/httpstub/HttpStub.java
@@ -1,22 +1,23 @@
 package uk.gov.di.orchestration.sharedtest.httpstub;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import org.eclipse.jetty.http.HttpHeader;
 import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.Response;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.ServerConnector;
-import org.eclipse.jetty.server.handler.AbstractHandler;
+import org.eclipse.jetty.util.Callback;
 import org.eclipse.jetty.util.ssl.SslContextFactory;
 import uk.gov.di.orchestration.sharedtest.exceptions.Unchecked;
 
 import java.io.File;
-import java.io.IOException;
+import java.nio.ByteBuffer;
 import java.security.KeyStore;
 import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.CopyOnWriteArrayList;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
 
 class HttpStub {
 
@@ -157,25 +158,25 @@ class HttpStub {
         return recordedRequests;
     }
 
-    private class Handler extends AbstractHandler {
+    private class Handler extends org.eclipse.jetty.server.Handler.Abstract {
+
         @Override
-        public void handle(
-                String target,
-                Request baseRequest,
-                HttpServletRequest request,
-                HttpServletResponse response)
-                throws IOException, ServletException {
-            recordedRequests.add(new RecordedRequest(baseRequest));
+        public boolean handle(Request request, Response response, Callback callback)
+                throws Exception {
+            recordedRequests.add(new RecordedRequest(request));
 
             RegisteredResponse registeredResponse =
-                    registeredResponses.get(baseRequest.getRequestURI());
+                    registeredResponses.get(request.getHttpURI().getPath());
 
             if (registeredResponse != null) {
                 response.setStatus(registeredResponse.getStatus());
-                response.setContentType(registeredResponse.getContentType());
-                response.getWriter().append(registeredResponse.getBody());
-                baseRequest.setHandled(true);
+                response.getHeaders()
+                        .put(HttpHeader.CONTENT_TYPE, registeredResponse.getContentType());
+                ByteBuffer content = UTF_8.encode(registeredResponse.getBody());
+                response.write(true, content, callback);
+                callback.succeeded();
             }
+            return true;
         }
     }
 }
--- orchestration-shared-test/src/main/java/uk/gov/di/orchestration/sharedtest/httpstub/RecordedRequest.java
+++ orchestration-shared-test/src/main/java/uk/gov/di/orchestration/sharedtest/httpstub/RecordedRequest.java
@@ -1,17 +1,15 @@
 package uk.gov.di.orchestration.sharedtest.httpstub;
 
+import org.eclipse.jetty.http.HttpURI;
+import org.eclipse.jetty.io.Content;
 import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.util.BufferUtil;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
 
 public class RecordedRequest {
     private final String requestURI;
-    private final Map<String, List<String>> headers;
     private final String querystring;
     private final String method;
     private final String url;
@@ -19,11 +17,11 @@ public class RecordedRequest {
 
     public RecordedRequest(Request request) {
         this.method = request.getMethod();
-        this.requestURI = request.getRequestURI();
-        this.querystring = request.getQueryString();
-        this.headers = getHeaders(request);
+        this.requestURI = request.getHttpURI().getPath();
+        this.querystring = request.getHttpURI().getQuery();
         this.entity = readEntity(request);
-        this.url = request.getRequestURL().toString();
+        HttpURI httpURI = HttpURI.build(request.getHttpURI()).query(null);
+        this.url = httpURI.asString();
     }
 
     public String getQuerystring() {
@@ -46,24 +44,14 @@ public class RecordedRequest {
         return url;
     }
 
-    public String getHeader(String name) {
-        List<String> values = headers.get(name.toLowerCase());
-        return values == null ? null : values.get(0);
-    }
-
     private static String readEntity(Request request) {
-        try (BufferedReader in = request.getReader()) {
-            return in.lines().collect(Collectors.joining());
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+        StringBuilder requestBody = new StringBuilder();
+        while (true) {
+            Content.Chunk chunk = request.read();
+            ByteBuffer buffer = chunk.getByteBuffer();
+            requestBody.append(BufferUtil.toString(buffer, StandardCharsets.UTF_8));
+            if (chunk.isLast()) break;
         }
-    }
-
-    private static Map<String, List<String>> getHeaders(Request request) {
-        return Collections.list(request.getHeaderNames()).stream()
-                .collect(
-                        Collectors.toUnmodifiableMap(
-                                String::toLowerCase,
-                                n -> List.copyOf(Collections.list(request.getHeaders(n)))));
+        return requestBody.toString();
     }
 }
