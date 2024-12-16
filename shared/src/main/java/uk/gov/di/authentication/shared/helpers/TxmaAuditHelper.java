88fcc381af05ece81c9e6c589202a65f3cd413b1
Ethan Mills <ethan.mills@digital.cabinet-office.gov.uk>
2024-12-16 18:58:06 +0000

    AUT-3924: Extract TXMA header method
    This will allow it to be reused more easily outside of the base frontend handler

---
+++ shared/src/main/java/uk/gov/di/authentication/shared/helpers/TxmaAuditHelper.java
@@ -0,0 +1,18 @@
+package uk.gov.di.authentication.shared.helpers;
+
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+
+import java.util.Optional;
+
+import static uk.gov.di.authentication.shared.helpers.RequestHeaderHelper.getOptionalHeaderValueFromHeaders;
+
+public class TxmaAuditHelper {
+    public static final String TXMA_AUDIT_ENCODED_HEADER = "txma-audit-encoded";
+
+    private TxmaAuditHelper() {}
+
+    public static Optional<String> getTxmaAuditEncodedHeader(APIGatewayProxyRequestEvent request) {
+        return getOptionalHeaderValueFromHeaders(
+                request.getHeaders(), TXMA_AUDIT_ENCODED_HEADER, false);
+    }
+}
