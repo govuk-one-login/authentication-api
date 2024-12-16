1a435e144075dc8a2752fea38e560728a4e1cd81
Gabriel.Swallow <gabriel.swallow@digital.cabinet-office.gov.uk>
2024-12-16 15:41:17 +0000

    ATO-1117: add AuthUserInfoClaims to orch-shared

---
+++ orchestration-shared/src/main/java/uk/gov/di/orchestration/shared/entity/AuthUserInfoClaims.java
@@ -0,0 +1,43 @@
+package uk.gov.di.orchestration.shared.entity;
+
+import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public enum AuthUserInfoClaims {
+    LEGACY_SUBJECT_ID("legacy_subject_id"),
+    PUBLIC_SUBJECT_ID("public_subject_id"),
+    LOCAL_ACCOUNT_ID("local_account_id"),
+    RP_PAIRWISE_ID("rp_pairwise_id"),
+    EMAIL("email"),
+    EMAIL_VERIFIED("email_verified"),
+    PHONE_NUMBER("phone_number"),
+    PHONE_VERIFIED("phone_number_verified"),
+    SALT("salt"),
+    VERIFIED_MFA_METHOD_TYPE("verified_mfa_method_type"),
+    CURRENT_CREDENTIAL_STRENGTH("current_credential_strength"),
+    NEW_ACCOUNT("new_account"),
+    UPLIFT_REQUIRED("uplift_required");
+
+    private final String value;
+
+    AuthUserInfoClaims(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public static Set<String> getAllValidClaims() {
+        return Arrays.stream(AuthUserInfoClaims.values())
+                .map(AuthUserInfoClaims::getValue)
+                .collect(Collectors.toSet());
+    }
+
+    public static boolean isValidClaim(String claim) {
+        return Arrays.stream(AuthUserInfoClaims.values())
+                .map(AuthUserInfoClaims::getValue)
+                .anyMatch(t -> t.equals(claim));
+    }
+}
---
+++ orchestration-shared/src/test/java/uk/gov/di/orchestration/shared/entity/AuthUserInfoClaimsTest.java
@@ -0,0 +1,59 @@
+package uk.gov.di.orchestration.shared.entity;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class AuthUserInfoClaimsTest {
+
+    static Stream<String> supportedClaims() {
+        return Stream.of(
+                "email_verified",
+                "salt",
+                "local_account_id",
+                "current_credential_strength",
+                "uplift_required",
+                "rp_pairwise_id",
+                "phone_number_verified",
+                "public_subject_id",
+                "legacy_subject_id",
+                "phone_number",
+                "verified_mfa_method_type",
+                "email",
+                "new_account");
+    }
+
+    static Stream<String> unsupportedClaims() {
+        return Stream.of("unsupported_claim", "another_unsupported_claim");
+    }
+
+    @Test
+    void shouldReturnCorrectNumberOfClaimsSupported() {
+        assertThat(AuthUserInfoClaims.getAllValidClaims().size(), equalTo(13));
+    }
+
+    @ParameterizedTest
+    @MethodSource("supportedClaims")
+    void shouldReturnNamesOfSupportedClaims(String supportedClaim) {
+        assertTrue(AuthUserInfoClaims.getAllValidClaims().contains(supportedClaim));
+    }
+
+    @ParameterizedTest
+    @MethodSource("supportedClaims")
+    void shouldReturnTrueForSupportedClaims(String claimName) {
+        assertTrue(AuthUserInfoClaims.isValidClaim(claimName));
+    }
+
+    @ParameterizedTest
+    @MethodSource("unsupportedClaims")
+    void shouldReturnFalseForUnsupportedClaims(String claimName) {
+        assertFalse(AuthUserInfoClaims.isValidClaim(claimName));
+    }
+}
