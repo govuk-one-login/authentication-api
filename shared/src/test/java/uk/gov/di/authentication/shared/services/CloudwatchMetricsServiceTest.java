33da8fd8bd0e90a20975e3ad77ef4f43bed569c1
Alex Wilson <alex.wilson@digital.cabinet-office.gov.uk>
2025-01-17 14:52:14 +0000

    BAU: Extract test configuration method
    Co-authored-by: Robin Harrison <robin.harrison@digital.cabinet-office.gov.uk>

--- shared/src/test/java/uk/gov/di/authentication/shared/services/CloudwatchMetricsServiceTest.java
+++ shared/src/test/java/uk/gov/di/authentication/shared/services/CloudwatchMetricsServiceTest.java
@@ -1,11 +1,9 @@
 package uk.gov.di.authentication.shared.services;
 
 import org.apache.commons.lang3.RandomStringUtils;
-import org.apache.commons.lang3.RandomUtils;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import software.amazon.cloudwatchlogs.emf.logger.MetricsLogger;
-import software.amazon.cloudwatchlogs.emf.model.DimensionSet;
 
 import java.util.Collections;
 import java.util.Map;
@@ -19,12 +17,7 @@ class CloudwatchMetricsServiceTest {
     void shouldAlwaysIncludeEnvironmentDimensionFromConfiguration() {
         var randomEnvironment = RandomStringUtils.secure().nextAlphanumeric(10);
 
-        var service = new CloudwatchMetricsService(new ConfigurationService() {
-            @Override
-            public String getEnvironment() {
-                return randomEnvironment;
-            }
-        });
+        var service = new CloudwatchMetricsService(configurationWithEnvironment(randomEnvironment));
 
         var dimensions = service.getDimensions(Collections.emptyMap());
 
@@ -36,12 +29,7 @@ class CloudwatchMetricsServiceTest {
     void shouldIncludeEnvironmentDimensionAndAllExtraDimensions() {
         var randomEnvironment = RandomStringUtils.secure().nextAlphanumeric(10);
 
-        var service = new CloudwatchMetricsService(new ConfigurationService() {
-            @Override
-            public String getEnvironment() {
-                return randomEnvironment;
-            }
-        });
+        var service = new CloudwatchMetricsService(configurationWithEnvironment(randomEnvironment));
 
         var dimensions = service.getDimensions(Map.of("Key1","Value1"));
 
@@ -54,16 +42,20 @@ class CloudwatchMetricsServiceTest {
     void shouldEmitMetricWithNamespace() {
         var metricsLogger = Mockito.mock(MetricsLogger.class);
 
-        var service = new CloudwatchMetricsService(new ConfigurationService() {
-            @Override
-            public String getEnvironment() {
-                return "test";
-            }
-        });
+        var service = new CloudwatchMetricsService(configurationWithEnvironment("test"));
 
         service.emitMetric("Metric", 1, Collections.emptyMap(), metricsLogger);
 
         Mockito.verify(metricsLogger).setNamespace("Authentication");
+    }
+
+    private static ConfigurationService configurationWithEnvironment(String test) {
+        return new ConfigurationService() {
+            @Override
+            public String getEnvironment() {
+                return test;
+            }
+        };
     }
 
 }
