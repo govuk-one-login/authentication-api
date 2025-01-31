24132855fa622346c5178d76b61c87260f97d13d
Benjamin Fernandes <benjamin.fernandes@digital.cabinet-office.gov.uk>
2025-01-31 14:46:44 +0000

    AUT-3884: Delete unused fields from AIS Intervention object

--- frontend-api/src/main/java/uk/gov/di/authentication/frontendapi/entity/Intervention.java
+++ frontend-api/src/main/java/uk/gov/di/authentication/frontendapi/entity/Intervention.java
@@ -3,10 +3,4 @@ package uk.gov.di.authentication.frontendapi.entity;
 import com.google.gson.annotations.Expose;
 import org.apache.logging.log4j.core.config.plugins.validation.constraints.Required;
 
-public record Intervention(
-        @Expose @Required String updatedAt,
-        @Expose @Required String appliedAt,
-        @Expose @Required String sentAt,
-        @Expose @Required String description,
-        @Expose @Required String reprovedIdentityAt,
-        @Expose @Required String resetPasswordAt) {}
+public record Intervention(@Expose @Required String appliedAt) {}
