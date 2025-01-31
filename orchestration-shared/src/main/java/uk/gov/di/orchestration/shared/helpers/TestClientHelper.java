3479be4fdac97b155f90d0b493451f209663ff5d
Craig Earl <craig.earl@digital.cabinet-office.gov.uk>
2025-01-31 17:25:40 +0000

    ATO-1395: Remove session id getter from ClientNotFoundException

--- orchestration-shared/src/main/java/uk/gov/di/orchestration/shared/helpers/TestClientHelper.java
+++ orchestration-shared/src/main/java/uk/gov/di/orchestration/shared/helpers/TestClientHelper.java
@@ -28,7 +28,7 @@ public class TestClientHelper {
         var clientRegistry =
                 userContext
                         .getClient()
-                        .orElseThrow(() -> new ClientNotFoundException(userContext.getSession()));
+                        .orElseThrow(() -> new ClientNotFoundException(userContext.getClientId()));
 
         var isTestClientWithAllowedEmail =
                 (clientRegistry.isTestClient()
