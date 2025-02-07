1dd9cee28bda3a3a92794bdf9b6769911d984e19
cargil <carly.gilson@digital.cabinet-office.gov.uk>
2025-02-07 16:48:27 +0000

    ATO-1413: Fix provisioning script This no longer works due to a change in the upstream script. Set some variables that are necessary for deployment

--- ci/stack-orchestration/provision_all.sh
+++ ci/stack-orchestration/provision_all.sh
@@ -2,19 +2,23 @@
 ENVIRONMENT=${1}
 
 PROVISION_COMMAND="../../../devplatform-deploy/stack-orchestration-tool/provisioner.sh"
+TAGS_FILE="$(pwd)/configuration/${ENVIRONMENT}/tags.json"
 
 export AUTO_APPLY_CHANGESET=true
 export SKIP_AWS_AUTHENTICATION=true
 export AWS_PAGER=""
+export TAGS_FILE
 
 # Provision dependencies
-for dir in configuration/"$ENVIRONMENT"/*/; do
+for dir in configuration/"${ENVIRONMENT}"/*/; do
   # shellcheck disable=SC2086
-  STACK=$(basename "$dir")
-  if [[ $STACK != "$ENVIRONMENT-orch-be-pipeline" && -f configuration/$ENVIRONMENT/$STACK/parameters.json ]]; then
-    $PROVISION_COMMAND "$ENVIRONMENT" "$STACK" "$STACK" LATEST &
+  STACK=$(basename "${dir}")
+  if [[ ${STACK} != "${ENVIRONMENT}-orch-be-pipeline" && -f configuration/${ENVIRONMENT}/${STACK}/parameters.json ]]; then
+    PARAMETERS_FILE="$(pwd)/configuration/${ENVIRONMENT}/${STACK}/parameters.json"
+    export PARAMETERS_FILE
+    ${PROVISION_COMMAND} "${ENVIRONMENT}" "${STACK}" "${STACK}" LATEST &
   fi
 done
 
 ### Provision secure pipelines
-$PROVISION_COMMAND "$ENVIRONMENT" "$ENVIRONMENT-orch-be-pipeline" sam-deploy-pipeline LATEST
+${PROVISION_COMMAND} "${ENVIRONMENT}" "${ENVIRONMENT}-orch-be-pipeline" sam-deploy-pipeline LATEST
