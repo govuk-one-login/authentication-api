ab6f21b55cec15fb2b34fab24cb661931d82b640
BeckaL <becka.lelew@digital.cabinet-office.gov.uk>
2025-02-11 18:39:17 +0000

    BAU: Correct invalid json in integration test
    This is now causing test failures as we need the json to be valid in order to emit
    audit events with the relevant metadata

--- integration-tests/src/test/java/uk/gov/di/authentication/api/ReverificationResultHandlerIntegrationTest.java
+++ integration-tests/src/test/java/uk/gov/di/authentication/api/ReverificationResultHandlerIntegrationTest.java
@@ -65,7 +65,7 @@ class ReverificationResultHandlerIntegrationTest extends ApiGatewayHandlerIntegr
             """
             {
                 "sub": "urn:uuid:f81d4fae-7dec-11d0-a765-00a0c91e6bf6",
-                "success": true"
+                "success": true
             }
             """;
 
