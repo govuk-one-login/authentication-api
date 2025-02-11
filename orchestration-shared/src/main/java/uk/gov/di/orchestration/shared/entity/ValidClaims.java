bb57a042b116a9cac170f6553d433e1e8ae928fa
Ryan Andrews <ryan.andrews@digital.cabinet-office.gov.uk>
2025-02-11 13:18:01 +0000

    ATO-1411: Remove social security as a valid claim

--- orchestration-shared/src/main/java/uk/gov/di/orchestration/shared/entity/ValidClaims.java
+++ orchestration-shared/src/main/java/uk/gov/di/orchestration/shared/entity/ValidClaims.java
@@ -12,7 +12,6 @@ public enum ValidClaims {
     ADDRESS("https://vocab.account.gov.uk/v1/address"),
     PASSPORT("https://vocab.account.gov.uk/v1/passport"),
     DRIVING_PERMIT("https://vocab.account.gov.uk/v1/drivingPermit"),
-    SOCIAL_SECURITY_RECORD("https://vocab.account.gov.uk/v1/socialSecurityRecord"),
     CORE_IDENTITY_JWT("https://vocab.account.gov.uk/v1/coreIdentityJWT"),
     RETURN_CODE("https://vocab.account.gov.uk/v1/returnCode"),
     INHERITED_IDENTITY_JWT("https://vocab.account.gov.uk/v1/inheritedIdentityJWT");
