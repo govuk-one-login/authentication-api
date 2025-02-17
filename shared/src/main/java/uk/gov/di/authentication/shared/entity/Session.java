81452530b570fe4f66be11119445d14c5b9c1e27
Craig Earl <craig.earl@digital.cabinet-office.gov.uk>
2025-02-17 15:17:28 +0000

    ATO-1406: Remove setter for session id in auth shared session

--- shared/src/main/java/uk/gov/di/authentication/shared/entity/Session.java
+++ shared/src/main/java/uk/gov/di/authentication/shared/entity/Session.java
@@ -52,11 +52,6 @@ public class Session {
         initializeCodeRequestMap();
     }
 
-    public Session setSessionId(String sessionId) {
-        this.sessionId = sessionId;
-        return this;
-    }
-
     public List<String> getClientSessions() {
         return clientSessions;
     }
