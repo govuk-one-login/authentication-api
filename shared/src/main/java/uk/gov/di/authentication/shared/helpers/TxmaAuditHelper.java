49840c42ef70bd39a9028b75be245fb0bc9f3ca8
Andrew Moores <147620897+andrew-moores@users.noreply.github.com>
2025-02-17 10:08:57 +0000

    Revert "AUT-3974: Include rpPairwiseId and journeyType in AUTH_REVERIFY_AUTHORISATION_REQUESTED extensions"

--- shared/src/main/java/uk/gov/di/authentication/shared/helpers/TxmaAuditHelper.java
+++ shared/src/main/java/uk/gov/di/authentication/shared/helpers/TxmaAuditHelper.java
@@ -1,19 +1,12 @@
 package uk.gov.di.authentication.shared.helpers;
 
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import uk.gov.di.authentication.shared.services.AuditService;
-import uk.gov.di.authentication.shared.services.AuthenticationService;
-import uk.gov.di.authentication.shared.services.ConfigurationService;
-import uk.gov.di.authentication.shared.state.UserContext;
 
 import java.util.Optional;
 
 import static uk.gov.di.authentication.shared.helpers.RequestHeaderHelper.getOptionalHeaderValueFromHeaders;
 
 public class TxmaAuditHelper {
-    private static final Logger LOG = LogManager.getLogger(TxmaAuditHelper.class);
     public static final String TXMA_AUDIT_ENCODED_HEADER = "txma-audit-encoded";
 
     private TxmaAuditHelper() {}
@@ -21,27 +14,5 @@ public class TxmaAuditHelper {
     public static Optional<String> getTxmaAuditEncodedHeader(APIGatewayProxyRequestEvent request) {
         return getOptionalHeaderValueFromHeaders(
                 request.getHeaders(), TXMA_AUDIT_ENCODED_HEADER, false);
-    }
-
-    public static String getRpPairwiseId(
-            AuthenticationService authenticationService,
-            ConfigurationService configurationService,
-            UserContext userContext) {
-        LOG.info("Calculating RP pairwise identifier");
-
-        var userProfile = userContext.getUserProfile();
-        var client = userContext.getClient();
-
-        if (userProfile.isEmpty() || client.isEmpty()) {
-            LOG.warn("Returning empty RP pairwise identifier - no user profile or client found");
-            return AuditService.UNKNOWN;
-        }
-
-        return ClientSubjectHelper.getSubject(
-                        userProfile.get(),
-                        client.get(),
-                        authenticationService,
-                        configurationService.getInternalSectorUri())
-                .toString();
     }
 }
