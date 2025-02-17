8521f9447e2e4a1c31017b59134ca431f0835634
Louisa Nash <louisa.nash@digital.cabinet-office.gov.uk>
2025-02-17 15:23:22 +0000

    ATO-1456: refactor AuthSessionItem DynamoDBAttribute

--- shared/src/main/java/uk/gov/di/authentication/shared/entity/AuthSessionItem.java
+++ shared/src/main/java/uk/gov/di/authentication/shared/entity/AuthSessionItem.java
@@ -14,6 +14,7 @@ public class AuthSessionItem {
     public static final String ATTRIBUTE_INTERNAL_COMMON_SUBJECT_ID = "InternalCommonSubjectId";
     public static final String ATTRIBUTE_UPLIFT_REQUIRED = "UpliftRequired";
     public static final String ATTRIBUTE_EMAIL = "Email";
+    public static final String ATTRIBUTE_TTL = "ttl";
 
     public enum AccountState {
         NEW,
@@ -76,7 +77,7 @@ public class AuthSessionItem {
         return this;
     }
 
-    @DynamoDbAttribute("ttl")
+    @DynamoDbAttribute(ATTRIBUTE_TTL)
     public long getTimeToLive() {
         return timeToLive;
     }
