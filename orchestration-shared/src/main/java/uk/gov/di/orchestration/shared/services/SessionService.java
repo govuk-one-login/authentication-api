b8a26b0378d3f143fb8558cd69542a3079bde60b
Craig Earl <craig.earl@digital.cabinet-office.gov.uk>
2025-02-17 15:21:06 +0000

    ATO-1406: Remove setter for session id in orch shared session

--- orchestration-shared/src/main/java/uk/gov/di/orchestration/shared/services/SessionService.java
+++ orchestration-shared/src/main/java/uk/gov/di/orchestration/shared/services/SessionService.java
@@ -36,7 +36,6 @@ public class SessionService {
 
     public Session copySessionForMaxAge(Session previousSession, String newSessionId) {
         var copiedSession = new Session(previousSession);
-        copiedSession.setSessionId(newSessionId);
         copiedSession.setAuthenticated(false).setCurrentCredentialStrength(null);
         copiedSession.resetClientSessions();
         return copiedSession;
@@ -64,7 +63,6 @@ public class SessionService {
     public Session updateWithNewSessionId(
             Session session, String oldSessionId, String newSessionId) {
         try {
-            session.setSessionId(newSessionId);
             session.resetProcessingIdentityAttempts();
             storeOrUpdateSession(session, oldSessionId, newSessionId);
             redisConnectionService.deleteValue(oldSessionId);
