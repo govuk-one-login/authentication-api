cda15c25141f4ce9fa8dcdedbe7acce209717597
Louisa Nash <louisa.nash@digital.cabinet-office.gov.uk>
2025-02-21 15:26:48 +0000

    ATO-1479: read email address from AuthSessionItem in ResetPasswordHandler and ResetPasswordRequestHandler

--- frontend-api/src/main/java/uk/gov/di/authentication/frontendapi/lambda/ResetPasswordHandler.java
+++ frontend-api/src/main/java/uk/gov/di/authentication/frontendapi/lambda/ResetPasswordHandler.java
@@ -155,7 +155,7 @@ public class ResetPasswordHandler extends BaseFrontendHandler<ResetPasswordCompl
             }
             var userCredentials =
                     authenticationService.getUserCredentialsFromEmail(
-                            userContext.getSession().getEmailAddress());
+                            userContext.getAuthSession().getEmailAddress());
 
             if (Objects.nonNull(userCredentials.getPassword())) {
                 if (verifyPassword(userCredentials.getPassword(), request.password())) {
@@ -167,7 +167,7 @@ public class ResetPasswordHandler extends BaseFrontendHandler<ResetPasswordCompl
             authenticationService.updatePassword(userCredentials.getEmail(), request.password());
             var userProfile =
                     authenticationService.getUserProfileByEmail(
-                            userContext.getSession().getEmailAddress());
+                            userContext.getAuthSession().getEmailAddress());
 
             LOG.info("Calculating internal common subject identifier");
             var internalCommonSubjectId =
