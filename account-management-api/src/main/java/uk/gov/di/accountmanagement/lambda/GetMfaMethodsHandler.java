17ee093ff4450522c1471ba372dd6d40fedb7425
BeckaL <becka.lelew@digital.cabinet-office.gov.uk>
2025-02-25 18:30:46 +0000

    AUT-3848: Return 400 if new handler invoked in integration or production
    We do not yet want to enable this api in integration or production, to allow for testing. At a later point, we can put this into config but for now we're doing it at the code level

--- account-management-api/src/main/java/uk/gov/di/accountmanagement/lambda/GetMfaMethodsHandler.java
+++ account-management-api/src/main/java/uk/gov/di/accountmanagement/lambda/GetMfaMethodsHandler.java
@@ -4,16 +4,56 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import uk.gov.di.authentication.shared.entity.ErrorResponse;
+import uk.gov.di.authentication.shared.helpers.RequestHeaderHelper;
+import uk.gov.di.authentication.shared.services.ConfigurationService;
 
+import java.util.List;
+import java.util.Map;
+
+import static uk.gov.di.authentication.shared.domain.RequestHeaders.SESSION_ID_HEADER;
+import static uk.gov.di.authentication.shared.helpers.ApiGatewayResponseHelper.generateApiGatewayProxyErrorResponse;
 import static uk.gov.di.authentication.shared.helpers.ApiGatewayResponseHelper.generateApiGatewayProxyResponse;
+import static uk.gov.di.authentication.shared.helpers.LogLineHelper.attachSessionIdToLogs;
 
 public class GetMfaMethodsHandler
         implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
-    public GetMfaMethodsHandler() {}
+    private final ConfigurationService configurationService;
+
+    private static final String PRODUCTION = "production";
+    private static final String INTEGRATION = "integration";
+
+    private static final Logger LOG = LogManager.getLogger(GetMfaMethodsHandler.class);
+
+    public GetMfaMethodsHandler() {
+        this(ConfigurationService.getInstance());
+    }
+
+    public GetMfaMethodsHandler(ConfigurationService configurationService) {
+        this.configurationService = configurationService;
+    }
 
     @Override
     public APIGatewayProxyResponseEvent handleRequest(
             APIGatewayProxyRequestEvent apiGatewayProxyRequestEvent, Context context) {
+        addSessionIdToLogs(apiGatewayProxyRequestEvent);
+
+        var disabledEnvironments = List.of(PRODUCTION, INTEGRATION);
+        if (disabledEnvironments.contains(configurationService.getEnvironment())) {
+            LOG.error(
+                    "Request to create MFA method in {} environment but feature is switched off.",
+                    configurationService.getEnvironment());
+            return generateApiGatewayProxyErrorResponse(400, ErrorResponse.ERROR_1063);
+        }
+
         return generateApiGatewayProxyResponse(200, "{\"hello\": \"world\"}");
+    }
+
+    private void addSessionIdToLogs(APIGatewayProxyRequestEvent input) {
+        Map<String, String> headers = input.getHeaders();
+        String sessionId = RequestHeaderHelper.getHeaderValueOrElse(headers, SESSION_ID_HEADER, "");
+        attachSessionIdToLogs(sessionId);
     }
 }
--- account-management-api/src/test/java/uk/gov/di/accountmanagement/lambda/GetMfaMethodsHandlerTest.java
+++ account-management-api/src/test/java/uk/gov/di/accountmanagement/lambda/GetMfaMethodsHandlerTest.java
@@ -4,28 +4,35 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import uk.gov.di.authentication.shared.services.ConfigurationService;
 
 import java.util.Map;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 import static uk.gov.di.accountmanagement.helpers.CommonTestVariables.VALID_HEADERS;
 import static uk.gov.di.authentication.sharedtest.matchers.APIGatewayProxyResponseEventMatcher.hasStatus;
 
 class GetMfaMethodsHandlerTest {
     private final Context context = mock(Context.class);
     private static final String SUBJECT_ID = "some-subject-id";
+    private static final ConfigurationService configurationService =
+            mock(ConfigurationService.class);
 
     private GetMfaMethodsHandler handler;
 
     @BeforeEach
     void setUp() {
-        handler = new GetMfaMethodsHandler();
+        handler = new GetMfaMethodsHandler(configurationService);
     }
 
     @Test
     void shouldReturn200AndDummyResponse() {
+        when(configurationService.getEnvironment()).thenReturn("test-environment");
         var event =
                 new APIGatewayProxyRequestEvent()
                         .withPathParameters((Map.of("publicSubjectId", SUBJECT_ID)))
@@ -35,5 +42,19 @@ class GetMfaMethodsHandlerTest {
 
         assertThat(result, hasStatus(200));
         assertEquals("{\"hello\": \"world\"}", result.getBody());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"production", "integration"})
+    void shouldReturn400IfRequestIsMadeInProductionOrIntegration(String environment) {
+        when(configurationService.getEnvironment()).thenReturn(environment);
+        var event =
+                new APIGatewayProxyRequestEvent()
+                        .withPathParameters((Map.of("publicSubjectId", SUBJECT_ID)))
+                        .withHeaders(VALID_HEADERS);
+
+        var result = handler.handleRequest(event, context);
+
+        assertThat(result, hasStatus(400));
     }
 }
