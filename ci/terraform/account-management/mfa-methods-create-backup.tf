4115c496b7429340868c3e33cfa92c843efdc14a
Andrew Moores <andrew.moores@digital.cabinet-office.gov.uk>
2025-02-25 12:11:47 +0000

    AUT-3485: Add deployment configuration for MFA MM
    Updated the name of the terraform module to better reflect what the
    POST method achieves.  Added config to output the rendered version of
    the OpenAPI spec.  Matching updates in the lambda terraform.

--- ci/terraform/account-management/mfa-methods-create-backup.tf
+++ ci/terraform/account-management/mfa-methods-create-backup.tf
@@ -1,35 +1,34 @@
-module "account_management_api_mfa_methods_role" {
+module "account_management_api_mfa_methods_create_backup_role" {
   source      = "../modules/lambda-role"
   environment = var.environment
-  role_name   = "account_management_api_mfa_methods_role"
+  role_name   = "account_management_api_mfa_methods_create_backup_role"
   vpc_arn     = local.vpc_arn
 
   policies_to_attach = [
     aws_iam_policy.dynamo_am_user_read_access_policy.arn,
     aws_iam_policy.dynamo_am_user_write_access_policy.arn,
-    aws_iam_policy.dynamo_am_user_delete_access_policy.arn,
   ]
   extra_tags = {
-    Service = "mfa-methods"
+    Service = "mfa-methods-create-backup"
   }
 }
 
-module "mfa-methods" {
+module "mfa-methods-create-backup" {
   source = "../modules/endpoint-lambda"
 
-  endpoint_name   = "mfa-methods"
+  endpoint_name = "mfa-methods-create-backup"
   handler_environment_variables = {
-    ENVIRONMENT                 = var.environment
-    REDIS_KEY                   = local.redis_key
-    TXMA_AUDIT_QUEUE_URL        = module.account_management_txma_audit.queue_url
-    INTERNAl_SECTOR_URI         = var.internal_sector_uri
+    ENVIRONMENT          = var.environment
+    REDIS_KEY            = local.redis_key
+    TXMA_AUDIT_QUEUE_URL = module.account_management_txma_audit.queue_url
+    INTERNAl_SECTOR_URI  = var.internal_sector_uri
   }
-  handler_function_name = "uk.gov.di.accountmanagement.lambda.MfaMethodsHandler::handleRequest"
+  handler_function_name = "uk.gov.di.accountmanagement.lambda.CreateBackupMFAMethod::handleRequest"
 
-  memory_size                 = lookup(var.performance_tuning, "mfa-methods", local.default_performance_parameters).memory
-  provisioned_concurrency     = lookup(var.performance_tuning, "mfa-methods", local.default_performance_parameters).concurrency
-  max_provisioned_concurrency = lookup(var.performance_tuning, "mfa-methods", local.default_performance_parameters).max_concurrency
-  scaling_trigger             = lookup(var.performance_tuning, "mfa-methods", local.default_performance_parameters).scaling_trigger
+  memory_size                 = lookup(var.performance_tuning, "mfa-methods-create-backup", local.default_performance_parameters).memory
+  provisioned_concurrency     = lookup(var.performance_tuning, "mfa-methods-create-backup", local.default_performance_parameters).concurrency
+  max_provisioned_concurrency = lookup(var.performance_tuning, "mfa-methods-create-backup", local.default_performance_parameters).max_concurrency
+  scaling_trigger             = lookup(var.performance_tuning, "mfa-methods-create-backup", local.default_performance_parameters).scaling_trigger
 
   source_bucket           = aws_s3_bucket.source_bucket.bucket
   lambda_zip_file         = aws_s3_object.account_management_api_release_zip.key
@@ -42,7 +41,7 @@ module "mfa-methods" {
   ]
   subnet_id                              = local.private_subnet_ids
   environment                            = var.environment
-  lambda_role_arn                        = module.account_management_api_mfa_methods_role.arn
+  lambda_role_arn                        = module.account_management_api_mfa_methods_create_backup_role.arn
   logging_endpoint_arns                  = var.logging_endpoint_arns
   cloudwatch_key_arn                     = data.terraform_remote_state.shared.outputs.cloudwatch_encryption_key_arn
   cloudwatch_log_retention               = var.cloudwatch_log_retention
@@ -52,5 +51,5 @@ module "mfa-methods" {
   slack_event_topic_arn = local.slack_event_sns_topic_arn
   dynatrace_secret      = local.dynatrace_secret
 
-  depends_on = [module.account_management_api_mfa_methods_role]
+  depends_on = [module.account_management_api_mfa_methods_create_backup_role]
 }
