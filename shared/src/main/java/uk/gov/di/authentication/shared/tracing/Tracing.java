6524124b07fef619dc8575010377ab034f22e63b
Tom Whitwell <tom.whitwell@digital.cabinet-office.gov.uk>
2025-02-26 14:13:28 +0000

    wip: prevent tracing when not in a handler

--- shared/src/main/java/uk/gov/di/authentication/shared/tracing/Tracing.java
+++ shared/src/main/java/uk/gov/di/authentication/shared/tracing/Tracing.java
@@ -4,6 +4,11 @@ import uk.gov.di.authentication.shared.annotations.ExcludeFromGeneratedCoverageR
 
 @ExcludeFromGeneratedCoverageReport
 public class Tracing {
+    private static final String traceHeaderProperty = "com.amazonaws.xray.traceHeader";
     public static final Boolean TRACING_ENABLED =
             Boolean.valueOf(System.getenv().getOrDefault("TRACING_ENABLED", "true"));
+
+    public static Boolean isTracingAllowed() {
+        return System.getProperty(traceHeaderProperty) != null && TRACING_ENABLED;
+    }
 }
