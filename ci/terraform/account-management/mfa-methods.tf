6b127ed6e62088a65cbf6a30e3ee4a3132a46815
Andrew Moores <andrew.moores@digital.cabinet-office.gov.uk>
2025-02-28 17:05:14 +0000

    Revert "AUT-3485: Add deployment configuration for MFA MM"
    This reverts commit 4115c496b7429340868c3e33cfa92c843efdc14a.

--- ci/terraform/account-management/mfa-methods.tf
+++ ci/terraform/account-management/mfa-methods.tf
@@ -1,34 +1,35 @@
-module "account_management_api_mfa_methods_create_backup_role" {
+module "account_management_api_mfa_methods_role" {
   source      = "../modules/lambda-role"
   environment = var.environment
-  role_name   = "account_management_api_mfa_methods_create_backup_role"
+  role_name   = "account_management_api_mfa_methods_role"
   vpc_arn     = local.vpc_arn
 
   policies_to_attach = [
     aws_iam_policy.dynamo_am_user_read_access_policy.arn,
     aws_iam_policy.dynamo_am_user_write_access_policy.arn,
+    aws_iam_policy.dynamo_am_user_delete_access_policy.arn,
   ]
   extra_tags = {
-    Service = "mfa-methods-create-backup"
+    Service = "mfa-methods"
   }
 }
 
-module "mfa-methods-create-backup" {
+module "mfa-methods" {
   source = "../modules/endpoint-lambda"
 
-  endpoint_name = "mfa-methods-create-backup"
+  endpoint_name   = "mfa-methods"
   handler_environment_variables = {
-    ENVIRONMENT          = var.environment
-    REDIS_KEY            = local.redis_key
-    TXMA_AUDIT_QUEUE_URL = module.account_management_txma_audit.queue_url
-    INTERNAl_SECTOR_URI  = var.internal_sector_uri
+    ENVIRONMENT                 = var.environment
+    REDIS_KEY                   = local.redis_key
+    TXMA_AUDIT_QUEUE_URL        = module.account_management_txma_audit.queue_url
+    INTERNAl_SECTOR_URI         = var.internal_sector_uri
   }
-  handler_function_name = "uk.gov.di.accountmanagement.lambda.CreateBackupMFAMethod::handleRequest"
+  handler_function_name = "uk.gov.di.accountmanagement.lambda.MfaMethodsHandler::handleRequest"
 
-  memory_size                 = lookup(var.performance_tuning, "mfa-methods-create-backup", local.default_performance_parameters).memory
-  provisioned_concurrency     = lookup(var.performance_tuning, "mfa-methods-create-backup", local.default_performance_parameters).concurrency
-  max_provisioned_concurrency = lookup(var.performance_tuning, "mfa-methods-create-backup", local.default_performance_parameters).max_concurrency
-  scaling_trigger             = lookup(var.performance_tuning, "mfa-methods-create-backup", local.default_performance_parameters).scaling_trigger
+  memory_size                 = lookup(var.performance_tuning, "mfa-methods", local.default_performance_parameters).memory
+  provisioned_concurrency     = lookup(var.performance_tuning, "mfa-methods", local.default_performance_parameters).concurrency
+  max_provisioned_concurrency = lookup(var.performance_tuning, "mfa-methods", local.default_performance_parameters).max_concurrency
+  scaling_trigger             = lookup(var.performance_tuning, "mfa-methods", local.default_performance_parameters).scaling_trigger
 
   source_bucket           = aws_s3_bucket.source_bucket.bucket
   lambda_zip_file         = aws_s3_object.account_management_api_release_zip.key
@@ -41,7 +42,7 @@ module "mfa-methods-create-backup" {
   ]
   subnet_id                              = local.private_subnet_ids
   environment                            = var.environment
-  lambda_role_arn                        = module.account_management_api_mfa_methods_create_backup_role.arn
+  lambda_role_arn                        = module.account_management_api_mfa_methods_role.arn
   logging_endpoint_arns                  = var.logging_endpoint_arns
   cloudwatch_key_arn                     = data.terraform_remote_state.shared.outputs.cloudwatch_encryption_key_arn
   cloudwatch_log_retention               = var.cloudwatch_log_retention
@@ -51,5 +52,5 @@ module "mfa-methods-create-backup" {
   slack_event_topic_arn = local.slack_event_sns_topic_arn
   dynatrace_secret      = local.dynatrace_secret
 
-  depends_on = [module.account_management_api_mfa_methods_create_backup_role]
+  depends_on = [module.account_management_api_mfa_methods_role]
 }
