f4d62ea557121712ddbf315fe6f3ca7bec39a3f9
BeckaL <25515510+BeckaL@users.noreply.github.com>
2025-03-10 14:12:00 +0000

    AUT-3487: Add infrastructure for delete endpoint

--- ci/terraform/account-management/api-gateway-method-management.tf
+++ ci/terraform/account-management/api-gateway-method-management.tf
@@ -10,6 +10,7 @@ locals {
     update-phone-number   = module.update_phone_number
     mfa-methods-retrieve  = module.mfa-methods-retrieve
     mfa-methods-create    = module.mfa-methods-create
+    mfa-methods-delete    = module.mfa-methods-delete
   }
   openapi_spec = templatefile(
     "${path.module}/${var.openapi_spec_filename}",
