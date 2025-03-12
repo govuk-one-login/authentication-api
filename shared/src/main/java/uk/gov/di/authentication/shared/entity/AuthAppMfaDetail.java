804c069b3848c3dc753c6119f82c0744d915c367
Aidan Comer <alhcomer@hotmail.co.uk>
2025-03-12 13:58:01 +0000

    BAU: Fix deserialization issue in API Gateway Test
    - When testing in API Gateway, the phone number SmsMfaDetail response for an SMS request was being returned as null
    - Added SerializedName field for MfaDetail implementations for consistency and ensuring specified serialization and deserialization
    - Added useCamelCase = true in objectMapper.readValue method which is used to deserialize json (using gson)
    - - This changes field naming policy of gson used from LOWER_CASE_WITH_UNDERSCORES to IDENTITY
    - - IDENTITY causes serialization and deserialization to occur without making changes to fields

--- shared/src/main/java/uk/gov/di/authentication/shared/entity/AuthAppMfaDetail.java
+++ shared/src/main/java/uk/gov/di/authentication/shared/entity/AuthAppMfaDetail.java
@@ -1,8 +1,10 @@
 package uk.gov.di.authentication.shared.entity;
 
 import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
 import uk.gov.di.authentication.shared.validation.Required;
 
 public record AuthAppMfaDetail(
-        @Expose @Required MFAMethodType mfaMethodType, @Expose @Required String credential)
+        @Expose @Required @SerializedName("mfaMethodType") MFAMethodType mfaMethodType,
+        @Expose @Required @SerializedName("credential") String credential)
         implements MfaDetail {}
