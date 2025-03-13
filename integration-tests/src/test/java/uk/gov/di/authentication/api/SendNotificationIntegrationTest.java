ccdc56533ac9a6d44d8305733f5d0825468c70ee
Ryan Andrews <ryan.andrews@digital.cabinet-office.gov.uk>
2025-03-13 16:50:34 +0000

    ATO-1511: Increment and reset in send notification handler
    Matches the same method calls on the shared session

--- integration-tests/src/test/java/uk/gov/di/authentication/api/SendNotificationIntegrationTest.java
+++ integration-tests/src/test/java/uk/gov/di/authentication/api/SendNotificationIntegrationTest.java
@@ -9,18 +9,22 @@ import uk.gov.di.authentication.shared.entity.JourneyType;
 import uk.gov.di.authentication.shared.entity.NotificationType;
 import uk.gov.di.authentication.shared.serialization.Json;
 import uk.gov.di.authentication.sharedtest.basetest.ApiGatewayHandlerIntegrationTest;
+import uk.gov.di.authentication.sharedtest.extensions.AuthSessionExtension;
 
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 import static uk.gov.di.authentication.sharedtest.helper.AuditAssertionsHelper.assertTxmaAuditEventsReceived;
 import static uk.gov.di.authentication.sharedtest.matchers.APIGatewayProxyResponseEventMatcher.hasStatus;
 
 class SendNotificationIntegrationTest extends ApiGatewayHandlerIntegrationTest {
     private static final String USER_EMAIL = "test@email.com";
     private String SESSION_ID;
+
+    private static final AuthSessionExtension authSessionExtension = new AuthSessionExtension();
 
     @BeforeEach
     void setup() throws Json.JsonException {
@@ -29,11 +33,12 @@ class SendNotificationIntegrationTest extends ApiGatewayHandlerIntegrationTest {
                 new SendNotificationHandler(
                         TXMA_ENABLED_CONFIGURATION_SERVICE, redisConnectionService);
         SESSION_ID = redis.createUnauthenticatedSessionWithEmail(USER_EMAIL);
-        authSessionStore.addSession(SESSION_ID);
+        authSessionExtension.addSession(SESSION_ID);
     }
 
     @Test
-    void shouldCallSendNotificationEndpointAndPlaceSuccessMessageOnAuditQueueWhenSuccessful() {
+    void shouldCallSendNotificationEndpointAndPlaceSuccessMessageOnAuditQueueWhenSuccessful()
+            throws Json.JsonException {
         var response =
                 makeRequest(
                         Optional.of(
@@ -45,6 +50,19 @@ class SendNotificationIntegrationTest extends ApiGatewayHandlerIntegrationTest {
                         Map.of());
 
         assertThat(response, hasStatus(204));
+        var session = redis.getSession(SESSION_ID);
+        var authSession = authSessionExtension.getSession(SESSION_ID).orElseThrow();
+        assertThat(
+                session.getCodeRequestCount(
+                        NotificationType.VERIFY_CHANGE_HOW_GET_SECURITY_CODES,
+                        JourneyType.ACCOUNT_RECOVERY),
+                equalTo(1));
+
+        assertThat(
+                authSession.getCodeRequestCount(
+                        NotificationType.VERIFY_CHANGE_HOW_GET_SECURITY_CODES,
+                        JourneyType.ACCOUNT_RECOVERY),
+                equalTo(1));
         assertTxmaAuditEventsReceived(
                 txmaAuditQueue,
                 List.of(FrontendAuditableEvent.AUTH_ACCOUNT_RECOVERY_EMAIL_CODE_SENT));
