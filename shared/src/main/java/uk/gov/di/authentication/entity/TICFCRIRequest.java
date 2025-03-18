3964dacc6eb96b3a134bd6e035f009854a7e6a0c
Matt Votsikas McLean <matt.votsikas-mclean@digital.cabinet-office.gov.uk>
2025-03-18 09:41:32 +0000

    AUT-2912: Add overrides to TICFCRIRequest
    SonarCloud is reporting that because a String[] was added as property that we should provide overrides of `equals`, `hashCode` and `toString`.
    
    This is because the default methods do not take into account the values of the array properly.

--- shared/src/main/java/uk/gov/di/authentication/entity/TICFCRIRequest.java
+++ shared/src/main/java/uk/gov/di/authentication/entity/TICFCRIRequest.java
@@ -8,7 +8,9 @@ import uk.gov.di.authentication.shared.entity.AuthSessionItem.ResetPasswordState
 import uk.gov.di.authentication.shared.entity.mfa.MFAMethodType;
 import uk.gov.di.authentication.shared.validation.Required;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 public record TICFCRIRequest(
         @Expose String sub,
@@ -54,5 +56,56 @@ public record TICFCRIRequest(
                 passwordReset ? "Y" : null,
                 mfaReset ? "Y" : null,
                 sanitisedMfaMethodType != null ? new String[] {sanitisedMfaMethodType} : null);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        TICFCRIRequest comparison = (TICFCRIRequest) o;
+
+        return Objects.equals(sub, comparison.sub)
+                && vtr == comparison.vtr
+                && Objects.equals(govukSigninJourneyId, comparison.govukSigninJourneyId)
+                && Objects.equals(authenticated, comparison.authenticated)
+                && Objects.equals(initialRegistration, comparison.initialRegistration)
+                && Objects.equals(passwordReset, comparison.passwordReset)
+                && Objects.equals(mfaReset, comparison.mfaReset)
+                && Arrays.equals(mfaMethod, comparison.mfaMethod);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(sub);
+        result = 31 * result * Objects.hash(vtr);
+        result = 31 * result * Objects.hash(govukSigninJourneyId);
+        result = 31 * result * Objects.hash(authenticated);
+        result = 31 * result * Objects.hash(initialRegistration);
+        result = 31 * result * Objects.hash(passwordReset);
+        result = 31 * result * Objects.hash(mfaReset);
+        result = 31 * result * Arrays.hashCode(mfaMethod);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "TICFCRIRequest{"
+                + "sub='"
+                + sub
+                + "', vtr='"
+                + vtr
+                + "', govukSigninJourneyId='"
+                + govukSigninJourneyId
+                + "', authenticated='"
+                + authenticated
+                + "', initialRegistration='"
+                + initialRegistration
+                + "', passwordReset='"
+                + passwordReset
+                + "', mfaReset='"
+                + mfaReset
+                + "', mfaMethod='"
+                + Arrays.toString(mfaMethod)
+                + "'}";
     }
 }
