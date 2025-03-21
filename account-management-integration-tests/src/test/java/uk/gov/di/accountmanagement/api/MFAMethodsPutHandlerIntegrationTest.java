9849d5dbc6ba94003390a5ed2f6195abc716d0a8
Aidan Comer <alhcomer@hotmail.co.uk>
2025-03-21 13:57:56 +0000

    AUT-4199: Add principal validation to MFAMethodsPutHandler

---
+++ account-management-integration-tests/src/test/java/uk/gov/di/accountmanagement/api/MFAMethodsPutHandlerIntegrationTest.java
@@ -0,0 +1,72 @@
+package uk.gov.di.accountmanagement.api;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import uk.gov.di.accountmanagement.lambda.MFAMethodsPutHandler;
+import uk.gov.di.authentication.shared.entity.ErrorResponse;
+import uk.gov.di.authentication.shared.helpers.ClientSubjectHelper;
+import uk.gov.di.authentication.shared.services.ConfigurationService;
+import uk.gov.di.authentication.sharedtest.basetest.ApiGatewayHandlerIntegrationTest;
+
+import java.util.Collections;
+import java.util.Map;
+import java.util.Optional;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static uk.gov.di.authentication.sharedtest.matchers.APIGatewayProxyResponseEventMatcher.hasJsonBody;
+
+class MFAMethodsPutHandlerIntegrationTest extends ApiGatewayHandlerIntegrationTest {
+    private static final String EMAIL = "joe.bloggs+3@digital.cabinet-office.gov.uk";
+    private static final String PASSWORD = "password-1";
+    private static final String INTERNAL_SECTOR_HOST = "test.account.gov.uk";
+    private static String TEST_INTERNAL_SUBJECT;
+    private static String publicSubjectId;
+
+    @BeforeEach
+    void setUp() {
+        ConfigurationService mfaMethodEnabledConfigurationService =
+                new ConfigurationService() {
+                    @Override
+                    public boolean isMfaMethodManagementApiEnabled() {
+                        return true;
+                    }
+                };
+        handler = new MFAMethodsPutHandler(mfaMethodEnabledConfigurationService);
+        publicSubjectId = userStore.signUp(EMAIL, PASSWORD);
+        byte[] salt = userStore.addSalt(EMAIL);
+        TEST_INTERNAL_SUBJECT =
+                ClientSubjectHelper.calculatePairwiseIdentifier(
+                        userStore.getUserProfileFromEmail(EMAIL).get().getSubjectID(),
+                        INTERNAL_SECTOR_HOST,
+                        salt);
+    }
+
+    @Test
+    void shouldReturn404WhenPrincipalIsInvalid() {
+        var response =
+                makeRequest(
+                        Optional.empty(),
+                        Collections.emptyMap(),
+                        Collections.emptyMap(),
+                        Map.of("publicSubjectId", publicSubjectId),
+                        Map.of("principalId", "invalid-internal-subject-id"));
+
+        assertEquals(404, response.getStatusCode());
+        assertThat(response, hasJsonBody(ErrorResponse.ERROR_1071));
+    }
+
+    @Test
+    void shouldReturn404WhenUserProfileIsNotFoundForPublicSubject() {
+        var response =
+                makeRequest(
+                        Optional.empty(),
+                        Collections.emptyMap(),
+                        Collections.emptyMap(),
+                        Map.of("publicSubjectId", "invalid-public-subject-id"),
+                        Map.of("principalId", TEST_INTERNAL_SUBJECT));
+
+        assertEquals(404, response.getStatusCode());
+        assertThat(response, hasJsonBody(ErrorResponse.ERROR_1056));
+    }
+}
