462ff3bcdb065c6b8101a33519cb531ff1224dca
Ryan Andrews <ryan.andrews@digital.cabinet-office.gov.uk>
2025-03-21 09:29:21 +0000

    ATO-1514: Increment and reset in identity progress handler

--- ipv-api/src/test/java/uk/gov/di/authentication/ipv/lambda/IdentityProgressFrontendHandlerTest.java
+++ ipv-api/src/test/java/uk/gov/di/authentication/ipv/lambda/IdentityProgressFrontendHandlerTest.java
@@ -306,6 +306,7 @@ public class IdentityProgressFrontendHandlerTest {
                                         REDIRECT_URI,
                                         STATE))));
         assertThat(session.getProcessingIdentityAttempts(), equalTo(0));
+        assertThat(orchSession.getProcessingIdentityAttempts(), equalTo(0));
         verify(cloudwatchMetricsService)
                 .incrementCounter(
                         "ProcessingIdentity",
