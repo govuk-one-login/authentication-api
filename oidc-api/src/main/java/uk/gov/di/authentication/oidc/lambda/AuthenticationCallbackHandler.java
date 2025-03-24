be4299e55a3aec5625307c907a7886e4438df68b
Craig Earl <craig.earl@digital.cabinet-office.gov.uk>
2025-03-24 14:27:25 +0000

    ATO-1420: Use orch client session in AuthenticationCallbackHandler

--- oidc-api/src/main/java/uk/gov/di/authentication/oidc/lambda/AuthenticationCallbackHandler.java
+++ oidc-api/src/main/java/uk/gov/di/authentication/oidc/lambda/AuthenticationCallbackHandler.java
@@ -98,7 +98,6 @@ import static uk.gov.di.orchestration.shared.helpers.LogLineHelper.attachLogFiel
 import static uk.gov.di.orchestration.shared.helpers.LogLineHelper.attachSessionIdToLogs;
 import static uk.gov.di.orchestration.shared.services.AuditService.MetadataPair.pair;
 import static uk.gov.di.orchestration.shared.services.AuditService.UNKNOWN;
-import static uk.gov.di.orchestration.shared.utils.ClientSessionMigrationUtils.getOrchClientSessionWithRetryIfNotEqual;
 import static uk.gov.di.orchestration.shared.utils.ClientSessionMigrationUtils.logIfClientSessionsAreNotEqual;
 
 public class AuthenticationCallbackHandler
@@ -295,8 +294,8 @@ public class AuthenticationCallbackHandler
                                             new AuthenticationCallbackException(
                                                     "ClientSession not found"));
             var orchClientSession =
-                    getOrchClientSessionWithRetryIfNotEqual(
-                                    clientSession, clientSessionId, orchClientSessionService)
+                    orchClientSessionService
+                            .getClientSession(clientSessionId)
                             .orElseThrow(
                                     () ->
                                             new AuthenticationCallbackException(
@@ -313,7 +312,7 @@ public class AuthenticationCallbackHandler
                             .withPersistentSessionId(persistentSessionId);
 
             var authenticationRequest =
-                    AuthenticationRequest.parse(clientSession.getAuthRequestParams());
+                    AuthenticationRequest.parse(orchClientSession.getAuthRequestParams());
 
             String clientId = authenticationRequest.getClientID().getValue();
             attachLogFieldToLogs(CLIENT_ID, clientId);
