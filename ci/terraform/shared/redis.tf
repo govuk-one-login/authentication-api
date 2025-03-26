e543646999f982de913f7887317dd869a195d620
Pawankumar Kushwaha <pawankumar.kushwaha@digital.cabinet-office.gov.uk>
2025-03-26 16:56:48 +0000

    AUT-4133: move frontend redis to Shared resource in API repo

--- ci/terraform/shared/redis.tf
+++ ci/terraform/shared/redis.tf
@@ -53,3 +53,47 @@ resource "aws_elasticache_replication_group" "sessions_store" {
     aws_sns_topic.slack_events,
   ]
 }
+
+data "aws_sns_topic" "slack_events" {
+  name = "${var.environment}-slack-events"
+}
+
+
+resource "aws_elasticache_subnet_group" "frontend_redis_session_store" {
+  name       = "${var.environment}-frontend-redis-subnet"
+  subnet_ids = local.private_subnet_ids
+}
+
+
+resource "aws_elasticache_replication_group" "frontend_sessions_store" {
+  automatic_failover_enabled  = true
+  preferred_cache_cluster_azs = data.aws_availability_zones.available.names
+  replication_group_id        = "${var.environment}-frontend-cache"
+  description                 = "A Redis cluster for storing user session data for the frontend"
+  node_type                   = var.redis_node_size
+  num_cache_clusters          = length(data.aws_availability_zones.available.names)
+  engine                      = "redis"
+  engine_version              = "6.x"
+  parameter_group_name        = "default.redis6.x"
+  port                        = local.redis_port_number
+  maintenance_window          = "sun:22:00-sun:23:00"
+  notification_topic_arn      = data.aws_sns_topic.slack_events.arn
+
+  multi_az_enabled = true
+
+  at_rest_encryption_enabled = true
+  transit_encryption_enabled = true
+  auth_token                 = random_password.redis_password.result
+  apply_immediately          = true
+
+  subnet_group_name = aws_elasticache_subnet_group.frontend_redis_session_store.name
+  security_group_ids = [
+    aws_security_group.frontend_redis_security_group.id
+  ]
+
+  lifecycle {
+    ignore_changes = [
+      engine_version
+    ]
+  }
+}
