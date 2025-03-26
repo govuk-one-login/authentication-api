c7ada9305b258a92605b7c448c57d892a744eb4e
Matt W <173997080+sw-mattw@users.noreply.github.com>
2025-03-26 10:53:52 +0000

    ATO-1185: add new OrchAuthCode service
    Note that the method signatures match those in AuthorisationCodeService.java which this service will eventually be replacing.

---
+++ orchestration-shared/src/main/java/uk/gov/di/orchestration/shared/services/OrchAuthCodeService.java
@@ -0,0 +1,159 @@
+package uk.gov.di.orchestration.shared.services;
+
+import com.nimbusds.oauth2.sdk.AuthorizationCode;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbTable;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+import uk.gov.di.orchestration.shared.entity.AuthCodeExchangeData;
+import uk.gov.di.orchestration.shared.entity.OrchAuthCodeItem;
+import uk.gov.di.orchestration.shared.exceptions.OrchAuthCodeException;
+import uk.gov.di.orchestration.shared.helpers.NowHelper;
+import uk.gov.di.orchestration.shared.serialization.Json;
+import uk.gov.di.orchestration.shared.serialization.Json.JsonException;
+
+import java.time.Clock;
+import java.time.temporal.ChronoUnit;
+import java.util.Optional;
+
+public class OrchAuthCodeService extends BaseDynamoService<OrchAuthCodeItem> {
+    private static final Logger LOG = LogManager.getLogger(OrchAuthCodeService.class);
+
+    private final long timeToLive;
+    private final NowHelper.NowClock nowClock;
+    private final Json objectMapper;
+
+    public OrchAuthCodeService(ConfigurationService configurationService) {
+        this(configurationService, Clock.systemUTC(), SerializationService.getInstance());
+    }
+
+    public OrchAuthCodeService(
+            ConfigurationService configurationService, Clock clock, Json objectMapper) {
+        super(OrchAuthCodeItem.class, "Orch-Auth-Code", configurationService, true);
+        this.timeToLive = configurationService.getAuthCodeExpiry();
+        this.nowClock = new NowHelper.NowClock(clock);
+        this.objectMapper = objectMapper;
+    }
+
+    public OrchAuthCodeService(
+            DynamoDbClient dynamoDbClient,
+            DynamoDbTable<OrchAuthCodeItem> dynamoDbTable,
+            ConfigurationService configurationService,
+            Clock clock) {
+        super(dynamoDbTable, dynamoDbClient);
+        this.timeToLive = configurationService.getAuthCodeExpiry();
+        this.nowClock = new NowHelper.NowClock(clock);
+        this.objectMapper = SerializationService.getInstance();
+    }
+
+    public AuthorizationCode generateAndSaveAuthorisationCode(
+            String clientId, String clientSessionId, String email, Long authTime) {
+        LOG.info("Generating and saving authorisation code to orch auth code store");
+        AuthorizationCode authorizationCode = new AuthorizationCode();
+
+        var exchangeData =
+                new AuthCodeExchangeData()
+                        .setClientId(clientId)
+                        .setClientSessionId(clientSessionId)
+                        .setEmail(email)
+                        .setAuthTime(authTime);
+
+        var itemTtl = nowClock.nowPlus(timeToLive, ChronoUnit.SECONDS).toInstant().getEpochSecond();
+
+        var authCodeItem =
+                new OrchAuthCodeItem()
+                        .withAuthCode(authorizationCode.getValue())
+                        .withIsUsed(false)
+                        .withTimeToLive(itemTtl);
+
+        try {
+            var exchangeDataSerialized = objectMapper.writeValueAsString(exchangeData);
+
+            authCodeItem = authCodeItem.withAuthCodeExchangeData(exchangeDataSerialized);
+        } catch (JsonException e) {
+            logAndThrowOrchAuthCodeException(
+                    "Error serializing exchange data for new orch auth code item.", e);
+        }
+
+        try {
+            put(authCodeItem);
+        } catch (Exception e) {
+            logAndThrowOrchAuthCodeException("Failed to add Orch auth code item", e);
+        }
+
+        return authorizationCode;
+    }
+
+    public Optional<AuthCodeExchangeData> getExchangeDataForCode(String code) {
+        LOG.info(
+                "Retrieving authorisation code exchange data from orch auth code store. Code: {}",
+                code);
+
+        Optional<OrchAuthCodeItem> authCodeItem = Optional.empty();
+
+        try {
+            authCodeItem = get(code);
+        } catch (Exception e) {
+            logAndThrowOrchAuthCodeException(
+                    String.format("Failed to get orch auth code item. Code: %s", code), e);
+        }
+
+        if (authCodeItem.isEmpty()) {
+            LOG.info("No orch auth code item found. Code: {}", code);
+            return Optional.empty();
+        }
+
+        Optional<OrchAuthCodeItem> unusedAuthCodeItem = authCodeItem.filter(c -> !c.getIsUsed());
+        if (unusedAuthCodeItem.isEmpty()) {
+            LOG.warn("Orch auth code item found with isUsed set to true. Code: {}", code);
+            return Optional.empty();
+        }
+
+        Optional<OrchAuthCodeItem> validAuthCodeItem =
+                unusedAuthCodeItem.filter(
+                        c -> c.getTimeToLive() > nowClock.now().toInstant().getEpochSecond());
+        if (validAuthCodeItem.isEmpty()) {
+            LOG.info("Orch auth code item with expired TTL found. Code: {}", code);
+            return Optional.empty();
+        }
+
+        var authCodeExchangeDataSerialized = validAuthCodeItem.get().getAuthCodeExchangeData();
+
+        Optional<AuthCodeExchangeData> authCodeExchangeData = Optional.empty();
+        try {
+            authCodeExchangeData =
+                    Optional.of(
+                            objectMapper.readValue(
+                                    authCodeExchangeDataSerialized, AuthCodeExchangeData.class));
+        } catch (JsonException e) {
+            logAndThrowOrchAuthCodeException(
+                    String.format(
+                            "Error deserializing exchange data for orch auth code item. Code: %s",
+                            code),
+                    e);
+        }
+
+        markAuthCodeAsUsed(authCodeItem.get());
+
+        return authCodeExchangeData;
+    }
+
+    private void markAuthCodeAsUsed(OrchAuthCodeItem authCodeItem) {
+        var item = authCodeItem.withIsUsed(true);
+
+        try {
+            update(item);
+        } catch (Exception e) {
+            logAndThrowOrchAuthCodeException(
+                    String.format(
+                            "Failed to mark orch auth code item as used. Code: %s",
+                            authCodeItem.getAuthCode()),
+                    e);
+        }
+    }
+
+    private void logAndThrowOrchAuthCodeException(String message, Exception e) {
+        LOG.error("{}. Error message: {}", message, e.getMessage());
+        throw new OrchAuthCodeException(message);
+    }
+}
---
+++ orchestration-shared/src/test/java/uk/gov/di/orchestration/shared/services/OrchAuthCodeServiceTest.java
@@ -0,0 +1,243 @@
+package uk.gov.di.orchestration.shared.services;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbTable;
+import software.amazon.awssdk.enhanced.dynamodb.Key;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+import software.amazon.awssdk.services.dynamodb.model.DynamoDbException;
+import uk.gov.di.orchestration.shared.entity.AuthCodeExchangeData;
+import uk.gov.di.orchestration.shared.entity.OrchAuthCodeItem;
+import uk.gov.di.orchestration.shared.exceptions.OrchAuthCodeException;
+import uk.gov.di.orchestration.shared.serialization.Json;
+
+import java.time.Clock;
+import java.time.Instant;
+import java.time.ZoneId;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+class OrchAuthCodeServiceTest {
+    private static final String CLIENT_ID = "test-client-id";
+    private static final String CLIENT_SESSION_ID = "test-client-session-id";
+    private static final String EMAIL = "test-email";
+    private static final long AUTH_TIME = 12345L;
+
+    private static final String AUTH_CODE = "test-auth-code";
+    private static final Instant CREATION_INSTANT = Instant.parse("2025-02-01T03:04:05.678Z");
+    private static final long VALID_TTL = CREATION_INSTANT.plusSeconds(100).getEpochSecond();
+    private static final long EXPIRED_TTL = CREATION_INSTANT.minusSeconds(100).getEpochSecond();
+    private static final Key AUTH_CODE_PARTITION_KEY =
+            Key.builder().partitionValue(AUTH_CODE).build();
+    private static final long AUTH_CODE_EXPIRY = 123L;
+
+    private final DynamoDbTable<OrchAuthCodeItem> table = mock(DynamoDbTable.class);
+    private final DynamoDbClient dynamoDbClient = mock(DynamoDbClient.class);
+    private final ConfigurationService configurationService = mock(ConfigurationService.class);
+    private final Json objectMapper = SerializationService.getInstance();
+    private OrchAuthCodeService orchAuthCodeService;
+
+    @BeforeEach
+    void setup() {
+        when(configurationService.getAuthCodeExpiry()).thenReturn(AUTH_CODE_EXPIRY);
+
+        orchAuthCodeService =
+                new OrchAuthCodeService(
+                        dynamoDbClient,
+                        table,
+                        configurationService,
+                        Clock.fixed(CREATION_INSTANT, ZoneId.systemDefault()));
+    }
+
+    @Test
+    void shouldStoreOrchAuthCodeItem() throws Json.JsonException {
+        orchAuthCodeService.generateAndSaveAuthorisationCode(
+                CLIENT_ID, CLIENT_SESSION_ID, EMAIL, AUTH_TIME);
+
+        var orchAuthCodeItemCaptor = ArgumentCaptor.forClass(OrchAuthCodeItem.class);
+        verify(table).putItem(orchAuthCodeItemCaptor.capture());
+        var capturedRequest = orchAuthCodeItemCaptor.getValue();
+
+        assertNotNull(capturedRequest.getAuthCode());
+
+        var expectedExchangeData = aAuthCodeExchangeDataEntity();
+        var expectedExchangeDataSerialized = objectMapper.writeValueAsString(expectedExchangeData);
+        assertEquals(expectedExchangeDataSerialized, capturedRequest.getAuthCodeExchangeData());
+
+        assertFalse(capturedRequest.getIsUsed());
+
+        var expectedTimeToLive = CREATION_INSTANT.plusSeconds(AUTH_CODE_EXPIRY).getEpochSecond();
+        assertEquals(expectedTimeToLive, capturedRequest.getTimeToLive());
+    }
+
+    @Test
+    void shouldThrowWhenFailingToStoreOrchAuthCode() {
+        withFailedPut();
+
+        assertThrows(
+                OrchAuthCodeException.class,
+                () ->
+                        orchAuthCodeService.generateAndSaveAuthorisationCode(
+                                CLIENT_ID, CLIENT_SESSION_ID, EMAIL, AUTH_TIME));
+    }
+
+    @Test
+    void shouldGetOrchAuthCodeExchangeDataByAuthCode() throws Json.JsonException {
+        var exchangeData = aAuthCodeExchangeDataEntity();
+        withValidOrchAuthCode(exchangeData);
+
+        var actualExchangeData = orchAuthCodeService.getExchangeDataForCode(AUTH_CODE);
+
+        assertTrue(actualExchangeData.isPresent());
+
+        assertEquals(exchangeData.getClientId(), actualExchangeData.get().getClientId());
+        assertEquals(
+                exchangeData.getClientSessionId(), actualExchangeData.get().getClientSessionId());
+        assertEquals(exchangeData.getEmail(), actualExchangeData.get().getEmail());
+        assertEquals(exchangeData.getAuthTime(), actualExchangeData.get().getAuthTime());
+    }
+
+    @Test
+    void shouldMarkAuthCodeAsUsedAfterSuccessfullyGettingOrchAuthCodeExchangeData()
+            throws Json.JsonException {
+        var exchangeData = aAuthCodeExchangeDataEntity();
+        withValidOrchAuthCode(exchangeData);
+
+        orchAuthCodeService.getExchangeDataForCode(AUTH_CODE);
+
+        var orchAuthCodeItemCaptor = ArgumentCaptor.forClass(OrchAuthCodeItem.class);
+        verify(table).updateItem(orchAuthCodeItemCaptor.capture());
+        var capturedRequest = orchAuthCodeItemCaptor.getValue();
+
+        assertTrue(capturedRequest.getIsUsed());
+    }
+
+    @Test
+    void shouldThrowWhenFailingToGetAuthCodeExchangeDataByAuthCode() {
+        withFailedGet();
+
+        assertThrows(
+                OrchAuthCodeException.class,
+                () -> orchAuthCodeService.getExchangeDataForCode(AUTH_CODE));
+    }
+
+    @Test
+    void shouldNotGetAuthCodeExchangeDataByAuthCodeWhenNoOrchAuthCodeItemExists() {
+        var authCode = "an-unknown-auth-code";
+
+        var exchangeData = orchAuthCodeService.getExchangeDataForCode(authCode);
+
+        assertTrue(exchangeData.isEmpty());
+    }
+
+    @Test
+    void shouldNotGetAuthCodeExchangeDataByAuthCodeWhenOrchAuthCodeItemExistsButIsMarkedAsUsed()
+            throws Json.JsonException {
+        withUsedOrchAuthCode();
+
+        var exchangeData = orchAuthCodeService.getExchangeDataForCode(AUTH_CODE);
+
+        assertTrue(exchangeData.isEmpty());
+    }
+
+    @Test
+    void shouldNotGetAuthCodeExchangeDataByAuthCodeWhenOrchAuthCodeItemExistsButTimeToLiveExpired()
+            throws Json.JsonException {
+        withExpiredOrchAuthCode();
+
+        var exchangeData = orchAuthCodeService.getExchangeDataForCode(AUTH_CODE);
+
+        assertTrue(exchangeData.isEmpty());
+    }
+
+    @Test
+    void shouldThrowWhenFailingToUpdateOrchAuthCodeItemWhenMarkingAsUsed()
+            throws Json.JsonException {
+        var exchangeData = aAuthCodeExchangeDataEntity();
+        withValidOrchAuthCode(exchangeData);
+
+        withFailedUpdate();
+
+        assertThrows(
+                OrchAuthCodeException.class,
+                () -> orchAuthCodeService.getExchangeDataForCode(AUTH_CODE));
+    }
+
+    private AuthCodeExchangeData aAuthCodeExchangeDataEntity() {
+        return new AuthCodeExchangeData()
+                .setClientId(CLIENT_ID)
+                .setClientSessionId(CLIENT_SESSION_ID)
+                .setEmail(EMAIL)
+                .setAuthTime(AUTH_TIME);
+    }
+
+    private void withValidOrchAuthCode(AuthCodeExchangeData exchangeData)
+            throws Json.JsonException {
+        var exchangeDataSerialized = objectMapper.writeValueAsString(exchangeData);
+
+        OrchAuthCodeItem orchAuthCodeItem =
+                new OrchAuthCodeItem()
+                        .withAuthCode(AUTH_CODE)
+                        .withAuthCodeExchangeData(exchangeDataSerialized)
+                        .withIsUsed(false)
+                        .withTimeToLive(VALID_TTL);
+
+        when(table.getItem(AUTH_CODE_PARTITION_KEY)).thenReturn(orchAuthCodeItem);
+    }
+
+    private void withUsedOrchAuthCode() throws Json.JsonException {
+        var exchangeData = aAuthCodeExchangeDataEntity();
+        var exchangeDataSerialized = objectMapper.writeValueAsString(exchangeData);
+
+        OrchAuthCodeItem orchAuthCodeItem =
+                new OrchAuthCodeItem()
+                        .withAuthCode(AUTH_CODE)
+                        .withAuthCodeExchangeData(exchangeDataSerialized)
+                        .withIsUsed(true)
+                        .withTimeToLive(VALID_TTL);
+
+        when(table.getItem(AUTH_CODE_PARTITION_KEY)).thenReturn(orchAuthCodeItem);
+    }
+
+    private void withExpiredOrchAuthCode() throws Json.JsonException {
+        var exchangeData = aAuthCodeExchangeDataEntity();
+        var exchangeDataSerialized = objectMapper.writeValueAsString(exchangeData);
+
+        OrchAuthCodeItem orchAuthCodeItem =
+                new OrchAuthCodeItem()
+                        .withAuthCode(AUTH_CODE)
+                        .withAuthCodeExchangeData(exchangeDataSerialized)
+                        .withIsUsed(false)
+                        .withTimeToLive(EXPIRED_TTL);
+
+        when(table.getItem(AUTH_CODE_PARTITION_KEY)).thenReturn(orchAuthCodeItem);
+    }
+
+    private void withFailedPut() {
+        doThrow(DynamoDbException.builder().message("Failed to put item in table").build())
+                .when(table)
+                .putItem(any(OrchAuthCodeItem.class));
+    }
+
+    private void withFailedGet() {
+        doThrow(DynamoDbException.builder().message("Failed to get from table").build())
+                .when(table)
+                .getItem(any(Key.class));
+    }
+
+    private void withFailedUpdate() {
+        doThrow(DynamoDbException.builder().message("Failed to update item in table").build())
+                .when(table)
+                .updateItem(any(OrchAuthCodeItem.class));
+    }
+}
