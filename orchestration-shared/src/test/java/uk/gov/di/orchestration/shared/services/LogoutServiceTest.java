ab09f74d0c5fbc9ab52812afddf265a2ef9e0088
Gabriel.Swallow <gabriel.swallow@digital.cabinet-office.gov.uk>
2025-03-26 12:22:26 +0000

    ATO-1116: add test assertion to improve coverage
    The case of a clientSession not existing for a clientId attached to the session was not tested and it was affecting coverage for this PR. There is no else, it is just whether we sent the backChannelLogout. If we can't find the clientSession, we can't do that, so this just asserts there was no attempt to.

--- orchestration-shared/src/test/java/uk/gov/di/orchestration/shared/services/LogoutServiceTest.java
+++ orchestration-shared/src/test/java/uk/gov/di/orchestration/shared/services/LogoutServiceTest.java
@@ -61,6 +61,7 @@ import static org.mockito.ArgumentMatchers.argThat;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.mockStatic;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoInteractions;
@@ -534,6 +535,8 @@ class LogoutServiceTest {
         verify(backChannelLogoutService)
                 .sendLogoutMessage(
                         argThat(withClientId("client-id-3")), eq("rp-pairwise-id-client-3"));
+        verify(backChannelLogoutService, never())
+                .sendLogoutMessage(argThat(withClientId("client-id-4")), anyString());
         verify(cloudwatchMetricsService).incrementLogout(Optional.of(CLIENT_ID));
 
         assertThat(response, hasStatus(302));
@@ -648,7 +651,11 @@ class LogoutServiceTest {
         destroySessionsRequest =
                 new DestroySessionsRequest(
                         SESSION_ID,
-                        List.of(CLIENT_SESSION_ID, "client-session-id-2", "client-session-id-3"));
+                        List.of(
+                                CLIENT_SESSION_ID,
+                                "client-session-id-2",
+                                "client-session-id-3",
+                                "client-session-id-4"));
     }
 
     private void setupClientSessionToken(JWT idToken) {
