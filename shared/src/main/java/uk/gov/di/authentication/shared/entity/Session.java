e33fd0b2645946d03b546c71d1144e4506d5c8a2
Ryan Andrews <ryan.andrews@digital.cabinet-office.gov.uk>
2025-03-27 17:05:27 +0000

    ATO-1565: Remove setter method

--- shared/src/main/java/uk/gov/di/authentication/shared/entity/Session.java
+++ shared/src/main/java/uk/gov/di/authentication/shared/entity/Session.java
@@ -79,11 +79,6 @@ public class Session {
         return preservedReauthCountsForAudit;
     }
 
-    public Session setCurrentCredentialStrength(CredentialTrustLevel currentCredentialStrength) {
-        this.currentCredentialStrength = currentCredentialStrength;
-        return this;
-    }
-
     public MFAMethodType getVerifiedMfaMethodType() {
         return verifiedMfaMethodType;
     }
