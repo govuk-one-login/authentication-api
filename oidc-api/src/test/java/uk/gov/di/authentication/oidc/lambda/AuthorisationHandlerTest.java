c5a3d195a0d50c17a687c29c13fc49a3e5886780
Gabriel.Swallow <gabriel.swallow@digital.cabinet-office.gov.uk>
2025-03-31 20:22:44 +0100

    ATO-1567: always request publicSubjectId for PUBLIC subjectType clients

--- oidc-api/src/test/java/uk/gov/di/authentication/oidc/lambda/AuthorisationHandlerTest.java
+++ oidc-api/src/test/java/uk/gov/di/authentication/oidc/lambda/AuthorisationHandlerTest.java
@@ -1770,6 +1770,31 @@ class AuthorisationHandlerTest {
         }
 
         @Test
+        void shouldAddPublicSubjectIdClaimIfClientHasPublicSubjectTypePresent()
+                throws com.nimbusds.oauth2.sdk.ParseException, ParseException {
+            when(clientService.getClient(CLIENT_ID.getValue()))
+                    .thenReturn(Optional.of(generateClientRegistry().withSubjectType("public")));
+
+            Map<String, String> requestParams = buildRequestParams(Map.of("scope", "openid"));
+            APIGatewayProxyRequestEvent event = withRequestEvent(requestParams);
+            event.setRequestContext(
+                    new ProxyRequestContext()
+                            .withIdentity(new RequestIdentity().withSourceIp("123.123.123.123")));
+            makeHandlerRequest(event);
+
+            verifyAuthorisationRequestParsedAuditEvent(
+                    AuditService.UNKNOWN, false, false, "MEDIUM_LEVEL");
+            ArgumentCaptor<JWTClaimsSet> argument = ArgumentCaptor.forClass(JWTClaimsSet.class);
+            verify(orchestrationAuthorizationService).getSignedAndEncryptedJWT(argument.capture());
+
+            var expectedClaim =
+                    ClaimsSetRequest.parse(
+                            "{\"userinfo\":{\"local_account_id\":null, \"verified_mfa_method_type\":null,\"current_credential_strength\":null,\"public_subject_id\":null,\"email\":null, \"uplift_required\":null}}");
+            var actualClaim = ClaimsSetRequest.parse(argument.getValue().getStringClaim("claim"));
+            assertEquals(actualClaim.toJSONObject(), expectedClaim.toJSONObject());
+        }
+
+        @Test
         void shouldAddLegacySubjectIdClaimIfGovUkAccountScopePresent()
                 throws com.nimbusds.oauth2.sdk.ParseException, ParseException {
             Map<String, String> requestParams =
