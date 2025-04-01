6a560ca0cf720858b25ba3c0b4f3fd35fd0ca829
Ryan Andrews <ryan.andrews@digital.cabinet-office.gov.uk>
2025-04-01 10:40:37 +0100

    ATO-1564: Remove setter from auth code integration test

--- integration-tests/src/test/java/uk/gov/di/authentication/api/AuthCodeIntegrationTest.java
+++ integration-tests/src/test/java/uk/gov/di/authentication/api/AuthCodeIntegrationTest.java
@@ -95,7 +95,6 @@ public class AuthCodeIntegrationTest extends ApiGatewayHandlerIntegrationTest {
         setupOrchSession();
         var clientSessionId = "some-client-session-id";
         setupAuthUserInfo(clientSessionId);
-        redis.setVerifiedMfaMethodType(sessionID, MFAMethodType.AUTH_APP);
         var creationDate = LocalDateTime.now();
         var authRequestParams = generateAuthRequest().toParameters();
         redis.addAuthRequestToSession(
