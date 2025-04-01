24dd309e5413ef35c6b1ed41f2a800aed651ce7e
Ryan Andrews <ryan.andrews@digital.cabinet-office.gov.uk>
2025-04-01 14:45:12 +0100

    ATO-1565: Remove setter from authorisation tests

--- integration-tests/src/test/java/uk/gov/di/authentication/api/AuthorisationIntegrationTest.java
+++ integration-tests/src/test/java/uk/gov/di/authentication/api/AuthorisationIntegrationTest.java
@@ -2036,10 +2036,9 @@ class AuthorisationIntegrationTest extends ApiGatewayHandlerIntegrationTest {
         return sessionId;
     }
 
-    private String givenAnExistingSession(CredentialTrustLevel credentialTrustLevel)
-            throws Json.JsonException {
-        String sessionId = redis.createSession();
-        redis.setSessionCredentialTrustLevel(sessionId, credentialTrustLevel);
+    private String givenAnExistingSession(CredentialTrustLevel credentialTrustLevel) {
+        var sessionId = IdGenerator.generate();
+        orchSessionExtension.addSession(new OrchSessionItem(sessionId));
         return sessionId;
     }
 
