184d31f26758c68a1c2f0812354ac66c18b71bb6
Benjamin Fernandes <benjamin.fernandes@digital.cabinet-office.gov.uk>
2025-04-02 16:20:48 +0100

    AUT-4061: Move all copies of NotificationService to /shared
    We previously had a duplicate in /shared and /account-management-api, along with tests stored in /frontend-api. Now, everything lives in /shared.

--- shared/src/main/java/uk/gov/di/authentication/shared/services/NotificationService.java
+++ shared/src/main/java/uk/gov/di/authentication/shared/services/NotificationService.java
@@ -21,11 +21,22 @@ public class NotificationService {
         this.configurationService = configurationService;
     }
 
+    public void sendEmail(String email, Map<String, Object> personalisation, TemplateAware type)
+            throws NotificationClientException {
+        sendEmail(email, personalisation, type, "");
+    }
+
     public void sendEmail(
             String email, Map<String, Object> personalisation, TemplateAware type, String reference)
             throws NotificationClientException {
         notifyClient.sendEmail(
                 type.getTemplateId(configurationService), email, personalisation, reference);
+    }
+
+    public void sendText(
+            String phoneNumber, Map<String, Object> personalisation, TemplateAware type)
+            throws NotificationClientException {
+        sendText(phoneNumber, personalisation, type, "");
     }
 
     public void sendText(
