19bf3a52da828583675a6f7176fa4ad9e3d6ff03
Tom Whitwell <tom.whitwell@digital.cabinet-office.gov.uk>
2025-04-04 10:38:19 +0100

    BAU: Globally switch from AWS X-Ray to OTEL
    Instead of pushing xray traces to AWS, we now ship OpenTelemetry spans.
    
    The annotations are now added to these otel spans, specifically to the
    current span (getCurrentSpan()).

--- shared/src/main/java/uk/gov/di/authentication/shared/helpers/InstrumentationHelper.java
+++ shared/src/main/java/uk/gov/di/authentication/shared/helpers/InstrumentationHelper.java
@@ -1,91 +1,121 @@
 package uk.gov.di.authentication.shared.helpers;
 
-import com.amazonaws.xray.AWSXRay;
+import io.opentelemetry.api.GlobalOpenTelemetry;
+import io.opentelemetry.api.common.AttributeKey;
+import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.api.trace.StatusCode;
+import io.opentelemetry.api.trace.Tracer;
+import io.opentelemetry.context.Scope;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import uk.gov.di.authentication.shared.annotations.ExcludeFromGeneratedCoverageReport;
 
+import java.util.Optional;
 import java.util.concurrent.Callable;
 
-import static java.util.Objects.nonNull;
+import static io.opentelemetry.context.Context.current;
+import static java.util.Objects.isNull;
 
+@ExcludeFromGeneratedCoverageReport
 public class InstrumentationHelper {
     private static final Logger LOG = LogManager.getLogger(InstrumentationHelper.class);
+    private static final Tracer tracer = GlobalOpenTelemetry.getTracer("instrumentation-helper");
 
     private static final boolean tracingEnabled =
             Boolean.parseBoolean(System.getenv().getOrDefault("TRACING_ENABLED", "true"));
 
     public static <T> T segmentedFunctionCall(String segmentName, Callable<T> callable) {
+        return instrument(segmentName, callable, null);
+    }
+
+    public static void segmentedFunctionCall(String segmentName, Runnable runnable) {
+        instrument(segmentName, null, runnable);
+    }
+
+    private static <T> T instrument(String segmentName, Callable<T> callable, Runnable runnable) {
+        Span span = null;
+
         if (tracingEnabled) {
-            var subSegment = AWSXRay.beginSubsegment(segmentName);
-            try {
-                return callable.call();
-            } catch (RuntimeException e) {
-                subSegment.addException(e);
-                throw e;
-            } catch (Exception e) {
-                subSegment.addException(e);
-                throw new RuntimeException(e);
-            } finally {
-                AWSXRay.endSubsegment();
+            span = tracer.spanBuilder(segmentName).startSpan();
+        }
+
+        try {
+            if (span != null) {
+                try (Scope scope = span.makeCurrent()) {
+                    return executeCall(callable, runnable);
+                }
+            } else {
+                return executeCall(callable, runnable);
             }
-        } else {
-            try {
-                return callable.call();
-            } catch (RuntimeException e) {
-                throw e;
-            } catch (Exception e) {
-                throw new RuntimeException(e);
+        } catch (RuntimeException e) {
+            recordError(span, e);
+            throw e;
+        } catch (Exception e) {
+            recordError(span, e);
+            throw new RuntimeException(e);
+        } finally {
+            if (span != null) {
+                span.end();
             }
         }
     }
 
-    public static void segmentedFunctionCall(String segmentName, Runnable runnable) {
-        if (tracingEnabled) {
-            var subSegment = AWSXRay.beginSubsegment(segmentName);
-            try {
-                runnable.run();
-            } catch (RuntimeException e) {
-                subSegment.addException(e);
-                throw e;
-            } catch (Exception e) {
-                subSegment.addException(e);
-                throw new RuntimeException(e);
-            } finally {
-                AWSXRay.endSubsegment();
-            }
+    private static <T> T executeCall(Callable<T> callable, Runnable runnable) throws Exception {
+        if (callable != null) {
+            return callable.call();
         } else {
             runnable.run();
+            return null;
+        }
+    }
+
+    private static void recordError(Span span, Exception e) {
+        if (span != null) {
+            span.recordException(e);
+            span.setAttribute(AttributeKey.stringKey("error.type"), e.getClass().getName());
+            span.setStatus(StatusCode.ERROR, e.getMessage());
+        }
+    }
+
+    public static <T> void addAnnotation(final AttributeKey<T> key, final T value) {
+        if (isNull(value)) {
+            return;
+        }
+        if (tracingEnabled) {
+            getCurrentSpan()
+                    .ifPresentOrElse(
+                            s -> s.setAttribute(key, value), InstrumentationHelper::noSpanPresent);
         }
     }
 
     public static void addAnnotation(final String key, final String value) {
-        if (tracingEnabled && nonNull(value)) {
-            AWSXRay.getCurrentSubsegmentOptional()
-                    .ifPresentOrElse(
-                            s -> s.putAnnotation(key, value),
-                            InstrumentationHelper::noSubSegmentPresent);
-        }
+        addAnnotation(AttributeKey.stringKey(key), value);
     }
 
     public static void addAnnotation(final String key, final Number value) {
-        if (tracingEnabled && nonNull(value)) {
-            AWSXRay.getCurrentSubsegmentOptional()
-                    .ifPresentOrElse(
-                            s -> s.putAnnotation(key, value),
-                            InstrumentationHelper::noSubSegmentPresent);
+        if (isNull(value)) {
+            return;
+        }
+        if (value instanceof Double || value instanceof Float) {
+            addAnnotation(AttributeKey.doubleKey(key), value.doubleValue());
+        } else {
+            addAnnotation(AttributeKey.longKey(key), value.longValue());
         }
     }
 
     public static void addAnnotation(final String key, final Boolean value) {
-        if (tracingEnabled && nonNull(value)) {
-            AWSXRay.getCurrentSubsegmentOptional()
-                    .ifPresentOrElse(
-                            s -> s.putAnnotation(key, value),
-                            InstrumentationHelper::noSubSegmentPresent);
-        }
+        addAnnotation(AttributeKey.booleanKey(key), value);
     }
 
-    private static void noSubSegmentPresent() {
-        LOG.warn("Could not add annotations to trace as no subsegment present");
+    private static Optional<Span> getCurrentSpan() {
+        Span span = Span.fromContext(current());
+        if (span.getSpanContext().isValid()) {
+            return Optional.of(span);
+        }
+        return Optional.empty();
+    }
+
+    private static void noSpanPresent() {
+        LOG.warn("Could not add annotations to trace as no span present");
     }
 }
