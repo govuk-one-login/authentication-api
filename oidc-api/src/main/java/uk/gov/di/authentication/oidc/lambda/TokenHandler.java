e1fa385bce2c180e65724e4ca4c295ea8e181367
Matt W <173997080+sw-mattw@users.noreply.github.com>
2025-04-08 11:44:54 +0100

    ATO-1205: update TokenHandler to use orch auth code store
    Note that authorisationCodeService will be removed in a subsequent commit to simplify the commit diffs.

--- oidc-api/src/main/java/uk/gov/di/authentication/oidc/lambda/TokenHandler.java
+++ oidc-api/src/main/java/uk/gov/di/authentication/oidc/lambda/TokenHandler.java
@@ -227,34 +227,25 @@ public class TokenHandler
                                     getSigningAlgorithm(clientRegistry)));
         }
 
-        Optional<AuthCodeExchangeData> authCodeExchangeDataMaybe =
-                segmentedFunctionCall(
-                        "authorisationCodeService",
-                        () ->
-                                authorisationCodeService.getExchangeDataForCode(
-                                        requestBody.get("code")));
+        Optional<AuthCodeExchangeData> authCodeExchangeDataMaybe;
+
+        try {
+            authCodeExchangeDataMaybe =
+                    orchAuthCodeService.getExchangeDataForCode(requestBody.get("code"));
+        } catch (Exception e) {
+            LOG.error(
+                    "Failed to retrieve authorisation code from orch auth code DynamoDB store. Error: {}",
+                    e.getMessage());
+            return generateApiGatewayProxyResponse(500, "Internal server error");
+        }
+
         if (authCodeExchangeDataMaybe.isEmpty()) {
             LOG.warn("Could not retrieve session data from code");
             return generateApiGatewayProxyResponse(
                     400, OAuth2Error.INVALID_GRANT.toJSONObject().toJSONString());
         }
+
         AuthCodeExchangeData authCodeExchangeData = authCodeExchangeDataMaybe.get();
-
-        try {
-            Optional<AuthCodeExchangeData> orchAuthCodeExchangeDataMaybe =
-                    orchAuthCodeService.getExchangeDataForCode(requestBody.get("code"));
-
-            /*
-                TODO: ATO-1205:
-                 - Need to rethrow exceptions (as RuntimeException?) or return a 500 api gateway proxy response ourselves.
-                 - Update the log in the catch clause to be level 'error' and remove Redis references (as by this point the DynamoDB store will be the primary).
-                 - Following the above updates, ensure the unit test which ensures unchecked exceptions are caught during consistency checks is updated.
-            */
-        } catch (Exception e) {
-            LOG.warn(
-                    "Failed to retrieve authorisation code from orch auth code DynamoDB store. NOTE: Redis is still the primary at present. Error: {}",
-                    e.getMessage());
-        }
 
         if (!Objects.equals(authCodeExchangeData.getClientId(), clientRegistry.getClientID())) {
             LOG.warn("Client ID from auth code does not match client ID from request body");
