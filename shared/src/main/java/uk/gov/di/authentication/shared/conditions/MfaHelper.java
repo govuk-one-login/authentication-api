fe5938c16bf7e8181c21d042fe5d1a54deae2230
Craig Earl <craig.earl@digital.cabinet-office.gov.uk>
2025-04-08 17:36:58 +0100

    ATO-1437: Remove unused MfaHelper method

--- shared/src/main/java/uk/gov/di/authentication/shared/conditions/MfaHelper.java
+++ shared/src/main/java/uk/gov/di/authentication/shared/conditions/MfaHelper.java
@@ -1,19 +1,13 @@
 package uk.gov.di.authentication.shared.conditions;
 
-import com.nimbusds.oauth2.sdk.ParseException;
-import com.nimbusds.openid.connect.sdk.AuthenticationRequest;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import uk.gov.di.authentication.entity.UserMfaDetail;
 import uk.gov.di.authentication.shared.entity.CredentialTrustLevel;
 import uk.gov.di.authentication.shared.entity.UserCredentials;
-import uk.gov.di.authentication.shared.entity.VectorOfTrust;
 import uk.gov.di.authentication.shared.entity.mfa.MFAMethod;
 import uk.gov.di.authentication.shared.entity.mfa.MFAMethodType;
-import uk.gov.di.authentication.shared.state.UserContext;
 
-import java.util.List;
-import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
 
@@ -23,19 +17,6 @@ public class MfaHelper {
     private static final Logger LOG = LogManager.getLogger(MfaHelper.class);
 
     private MfaHelper() {}
-
-    public static boolean mfaRequired(Map<String, List<String>> authRequestParams) {
-        AuthenticationRequest authRequest;
-        try {
-            authRequest = AuthenticationRequest.parse(authRequestParams);
-        } catch (ParseException e) {
-            throw new RuntimeException();
-        }
-        List<String> vtr = authRequest.getCustomParameter("vtr");
-        VectorOfTrust vectorOfTrust = VectorOfTrust.parseFromAuthRequestAttribute(vtr);
-
-        return mfaRequired(vectorOfTrust.getCredentialTrustLevel());
-    }
 
     public static boolean mfaRequired(CredentialTrustLevel credentialTrustLevel) {
         return !Objects.equals(credentialTrustLevel, LOW_LEVEL);
@@ -48,28 +29,11 @@ public class MfaHelper {
     }
 
     public static UserMfaDetail getUserMFADetail(
-            UserContext userContext,
-            UserCredentials userCredentials,
-            String phoneNumber,
-            boolean isPhoneNumberVerified) {
-        var isMfaRequired = mfaRequired(userContext.getClientSession().getAuthRequestParams());
-        return getUserMFADetail(isMfaRequired, userCredentials, phoneNumber, isPhoneNumberVerified);
-    }
-
-    public static UserMfaDetail getUserMFADetail(
             CredentialTrustLevel credentialTrustLevel,
             UserCredentials userCredentials,
             String phoneNumber,
             boolean isPhoneNumberVerified) {
         var isMfaRequired = mfaRequired(credentialTrustLevel);
-        return getUserMFADetail(isMfaRequired, userCredentials, phoneNumber, isPhoneNumberVerified);
-    }
-
-    private static UserMfaDetail getUserMFADetail(
-            boolean isMfaRequired,
-            UserCredentials userCredentials,
-            String phoneNumber,
-            boolean isPhoneNumberVerified) {
         var enabledMethod = getPrimaryMFAMethod(userCredentials);
 
         if (enabledMethod.filter(MFAMethod::isMethodVerified).isPresent()) {
