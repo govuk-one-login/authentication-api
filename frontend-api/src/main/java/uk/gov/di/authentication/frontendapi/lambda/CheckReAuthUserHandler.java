5ecab1364a85fa4977eecb360936f10c831221c2
Craig Earl <craig.earl@digital.cabinet-office.gov.uk>
2025-04-08 18:14:01 +0100

    ATO-1437: Use new BaseFrontendHandler method in CheckReAuthUserHandler
    - I've added a new helper method to AuthSessionExtension so we can add and update a session in one call.

--- frontend-api/src/main/java/uk/gov/di/authentication/frontendapi/lambda/CheckReAuthUserHandler.java
+++ frontend-api/src/main/java/uk/gov/di/authentication/frontendapi/lambda/CheckReAuthUserHandler.java
@@ -107,7 +107,7 @@ public class CheckReAuthUserHandler extends BaseFrontendHandler<CheckReauthUserR
     @Override
     public APIGatewayProxyResponseEvent handleRequest(
             APIGatewayProxyRequestEvent input, Context context) {
-        return super.handleRequest(input, context);
+        return super.handleRequestWithoutClientSession(input, context);
     }
 
     @Override
