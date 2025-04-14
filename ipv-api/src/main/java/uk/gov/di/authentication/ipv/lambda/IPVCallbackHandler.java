4efc449479f70e7d78903fdf76fe7d67f81c9a42
Gabriel.Swallow <gabriel.swallow@digital.cabinet-office.gov.uk>
2025-04-14 14:58:51 +0100

    ATO-1117: remove log to verify localAccountId is in sync

--- ipv-api/src/main/java/uk/gov/di/authentication/ipv/lambda/IPVCallbackHandler.java
+++ ipv-api/src/main/java/uk/gov/di/authentication/ipv/lambda/IPVCallbackHandler.java
@@ -27,7 +27,6 @@ import uk.gov.di.orchestration.shared.api.AuthFrontend;
 import uk.gov.di.orchestration.shared.api.CommonFrontend;
 import uk.gov.di.orchestration.shared.api.OrchFrontend;
 import uk.gov.di.orchestration.shared.entity.AccountIntervention;
-import uk.gov.di.orchestration.shared.entity.AuthUserInfoClaims;
 import uk.gov.di.orchestration.shared.entity.ClientRegistry;
 import uk.gov.di.orchestration.shared.entity.DestroySessionsRequest;
 import uk.gov.di.orchestration.shared.entity.OrchSessionItem;
@@ -366,12 +365,6 @@ public class IPVCallbackHandler
                         Objects.equals(
                                 userProfile.getPhoneNumber(), authUserInfo.getPhoneNumber()));
             }
-            LOG.info(
-                    "is subjectId the same on authUserInfo as on UserProfile: {}",
-                    Objects.equals(
-                            userProfile.getSubjectID(),
-                            authUserInfo.getClaim(AuthUserInfoClaims.LOCAL_ACCOUNT_ID.getValue())));
-
             //
 
             var tokenResponse =
