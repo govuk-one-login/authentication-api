842855ddc79d2481b05bc1fbbc9e39afacd7809d
Pawankumar Kushwaha <pawankumar.kushwaha@digital.cabinet-office.gov.uk>
2025-04-15 13:42:41 +0100

    BAU: only deploye resource unrestricted enviorment

--- ci/terraform/account-management/s3.tf
+++ ci/terraform/account-management/s3.tf
@@ -1,17 +1,24 @@
-resource "aws_s3_bucket" "am-api-acceptance-tests-otp-bucket" {
-  # checkov:skip=CKV_AWS_21:Bucket used for testing only so doesn't need encryption
-  # checkov:skip=CKV_AWS_21:Bucket used for testing only so doesn't need versioning
-  # checkov:skip=CKV_AWS_144:Bucket used for testing only so doesn't need cross-region replication
-  # checkov:skip=CKV_AWS_145:Bucket used for testing only so doesn't need encryption
-  # checkov:skip=CKV_AWS_18:Bucket used for testing only so doesn't need access logging
-  # checkov:skip=CKV2_AWS_61:Bucket used for testing only so doesnt need lifecycle configuration
-  # checkov:skip=CKV2_AWS_62:Bucket used for testing only so doesn't need event notifications
+
+locals {
+  restricted_environments = ["production", "integration"]
+  create_resource         = !contains(local.restricted_environments, var.environment)
+}
+
+resource "aws_s3_bucket" "am_api_acceptance_tests_otp_bucket" {
+  count = local.create_resource ? 1 : 0
+  #checkov:skip=CKV_AWS_21:Bucket used for testing only so doesn't need encryption
+  #checkov:skip=CKV_AWS_21:Bucket used for testing only so doesn't need versioning
+  #checkov:skip=CKV_AWS_144:Bucket used for testing only so doesn't need cross-region replication
+  #checkov:skip=CKV_AWS_145:Bucket used for testing only so doesn't need encryption
+  #checkov:skip=CKV_AWS_18:Bucket used for testing only so doesn't need access logging
+  #checkov:skip=CKV2_AWS_61:Bucket used for testing only so doesnt need lifecycle configuration
+  #checkov:skip=CKV2_AWS_62:Bucket used for testing only so doesn't need event notifications
   bucket = "${var.environment}-am-api-acceptance-tests-otp"
 }
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "am_api_acceptance_tests_otp_bucket" {
-  count  = contains(["production", "integration"], var.environment) ? 0 : 1
-  bucket = aws_s3_bucket.am-api-acceptance-tests-otp-bucket.id
+  count  = local.create_resource ? 1 : 0
+  bucket = aws_s3_bucket.am_api_acceptance_tests_otp_bucket[0].id
 
   rule {
     apply_server_side_encryption_by_default {
@@ -21,8 +28,8 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "am_api_acceptance
 }
 
 resource "aws_s3_bucket_ownership_controls" "am_api_acceptance_tests_otp_bucket" {
-  count  = contains(["production", "integration"], var.environment) ? 0 : 1
-  bucket = aws_s3_bucket.am-api-acceptance-tests-otp-bucket.id
+  count  = local.create_resource ? 1 : 0
+  bucket = aws_s3_bucket.am_api_acceptance_tests_otp_bucket[0].id
 
   rule {
     object_ownership = "BucketOwnerEnforced"
@@ -31,8 +38,8 @@ resource "aws_s3_bucket_ownership_controls" "am_api_acceptance_tests_otp_bucket"
 }
 
 resource "aws_s3_bucket_public_access_block" "am_api_acceptance_tests_otp_private_bucket" {
-  count                   = contains(["production", "integration"], var.environment) ? 0 : 1
-  bucket                  = aws_s3_bucket.am-api-acceptance-tests-otp-bucket.id
+  count                   = local.create_resource ? 1 : 0
+  bucket                  = aws_s3_bucket.am_api_acceptance_tests_otp_bucket[0].id
   block_public_acls       = true
   ignore_public_acls      = true
   block_public_policy     = true
@@ -40,7 +47,7 @@ resource "aws_s3_bucket_public_access_block" "am_api_acceptance_tests_otp_privat
 }
 
 resource "aws_iam_policy" "am_api_acceptance_tests_otp_bucket_policy" {
-  count       = contains(["production", "integration"], var.environment) ? 0 : 1
+  count       = local.create_resource ? 1 : 0
   name        = "${var.environment}-s3-am_api_acceptance_tests_otp_bucket-policy"
   path        = "/"
   description = "IAM policy for managing S3 connection to the S3 Acceptance Tests bucket"
@@ -50,7 +57,7 @@ resource "aws_iam_policy" "am_api_acceptance_tests_otp_bucket_policy" {
 
 
 data "aws_iam_policy_document" "s3_am_api_acceptance_tests_otp_bucket_policy_document" {
-  count = contains(["production", "integration"], var.environment) ? 0 : 1
+  count = local.create_resource ? 1 : 0
   statement {
     sid    = "AllowAccessToWriteToS3"
     effect = "Allow"
@@ -59,21 +66,21 @@ data "aws_iam_policy_document" "s3_am_api_acceptance_tests_otp_bucket_policy_doc
       "s3:PutObject",
     ]
     resources = [
-      aws_s3_bucket.am-api-acceptance-tests-otp-bucket.arn,
-      "${aws_s3_bucket.am-api-acceptance-tests-otp-bucket.arn}/*",
+      aws_s3_bucket.am_api_acceptance_tests_otp_bucket[0].arn,
+      "${aws_s3_bucket.am_api_acceptance_tests_otp_bucket[0].arn}/*",
     ]
   }
 }
 
 
 resource "aws_iam_role_policy_attachment" "notification_lambda_smoketest_s3" {
-  count      = contains(["production", "integration"], var.environment) ? 0 : 1
+  count      = local.create_resource ? 1 : 0
   role       = module.account_management_api_send_notification_role.name
   policy_arn = aws_iam_policy.am_api_acceptance_tests_otp_bucket_policy[0].arn
 }
 
 resource "aws_iam_role_policy_attachment" "notification_sqs_lambda_s3" {
-  count      = contains(["production", "integration"], var.environment) ? 0 : 1
+  count      = local.create_resource ? 1 : 0
   role       = module.account_management_sqs_role.name
   policy_arn = aws_iam_policy.am_api_acceptance_tests_otp_bucket_policy[0].arn
 }
