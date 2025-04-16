58a56e2363cc2d073d311b5f87dbc80fefacc789
Ryan Andrews <ryan.andrews@digital.cabinet-office.gov.uk>
2025-04-16 16:55:46 +0100

    ATO-1515: Remove add usage from auth callback tests

--- oidc-api/src/test/java/uk/gov/di/authentication/oidc/lambda/AuthenticationCallbackHandlerTest.java
+++ oidc-api/src/test/java/uk/gov/di/authentication/oidc/lambda/AuthenticationCallbackHandlerTest.java
@@ -1385,7 +1385,6 @@ class AuthenticationCallbackHandlerTest {
 
         private void withPreviousSharedSessionDueToMaxAge() {
             var previousSharedSession = new Session();
-            PREVIOUS_CLIENT_SESSIONS.forEach(previousSharedSession::addClientSession);
             previousSharedSession.setEmailAddress(TEST_EMAIL_ADDRESS);
             when(sessionService.getSession(PREVIOUS_SESSION_ID))
                     .thenReturn(Optional.of(previousSharedSession));
@@ -1410,7 +1409,7 @@ class AuthenticationCallbackHandlerTest {
         }
 
         private Session withMaxAgeSharedSession() {
-            var session = new Session().addClientSession(CLIENT_SESSION_ID);
+            var session = new Session();
             when(sessionService.getSession(SESSION_ID)).thenReturn(Optional.of(session));
             return session;
         }
