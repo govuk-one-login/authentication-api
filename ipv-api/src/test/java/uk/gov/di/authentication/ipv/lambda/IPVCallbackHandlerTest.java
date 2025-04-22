4cc2a90aabb4cce18d567bb24c52a510859cc627
Gabriel.Swallow <gabriel.swallow@digital.cabinet-office.gov.uk>
2025-04-22 16:57:02 +0100

    ATO-1117: rename test to check for AuthUserInfo not UserProfile

--- ipv-api/src/test/java/uk/gov/di/authentication/ipv/lambda/IPVCallbackHandlerTest.java
+++ ipv-api/src/test/java/uk/gov/di/authentication/ipv/lambda/IPVCallbackHandlerTest.java
@@ -816,7 +816,7 @@ class IPVCallbackHandlerTest {
     }
 
     @Test
-    void shouldRedirectToFrontendErrorPageWhenUserProfileNotFound() throws ParseException {
+    void shouldRedirectToFrontendErrorPageWhenAuthUserInfoNotFound() throws ParseException {
         usingValidSession();
         usingValidClientSession();
         Map<String, String> responseHeaders = new HashMap<>();
