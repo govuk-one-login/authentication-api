4885c78990a9622376f48f5ba071e1414fbd7725
Matt Votsikas McLean <matt.votsikas-mclean@digital.cabinet-office.gov.uk>
2025-04-28 09:00:55 +0100

    AUT-1416: Convert from `MFAMethod` to `MfaMethodResponse`
    The `MfaMethodsService` returns an API agnostic record instances `MFAMethod`, which need to be converted to the new `MfaMethodResponse` for frontend-api responses.
    
    This new converter takes a set of `MFAMethod`s and utilising a new static `from` method on `MfaMethodResponse` does the conversion into the correct record types.
    
    This is in anticipation of needing the `LoginHandler` to return the `mfaMethods` of the user in the new `MfaMethodResponse` record.

--- frontend-api/src/main/java/uk/gov/di/authentication/frontendapi/entity/mfa/MfaMethodResponse.java
+++ frontend-api/src/main/java/uk/gov/di/authentication/frontendapi/entity/mfa/MfaMethodResponse.java
@@ -1,7 +1,11 @@
 package uk.gov.di.authentication.frontendapi.entity.mfa;
 
 import uk.gov.di.authentication.shared.entity.PriorityIdentifier;
+import uk.gov.di.authentication.shared.entity.Result;
+import uk.gov.di.authentication.shared.entity.mfa.MFAMethod;
 import uk.gov.di.authentication.shared.entity.mfa.MFAMethodType;
+
+import static uk.gov.di.authentication.frontendapi.helpers.FrontendApiPhoneNumberHelper.redactPhoneNumber;
 
 public sealed interface MfaMethodResponse permits SmsMfaMethodResponse, AuthAppMfaMethodResponse {
     String id();
@@ -9,4 +13,37 @@ public sealed interface MfaMethodResponse permits SmsMfaMethodResponse, AuthAppM
     PriorityIdentifier priority();
 
     MFAMethodType type();
+
+    static Result<String, MfaMethodResponse> from(MFAMethod mfaMethod) {
+        String id = mfaMethod.getMfaIdentifier();
+        MFAMethodType type;
+        try {
+            type = MFAMethodType.valueOf(mfaMethod.getMfaMethodType());
+        } catch (NullPointerException | IllegalArgumentException e) {
+            return Result.failure("Unsupported MFA method type: " + mfaMethod.getMfaMethodType());
+        }
+
+        PriorityIdentifier priority;
+        try {
+            priority = PriorityIdentifier.valueOf(mfaMethod.getPriority());
+        } catch (NullPointerException | IllegalArgumentException e) {
+            return Result.failure("Unsupported PriorityIdentifier: " + mfaMethod.getPriority());
+        }
+
+        return switch (type) {
+            case SMS -> {
+                String phoneNumber = mfaMethod.getDestination();
+                yield Result.success(
+                        new SmsMfaMethodResponse(
+                                id,
+                                MFAMethodType.SMS,
+                                priority,
+                                phoneNumber != null ? redactPhoneNumber(phoneNumber) : null));
+            }
+            case AUTH_APP -> Result.success(
+                    new AuthAppMfaMethodResponse(id, MFAMethodType.AUTH_APP, priority));
+            default -> Result.failure(
+                    "Unsupported MFA method type: " + mfaMethod.getMfaMethodType());
+        };
+    }
 }
