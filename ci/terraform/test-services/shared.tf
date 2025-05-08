750c887bd34f56c57080f65cebde38d203e50df4
dbes-gds <daniel.besbrode@digital.cabinet-office.gov.uk>
2025-05-08 14:40:48 +0100

    AUT-4196: grant delete synthetics user access to the user-credentials cmk

--- ci/terraform/test-services/shared.tf
+++ ci/terraform/test-services/shared.tf
@@ -9,11 +9,12 @@ data "terraform_remote_state" "shared" {
 }
 
 locals {
-  lambda_code_signing_configuration_arn = data.terraform_remote_state.shared.outputs.lambda_code_signing_configuration_arn
-  authentication_vpc_arn                = data.terraform_remote_state.shared.outputs.authentication_vpc_arn
-  authentication_private_subnet_ids     = data.terraform_remote_state.shared.outputs.authentication_private_subnet_ids
-  authentication_security_group_id      = data.terraform_remote_state.shared.outputs.authentication_security_group_id
-  user_profile_kms_key_arn              = data.terraform_remote_state.shared.outputs.user_profile_kms_key_arn
+  lambda_code_signing_configuration_arn  = data.terraform_remote_state.shared.outputs.lambda_code_signing_configuration_arn
+  authentication_vpc_arn                 = data.terraform_remote_state.shared.outputs.authentication_vpc_arn
+  authentication_private_subnet_ids      = data.terraform_remote_state.shared.outputs.authentication_private_subnet_ids
+  authentication_security_group_id       = data.terraform_remote_state.shared.outputs.authentication_security_group_id
+  user_profile_kms_key_arn               = data.terraform_remote_state.shared.outputs.user_profile_kms_key_arn
+  user_credentials_encryption_policy_arn = data.terraform_remote_state.shared.outputs.user_credentials_encryption_policy_arn
 
   slack_event_sns_topic_arn = data.terraform_remote_state.shared.outputs.slack_event_sns_topic_arn
   aws_account_alias         = data.terraform_remote_state.shared.outputs.aws_account_alias
