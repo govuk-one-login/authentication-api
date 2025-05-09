0971653a6871ecc7e9c11b96ee2d6aba2988bcea
Monjurul Haque <monjurul.haque@digital.cabinet-office.gov.uk>
2025-05-09 18:06:16 +0100

    AUT-4302: Session redis access from new_auth private subnets

--- ci/terraform/shared/security-groups.tf
+++ ci/terraform/shared/security-groups.tf
@@ -89,6 +89,19 @@ resource "aws_security_group_rule" "allow_incoming_session_redis_from_new_auth" 
   type        = "ingress"
 }
 
+resource "aws_security_group_rule" "allow_incoming_session_redis_from_new_auth_private_subnet" {
+  count = length(var.new_auth_privatesub_cidr_blocks) == 0 ? 0 : 1
+
+  description       = "Allow ingress to Redis from new Auth equivalent environment private subnets"
+  security_group_id = aws_security_group.redis_security_group.id
+
+  from_port   = local.redis_port_number
+  protocol    = "tcp"
+  cidr_blocks = var.new_auth_privatesub_cidr_blocks
+  to_port     = local.redis_port_number
+  type        = "ingress"
+}
+
 # Frontend Redis security group
 
 resource "aws_security_group" "frontend_redis_security_group" {
