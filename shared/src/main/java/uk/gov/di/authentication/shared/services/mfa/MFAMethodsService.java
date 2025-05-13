969f8543f1429f756273c0f9ec2f64aec89f4c97
Gabriel.Swallow <gabriel.swallow@digital.cabinet-office.gov.uk>
2025-05-13 16:52:23 +0100

    AUT-4315: define new interface for getMfaMethods
    The current method getMfaMethods makes a dynamoDb call to get UserProfile, when in the use case for this ticket, we already have access to UserProfile in the handler. By defining using this new interface, we limit calls to dynamoDb.

--- shared/src/main/java/uk/gov/di/authentication/shared/services/mfa/MFAMethodsService.java
+++ shared/src/main/java/uk/gov/di/authentication/shared/services/mfa/MFAMethodsService.java
@@ -99,6 +99,11 @@ public class MFAMethodsService {
             String email, boolean readOnly) {
         var userProfile = persistentService.getUserProfileByEmail(email);
         var userCredentials = persistentService.getUserCredentialsFromEmail(email);
+        return getMfaMethods(userProfile, userCredentials, readOnly);
+    }
+
+    public Result<MfaRetrieveFailureReason, List<MFAMethod>> getMfaMethods(
+            UserProfile userProfile, UserCredentials userCredentials, boolean readOnly) {
         if (userProfile == null || userCredentials == null) {
             return Result.failure(USER_DOES_NOT_HAVE_ACCOUNT);
         }
