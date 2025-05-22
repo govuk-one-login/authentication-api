16a94e677527d03228ac371f07625de2a90aae60
Ryan Andrews <ryan.andrews@digital.cabinet-office.gov.uk>
2025-05-22 15:03:27 +0100

    ATO-1643: Remove redundant write to redis in signup

--- frontend-api/src/main/java/uk/gov/di/authentication/frontendapi/lambda/SignUpHandler.java
+++ frontend-api/src/main/java/uk/gov/di/authentication/frontendapi/lambda/SignUpHandler.java
@@ -165,8 +165,6 @@ public class SignUpHandler extends BaseFrontendHandler<SignupRequest>
                     pair("rpPairwiseId", rpPairwiseId));
 
             LOG.info("Setting internal common subject identifier in user session");
-            sessionService.storeOrUpdateSession(
-                    userContext.getSession(), userContext.getAuthSession().getSessionId());
 
             authSessionService.updateSession(
                     authSessionItem
