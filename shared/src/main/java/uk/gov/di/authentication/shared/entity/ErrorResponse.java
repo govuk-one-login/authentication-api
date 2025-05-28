acb344d61ad273b160a312147906a8fef08f10c7
Andrew Moores <andrew.moores@digital.cabinet-office.gov.uk>
2025-05-28 17:08:44 +0100

    AUT-4320: Refactor to reduce complexity

--- shared/src/main/java/uk/gov/di/authentication/shared/entity/ErrorResponse.java
+++ shared/src/main/java/uk/gov/di/authentication/shared/entity/ErrorResponse.java
@@ -97,7 +97,7 @@ public enum ErrorResponse {
     ERROR_1083(1083, "User's account is suspended"),
     ERROR_1084(1084, "User's account is blocked"),
     NO_USER_PROFILE_FOR_EMAIL(1085, "Email from request does not have a user profile"),
-    NO_USER_CREDENTIALS_FOR_EMAIL(1086, "Email from request does not have any user credentials");
+    USER_DOES_NOT_HAVE_ACCOUNT(1086, "Email from request does not have any user credentials");
 
     private int code;
 
