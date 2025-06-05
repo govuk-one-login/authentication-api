c549b65c0edae203db97defb805498f235795a77
Ryan Andrews <ryan.andrews@digital.cabinet-office.gov.uk>
2025-06-05 13:46:49 +0100

    ATO-1471: Remove unused dynamo policies

--- ci/terraform/oidc/dynamo-policies.tf
+++ ci/terraform/oidc/dynamo-policies.tf
@@ -10,10 +10,6 @@ data "aws_dynamodb_table" "client_registry_table" {
   name = "${var.environment}-client-registry"
 }
 
-data "aws_dynamodb_table" "identity_credentials_table" {
-  name = "${var.environment}-identity-credentials"
-}
-
 data "aws_dynamodb_table" "doc_app_cri_credential_table" {
   name = "${var.environment}-doc-app-credential"
 }
@@ -180,95 +176,6 @@ data "aws_iam_policy_document" "dynamo_client_registration_read_policy_document"
       "kms:DescribeKey",
     ]
     resources = [local.client_registry_encryption_key_arn]
-  }
-}
-
-data "aws_iam_policy_document" "dynamo_identity_write_access_policy_document" {
-  statement {
-    sid    = "AllowAccessToDynamoTables"
-    effect = "Allow"
-
-    actions = [
-      "dynamodb:UpdateItem",
-      "dynamodb:PutItem",
-    ]
-    resources = [
-      data.aws_dynamodb_table.identity_credentials_table.arn,
-    ]
-  }
-
-  statement {
-    sid    = "AllowAccessToKms"
-    effect = "Allow"
-    actions = [
-      "kms:Encrypt",
-      "kms:Decrypt",
-      "kms:ReEncrypt*",
-      "kms:GenerateDataKey*",
-      "kms:CreateGrant",
-      "kms:DescribeKey",
-    ]
-    resources = [local.identity_credentials_encryption_key_arn]
-  }
-}
-
-data "aws_iam_policy_document" "dynamo_identity_delete_access_policy_document" {
-  statement {
-    sid    = "AllowAccessToDynamoTables"
-    effect = "Allow"
-
-    actions = [
-      "dynamodb:DeleteItem",
-    ]
-    resources = [
-      data.aws_dynamodb_table.identity_credentials_table.arn,
-    ]
-  }
-
-  statement {
-    sid    = "AllowAccessToKms"
-    effect = "Allow"
-    actions = [
-      "kms:Encrypt",
-      "kms:Decrypt",
-      "kms:ReEncrypt*",
-      "kms:GenerateDataKey*",
-      "kms:CreateGrant",
-      "kms:DescribeKey",
-    ]
-    resources = [local.identity_credentials_encryption_key_arn]
-  }
-}
-
-data "aws_iam_policy_document" "dynamo_identity_read_access_policy_document" {
-  statement {
-    sid    = "AllowAccessToDynamoTables"
-    effect = "Allow"
-
-    actions = [
-      "dynamodb:BatchGetItem",
-      "dynamodb:DescribeTable",
-      "dynamodb:Get*",
-      "dynamodb:Query",
-      "dynamodb:Scan",
-    ]
-    resources = [
-      data.aws_dynamodb_table.identity_credentials_table.arn,
-    ]
-  }
-
-  statement {
-    sid    = "AllowAccessToKms"
-    effect = "Allow"
-    actions = [
-      "kms:Encrypt",
-      "kms:Decrypt",
-      "kms:ReEncrypt*",
-      "kms:GenerateDataKey*",
-      "kms:CreateGrant",
-      "kms:DescribeKey",
-    ]
-    resources = [local.identity_credentials_encryption_key_arn]
   }
 }
 
@@ -949,29 +856,6 @@ resource "aws_iam_policy" "dynamo_user_write_access_policy" {
   policy = data.aws_iam_policy_document.dynamo_user_write_policy_document.json
 }
 
-resource "aws_iam_policy" "dynamo_identity_credentials_write_access_policy" {
-  name_prefix = "dynamo-access-policy"
-  path        = "/${var.environment}/oidc-default/"
-  description = "IAM policy for managing write permissions to the Dynamo Identity credentials table"
-
-  policy = data.aws_iam_policy_document.dynamo_identity_write_access_policy_document.json
-}
-
-resource "aws_iam_policy" "dynamo_identity_credentials_read_access_policy" {
-  name_prefix = "dynamo-access-policy"
-  path        = "/${var.environment}/oidc-default/"
-  description = "IAM policy for managing read permissions to the Dynamo Identity credentials table"
-
-  policy = data.aws_iam_policy_document.dynamo_identity_read_access_policy_document.json
-}
-
-resource "aws_iam_policy" "dynamo_identity_credentials_delete_access_policy" {
-  name_prefix = "dynamo-access-policy"
-  path        = "/${var.environment}/oidc-default/"
-  description = "IAM policy for managing delete permissions to the Dynamo Identity credentials table"
-
-  policy = data.aws_iam_policy_document.dynamo_identity_delete_access_policy_document.json
-}
 
 resource "aws_iam_policy" "dynamo_doc_app_write_access_policy" {
   name_prefix = "dynamo-access-policy"
