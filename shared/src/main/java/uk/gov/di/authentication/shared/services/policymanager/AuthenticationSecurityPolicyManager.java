332d1fb0831b8d3fabca249d37e1e5b2f33137e3
Andrew Moores <andrew.moores@digital.cabinet-office.gov.uk>
2025-06-05 14:01:03 +0100

    BAU: bump notification lambda

--- shared/src/main/java/uk/gov/di/authentication/shared/services/policymanager/AuthenticationSecurityPolicyManager.java
+++ shared/src/main/java/uk/gov/di/authentication/shared/services/policymanager/AuthenticationSecurityPolicyManager.java
@@ -1,9 +1,12 @@
-package uk.gov.di.authentication.shared.services;
+package uk.gov.di.authentication.shared.services.policymanager;
 
 import uk.gov.di.authentication.shared.entity.JourneyType;
 
 public interface AuthenticationSecurityPolicyManager {
 
+    // Delegation  methods to AuthenticationAttemptsService
+
+    // MFA
     boolean isMfaVerificationLocked(String subjectId, JourneyType journeyType, String activeMfaMethodId);
     boolean isMfaCodeResendLocked(String subjectId, JourneyType journeyType, String activeMfaMethodId);
     boolean isEmailVerificationLocked(String subjectId, JourneyType journeyType);
@@ -11,11 +14,21 @@ public interface AuthenticationSecurityPolicyManager {
 
     void incrementAttemptCount(String subjectId, JourneyType journeyType, String countType);
 
-    int getCount(String subjectId, JourneyType journeyType, String countType);
-
     void resetAttempts(String subjectId);
 
+    // Delegation methods to RedisConnectionService
+
+    // Auth Session table
+//    public void updateSession(AuthSessionItem sessionItem) {
+//        authSessionService.updateSession(
+//                authSessionItem.incrementCodeRequestCount(
+//                        request.getNotificationType(), request.getJourneyType()));
+//    }
+
+
+
     // Future extensions?
+    int getCount(String subjectId, JourneyType journeyType, String countType);
 
     boolean isAccountUnderIntervention(String subjectId);
     boolean isEmailBlockedByFraud(String subjectId);
