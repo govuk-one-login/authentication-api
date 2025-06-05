808a6a0be71e948ff223c678892df6b353744495
Ryan Andrews <ryan.andrews@digital.cabinet-office.gov.uk>
2025-06-05 13:48:31 +0100

    ATO-1471: Remove KMS key policy

--- ci/terraform/shared/kms-policies.tf
+++ ci/terraform/shared/kms-policies.tf
@@ -64,27 +64,6 @@ data "aws_iam_policy_document" "doc_app_credential_encryption_key_policy_documen
   }
 }
 
-resource "aws_iam_policy" "identity_credentials_encryption_key_kms_policy" {
-  name        = "${var.environment}-identity-credentials-table-encryption-key-kms-policy"
-  path        = "/"
-  description = "IAM policy for managing KMS encryption of the identity credentials table"
-
-  policy = data.aws_iam_policy_document.identity_credentials_encryption_key_policy_document.json
-}
-
-data "aws_iam_policy_document" "identity_credentials_encryption_key_policy_document" {
-  statement {
-    sid    = "AllowAccessToIdentityCredentialsTableKmsEncryptionKey"
-    effect = "Allow"
-
-    actions = [
-      "kms:*",
-    ]
-    resources = [
-      aws_kms_key.identity_credentials_table_encryption_key.arn
-    ]
-  }
-}
 
 resource "aws_iam_policy" "client_registry_encryption_key_kms_policy" {
   name        = "${var.environment}-client-registry-table-encryption-key-kms-policy"
