1642edae5e8d7321e6954e89fc987022b2b8df80
Ryan Andrews <ryan.andrews@digital.cabinet-office.gov.uk>
2025-06-11 15:17:06 +0100

    ATO-1724: Add session not found exception

---
+++ orchestration-shared/src/main/java/uk/gov/di/orchestration/shared/exceptions/SessionNotFoundException.java
@@ -0,0 +1,8 @@
+package uk.gov.di.orchestration.shared.exceptions;
+
+public class SessionNotFoundException extends Exception {
+
+    public SessionNotFoundException(String message) {
+        super(message);
+    }
+}
