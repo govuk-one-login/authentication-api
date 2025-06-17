49f0d42260d173c3f57d8e11d506168184187f1a
Matt W <173997080+sw-mattw@users.noreply.github.com>
2025-06-17 18:27:58 +0100

    AUT-4371: Add template IDs to tfvars

--- ci/terraform/delivery-receipts/variables.tf
+++ ci/terraform/delivery-receipts/variables.tf
@@ -74,6 +74,9 @@ variable "notify_template_map" {
     TERMS_AND_CONDITIONS_BULK_EMAIL_TEMPLATE_ID            = "067548f2-420d-4da9-923f-ec9a941706cf"
     REPORT_SUSPICIOUS_ACTIVITY_EMAIL_TEMPLATE_ID           = "2b3170b5-159e-457f-a282-f30f6006dc32"
     BACKUP_METHOD_ADDED_TEMPLATE_ID                        = "2abd5f54-15b6-4957-b4d3-f310f2437b9f"
+    CHANGED_AUTHENTICATOR_APP_TEMPLATE_ID                  = "b0bb3667-985b-428c-9eb3-6b778b50fb6b"
+    CHANGED_DEFAULT_MFA_TEMPLATE_ID                        = "ab62d5fa-79a8-4dba-beb0-283118d2450f"
+    SWITCHED_MFA_METHODS_TEMPLATE_ID                       = "be78564b-b9a6-4b3d-b438-7b30e45caf54"
   }
 }
 
