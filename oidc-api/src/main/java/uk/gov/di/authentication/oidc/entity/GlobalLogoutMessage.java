a7ccae9a7a4103f8047fcd11da090460bba8ac78
Craig Earl <craig.earl@digital.cabinet-office.gov.uk>
2025-06-18 11:52:42 +0100

    ATO-1658: Fix format of incoming event
    - We were missing some fields on the logout message, as defined in the event mapping in TxMA.
    - Also removes duplicated tests (that are testing the same thing with different fields)

--- oidc-api/src/main/java/uk/gov/di/authentication/oidc/entity/GlobalLogoutMessage.java
+++ oidc-api/src/main/java/uk/gov/di/authentication/oidc/entity/GlobalLogoutMessage.java
@@ -4,21 +4,39 @@ import com.google.gson.annotations.Expose;
 import uk.gov.di.orchestration.shared.validation.Required;
 
 public class GlobalLogoutMessage {
-    @Expose @Required private String internalCommonSubjectId;
+    @Expose @Required private String clientId;
+    @Expose @Required private String eventId;
     @Expose @Required private String sessionId;
     @Expose @Required private String clientSessionId;
+    @Expose @Required private String internalCommonSubjectId;
+    @Expose @Required private String persistentSessionId;
+    @Expose @Required private String ipAddress;
 
     public GlobalLogoutMessage() {}
 
     public GlobalLogoutMessage(
-            String internalCommonSubjectId, String sessionId, String clientSessionId) {
-        this.internalCommonSubjectId = internalCommonSubjectId;
+            String clientId,
+            String eventId,
+            String sessionId,
+            String clientSessionId,
+            String internalCommonSubjectId,
+            String persistentSessionId,
+            String ipAddress) {
+        this.clientId = clientId;
+        this.eventId = eventId;
         this.sessionId = sessionId;
         this.clientSessionId = clientSessionId;
+        this.internalCommonSubjectId = internalCommonSubjectId;
+        this.persistentSessionId = persistentSessionId;
+        this.ipAddress = ipAddress;
     }
 
-    public String getInternalCommonSubjectId() {
-        return internalCommonSubjectId;
+    public String getClientId() {
+        return clientId;
+    }
+
+    public String getEventId() {
+        return eventId;
     }
 
     public String getSessionId() {
@@ -27,5 +45,17 @@ public class GlobalLogoutMessage {
 
     public String getClientSessionId() {
         return clientSessionId;
+    }
+
+    public String getInternalCommonSubjectId() {
+        return internalCommonSubjectId;
+    }
+
+    public String getPersistentSessionId() {
+        return persistentSessionId;
+    }
+
+    public String getIpAddress() {
+        return ipAddress;
     }
 }
