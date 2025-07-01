da8b0e0f40798caa867ad706a69d61d708ce1817
dbes-gds <daniel.besbrode@digital.cabinet-office.gov.uk>
2025-07-01 18:50:50 +0100

    OLH-2698: Submit AUTH_MFA_METHOD_DELETE_COMPLETED in MFAMethodsDeleteHandler

--- account-management-api/src/main/java/uk/gov/di/accountmanagement/lambda/MFAMethodsDeleteHandler.java
+++ account-management-api/src/main/java/uk/gov/di/accountmanagement/lambda/MFAMethodsDeleteHandler.java
@@ -9,27 +9,44 @@ import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.ThreadContext;
 import uk.gov.di.accountmanagement.entity.NotificationType;
 import uk.gov.di.accountmanagement.entity.NotifyRequest;
+import uk.gov.di.accountmanagement.helpers.AuditHelper;
 import uk.gov.di.accountmanagement.helpers.PrincipalValidationHelper;
 import uk.gov.di.accountmanagement.services.AwsSqsClient;
+import uk.gov.di.audit.AuditContext;
 import uk.gov.di.authentication.shared.entity.ErrorResponse;
+import uk.gov.di.authentication.shared.entity.JourneyType;
+import uk.gov.di.authentication.shared.entity.Result;
 import uk.gov.di.authentication.shared.entity.UserProfile;
+import uk.gov.di.authentication.shared.entity.mfa.MFAMethod;
+import uk.gov.di.authentication.shared.entity.mfa.MFAMethodType;
+import uk.gov.di.authentication.shared.helpers.ClientSessionIdHelper;
+import uk.gov.di.authentication.shared.helpers.ClientSubjectHelper;
+import uk.gov.di.authentication.shared.helpers.IpAddressHelper;
 import uk.gov.di.authentication.shared.helpers.LocaleHelper;
+import uk.gov.di.authentication.shared.helpers.PersistentIdHelper;
 import uk.gov.di.authentication.shared.helpers.RequestHeaderHelper;
 import uk.gov.di.authentication.shared.serialization.Json;
+import uk.gov.di.authentication.shared.services.AuditService;
 import uk.gov.di.authentication.shared.services.ConfigurationService;
 import uk.gov.di.authentication.shared.services.DynamoService;
 import uk.gov.di.authentication.shared.services.SerializationService;
 import uk.gov.di.authentication.shared.services.mfa.MFAMethodsService;
 
+import java.util.List;
 import java.util.Map;
 
+import static uk.gov.di.authentication.shared.domain.AuditableEvent.AUDIT_EVENT_EXTENSIONS_JOURNEY_TYPE;
+import static uk.gov.di.authentication.shared.domain.AuditableEvent.AUDIT_EVENT_EXTENSIONS_MFA_METHOD;
+import static uk.gov.di.authentication.shared.domain.AuditableEvent.AUDIT_EVENT_EXTENSIONS_MFA_TYPE;
 import static uk.gov.di.authentication.shared.domain.RequestHeaders.SESSION_ID_HEADER;
+import static uk.gov.di.authentication.shared.entity.AuthSessionItem.ATTRIBUTE_CLIENT_ID;
 import static uk.gov.di.authentication.shared.helpers.ApiGatewayResponseHelper.generateApiGatewayProxyErrorResponse;
 import static uk.gov.di.authentication.shared.helpers.ApiGatewayResponseHelper.generateEmptySuccessApiGatewayResponse;
 import static uk.gov.di.authentication.shared.helpers.InstrumentationHelper.segmentedFunctionCall;
 import static uk.gov.di.authentication.shared.helpers.LocaleHelper.getUserLanguageFromRequestHeaders;
 import static uk.gov.di.authentication.shared.helpers.LocaleHelper.matchSupportedLanguage;
 import static uk.gov.di.authentication.shared.helpers.LogLineHelper.attachSessionIdToLogs;
+import static uk.gov.di.authentication.shared.services.AuditService.MetadataPair.pair;
 
 public class MFAMethodsDeleteHandler
         implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
@@ -41,6 +58,7 @@ public class MFAMethodsDeleteHandler
     private final MFAMethodsService mfaMethodsService;
     private final DynamoService dynamoService;
     private final AwsSqsClient sqsClient;
+    private final AuditService auditService;
 
     public MFAMethodsDeleteHandler() {
         this(ConfigurationService.getInstance());
@@ -55,17 +73,20 @@ public class MFAMethodsDeleteHandler
                         configurationService.getAwsRegion(),
                         configurationService.getEmailQueueUri(),
                         configurationService.getSqsEndpointUri());
+        this.auditService = new AuditService(configurationService);
     }
 
     public MFAMethodsDeleteHandler(
             ConfigurationService configurationService,
             MFAMethodsService mfaMethodsService,
             DynamoService dynamoService,
-            AwsSqsClient sqsClient) {
+            AwsSqsClient sqsClient,
+            AuditService auditService) {
         this.configurationService = configurationService;
         this.mfaMethodsService = mfaMethodsService;
         this.dynamoService = dynamoService;
         this.sqsClient = sqsClient;
+        this.auditService = auditService;
     }
 
     @Override
@@ -139,6 +160,15 @@ public class MFAMethodsDeleteHandler
 
         LOG.info("Successfully deleted MFA method {}", mfaIdentifier);
 
+        Result<ErrorResponse, AuditContext> auditContextResult =
+                buildAuditContext(input, userProfile, deleteResult.getSuccess());
+        //        if (auditContextResult.isFailure()) {
+        //            return generateApiGatewayProxyErrorResponse(401,
+        // auditContextResult.getFailure());
+        //        }
+        //        auditService.submitAuditEvent(AUTH_MFA_METHOD_DELETE_COMPLETED,
+        // auditContextResult.getSuccess());
+
         LocaleHelper.SupportedLanguage userLanguage =
                 matchSupportedLanguage(
                         getUserLanguageFromRequestHeaders(
@@ -160,5 +190,55 @@ public class MFAMethodsDeleteHandler
         Map<String, String> headers = input.getHeaders();
         String sessionId = RequestHeaderHelper.getHeaderValueOrElse(headers, SESSION_ID_HEADER, "");
         attachSessionIdToLogs(sessionId);
+    }
+
+    private Result<ErrorResponse, AuditContext> buildAuditContext(
+            APIGatewayProxyRequestEvent input, UserProfile userProfile, MFAMethod mfaMethod) {
+        try {
+
+            var phoneNumber =
+                    mfaMethod.getMfaMethodType().equals(MFAMethodType.SMS)
+                            ? mfaMethod.getDestination()
+                            : AuditService.UNKNOWN;
+
+            var metadataPairs =
+                    new AuditService.MetadataPair[] {
+                        pair(
+                                AUDIT_EVENT_EXTENSIONS_JOURNEY_TYPE,
+                                JourneyType.ACCOUNT_MANAGEMENT.getValue()),
+                        pair(
+                                AUDIT_EVENT_EXTENSIONS_MFA_METHOD,
+                                mfaMethod.getPriority().toLowerCase()),
+                        pair(
+                                AUDIT_EVENT_EXTENSIONS_MFA_TYPE,
+                                mfaMethod.getMfaMethodType().toLowerCase())
+                    };
+
+            var context =
+                    new AuditContext(
+                            input.getRequestContext()
+                                    .getAuthorizer()
+                                    .getOrDefault(ATTRIBUTE_CLIENT_ID, AuditService.UNKNOWN)
+                                    .toString(),
+                            ClientSessionIdHelper.extractSessionIdFromHeaders(input.getHeaders()),
+                            RequestHeaderHelper.getHeaderValueOrElse(
+                                    input.getHeaders(), SESSION_ID_HEADER, ""),
+                            ClientSubjectHelper.getSubjectWithSectorIdentifier(
+                                            userProfile,
+                                            configurationService.getInternalSectorUri(),
+                                            dynamoService)
+                                    .getValue(),
+                            userProfile.getEmail(),
+                            IpAddressHelper.extractIpAddress(input),
+                            phoneNumber,
+                            PersistentIdHelper.extractPersistentIdFromHeaders(input.getHeaders()),
+                            AuditHelper.getTxmaAuditEncoded(input.getHeaders()),
+                            List.of(metadataPairs));
+
+            return Result.success(context);
+        } catch (Exception e) {
+            LOG.error("Error building audit context", e);
+            return Result.failure(ErrorResponse.ERROR_1071);
+        }
     }
 }
