72cad396bbfbac75be24ce6de8820316d5235771
Craig Earl <craig.earl@digital.cabinet-office.gov.uk>
2025-07-14 11:07:57 +0100

    ATO-1802: Add dynamo class for SlidingWindowData
    - Dynamo has a LocalDateTimeAttributeConverter we can use (it just stores it as a string using the `toString()` method)

---
+++ oidc-api/src/main/java/uk/gov/di/authentication/oidc/entity/SlidingWindowData.java
@@ -0,0 +1,93 @@
+package uk.gov.di.authentication.oidc.entity;
+
+import software.amazon.awssdk.enhanced.dynamodb.internal.converter.attribute.LocalDateTimeAttributeConverter;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbAttribute;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbBean;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbConvertedBy;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbPartitionKey;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbSortKey;
+
+import java.time.LocalDateTime;
+
+@DynamoDbBean
+public class SlidingWindowData {
+
+    public static final String ATTRIBUTE_CLIENT_ID = "ClientId";
+    public static final String ATTRIBUTE_PERIOD_START_TIME = "PeriodStartTime";
+    public static final String ATTRIBUTE_REQUEST_COUNT = "RequestCount";
+    public static final String ATTRIBUTE_TTL = "ttl";
+
+    private String clientId;
+    private LocalDateTime periodStartTime;
+    private Long requestCount;
+    private long timeToLive;
+
+    public SlidingWindowData() {}
+
+    public SlidingWindowData(
+            String clientId, LocalDateTime periodStartTime, Long requestCount, long timeToLive) {
+        this.clientId = clientId;
+        this.periodStartTime = periodStartTime;
+        this.requestCount = requestCount;
+        this.timeToLive = timeToLive;
+    }
+
+    @DynamoDbPartitionKey
+    @DynamoDbAttribute(ATTRIBUTE_CLIENT_ID)
+    public String getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(String clientId) {
+        this.clientId = clientId;
+    }
+
+    public SlidingWindowData withClientId(String clientId) {
+        this.clientId = clientId;
+        return this;
+    }
+
+    @DynamoDbSortKey
+    @DynamoDbAttribute(ATTRIBUTE_PERIOD_START_TIME)
+    @DynamoDbConvertedBy(LocalDateTimeAttributeConverter.class)
+    public LocalDateTime getPeriodStartTime() {
+        return periodStartTime;
+    }
+
+    public void setPeriodStartTime(LocalDateTime periodStartTime) {
+        this.periodStartTime = periodStartTime;
+    }
+
+    public SlidingWindowData withPeriodStartTime(LocalDateTime periodStartTime) {
+        this.periodStartTime = periodStartTime;
+        return this;
+    }
+
+    @DynamoDbAttribute(ATTRIBUTE_REQUEST_COUNT)
+    public Long getRequestCount() {
+        return requestCount;
+    }
+
+    public void setRequestCount(Long requestCount) {
+        this.requestCount = requestCount;
+    }
+
+    public SlidingWindowData withRequestCount(Long requestCount) {
+        this.requestCount = requestCount;
+        return this;
+    }
+
+    @DynamoDbAttribute(ATTRIBUTE_TTL)
+    public long getTimeToLive() {
+        return timeToLive;
+    }
+
+    public void setTimeToLive(long timeToLive) {
+        this.timeToLive = timeToLive;
+    }
+
+    public SlidingWindowData withTimeToLive(long timeToLive) {
+        this.timeToLive = timeToLive;
+        return this;
+    }
+}
