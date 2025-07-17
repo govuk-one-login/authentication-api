9d55cc477996e5aa7d130b4f121c22f605e701fb
Ryan Andrews <ryan.andrews@digital.cabinet-office.gov.uk>
2025-07-17 14:27:04 +0100

    ATO-1782: Add metric for limit exceeded
    Adds a metric with the limit exceeded with the client id and
    decision attached

--- oidc-api/src/main/java/uk/gov/di/authentication/oidc/services/RateLimitService.java
+++ oidc-api/src/main/java/uk/gov/di/authentication/oidc/services/RateLimitService.java
@@ -1,15 +1,25 @@
 package uk.gov.di.authentication.oidc.services;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import uk.gov.di.authentication.oidc.entity.ClientRateLimitConfig;
 import uk.gov.di.authentication.oidc.entity.RateLimitAlgorithm;
 import uk.gov.di.authentication.oidc.entity.RateLimitDecision;
+import uk.gov.di.orchestration.shared.services.CloudwatchMetricsService;
+
+import java.util.Map;
 
 public class RateLimitService {
 
     private final RateLimitAlgorithm rateLimitAlgorithm;
+    private final CloudwatchMetricsService cloudwatchMetricsService;
+    private static final Logger LOG = LogManager.getLogger(RateLimitService.class);
 
-    public RateLimitService(RateLimitAlgorithm rateLimitAlgorithm) {
+    public RateLimitService(
+            RateLimitAlgorithm rateLimitAlgorithm,
+            CloudwatchMetricsService cloudwatchMetricsService) {
         this.rateLimitAlgorithm = rateLimitAlgorithm;
+        this.cloudwatchMetricsService = cloudwatchMetricsService;
     }
 
     public RateLimitDecision getClientRateLimitDecision(
@@ -18,9 +28,28 @@ public class RateLimitService {
             return RateLimitDecision.NOT_CONFIGURED_NO_ACTION;
         }
 
-        if (rateLimitAlgorithm.hasRateLimitExceeded(clientRateLimitConfig)) {
-            return RateLimitDecision.OVER_LIMIT_RETURN_TO_RP;
+        var rateLimitExceeded = rateLimitAlgorithm.hasRateLimitExceeded(clientRateLimitConfig);
+
+        if (rateLimitExceeded) {
+            var decision = RateLimitDecision.OVER_LIMIT_RETURN_TO_RP;
+            emitRateLimitExceededMetric(decision, clientRateLimitConfig.clientID());
+
+            return decision;
         }
         return RateLimitDecision.UNDER_LIMIT_NO_ACTION;
+    }
+
+    private void emitRateLimitExceededMetric(RateLimitDecision rateLimitDecision, String clientId) {
+        try {
+            cloudwatchMetricsService.incrementCounter(
+                    "RpRateLimitExceeded",
+                    Map.of(
+                            "clientId",
+                            clientId,
+                            "action",
+                            rateLimitDecision.getAction().toString()));
+        } catch (Exception e) {
+            LOG.warn("Error incrementing RpRateLimitExceeded metric", e);
+        }
     }
 }
