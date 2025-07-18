78dfbdb49f054ceb69f94983f86890125b4ec3db
Matt Votsikas McLean <matt.votsikas-mclean@digital.cabinet-office.gov.uk>
2025-07-18 11:49:07 +0100

    AUT-4560: Add methods for verify MFA OTP actions
    Create methods for verifying the MFA OTP and recording when the correct or incorrect MFA is received.
    
    The intent in the UserPermissionService is to handle the business logic for the lockout and work with the right backend datastores to increment, reset and check the counts and blocks in different JourneyType contexts.
    
    This leaves a straightforward API for handlers to use to check and record user actions, without having to know about how it's implemented.
    
    This also adds an example handler to give a demonstration of how this can be used.

--- user-permissions/src/main/java/uk/gov/di/authentication/userpermissions/UserPermissions.java
+++ user-permissions/src/main/java/uk/gov/di/authentication/userpermissions/UserPermissions.java
@@ -1,3 +1,19 @@
 package uk.gov.di.authentication.userpermissions;
 
-public interface UserPermissions {}
+import uk.gov.di.authentication.shared.entity.JourneyType;
+import uk.gov.di.authentication.shared.entity.Result;
+import uk.gov.di.authentication.userpermissions.entity.Decision;
+import uk.gov.di.authentication.userpermissions.entity.DecisionError;
+import uk.gov.di.authentication.userpermissions.entity.RecordError;
+import uk.gov.di.authentication.userpermissions.entity.UserPermissionContext;
+
+public interface UserPermissions {
+    public Result<DecisionError, Decision> canUserVerifyMfaOtp(
+            JourneyType journeyType, UserPermissionContext userPermissionContext);
+
+    public Result<RecordError, Void> recordIncorrectMfaOtpReceived(
+            JourneyType journeyType, UserPermissionContext userPermissionContext);
+
+    public Result<RecordError, Void> recordCorrectMfaOtpReceived(
+            JourneyType journeyType, UserPermissionContext userPermissionContext);
+}
---
+++ user-permissions/src/main/java/uk/gov/di/authentication/userpermissions/example/ExampleMfaVerificationHandler.java
@@ -0,0 +1,63 @@
+package uk.gov.di.authentication.userpermissions.example;
+
+import uk.gov.di.audit.AuditContext;
+import uk.gov.di.authentication.shared.entity.AuthSessionItem;
+import uk.gov.di.authentication.shared.entity.JourneyType;
+import uk.gov.di.authentication.userpermissions.UserPermissions;
+import uk.gov.di.authentication.userpermissions.entity.Decision;
+import uk.gov.di.authentication.userpermissions.entity.UserPermissionContext;
+
+import static java.lang.String.format;
+import static uk.gov.di.authentication.shared.services.AuditService.MetadataPair.pair;
+
+public class ExampleMfaVerificationHandler {
+    private static final String EXPECTED_OTP = "372615";
+
+    private UserPermissions userPermissions;
+
+    public String handle(String submittedOtp) {
+        JourneyType journeyType = JourneyType.SIGN_IN;
+        var userPermissionContext =
+                UserPermissionContext.builder()
+                        .withInternalSubjectId("439qklhxm39qfja3sdg43")
+                        .withRpPairwiseId(
+                                "urn:fdc:gov.uk:2022:D0eushEU31EeUDUi_EJVz2seIGwfF_QfTZSm_yq1Rfs")
+                        .withEmailAddress("test@example.com")
+                        .withAuthSessionItem(new AuthSessionItem())
+                        .build();
+
+        var checkResult = userPermissions.canUserVerifyMfaOtp(journeyType, userPermissionContext);
+        if (checkResult.isFailure()) {
+            return (format("500: %s", checkResult.getFailure().name()));
+        }
+
+        var decision = checkResult.getSuccess();
+        if (decision instanceof Decision.TemporarilyLockedOut lockedOut) {
+            sendAuditEvent(
+                    "LOCKED_OUT",
+                    AuditContext.emptyAuditContext()
+                            .withMetadataItem(pair("reason", lockedOut.forbiddenReason()))
+                            .withMetadataItem(pair("until", lockedOut.lockedUntil())));
+            return (format(
+                    "403: User is temporarily locked out due to %s until %s",
+                    lockedOut.forbiddenReason(), lockedOut.lockedUntil()));
+        }
+
+        if (!submittedOtp.equals(EXPECTED_OTP)) {
+            userPermissions.recordIncorrectMfaOtpReceived(journeyType, userPermissionContext);
+            return ("400: Incorrect OTP received");
+        }
+
+        sendAuditEvent(
+                "OTP_VERIFIED",
+                AuditContext.emptyAuditContext()
+                        .withMetadataItem(pair("attempts", decision.attemptCount() + 1)));
+
+        userPermissions.recordCorrectMfaOtpReceived(journeyType, userPermissionContext);
+        return ("200: Success");
+    }
+
+    private void sendAuditEvent(String eventName, AuditContext auditContext) {
+        System.out.println(eventName + auditContext.toString());
+    }
+}
