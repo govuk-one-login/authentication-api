7f2d67c3118dc3470c0a772b2cd12288fcaef851
Craig Earl <craig.earl@digital.cabinet-office.gov.uk>
2025-07-22 13:00:31 +0100

    ATO-1860: Add test coverage for JwksUtils.retrieveJwksFromUrl

--- orchestration-shared-test/src/main/java/uk/gov/di/orchestration/sharedtest/extensions/JwksExtension.java
+++ orchestration-shared-test/src/main/java/uk/gov/di/orchestration/sharedtest/extensions/JwksExtension.java
@@ -5,7 +5,11 @@ import org.junit.jupiter.api.extension.BeforeAllCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import uk.gov.di.orchestration.sharedtest.httpstub.HttpStubExtension;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class JwksExtension extends HttpStubExtension implements BeforeAllCallback {
+    private String endpoint = "/.well-known/jwks.json";
 
     public JwksExtension(int port) {
         super(port);
@@ -15,11 +19,16 @@ public class JwksExtension extends HttpStubExtension implements BeforeAllCallbac
         super();
     }
 
+    public URL getJwksUrl() throws MalformedURLException {
+        return uri(endpoint).toURL();
+    }
+
     public void init(JWKSet jwkSet) {
         init("/.well-known/jwks.json", jwkSet);
     }
 
     public void init(String path, JWKSet jwkSet) {
+        endpoint = path;
         register(path, 200, "application/json", jwkSet.toPublicJWKSet().toString());
     }
 
