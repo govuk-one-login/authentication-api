def3747a3f805ebf1144f33069711c6323e780aa
Andrew Moores <andrew.moores@digital.cabinet-office.gov.uk>
2025-07-24 12:09:31 +0100

    AUT-4569: standardize CheckUserExistsIntegrationTest
    Add test constants, organize into @Nested classes with @DisplayName
    annotations, update method names to describe user actions, add helper
    methods to reduce duplication, remove unused imports and clean up
    formatting.

--- integration-tests/src/test/java/uk/gov/di/authentication/api/QCheckUserExistsIntegrationTest.java
+++ integration-tests/src/test/java/uk/gov/di/authentication/api/QCheckUserExistsIntegrationTest.java
@@ -10,16 +10,17 @@ import org.junit.jupiter.params.provider.EnumSource;
 import uk.gov.di.authentication.frontendapi.entity.CheckUserExistsRequest;
 import uk.gov.di.authentication.frontendapi.entity.CheckUserExistsResponse;
 import uk.gov.di.authentication.frontendapi.lambda.CheckUserExistsHandler;
-import uk.gov.di.authentication.shared.entity.CodeRequestType;
 import uk.gov.di.authentication.shared.entity.ErrorResponse;
 import uk.gov.di.authentication.shared.entity.JourneyType;
 import uk.gov.di.authentication.shared.entity.mfa.MFAMethodType;
+import uk.gov.di.authentication.shared.helpers.ClientSubjectHelper;
 import uk.gov.di.authentication.shared.helpers.IdGenerator;
 import uk.gov.di.authentication.shared.serialization.Json.JsonException;
 import uk.gov.di.authentication.shared.services.CodeStorageService;
 import uk.gov.di.authentication.sharedtest.basetest.ApiGatewayHandlerIntegrationTest;
 
 import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -39,18 +40,17 @@ import static uk.gov.di.authentication.sharedtest.helper.AuditAssertionsHelper.a
 import static uk.gov.di.authentication.sharedtest.matchers.APIGatewayProxyResponseEventMatcher.hasJsonBody;
 import static uk.gov.di.authentication.sharedtest.matchers.APIGatewayProxyResponseEventMatcher.hasStatus;
 
-class CheckUserExistsIntegrationTest extends ApiGatewayHandlerIntegrationTest {
+class QCheckUserExistsIntegrationTest extends ApiGatewayHandlerIntegrationTest {
 
-    private static final URI REDIRECT_URI =
-            URI.create(System.getenv("STUB_RELYING_PARTY_REDIRECT_URI"));
-    private static final ClientID CLIENT_ID = new ClientID("test-client");
-    private static final String CLIENT_NAME = "some-client-name";
-    private static final String SECTOR_IDENTIFIER_HOST = "test.com";
-    private static final String TEST_EMAIL_1 = "joe.bloggs+1@digital.cabinet-office.gov.uk";
+    private static final String TEST_EMAIL = "joe.bloggs+1@digital.cabinet-office.gov.uk";
     private static final String TEST_EMAIL_2 = "joe.bloggs+2@digital.cabinet-office.gov.uk";
     private static final String TEST_PASSWORD = "password-1";
     private static final String TEST_PHONE_NUMBER = "+44987654321";
-    private static final String TEST_CREDENTIAL = "credential";
+    private static final String MFA_CREDENTIAL = "credential";
+    private static final URI REDIRECT_URI = URI.create(System.getenv("STUB_RELYING_PARTY_REDIRECT_URI"));
+    private static final ClientID CLIENT_ID = new ClientID("test-client");
+    private static final String CLIENT_NAME = "some-client-name";
+    private static final String SECTOR_IDENTIFIER_HOST = "test.com";
 
     @BeforeEach
     void setUp() {
@@ -59,92 +59,102 @@ class CheckUserExistsIntegrationTest extends ApiGatewayHandlerIntegrationTest {
     }
 
     @Nested
-    @DisplayName("Successful user lookup")
-    class SuccessfulUserLookup {
+    @DisplayName("Successful user existence checks")
+    class SuccessfulUserExistenceChecks {
 
         @ParameterizedTest
         @EnumSource(value = MFAMethodType.class, names = {"SMS", "AUTH_APP"})
-        @DisplayName("User can check if their account exists with MFA configured")
-        void userCanCheckIfTheirAccountExistsWithMfaConfigured(MFAMethodType mfaMethodType)
-                throws JsonException {
-            var sessionId = setupUserAndSession(TEST_EMAIL_1, mfaMethodType);
+        @DisplayName("User can check if account exists with MFA method configured")
+        void userCanCheckIfAccountExistsWithMfaMethodConfigured(MFAMethodType mfaMethodType) throws JsonException, URISyntaxException {
+            var sessionId = setupSessionAndClient();
             var clientSessionId = IdGenerator.generate();
+            userStore.signUp(TEST_EMAIL, TEST_PASSWORD);
+            var salt = userStore.addSalt(TEST_EMAIL);
 
-            var request = new CheckUserExistsRequest(TEST_EMAIL_1);
+            var userProfile = userStore.getUserProfileFromEmail(TEST_EMAIL).orElseThrow();
+            var expectedInternalCommonSubjectId = ClientSubjectHelper.calculatePairwiseIdentifier(
+                    userProfile.getSubjectID(),
+                    new URI(TXMA_ENABLED_CONFIGURATION_SERVICE.getInternalSectorUri()),
+                    salt);
+
+            if (MFAMethodType.SMS == mfaMethodType) {
+                userStore.addMfaMethod(TEST_EMAIL, mfaMethodType, false, true, MFA_CREDENTIAL);
+                userStore.addVerifiedPhoneNumber(TEST_EMAIL, TEST_PHONE_NUMBER);
+            } else {
+                userStore.addMfaMethod(TEST_EMAIL, mfaMethodType, true, true, MFA_CREDENTIAL);
+            }
+
+            var request = new CheckUserExistsRequest(TEST_EMAIL);
             var response = makeRequest(
                     Optional.of(request),
                     constructFrontendHeaders(sessionId, clientSessionId),
                     Map.of());
 
             assertThat(response, hasStatus(200));
-            CheckUserExistsResponse checkUserExistsResponse =
-                    objectMapper.readValue(response.getBody(), CheckUserExistsResponse.class);
-            assertThat(checkUserExistsResponse.email(), equalTo(TEST_EMAIL_1));
+            CheckUserExistsResponse checkUserExistsResponse = objectMapper.readValue(response.getBody(), CheckUserExistsResponse.class);
+            assertThat(checkUserExistsResponse.email(), equalTo(TEST_EMAIL));
             assertThat(checkUserExistsResponse.mfaMethodType(), equalTo(mfaMethodType));
             assertTrue(checkUserExistsResponse.doesUserExist());
-            
+            assertThat(
+                    authSessionStore.getSession(sessionId).orElseThrow().getInternalCommonSubjectId(),
+                    equalTo(expectedInternalCommonSubjectId));
             if (MFAMethodType.SMS.equals(mfaMethodType)) {
                 assertThat(checkUserExistsResponse.phoneNumberLastThree(), equalTo("321"));
-            } else {
+            } else if (MFAMethodType.AUTH_APP.equals(mfaMethodType)) {
                 assertNull(checkUserExistsResponse.phoneNumberLastThree());
             }
-            
             assertTxmaAuditEventsReceived(txmaAuditQueue, List.of(AUTH_CHECK_USER_KNOWN_EMAIL));
         }
 
         @Test
-        @DisplayName("User can check account status when MFA is temporarily locked")
-        void userCanCheckAccountStatusWhenMfaIsTemporarilyLocked() throws JsonException {
-            var sessionId = setupUserAndSession(TEST_EMAIL_1, MFAMethodType.AUTH_APP);
+        @DisplayName("User can check account existence and receive lockout information for Auth App MFA")
+        void userCanCheckAccountExistenceAndReceiveLockoutInformationForAuthAppMfa() throws JsonException {
+            var sessionId = setupSessionAndClient();
             var clientSessionId = IdGenerator.generate();
-            var codeRequestType = CodeRequestType.getCodeRequestType(MFAMethodType.AUTH_APP, JourneyType.SIGN_IN);
-            var codeBlockedKeyPrefix = CODE_BLOCKED_KEY_PREFIX + codeRequestType;
-            redis.blockMfaCodesForEmail(TEST_EMAIL_1, codeBlockedKeyPrefix);
+            userStore.signUp(TEST_EMAIL, TEST_PASSWORD);
+            userStore.addMfaMethod(TEST_EMAIL, MFAMethodType.AUTH_APP, true, true, MFA_CREDENTIAL);
 
-            var request = new CheckUserExistsRequest(TEST_EMAIL_1);
+            var codeBlockedKeyPrefix = CODE_BLOCKED_KEY_PREFIX + "AUTH_APP_SIGN_IN";
+            redis.blockMfaCodesForEmail(TEST_EMAIL, codeBlockedKeyPrefix);
+
+            var request = new CheckUserExistsRequest(TEST_EMAIL);
             var response = makeRequest(
                     Optional.of(request),
                     constructFrontendHeaders(sessionId, clientSessionId),
                     Map.of());
 
             assertThat(response, hasStatus(200));
-            CheckUserExistsResponse checkUserExistsResponse =
-                    objectMapper.readValue(response.getBody(), CheckUserExistsResponse.class);
-            assertThat(checkUserExistsResponse.email(), equalTo(TEST_EMAIL_1));
+            CheckUserExistsResponse checkUserExistsResponse = objectMapper.readValue(response.getBody(), CheckUserExistsResponse.class);
+            assertThat(checkUserExistsResponse.email(), equalTo(TEST_EMAIL));
             assertThat(checkUserExistsResponse.mfaMethodType(), equalTo(MFAMethodType.AUTH_APP));
             assertTrue(checkUserExistsResponse.doesUserExist());
-            
             var lockoutInformation = checkUserExistsResponse.lockoutInformation();
             assertNotNull(lockoutInformation);
             assertThat(lockoutInformation.get(0).lockTTL() > 0, is(true));
             assertThat(lockoutInformation.get(0).journeyType(), is(JourneyType.SIGN_IN));
             assertThat(lockoutInformation.get(0).mfaMethodType(), is(MFAMethodType.AUTH_APP));
-            
+
             assertTxmaAuditEventsReceived(txmaAuditQueue, List.of(AUTH_CHECK_USER_KNOWN_EMAIL));
         }
 
         @Test
         @DisplayName("User can check if account does not exist")
         void userCanCheckIfAccountDoesNotExist() throws JsonException {
-            var sessionId = IdGenerator.generate();
-            authSessionStore.addSession(sessionId);
+            var sessionId = setupSessionAndClient(TEST_EMAIL_2);
             var clientSessionId = IdGenerator.generate();
-            setupClient(TEST_EMAIL_2, sessionId);
-
             var request = new CheckUserExistsRequest(TEST_EMAIL_2);
+
             var response = makeRequest(
                     Optional.of(request),
                     constructFrontendHeaders(sessionId, clientSessionId),
                     Map.of());
 
             assertThat(response, hasStatus(200));
-            CheckUserExistsResponse checkUserExistsResponse =
-                    objectMapper.readValue(response.getBody(), CheckUserExistsResponse.class);
+            CheckUserExistsResponse checkUserExistsResponse = objectMapper.readValue(response.getBody(), CheckUserExistsResponse.class);
             assertThat(checkUserExistsResponse.email(), equalTo(TEST_EMAIL_2));
             assertThat(checkUserExistsResponse.mfaMethodType(), equalTo(MFAMethodType.NONE));
             assertFalse(checkUserExistsResponse.doesUserExist());
             assertNull(checkUserExistsResponse.phoneNumberLastThree());
-            
             assertTxmaAuditEventsReceived(txmaAuditQueue, List.of(AUTH_CHECK_USER_NO_ACCOUNT_WITH_EMAIL));
         }
     }
@@ -154,8 +164,8 @@ class CheckUserExistsIntegrationTest extends ApiGatewayHandlerIntegrationTest {
     class ErrorCases {
 
         @Test
-        @DisplayName("User cannot check account when temporarily locked")
-        void userCannotCheckAccountWhenTemporarilyLocked() {
+        @DisplayName("Account temporarily locked error prevents user from checking account existence")
+        void accountTemporarilyLockedErrorPreventsUserFromCheckingAccountExistence() throws JsonException {
             var sessionId = IdGenerator.generate();
             authSessionStore.addSession(sessionId);
             redis.blockMfaCodesForEmail(
@@ -166,37 +176,30 @@ class CheckUserExistsIntegrationTest extends ApiGatewayHandlerIntegrationTest {
             var response = makeRequest(
                     Optional.of(request),
                     constructFrontendHeaders(sessionId),
+                    Map.of("Session-Id", sessionId, "X-API-Key", FRONTEND_API_KEY),
                     Map.of());
 
             assertThat(response, hasStatus(400));
             assertThat(response, hasJsonBody(ErrorResponse.ACCT_TEMPORARILY_LOCKED));
-            
             assertTxmaAuditEventsReceived(txmaAuditQueue, List.of(AUTH_ACCOUNT_TEMPORARILY_LOCKED));
         }
     }
 
-    private String setupUserAndSession(String emailAddress, MFAMethodType mfaMethodType) {
+    private String setupSessionAndClient() {
+        return setupSessionAndClient(TEST_EMAIL);
+    }
+
+    private String setupSessionAndClient(String emailAddress) {
         var sessionId = IdGenerator.generate();
         authSessionStore.addSession(sessionId);
         authSessionStore.addClientIdToSession(sessionId, CLIENT_ID.getValue());
-        
-        userStore.signUp(emailAddress, TEST_PASSWORD);
-        userStore.addSalt(emailAddress);
-
-        if (MFAMethodType.SMS == mfaMethodType) {
-            userStore.addMfaMethod(emailAddress, mfaMethodType, false, true, TEST_CREDENTIAL);
-            userStore.addVerifiedPhoneNumber(emailAddress, TEST_PHONE_NUMBER);
-        } else {
-            userStore.addMfaMethod(emailAddress, mfaMethodType, true, true, TEST_CREDENTIAL);
-        }
-
-        setupClient(emailAddress, sessionId);
-        
-        return sessionId;
-    }
-
-    private void setupClient(String emailAddress, String sessionId) {
-        registerClient(emailAddress, CLIENT_ID, CLIENT_NAME, REDIRECT_URI, "https://" + SECTOR_IDENTIFIER_HOST);
+        registerClient(
+                emailAddress,
+                CLIENT_ID,
+                CLIENT_NAME,
+                REDIRECT_URI,
+                "https://" + SECTOR_IDENTIFIER_HOST);
         authSessionStore.addRpSectorIdentifierHostToSession(sessionId, SECTOR_IDENTIFIER_HOST);
+        return sessionId;
     }
 }
