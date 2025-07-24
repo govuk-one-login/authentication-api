ea74d394df2e66155045910124bccdbc4194eb24
Tom Whitwell <tom.whitwell@digital.cabinet-office.gov.uk>
2025-07-24 13:08:07 +0100

    wip: example second step of role migration

--- ci/terraform/account-management/authenticate.tf
+++ ci/terraform/account-management/authenticate.tf
@@ -71,7 +71,7 @@ module "authenticate" {
 
   subnet_id                              = local.private_subnet_ids
   environment                            = var.environment
-  lambda_role_arn                        = module.account_management_api_authenticate_role.arn
+  lambda_role_arn                        = module.account_management_api_authenticate_role_interim.arn
   logging_endpoint_arns                  = var.logging_endpoint_arns
   cloudwatch_key_arn                     = data.terraform_remote_state.shared.outputs.cloudwatch_encryption_key_arn
   cloudwatch_log_retention               = var.cloudwatch_log_retention
@@ -82,5 +82,5 @@ module "authenticate" {
   dynatrace_secret      = local.dynatrace_secret
 
 
-  depends_on = [module.account_management_api_authenticate_role]
+  depends_on = [module.account_management_api_authenticate_role_interim]
 }
