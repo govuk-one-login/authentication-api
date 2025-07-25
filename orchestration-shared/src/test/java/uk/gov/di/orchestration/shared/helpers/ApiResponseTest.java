6707b05f0b23734ec90aaeb7ed78c352b1def561
Alex Wilson <alex.wilson@digital.cabinet-office.gov.uk>
2025-07-25 12:40:05 +0100

    BAU: Add tests for `badRequest` response behaviour

--- orchestration-shared/src/test/java/uk/gov/di/orchestration/shared/helpers/ApiResponseTest.java
+++ orchestration-shared/src/test/java/uk/gov/di/orchestration/shared/helpers/ApiResponseTest.java
@@ -1,6 +1,11 @@
 package uk.gov.di.orchestration.shared.helpers;
 
+import com.nimbusds.oauth2.sdk.AccessTokenResponse;
+import com.nimbusds.oauth2.sdk.ErrorObject;
 import com.nimbusds.oauth2.sdk.OAuth2Error;
+import com.nimbusds.oauth2.sdk.token.Tokens;
+import com.nimbusds.oauth2.sdk.token.TypelessAccessToken;
+import net.minidev.json.JSONObject;
 import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.CoreMatchers.is;
@@ -17,6 +22,19 @@ class ApiResponseTest {
     }
 
     @Test
+    void okWithUnserializableObjectReturnsError() {
+        var unserializableResponse =
+                new AccessTokenResponse(new Tokens(new TypelessAccessToken("a"), null));
+
+        var response = ApiResponse.ok(unserializableResponse);
+
+        assertThat(response.getStatusCode(), is(400));
+        assertThat(
+                response.getBody(),
+                is("{\"error_description\":\"Invalid request\",\"error\":\"invalid_request\"}"));
+    }
+
+    @Test
     void badRequestReturnsAsExpected() {
         var response = ApiResponse.badRequest("error");
 
@@ -27,6 +45,24 @@ class ApiResponseTest {
     @Test
     void badRequestWithErrorObjectReturnsAsExpected() {
         var response = ApiResponse.badRequest(OAuth2Error.INVALID_REQUEST);
+
+        assertThat(response.getStatusCode(), is(400));
+        assertThat(
+                response.getBody(),
+                is("{\"error_description\":\"Invalid request\",\"error\":\"invalid_request\"}"));
+    }
+
+    @Test
+    void badRequestWithUnserializableObjectReturnsError() {
+        var unserializableErrorObject =
+                new ErrorObject("code") {
+                    @Override
+                    public JSONObject toJSONObject() {
+                        throw new RuntimeException();
+                    }
+                };
+
+        var response = ApiResponse.badRequest(unserializableErrorObject);
 
         assertThat(response.getStatusCode(), is(400));
         assertThat(
