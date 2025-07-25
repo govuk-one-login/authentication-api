2aeceaff193f0c30cfd0e4539e54b4fab5f139cf
Alex Wilson <alex.wilson@digital.cabinet-office.gov.uk>
2025-07-25 11:57:05 +0100

    BAU: Add tests for `badRequest` response behaviour

--- orchestration-shared/src/test/java/uk/gov/di/orchestration/shared/helpers/ApiResponseTest.java
+++ orchestration-shared/src/test/java/uk/gov/di/orchestration/shared/helpers/ApiResponseTest.java
@@ -1,5 +1,6 @@
 package uk.gov.di.orchestration.shared.helpers;
 
+import com.nimbusds.oauth2.sdk.OAuth2Error;
 import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.CoreMatchers.is;
@@ -13,5 +14,23 @@ class ApiResponseTest {
 
         assertThat(response.getStatusCode(), is(200));
         assertThat(response.getBody(), is("\"test\""));
+    }
+
+    @Test
+    void badRequestReturnsAsExpected() {
+        var response = ApiResponse.badRequest("error");
+
+        assertThat(response.getStatusCode(), is(400));
+        assertThat(response.getBody(), is("\"error\""));
+    }
+
+    @Test
+    void badRequestWithErrorObjectReturnsAsExpected() {
+        var response = ApiResponse.badRequest(OAuth2Error.INVALID_REQUEST);
+
+        assertThat(response.getStatusCode(), is(400));
+        assertThat(
+                response.getBody(),
+                is("{\"error_description\":\"Invalid request\",\"error\":\"invalid_request\"}"));
     }
 }
