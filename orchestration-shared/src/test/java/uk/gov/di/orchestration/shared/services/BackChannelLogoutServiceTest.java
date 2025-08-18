c338b74a61648137b3c5a6bd954474d22b8deab2
Craig Earl <craig.earl@digital.cabinet-office.gov.uk>
2025-08-18 15:32:18 +0100

    ATO-1905: Remove use of AuthenticationService in BackChannelLogoutServiceTest
    BackChannelLogoutService does not use AuthenticationService anymore, so we can remove the unnecessary mocks and redundant test.

--- orchestration-shared/src/test/java/uk/gov/di/orchestration/shared/services/BackChannelLogoutServiceTest.java
+++ orchestration-shared/src/test/java/uk/gov/di/orchestration/shared/services/BackChannelLogoutServiceTest.java
@@ -3,41 +3,31 @@ package uk.gov.di.orchestration.shared.services;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.ArgumentMatchers;
-import org.mockito.Mockito;
 import uk.gov.di.orchestration.shared.entity.BackChannelLogoutMessage;
 import uk.gov.di.orchestration.shared.entity.ClientRegistry;
-import uk.gov.di.orchestration.shared.entity.UserProfile;
+import uk.gov.di.orchestration.shared.helpers.SaltHelper;
 
-import java.nio.charset.StandardCharsets;
-import java.util.Optional;
 import java.util.stream.Stream;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
 import static uk.gov.di.orchestration.shared.helpers.ClientSubjectHelper.calculatePairwiseIdentifier;
 
 class BackChannelLogoutServiceTest {
 
-    private final AwsSqsClient sqs = Mockito.mock(AwsSqsClient.class);
-    private final AuthenticationService authenticationService =
-            Mockito.mock(AuthenticationService.class);
+    private final AwsSqsClient sqs = mock(AwsSqsClient.class);
     private final BackChannelLogoutService service = new BackChannelLogoutService(sqs);
     private static final String RP_SECTOR_HOST = "example.sign-in.service.gov.uk";
     private static final String SUBJECT_ID = "subject";
-    private static String rpPairwiseId;
 
     @Test
     void shouldPostBackChannelLogoutMessageToSqsForPairwiseClients() {
-        var user = new UserProfile().withPublicSubjectID("public").withSubjectID(SUBJECT_ID);
-
-        Mockito.when(authenticationService.getUserProfileByEmailMaybe("test@test.com"))
-                .thenReturn(Optional.of(user));
-        Mockito.when(authenticationService.getOrGenerateSalt(user))
-                .thenReturn("salt".getBytes(StandardCharsets.UTF_8));
-
-        rpPairwiseId =
+        var rpPairwiseId =
                 calculatePairwiseIdentifier(
-                        SUBJECT_ID, RP_SECTOR_HOST, authenticationService.getOrGenerateSalt(user));
+                        SUBJECT_ID, RP_SECTOR_HOST, SaltHelper.generateNewSalt());
 
         service.sendLogoutMessage(
                 new ClientRegistry()
@@ -49,15 +39,13 @@ class BackChannelLogoutServiceTest {
 
         var captor = ArgumentCaptor.forClass(BackChannelLogoutMessage.class);
 
-        Mockito.verify(sqs).sendAsync(captor.capture());
+        verify(sqs).sendAsync(captor.capture());
 
         var message = captor.getValue();
 
         assertThat(message.getClientId(), is("client-id"));
         assertThat(message.getLogoutUri(), is("http://localhost:8080/back-channel-logout"));
-        assertThat(
-                message.getSubjectId(),
-                is("urn:fdc:gov.uk:2022:tGOB5t5fRAX_Fio6qRIj8KPDL7vOg5gCqI4l5nDZCDs"));
+        assertThat(message.getSubjectId(), is(rpPairwiseId));
     }
 
     @Test
@@ -71,20 +59,6 @@ class BackChannelLogoutServiceTest {
                         clientRegistry ->
                                 service.sendLogoutMessage(clientRegistry, "dummy-rpPairwiseId"));
 
-        Mockito.verify(sqs, Mockito.never()).send(ArgumentMatchers.anyString());
-    }
-
-    @Test
-    void shouldNotPostMessageToSqsIfUserProfileDoesNotExist() {
-        Mockito.when(authenticationService.getUserProfileByEmailMaybe("test@test.com"))
-                .thenReturn(Optional.empty());
-
-        service.sendLogoutMessage(
-                new ClientRegistry()
-                        .withClientID("client-id")
-                        .withBackChannelLogoutUri("http://localhost:8080/back-channel-logout"),
-                "dummy-rpPairwiseId");
-
-        Mockito.verify(sqs, Mockito.never()).send(ArgumentMatchers.anyString());
+        verify(sqs, never()).send(ArgumentMatchers.anyString());
     }
 }
