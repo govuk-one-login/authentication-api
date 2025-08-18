ad52813cee76072268b3bd85dbf82ebf7a653cfb
Craig Earl <craig.earl@digital.cabinet-office.gov.uk>
2025-08-18 15:58:39 +0100

    ATO-1906: Safe delete AuthenticationService

--- orchestration-shared/src/main/java/uk/gov/di/orchestration/shared/services/DynamoService.java
+++ orchestration-shared/src/main/java/uk/gov/di/orchestration/shared/services/DynamoService.java
@@ -4,16 +4,12 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbTable;
-import software.amazon.awssdk.enhanced.dynamodb.Key;
 import software.amazon.awssdk.enhanced.dynamodb.TableSchema;
 import uk.gov.di.orchestration.shared.dynamodb.DynamoClientHelper;
 import uk.gov.di.orchestration.shared.entity.UserCredentials;
 import uk.gov.di.orchestration.shared.entity.UserProfile;
 
-import java.util.Locale;
-import java.util.Optional;
-
-public class DynamoService implements AuthenticationService {
+public class DynamoService {
     private final DynamoDbTable<UserProfile> dynamoUserProfileTable;
     private final DynamoDbEnhancedClient dynamoDbEnhancedClient;
     private static final String USER_PROFILE_TABLE = "user-profile";
@@ -44,22 +40,6 @@ public class DynamoService implements AuthenticationService {
                 dynamoDbEnhancedClient.table(
                         userProfileTableName, TableSchema.fromBean(UserProfile.class));
         warmUp();
-    }
-
-    @Override
-    public UserProfile getUserProfileByEmail(String email) {
-        if (email != null) {
-            return dynamoUserProfileTable.getItem(
-                    Key.builder().partitionValue(email.toLowerCase(Locale.ROOT)).build());
-        } else {
-            LOG.warn("Cannot get user profile as email is null");
-            return null;
-        }
-    }
-
-    @Override
-    public Optional<UserProfile> getUserProfileByEmailMaybe(String email) {
-        return Optional.ofNullable(getUserProfileByEmail(email));
     }
 
     private void warmUp() {
