048f88cd3aa7cc05ed198202343f49e97e961ca0
Alex Wilson <alex.wilson@digital.cabinet-office.gov.uk>
2025-09-02 17:30:57 +0100

    BAU: Add basic `ResultMatcher` for unit testing outputs

--- orchestration-shared/src/test/java/uk/gov/di/orchestration/result/ResultTest.java
+++ orchestration-shared/src/test/java/uk/gov/di/orchestration/result/ResultTest.java
@@ -2,29 +2,24 @@ package uk.gov.di.orchestration.result;
 
 import org.junit.jupiter.api.Test;
 
-import static java.util.function.Function.identity;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static uk.gov.di.orchestration.sharedtest.matchers.ResultMatcher.errWithValue;
+import static uk.gov.di.orchestration.sharedtest.matchers.ResultMatcher.okWithValue;
 
 class ResultTest {
 
     @Test
     void okayResolvesValue() {
-        var ok =
-                Result.<String, String>ok("hello")
-                        .map(String::toUpperCase)
-                        .resolve(identity(), (x) -> "");
+        var ok = Result.<String, String>ok("hello").map(String::toUpperCase);
 
-        assertThat(ok, is("HELLO"));
+        assertThat(ok, is(okWithValue("HELLO")));
     }
 
     @Test
     void errResolvesValueWithoutApplyingMap() {
-        var ok =
-                Result.<String, String>err("goodbye")
-                        .map(String::toUpperCase)
-                        .resolve((x) -> "", identity());
+        var err = Result.<String, String>err("hello").map(String::toUpperCase);
 
-        assertThat(ok, is("goodbye"));
+        assertThat(err, is(errWithValue("hello")));
     }
 }
