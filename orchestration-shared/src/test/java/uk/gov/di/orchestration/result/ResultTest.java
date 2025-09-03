42309072f817dd420d5001e18a1197cacc753657
Alex Wilson <alex.wilson@digital.cabinet-office.gov.uk>
2025-09-03 09:26:40 +0100

    BAU: Add tests for `.resolve()` on `Result`

--- orchestration-shared/src/test/java/uk/gov/di/orchestration/result/ResultTest.java
+++ orchestration-shared/src/test/java/uk/gov/di/orchestration/result/ResultTest.java
@@ -2,6 +2,7 @@ package uk.gov.di.orchestration.result;
 
 import org.junit.jupiter.api.Test;
 
+import static java.util.function.Function.identity;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static uk.gov.di.orchestration.sharedtest.matchers.ResultMatcher.errWithValue;
@@ -10,16 +11,36 @@ import static uk.gov.di.orchestration.sharedtest.matchers.ResultMatcher.okWithVa
 class ResultTest {
 
     @Test
-    void okayResolvesValue() {
+    void mapAppliesToOkValue() {
         var ok = Result.<String, String>ok("hello").map(String::toUpperCase);
 
         assertThat(ok, is(okWithValue("HELLO")));
     }
 
     @Test
-    void errResolvesValueWithoutApplyingMap() {
+    void mapDoesNotApplyToErrValue() {
         var err = Result.<String, String>err("hello").map(String::toUpperCase);
 
         assertThat(err, is(errWithValue("hello")));
+    }
+
+    @Test
+    void resolveAppliesToOkValue() {
+        var ok =
+                Result.<String, String>ok("hello")
+                        .map(String::toUpperCase)
+                        .resolve(identity(), identity());
+
+        assertThat(ok, is("HELLO"));
+    }
+
+    @Test
+    void resolveAppliesToErrValue() {
+        var err =
+                Result.<String, String>err("hello")
+                        .map(String::toUpperCase)
+                        .resolve(identity(), identity());
+
+        assertThat(err, is("hello"));
     }
 }
