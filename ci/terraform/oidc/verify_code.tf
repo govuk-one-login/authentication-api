95e7d40af5552a868934706b61622589747bbe6c
Ryan Andrews <ryan.andrews@digital.cabinet-office.gov.uk>
2025-09-03 09:42:22 +0100

    ATO-1878: Duplicate verify code role

--- ci/terraform/oidc/verify_code.tf
+++ ci/terraform/oidc/verify_code.tf
@@ -1,3 +1,34 @@
+# ATO-1878: Duplicate role so we can combine policies
+module "frontend_api_verify_code_role_with_combined_auth_attempts_policy" {
+  source      = "../modules/lambda-role"
+  environment = var.environment
+  role_name   = "frontend-api-verify-code-role"
+  vpc_arn     = local.authentication_vpc_arn
+
+  policies_to_attach = [
+    aws_iam_policy.audit_signing_key_lambda_kms_signing_policy.arn,
+    aws_iam_policy.dynamo_user_read_access_policy.arn,
+    aws_iam_policy.dynamo_user_write_access_policy.arn,
+    aws_iam_policy.dynamo_client_registry_read_access_policy.arn,
+    aws_iam_policy.dynamo_account_modifiers_read_access_policy.arn,
+    aws_iam_policy.dynamo_account_modifiers_write_access_policy.arn,
+    aws_iam_policy.lambda_sns_policy.arn,
+    aws_iam_policy.redis_parameter_policy.arn,
+    aws_iam_policy.dynamo_authentication_attempt_write_policy.arn,
+    aws_iam_policy.dynamo_authentication_attempt_read_policy.arn,
+    aws_iam_policy.dynamo_authentication_attempt_delete_policy.arn,
+    aws_iam_policy.dynamo_auth_session_read_policy.arn,
+    aws_iam_policy.dynamo_auth_session_write_policy.arn,
+    module.oidc_txma_audit.access_policy_arn,
+    local.account_modifiers_encryption_policy_arn,
+    local.client_registry_encryption_policy_arn,
+    local.user_credentials_encryption_policy_arn
+  ]
+  extra_tags = {
+    Service = "verify-code"
+  }
+}
+
 module "frontend_api_verify_code_role" {
   source      = "../modules/lambda-role"
   environment = var.environment
