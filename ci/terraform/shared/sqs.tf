0ada47ba716ea7311ca00cb1de2910ec5c47c686
Aidan Comer <alhcomer@hotmail.co.uk>
2025-09-15 16:51:45 +0100

    AUT-4207: Delete delay_seconds - The AWS default is 0 seconds - The 10s delay was causing unnecessary latency

--- ci/terraform/shared/sqs.tf
+++ ci/terraform/shared/sqs.tf
@@ -1,6 +1,5 @@
 resource "aws_sqs_queue" "pending_email_check_queue" {
   name                       = "${var.environment}-pending-email-check-queue"
-  delay_seconds              = 10
   max_message_size           = 2048
   message_retention_seconds  = 1209600
   receive_wait_time_seconds  = 10
