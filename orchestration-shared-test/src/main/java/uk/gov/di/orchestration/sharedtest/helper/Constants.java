b4635cd5d015d5b48bf375e752def428adef4432
Craig Earl <craig.earl@digital.cabinet-office.gov.uk>
2025-09-22 12:04:36 +0100

    ATO-1982: Create unit test for new service

--- orchestration-shared-test/src/main/java/uk/gov/di/orchestration/sharedtest/helper/Constants.java
+++ orchestration-shared-test/src/main/java/uk/gov/di/orchestration/sharedtest/helper/Constants.java
@@ -1,8 +1,11 @@
 package uk.gov.di.orchestration.sharedtest.helper;
 
+import com.nimbusds.oauth2.sdk.id.State;
+
 public final class Constants {
     private Constants() {}
 
+    public static final State STATE = new State();
     public static final String ENVIRONMENT = "test";
     public static final String TEST_CLIENT_ID = "test-client-id";
     public static final String SESSION_ID = "session-id";
