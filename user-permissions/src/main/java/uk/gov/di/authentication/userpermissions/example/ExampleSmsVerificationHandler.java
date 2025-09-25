2259d89e0a3154d4a066a5fc5bbbd48cd9a75289
Matt Votsikas McLean <matt.votsikas-mclean@digital.cabinet-office.gov.uk>
2025-09-25 16:26:42 +0100

    AUT-4572: Use implementations of interface in example
    Making it a little bit clearer how to use the `user-permissions` project

--- user-permissions/src/main/java/uk/gov/di/authentication/userpermissions/example/ExampleSmsVerificationHandler.java
+++ user-permissions/src/main/java/uk/gov/di/authentication/userpermissions/example/ExampleSmsVerificationHandler.java
@@ -3,8 +3,11 @@ package uk.gov.di.authentication.userpermissions.example;
 import uk.gov.di.audit.AuditContext;
 import uk.gov.di.authentication.shared.entity.AuthSessionItem;
 import uk.gov.di.authentication.shared.entity.JourneyType;
+import uk.gov.di.authentication.shared.services.ConfigurationService;
+import uk.gov.di.authentication.userpermissions.PermissionDecisionManager;
 import uk.gov.di.authentication.userpermissions.PermissionDecisions;
 import uk.gov.di.authentication.userpermissions.UserActions;
+import uk.gov.di.authentication.userpermissions.UserActionsManager;
 import uk.gov.di.authentication.userpermissions.entity.Decision;
 import uk.gov.di.authentication.userpermissions.entity.UserPermissionContext;
 
@@ -14,8 +17,20 @@ import static uk.gov.di.authentication.shared.services.AuditService.MetadataPair
 public class ExampleSmsVerificationHandler {
     private static final String EXPECTED_OTP = "372615";
 
-    private PermissionDecisions permissionDecisions;
-    private UserActions userActions;
+    private final PermissionDecisions permissionDecisions;
+    private final UserActions userActions;
+
+    public ExampleSmsVerificationHandler() {
+        var configurationService = ConfigurationService.getInstance();
+        this.permissionDecisions = new PermissionDecisionManager(configurationService);
+        this.userActions = new UserActionsManager(configurationService);
+    }
+
+    public ExampleSmsVerificationHandler(
+            PermissionDecisions permissionDecisions, UserActions userActions) {
+        this.permissionDecisions = permissionDecisions;
+        this.userActions = userActions;
+    }
 
     public String handle(String submittedOtp) {
         JourneyType journeyType = JourneyType.SIGN_IN;
