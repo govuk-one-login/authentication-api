0f26b585e19c063d49396ca6b4f7d08bc725472b
Ryan Andrews <ryan.andrews@digital.cabinet-office.gov.uk>
2025-09-30 12:21:48 +0100

    ATO-1885: Replace method in verify code handler
    Replaces client registry based method with secrets manager based
    one

--- frontend-api/src/main/java/uk/gov/di/authentication/frontendapi/lambda/VerifyCodeHandler.java
+++ frontend-api/src/main/java/uk/gov/di/authentication/frontendapi/lambda/VerifyCodeHandler.java
@@ -70,7 +70,6 @@ import static uk.gov.di.authentication.shared.entity.NotificationType.VERIFY_EMA
 import static uk.gov.di.authentication.shared.helpers.ApiGatewayResponseHelper.generateApiGatewayProxyErrorResponse;
 import static uk.gov.di.authentication.shared.helpers.ApiGatewayResponseHelper.generateEmptySuccessApiGatewayResponse;
 import static uk.gov.di.authentication.shared.helpers.PersistentIdHelper.extractPersistentIdFromHeaders;
-import static uk.gov.di.authentication.shared.helpers.TestClientHelper.isTestClientWithAllowedEmail;
 import static uk.gov.di.authentication.shared.services.AuditService.MetadataPair.pair;
 import static uk.gov.di.authentication.shared.services.CodeStorageService.CODE_BLOCKED_KEY_PREFIX;
 import static uk.gov.di.authentication.shared.services.CodeStorageService.CODE_REQUEST_BLOCKED_KEY_PREFIX;
@@ -362,9 +361,8 @@ public class VerifyCodeHandler extends BaseFrontendHandler<VerifyCodeRequest>
             NotificationType notificationType,
             AuthSessionItem authSession,
             UserContext userContext,
-            Optional<MFAMethod> shouldBeRequestedSmsMfaMethod)
-            throws ClientNotFoundException {
-        if (isTestClientWithAllowedEmail(userContext, configurationService))
+            Optional<MFAMethod> shouldBeRequestedSmsMfaMethod) {
+        if (testClientHelper.isTestJourney(userContext, configurationService))
             return getOtpCodeForTestClient(notificationType);
 
         String emailAddress = authSession.getEmailAddress();
