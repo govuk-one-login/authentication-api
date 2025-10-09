c1ca47f16d73d84fb19947bc2ea92c5ffcd5cd91
Andrew Moores <andrew.moores@digital.cabinet-office.gov.uk>
2025-10-09 12:50:16 +0100

    AUT-4822: Improve logging for Notify 429

--- frontend-api/src/main/java/uk/gov/di/authentication/frontendapi/lambda/NotificationHandler.java
+++ frontend-api/src/main/java/uk/gov/di/authentication/frontendapi/lambda/NotificationHandler.java
@@ -193,9 +193,12 @@ public class NotificationHandler implements RequestHandler<SQSEvent, SQSBatchRes
             LOG.error(
                     "Error sending with Notify using NotificationType: {}, reference: {}",
                     request.getNotificationType(),
-                    reference);
+                    reference,
+                    e);
 
             if (e.getHttpResult() == 429 && isPhoneNotification(request.getNotificationType())) {
+                LOG.error("Notify response was a 429");
+
                 String destinationType =
                         maybeGetCountry(request.getDestination())
                                 .map(country -> "GB".equals(country) ? "DOMESTIC" : "INTERNATIONAL")
