08712f281504dd2303c748e510b5eac67ccbb3ae
dbes-gds <daniel.besbrode@digital.cabinet-office.gov.uk>
2025-10-10 16:57:23 +0100

    WIP: emit metric for number of accounts with internation numbers

--- utils/src/main/java/uk/gov/di/authentication/utils/lambda/AccountMetricPublishHandler.java
+++ utils/src/main/java/uk/gov/di/authentication/utils/lambda/AccountMetricPublishHandler.java
@@ -3,8 +3,15 @@ package uk.gov.di.authentication.utils.lambda;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.ScheduledEvent;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbTable;
+import software.amazon.awssdk.enhanced.dynamodb.TableSchema;
+import software.amazon.awssdk.enhanced.dynamodb.model.ScanEnhancedRequest;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+import software.amazon.awssdk.services.dynamodb.model.AttributeValue;
 import software.amazon.awssdk.services.dynamodb.model.DescribeTableRequest;
+import software.amazon.awssdk.enhanced.dynamodb.Expression;
+import uk.gov.di.authentication.shared.entity.UserProfile;
 import uk.gov.di.authentication.shared.services.CloudwatchMetricsService;
 import uk.gov.di.authentication.shared.services.ConfigurationService;
 
@@ -12,25 +19,30 @@ import java.util.Map;
 
 import static java.text.MessageFormat.format;
 import static uk.gov.di.authentication.shared.dynamodb.DynamoClientHelper.createDynamoClient;
+import static uk.gov.di.authentication.shared.dynamodb.DynamoClientHelper.createDynamoEnhancedClient;
 
 public class AccountMetricPublishHandler implements RequestHandler<ScheduledEvent, Long> {
 
     private final ConfigurationService configurationService;
     private final DynamoDbClient client;
+    private final DynamoDbEnhancedClient enhancedClient;
     private final CloudwatchMetricsService cloudwatchMetricsService;
 
     public AccountMetricPublishHandler(
             ConfigurationService configurationService,
             DynamoDbClient client,
+            DynamoDbEnhancedClient enhancedClient,
             CloudwatchMetricsService cloudwatchMetricsService) {
         this.configurationService = configurationService;
         this.client = client;
+        this.enhancedClient = enhancedClient;
         this.cloudwatchMetricsService = cloudwatchMetricsService;
     }
 
     public AccountMetricPublishHandler() {
         this.configurationService = ConfigurationService.getInstance();
         client = createDynamoClient(configurationService);
+        enhancedClient = createDynamoEnhancedClient(configurationService);
         cloudwatchMetricsService = new CloudwatchMetricsService();
     }
 
@@ -52,10 +64,31 @@ public class AccountMetricPublishHandler implements RequestHandler<ScheduledEven
                         .orElseThrow()
                         .itemCount();
 
+        var nonUkPhoneNumbers = countNonUkPhoneNumbersForVerifiedAccounts();
+
         cloudwatchMetricsService.putEmbeddedValue("NumberOfAccounts", numberOfAccounts, Map.of());
         cloudwatchMetricsService.putEmbeddedValue(
                 "NumberOfVerifiedAccounts", numberOfVerifiedAccounts, Map.of());
+        cloudwatchMetricsService.putEmbeddedValue(
+                "NonUkPhoneNumbersVerifiedAccounts", nonUkPhoneNumbers, Map.of());
 
         return numberOfVerifiedAccounts;
+    }
+
+    private long countNonUkPhoneNumbersForVerifiedAccounts() {
+        var tableName = format("{0}-user-profile", configurationService.getEnvironment());
+        var table = enhancedClient.table(tableName, TableSchema.fromBean(UserProfile.class));
+        
+        var filterExpression = Expression.builder()
+                .expression("accountVerified = :verified AND attribute_exists(PhoneNumber) AND NOT begins_with(PhoneNumber, :ukPrefix)")
+                .putExpressionValue(":verified", AttributeValue.builder().n("1").build())
+                .putExpressionValue(":ukPrefix", AttributeValue.builder().s("+44").build())
+                .build();
+        
+        var scanRequest = ScanEnhancedRequest.builder()
+                .filterExpression(filterExpression)
+                .build();
+        
+        return table.scan(scanRequest).items().stream().count();
     }
 }
--- utils/src/test/java/uk/gov/di/authentication/utils/lambda/AccountMetricPublishHandlerTest.java
+++ utils/src/test/java/uk/gov/di/authentication/utils/lambda/AccountMetricPublishHandlerTest.java
@@ -3,13 +3,21 @@ package uk.gov.di.authentication.utils.lambda;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.events.ScheduledEvent;
 import org.junit.jupiter.api.Test;
+import software.amazon.awssdk.core.pagination.sync.SdkIterable;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbTable;
+import software.amazon.awssdk.enhanced.dynamodb.model.PageIterable;
+import software.amazon.awssdk.enhanced.dynamodb.model.ScanEnhancedRequest;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 import software.amazon.awssdk.services.dynamodb.model.DescribeTableRequest;
 import software.amazon.awssdk.services.dynamodb.model.DescribeTableResponse;
 import software.amazon.awssdk.services.dynamodb.model.GlobalSecondaryIndexDescription;
 import software.amazon.awssdk.services.dynamodb.model.TableDescription;
+import uk.gov.di.authentication.shared.entity.UserProfile;
 import uk.gov.di.authentication.shared.services.CloudwatchMetricsService;
 import uk.gov.di.authentication.shared.services.ConfigurationService;
+
+import java.util.stream.Stream;
 
 import java.util.Map;
 import java.util.NoSuchElementException;
@@ -29,9 +37,16 @@ class AccountMetricPublishHandlerTest {
     private final CloudwatchMetricsService cloudwatchMetricsService =
             mock(CloudwatchMetricsService.class);
     private final DynamoDbClient client = mock(DynamoDbClient.class);
+    private final DynamoDbEnhancedClient enhancedClient = mock(DynamoDbEnhancedClient.class);
+    @SuppressWarnings("unchecked")
+    private final DynamoDbTable<UserProfile> table = mock(DynamoDbTable.class);
+    @SuppressWarnings("unchecked")
+    private final PageIterable<UserProfile> pageIterable = mock(PageIterable.class);
+    @SuppressWarnings("unchecked")
+    private final SdkIterable<UserProfile> sdkIterable = mock(SdkIterable.class);
 
     private final AccountMetricPublishHandler handler =
-            new AccountMetricPublishHandler(configurationService, client, cloudwatchMetricsService);
+            new AccountMetricPublishHandler(configurationService, client, enhancedClient, cloudwatchMetricsService);
 
     @Test
     void shouldPublishMetrics() {
@@ -54,6 +69,10 @@ class AccountMetricPublishHandlerTest {
                                                 .itemCount(5100l)
                                                 .build())
                                 .build());
+        when(enhancedClient.<UserProfile>table(any(String.class), any())).thenReturn(table);
+        when(table.scan(any(ScanEnhancedRequest.class))).thenReturn(pageIterable);
+        when(pageIterable.items()).thenReturn(sdkIterable);
+        when(sdkIterable.stream()).thenReturn(Stream.empty());
 
         handler.handleRequest(mock(ScheduledEvent.class), mock(Context.class));
 
@@ -61,6 +80,8 @@ class AccountMetricPublishHandlerTest {
                 .putEmbeddedValue("NumberOfAccounts", 5100, Map.of());
         verify(cloudwatchMetricsService, times(1))
                 .putEmbeddedValue("NumberOfVerifiedAccounts", 5000, Map.of());
+        verify(cloudwatchMetricsService, times(1))
+                .putEmbeddedValue("NonUkPhoneNumbersVerifiedAccounts", 0L, Map.of());
     }
 
     @Test
@@ -80,6 +101,10 @@ class AccountMetricPublishHandlerTest {
                                                 .itemCount(5100l)
                                                 .build())
                                 .build());
+        when(enhancedClient.<UserProfile>table(any(String.class), any())).thenReturn(table);
+        when(table.scan(any(ScanEnhancedRequest.class))).thenReturn(pageIterable);
+        when(pageIterable.items()).thenReturn(sdkIterable);
+        when(sdkIterable.stream()).thenReturn(Stream.empty());
 
         assertThrows(
                 NoSuchElementException.class,
