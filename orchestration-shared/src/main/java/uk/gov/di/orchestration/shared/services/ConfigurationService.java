b57ff8670102335a04ebfabc72ff5184b04a91f9
Craig Earl <craig.earl@digital.cabinet-office.gov.uk>
2025-10-22 14:28:31 +0100

    ATO-1550: Conditionally set authstub parameters using feature flag
    - Update the authStubEnabled variable in the dev env config to enable/disable the auth stub

--- orchestration-shared/src/main/java/uk/gov/di/orchestration/shared/services/ConfigurationService.java
+++ orchestration-shared/src/main/java/uk/gov/di/orchestration/shared/services/ConfigurationService.java
@@ -231,7 +231,7 @@ public class ConfigurationService implements BaseLambdaConfiguration, AuditPubli
     }
 
     public String getOrchestrationToAuthenticationEncryptionPublicKey() {
-        var paramName = format("/orch-stubs/{0}-auth-stub-auth-public-encryption-key", getEnvironment());
+        var paramName = System.getenv("ORCH_TO_AUTH_ENCRYPTION_KEY_PARAM");
         try {
             var request =
                     GetParameterRequest.builder().withDecryption(true).name(paramName).build();
