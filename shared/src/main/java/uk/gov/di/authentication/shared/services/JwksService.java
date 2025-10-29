a8432d20c6d7d228895b4f326e9567a2da477b02
Aidan Comer <alhcomer@hotmail.co.uk>
2025-10-29 14:53:07 +0000

    Revert "AUT-4648: Use IPV stub JWKS endpoint"

--- shared/src/main/java/uk/gov/di/authentication/shared/services/JwksService.java
+++ shared/src/main/java/uk/gov/di/authentication/shared/services/JwksService.java
@@ -1,9 +1,15 @@
 package uk.gov.di.authentication.shared.services;
 
+import com.nimbusds.jose.KeySourceException;
 import com.nimbusds.jose.jwk.ECKey;
 import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.JWKMatcher;
+import com.nimbusds.jose.jwk.JWKSelector;
 import com.nimbusds.jose.jwk.KeyUse;
 import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.jwk.source.JWKSourceBuilder;
+import com.nimbusds.jose.proc.SecurityContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.bouncycastle.asn1.x509.SubjectPublicKeyInfo;
@@ -13,6 +19,7 @@ import software.amazon.awssdk.services.kms.model.GetPublicKeyRequest;
 import software.amazon.awssdk.services.kms.model.GetPublicKeyResponse;
 import uk.gov.di.authentication.shared.helpers.CryptoProviderHelper;
 
+import java.net.URL;
 import java.security.PublicKey;
 import java.security.interfaces.ECPublicKey;
 import java.security.interfaces.RSAPublicKey;
@@ -73,6 +80,26 @@ public class JwksService {
 
         LOG.info("Retrieving MFA Reset JAR deprecated signing public key");
         return getPublicJWKWithKeyId(deprecatedKeyAlias);
+    }
+
+    public JWK retrieveJwkFromURLWithKeyId(URL url, String keyId) {
+        JWKSelector selector = new JWKSelector(new JWKMatcher.Builder().keyID(keyId).build());
+        JWKSource<SecurityContext> jwkSource =
+                JWKSourceBuilder.create(url)
+                        .retrying(true)
+                        .refreshAheadCache(false)
+                        .cache(false)
+                        .rateLimited(false)
+                        .build();
+        try {
+            LOG.info("Retrieving JWKSet with URL: {}", url);
+            return jwkSource.get(selector, null).stream()
+                    .findFirst()
+                    .orElseThrow(() -> new KeySourceException("No key found with given keyID"));
+        } catch (KeySourceException e) {
+            LOG.error("Unable to load JWKSet", e);
+            throw new RuntimeException(e);
+        }
     }
 
     private JWK getPublicJWKWithKeyId(String keyId) {
