d7d22c8f4fd5114ffdf735fc0b911c45e90a36a5
Ryan Andrews <ryan.andrews@digital.cabinet-office.gov.uk>
2025-11-07 15:26:15 +0000

    BAU: Remove Instrumentation Helper
    We no longer use X-Ray for our instrumentation and this is no longer
    used so we can safely delete it

--- orchestration-shared/src/main/java/uk/gov/di/orchestration/shared/helpers/InstrumentationHelper.java
+++
@@ -1,8 +0,0 @@
-package uk.gov.di.orchestration.shared.helpers;
-
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
-public class InstrumentationHelper {
-    private static final Logger LOG = LogManager.getLogger(InstrumentationHelper.class);
-}
