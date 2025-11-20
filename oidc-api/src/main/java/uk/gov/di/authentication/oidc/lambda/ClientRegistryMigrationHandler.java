b39d2d2da84e11fc30a772f806964cde7df666cd
Ryan Andrews <ryan.andrews@digital.cabinet-office.gov.uk>
2025-11-20 16:54:52 +0000

    ATO-2080: Add client registry migration handler
    Adds a client registry migration handler which reads from the
    auth table writes to the orch table using the migration service
    we introduced previously. This also logs a hash of each tables
    contents so we can be sure that the data in both tables is equivalent

--- oidc-api/src/main/java/uk/gov/di/authentication/oidc/lambda/ClientRegistryMigrationHandler.java
+++ oidc-api/src/main/java/uk/gov/di/authentication/oidc/lambda/ClientRegistryMigrationHandler.java
@@ -2,11 +2,81 @@ package uk.gov.di.authentication.oidc.lambda;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.bouncycastle.jcajce.provider.digest.SHA256;
+import org.bouncycastle.util.encoders.Hex;
+import software.amazon.awssdk.services.dynamodb.model.AttributeValue;
+import uk.gov.di.authentication.oidc.services.ClientRegistryMigrationService;
+import uk.gov.di.orchestration.shared.helpers.LogLineHelper;
+import uk.gov.di.orchestration.shared.services.ConfigurationService;
+
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.Map;
+
+import static uk.gov.di.orchestration.shared.helpers.LogLineHelper.attachLogFieldToLogs;
+import static uk.gov.di.orchestration.shared.helpers.LogLineHelper.attachTraceId;
 
 public class ClientRegistryMigrationHandler implements RequestHandler<Object, String> {
+    private static final Logger LOG = LogManager.getLogger(ClientRegistryMigrationHandler.class);
+    private final ClientRegistryMigrationService authClientRegistry;
+    private final ClientRegistryMigrationService orchClientRegistry;
+
+    public ClientRegistryMigrationHandler(ConfigurationService configurationService) {
+        this.authClientRegistry = new ClientRegistryMigrationService(configurationService, false);
+        this.orchClientRegistry = new ClientRegistryMigrationService(configurationService, true);
+    }
+
+    public ClientRegistryMigrationHandler(
+            ClientRegistryMigrationService authClientRegistryMigrationService,
+            ClientRegistryMigrationService orchClientRegistryMigrationService) {
+        this.authClientRegistry = authClientRegistryMigrationService;
+        this.orchClientRegistry = orchClientRegistryMigrationService;
+    }
+
+    public ClientRegistryMigrationHandler() {
+        this(ConfigurationService.getInstance());
+    }
 
     @Override
     public String handleRequest(Object ignored, Context context) {
-        return "HelloWorld";
+        attachTraceId();
+        attachLogFieldToLogs(LogLineHelper.LogFieldName.AWS_REQUEST_ID, context.getAwsRequestId());
+        LOG.info("Migrate client registry handler invoked");
+        return migrateClientRegistry();
+    }
+
+    private String migrateClientRegistry() {
+        var authClients = authClientRegistry.getAllClients();
+        LOG.info("Found {} clients in Auth table", authClients.size());
+        logHashOfEntries(authClients, "Auth client registry hash");
+
+        authClients.forEach(orchClientRegistry::putClientToDynamo);
+        LOG.info("Finished writing clients to Orch table");
+
+        var orchClients = orchClientRegistry.getAllClients();
+        LOG.info("Found {} clients in Orch table", orchClients.size());
+        logHashOfEntries(orchClients, "Orch client registry hash");
+
+        return "Finished";
+    }
+
+    private void logHashOfEntries(List<Map<String, AttributeValue>> clients, String logPrefix) {
+        var totalDigest = new SHA256.Digest();
+        clients.stream()
+                .map(this::hashRawClient)
+                .forEach(hash -> totalDigest.update(hash.getBytes(StandardCharsets.UTF_8)));
+        var totalHashString = new String(Hex.encode(totalDigest.digest()), StandardCharsets.UTF_8);
+        LOG.info("{}: {}", logPrefix, totalHashString);
+    }
+
+    private String hashRawClient(Map<String, AttributeValue> client) {
+        var digest = new SHA256.Digest();
+        client.forEach(
+                (key, value) ->
+                        digest.update(
+                                (key + "=" + value.toString()).getBytes(StandardCharsets.UTF_8)));
+        return new String(Hex.encode(digest.digest()), StandardCharsets.UTF_8);
     }
 }
---
+++ oidc-api/src/test/java/uk/gov/di/authentication/oidc/lambda/ClientRegistryMigrationHandlerTest.java
@@ -0,0 +1,98 @@
+package uk.gov.di.authentication.oidc.lambda;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.nimbusds.openid.connect.sdk.SubjectType;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+import software.amazon.awssdk.services.dynamodb.model.AttributeValue;
+import uk.gov.di.authentication.oidc.services.ClientRegistryMigrationService;
+import uk.gov.di.orchestration.sharedtest.logging.CaptureLoggingExtension;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasItem;
+import static org.mockito.ArgumentMatchers.anyMap;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static uk.gov.di.orchestration.sharedtest.logging.LogEventMatcher.withMessageContaining;
+
+class ClientRegistryMigrationHandlerTest {
+    private final ClientRegistryMigrationService authClientRegistryMigrationService =
+            mock(ClientRegistryMigrationService.class);
+    private final ClientRegistryMigrationService orchClientRegistryMigrationService =
+            mock(ClientRegistryMigrationService.class);
+    private final Context mockContext = mock(Context.class);
+    private ClientRegistryMigrationHandler clientRegistryMigrationHandler;
+
+    @RegisterExtension
+    private final CaptureLoggingExtension logging =
+            new CaptureLoggingExtension(ClientRegistryMigrationHandler.class);
+
+    @BeforeEach
+    void setup() {
+        clientRegistryMigrationHandler =
+                new ClientRegistryMigrationHandler(
+                        authClientRegistryMigrationService, orchClientRegistryMigrationService);
+    }
+
+    @Test
+    void itReadsFromAuthTableAndWritesToOrchTableAndLogsAHash() {
+        when(authClientRegistryMigrationService.getAllClients())
+                .thenReturn(
+                        List.of(
+                                generateUnmappedClientRegistryDynamoItem("test-client-1"),
+                                generateUnmappedClientRegistryDynamoItem("test-client-2")));
+
+        when(orchClientRegistryMigrationService.getAllClients())
+                .thenReturn(
+                        List.of(
+                                generateUnmappedClientRegistryDynamoItem("test-client-1"),
+                                generateUnmappedClientRegistryDynamoItem("test-client-2")));
+
+        clientRegistryMigrationHandler.handleRequest(null, mockContext);
+
+        verify(authClientRegistryMigrationService).getAllClients();
+        verify(orchClientRegistryMigrationService, times(2)).putClientToDynamo(anyMap());
+
+        assertThat(
+                logging.events(), hasItem(withMessageContaining("Found 2 clients in Auth table")));
+        assertThat(logging.events(), hasItem(withMessageContaining("Auth client registry hash")));
+        assertThat(
+                logging.events(), hasItem(withMessageContaining("Found 2 clients in Orch table")));
+        assertThat(logging.events(), hasItem(withMessageContaining("Orch client registry hash")));
+    }
+
+    private Map<String, AttributeValue> generateUnmappedClientRegistryDynamoItem(String clientId) {
+        return Map.ofEntries(
+                Map.entry(
+                        "clientID",
+                        AttributeValue.fromS(clientId != null ? clientId : "test-client")),
+                Map.entry("ClientName", AttributeValue.fromS("test-client")),
+                Map.entry("PublicKey", AttributeValue.fromS("example-key")),
+                Map.entry(
+                        "Scopes",
+                        AttributeValue.fromL(
+                                List.of(
+                                        AttributeValue.fromS("openid"),
+                                        AttributeValue.fromS("email"),
+                                        AttributeValue.fromS("phone")))),
+                Map.entry(
+                        "RedirectUrls",
+                        AttributeValue.fromL(List.of(AttributeValue.fromS("https://example.com")))),
+                Map.entry(
+                        "Contacts",
+                        AttributeValue.fromL(List.of(AttributeValue.fromS("example@example.com")))),
+                Map.entry(
+                        "PostLogoutRedirectUrls",
+                        AttributeValue.fromL(
+                                List.of(AttributeValue.fromS("https://example.com/post-logout")))),
+                Map.entry("ServiceType", AttributeValue.fromS("MANDATORY")),
+                Map.entry("SectorIdentifierUri", AttributeValue.fromS("https://example.com")),
+                Map.entry("SubjectType", AttributeValue.fromS(SubjectType.PAIRWISE.toString())));
+    }
+}
