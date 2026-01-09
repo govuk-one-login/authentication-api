8fe5f0646c66efee4fe75d8674b5998e0d4bba22
Christian Sophocleous <145482922+ChristianSoftwire@users.noreply.github.com>
2026-01-09 16:41:03 +0000

    AUT-4985: Check if signingKey is valid given publicToken and publicTestToken
    - Need to create a list of signingKeys with the orch signing key and an auth owned test signing key. This auth owned test key will be used for our acceptance tests and will remove the dependency on orch keys
    - Compare the signing key from the accessToken with the list of valid signing keys to verify the signature. If any match, return true, else return false
    
    Signed-off-by: Christian Sophocleous <145482922+ChristianSoftwire@users.noreply.github.com>

--- shared/src/main/java/uk/gov/di/authentication/shared/services/TokenValidationService.java
+++ shared/src/main/java/uk/gov/di/authentication/shared/services/TokenValidationService.java
@@ -4,6 +4,7 @@ import com.nimbusds.jose.JOSEException;
 import com.nimbusds.jose.JWSAlgorithm;
 import com.nimbusds.jose.crypto.ECDSAVerifier;
 import com.nimbusds.jose.crypto.RSASSAVerifier;
+import com.nimbusds.jose.jwk.JWK;
 import com.nimbusds.jwt.JWTClaimsSet;
 import com.nimbusds.jwt.SignedJWT;
 import com.nimbusds.jwt.util.DateUtils;
@@ -16,6 +17,8 @@ import uk.gov.di.authentication.shared.helpers.NowHelper;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Stream;
 
 public class TokenValidationService {
 
@@ -68,10 +71,18 @@ public class TokenValidationService {
                         new RSASSAVerifier(
                                 jwksService.getPublicTokenRsaJwkWithOpaqueId().toRSAKey()));
             } else {
-                return jwt.verify(
-                        new ECDSAVerifier(jwksService.getPublicTokenJwkWithOpaqueId().toECKey()));
-            }
+                var realKey = jwksService.getPublicTokenJwkWithOpaqueId();
+                var acceptanceTestKey = jwksService.getPublicTestTokenJwkWithOpaqueId();
 
+                Stream<JWK> signingKeys =
+                        Stream.of(realKey, acceptanceTestKey).filter(Objects::nonNull);
+
+                return signingKeys.anyMatch(
+                        key -> {
+                            boolean isTestKey = key.equals(acceptanceTestKey);
+                            return isSignatureValidWithKey(jwt, key, isTestKey);
+                        });
+            }
         } catch (JOSEException | java.text.ParseException e) {
             LOG.warn("Unable to validate Signature of Token", e);
             return false;
@@ -89,5 +100,17 @@ public class TokenValidationService {
             return false;
         }
         return true;
+    }
+
+    private boolean isSignatureValidWithKey(SignedJWT jwt, JWK trustedKey, boolean isTestKey) {
+        try {
+            boolean isValid = jwt.verify(new ECDSAVerifier(trustedKey.toECKey()));
+            if (isValid && !configuration.getEnvironment().equals("production")) {
+                LOG.info("Token signature validated using {} key", isTestKey ? "test" : "real");
+            }
+            return isValid;
+        } catch (JOSEException e) {
+            return false;
+        }
     }
 }
