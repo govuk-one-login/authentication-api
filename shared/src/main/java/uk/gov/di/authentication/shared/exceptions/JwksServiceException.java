bb78abc1bf570e87c06c09bf9e26ebe4ca4ceb43
Louisa Nash <louisa.nash@digital.cabinet-office.gov.uk>
2026-01-14 09:34:33 +0000

    ATO-2068: create new JwksServiceException for initialization error

---
+++ shared/src/main/java/uk/gov/di/authentication/shared/exceptions/JwksServiceException.java
@@ -0,0 +1,8 @@
+package uk.gov.di.authentication.shared.exceptions;
+
+public class JwksServiceException extends RuntimeException {
+
+    public JwksServiceException(String message) {
+        super(message);
+    }
+}
