220de5d231abcfbc2b67dfe2d11511911b38c207
Matt W <173997080+sw-mattw@users.noreply.github.com>
2026-01-15 15:46:22 +0000

    AUT-5072: Add ability to disable support for international SMS
    Co-authored-by: mattvot

--- frontend-api/src/test/java/uk/gov/di/authentication/frontendapi/lambda/SendNotificationHandlerTest.java
+++ frontend-api/src/test/java/uk/gov/di/authentication/frontendapi/lambda/SendNotificationHandlerTest.java
@@ -193,6 +193,7 @@ class SendNotificationHandlerTest {
         when(codeGeneratorService.sixDigitCode()).thenReturn(TEST_SIX_DIGIT_CODE);
         when(configurationService.getCodeMaxRetries()).thenReturn(6);
         when(configurationService.getEnvironment()).thenReturn("unit-test");
+        when(configurationService.isInternalApiNewInternationalSmsEnabled()).thenReturn(true);
 
         var userCreds =
                 new UserCredentials()
@@ -1055,6 +1056,38 @@ class SendNotificationHandlerTest {
                 assertThat(result, hasJsonBody(ErrorResponse.INVALID_PHONE_NUMBER));
                 verifyNoInteractions(emailSqsClient);
                 verifyNoInteractions(auditService);
+            }
+
+            @ParameterizedTest
+            @EnumSource(
+                    value = JourneyType.class,
+                    names = {"REGISTRATION", "ACCOUNT_RECOVERY"})
+            void shouldReturn400WhenInternationalNumberAndFeatureFlagDisabled(
+                    JourneyType journeyType) {
+                when(configurationService.isInternalApiNewInternationalSmsEnabled())
+                        .thenReturn(false);
+                usingValidSession();
+
+                var body =
+                        format(
+                                "{ \"email\": \"%s\", \"notificationType\": \"%s\", \"phoneNumber\": \"%s\", \"journeyType\": \"%s\" }",
+                                EMAIL,
+                                VERIFY_PHONE_NUMBER,
+                                CommonTestVariables.INTERNATIONAL_MOBILE_NUMBER,
+                                journeyType);
+                var event = apiRequestEventWithHeadersAndBody(VALID_HEADERS, body);
+
+                var result = handler.handleRequest(event, context);
+
+                assertEquals(400, result.getStatusCode());
+                assertTrue(
+                        result.getBody()
+                                .contains(
+                                        String.valueOf(
+                                                ErrorResponse
+                                                        .INTERNATIONAL_PHONE_NUMBER_NOT_SUPPORTED
+                                                        .getCode())));
+                verifyNoInteractions(emailSqsClient);
             }
 
             @Test
