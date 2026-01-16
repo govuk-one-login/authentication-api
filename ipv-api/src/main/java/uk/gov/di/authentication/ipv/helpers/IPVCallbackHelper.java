13955d722cce680250d4c638454ee335ce8e82e6
cargil <carly.gilson@digital.cabinet-office.gov.uk>
2026-01-16 11:30:18 +0000

    ATO-2263: Fix client dimension in SignIn metric We were incorrectly adding clientSessionId as a dimension to the metric instead of clientId, which means a new metric is created for every journey. Updated this value to use client id

--- ipv-api/src/main/java/uk/gov/di/authentication/ipv/helpers/IPVCallbackHelper.java
+++ ipv-api/src/main/java/uk/gov/di/authentication/ipv/helpers/IPVCallbackHelper.java
@@ -221,7 +221,7 @@ public class IPVCallbackHelper {
                 rpPairwiseSubjectId,
                 internalPairwiseSubjectId,
                 authCode);
-        sendCloudwatchMetrics(orchSession, clientSessionId, clientId, clientName);
+        sendCloudwatchMetrics(orchSession, clientId, clientName);
         authCodeResponseService.saveSession(false, orchSessionService, orchSession);
         return authenticationResponse;
     }
@@ -260,13 +260,9 @@ public class IPVCallbackHelper {
     }
 
     private void sendCloudwatchMetrics(
-            OrchSessionItem orchSession,
-            String clientSessionId,
-            String clientId,
-            String clientName) {
+            OrchSessionItem orchSession, String clientId, String clientName) {
         var dimensions =
-                authCodeResponseService.getDimensions(
-                        orchSession, clientName, clientSessionId, false);
+                authCodeResponseService.getDimensions(orchSession, clientName, clientId, false);
 
         cloudwatchMetricsService.incrementCounter("SignIn", dimensions);
 
