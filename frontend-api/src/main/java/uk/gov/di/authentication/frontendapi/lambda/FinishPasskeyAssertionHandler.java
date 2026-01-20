5e9ee7c460aba17821ea22c240097079afdaca5c
Benjamin Fernandes <benjamin.fernandes@digital.cabinet-office.gov.uk>
2026-01-20 18:12:20 +0000

    AUT-4938: Add RelyingParty to FinishPasskeyAssertionHandler
    This will be used in upcoming changes to finish the passkey authentication assertion

--- frontend-api/src/main/java/uk/gov/di/authentication/frontendapi/lambda/FinishPasskeyAssertionHandler.java
+++ frontend-api/src/main/java/uk/gov/di/authentication/frontendapi/lambda/FinishPasskeyAssertionHandler.java
@@ -4,9 +4,11 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.yubico.webauthn.RelyingParty;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import uk.gov.di.authentication.frontendapi.entity.FinishPasskeyAssertionRequest;
+import uk.gov.di.authentication.frontendapi.services.webauthn.RelyingPartyProvider;
 import uk.gov.di.authentication.shared.lambda.BaseFrontendHandler;
 import uk.gov.di.authentication.shared.services.AuthSessionService;
 import uk.gov.di.authentication.shared.services.AuthenticationService;
@@ -19,6 +21,7 @@ public class FinishPasskeyAssertionHandler
         extends BaseFrontendHandler<FinishPasskeyAssertionRequest>
         implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
     private static final Logger LOG = LogManager.getLogger(FinishPasskeyAssertionHandler.class);
+    private final RelyingParty relyingParty;
 
     public FinishPasskeyAssertionHandler() {
         this(ConfigurationService.getInstance());
@@ -27,16 +30,19 @@ public class FinishPasskeyAssertionHandler
     public FinishPasskeyAssertionHandler(
             ConfigurationService configurationService,
             AuthenticationService authenticationService,
-            AuthSessionService authSessionService) {
+            AuthSessionService authSessionService,
+            RelyingParty relyingParty) {
         super(
                 FinishPasskeyAssertionRequest.class,
                 configurationService,
                 authenticationService,
                 authSessionService);
+        this.relyingParty = relyingParty;
     }
 
     public FinishPasskeyAssertionHandler(ConfigurationService configurationService) {
         super(FinishPasskeyAssertionRequest.class, configurationService);
+        this.relyingParty = RelyingPartyProvider.provide(configurationService);
     }
 
     @Override
