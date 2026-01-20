e7ad117dc97ab2341f09f600c7195c8812016539
Benjamin Fernandes <benjamin.fernandes@digital.cabinet-office.gov.uk>
2026-01-20 17:54:12 +0000

    AUT-4938: Add FinishPasskeyAssertionHandler
    - Add the FinishPasskeyAssertionHandler skeleton with an empty FinishPasskeyAssertionRequest request
    - Add unit test

---
+++ frontend-api/src/main/java/uk/gov/di/authentication/frontendapi/lambda/FinishPasskeyAssertionHandler.java
@@ -0,0 +1,57 @@
+package uk.gov.di.authentication.frontendapi.lambda;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import uk.gov.di.authentication.frontendapi.entity.FinishPasskeyAssertionRequest;
+import uk.gov.di.authentication.shared.lambda.BaseFrontendHandler;
+import uk.gov.di.authentication.shared.services.AuthSessionService;
+import uk.gov.di.authentication.shared.services.AuthenticationService;
+import uk.gov.di.authentication.shared.services.ConfigurationService;
+import uk.gov.di.authentication.shared.state.UserContext;
+
+import static uk.gov.di.authentication.shared.helpers.ApiGatewayResponseHelper.generateApiGatewayProxyResponse;
+
+public class FinishPasskeyAssertionHandler
+        extends BaseFrontendHandler<FinishPasskeyAssertionRequest>
+        implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+    private static final Logger LOG = LogManager.getLogger(FinishPasskeyAssertionHandler.class);
+
+    public FinishPasskeyAssertionHandler() {
+        this(ConfigurationService.getInstance());
+    }
+
+    public FinishPasskeyAssertionHandler(
+            ConfigurationService configurationService,
+            AuthenticationService authenticationService,
+            AuthSessionService authSessionService) {
+        super(
+                FinishPasskeyAssertionRequest.class,
+                configurationService,
+                authenticationService,
+                authSessionService);
+    }
+
+    public FinishPasskeyAssertionHandler(ConfigurationService configurationService) {
+        super(FinishPasskeyAssertionRequest.class, configurationService);
+    }
+
+    @Override
+    public APIGatewayProxyResponseEvent handleRequest(
+            APIGatewayProxyRequestEvent input, Context context) {
+        return super.handleRequest(input, context);
+    }
+
+    @Override
+    public APIGatewayProxyResponseEvent handleRequestWithUserContext(
+            APIGatewayProxyRequestEvent input,
+            Context context,
+            FinishPasskeyAssertionRequest request,
+            UserContext userContext) {
+        LOG.info("FinishPasskeyAssertionHandler called");
+        return generateApiGatewayProxyResponse(200, "");
+    }
+}
---
+++ frontend-api/src/test/java/uk/gov/di/authentication/frontendapi/lambda/FinishPasskeyAssertionHandlerTest.java
@@ -0,0 +1,73 @@
+package uk.gov.di.authentication.frontendapi.lambda;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+import uk.gov.di.authentication.shared.entity.AuthSessionItem;
+import uk.gov.di.authentication.shared.services.AuthSessionService;
+import uk.gov.di.authentication.shared.services.AuthenticationService;
+import uk.gov.di.authentication.shared.services.ConfigurationService;
+import uk.gov.di.authentication.sharedtest.logging.CaptureLoggingExtension;
+
+import java.util.Optional;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.not;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static uk.gov.di.authentication.shared.helpers.CommonTestVariables.IP_ADDRESS;
+import static uk.gov.di.authentication.shared.helpers.CommonTestVariables.SESSION_ID;
+import static uk.gov.di.authentication.shared.helpers.CommonTestVariables.VALID_HEADERS;
+import static uk.gov.di.authentication.sharedtest.helper.RequestEventHelper.contextWithSourceIp;
+import static uk.gov.di.authentication.sharedtest.logging.LogEventMatcher.withMessageContaining;
+import static uk.gov.di.authentication.sharedtest.matchers.APIGatewayProxyResponseEventMatcher.hasStatus;
+
+class FinishPasskeyAssertionHandlerTest {
+    private final Context context = mock(Context.class);
+    private final AuthenticationService authenticationService = mock(AuthenticationService.class);
+    private final AuthSessionService authSessionService = mock(AuthSessionService.class);
+    private final ConfigurationService configurationService = mock(ConfigurationService.class);
+    private FinishPasskeyAssertionHandler handler;
+    private final AuthSessionItem authSession = new AuthSessionItem().withSessionId(SESSION_ID);
+
+    @RegisterExtension
+    public final CaptureLoggingExtension logging =
+            new CaptureLoggingExtension(FinishPasskeyAssertionHandler.class);
+
+    @AfterEach
+    void tearDown() {
+        assertThat(logging.events(), not(hasItem(withMessageContaining(SESSION_ID))));
+    }
+
+    @BeforeEach
+    void setup() {
+        when(context.getAwsRequestId()).thenReturn("aws-session-id");
+        when(authSessionService.getSessionFromRequestHeaders(any()))
+                .thenReturn(Optional.of(authSession));
+
+        handler =
+                new FinishPasskeyAssertionHandler(
+                        configurationService, authenticationService, authSessionService);
+    }
+
+    @Test
+    void shouldReturn200ForValidRequest() {
+        var result = handler.handleRequest(finishPasskeyAssertionRequest(), context);
+
+        assertThat(result, hasStatus(200));
+        assertEquals("", result.getBody());
+    }
+
+    private APIGatewayProxyRequestEvent finishPasskeyAssertionRequest() {
+        return new APIGatewayProxyRequestEvent()
+                .withHeaders(VALID_HEADERS)
+                .withBody("{}")
+                .withRequestContext(contextWithSourceIp(IP_ADDRESS));
+    }
+}
