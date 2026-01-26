ce74c4f61ef1dc4ef87570d74f8b121fb301bb32
Matt W <173997080+sw-mattw@users.noreply.github.com>
2026-01-26 15:37:49 +0000

    AUT-5144: Set up `InternationalSmsRequestCount` bean

---
+++ shared/src/test/java/uk/gov/di/authentication/shared/entity/InternationalSmsSendCountTest.java
@@ -0,0 +1,24 @@
+package uk.gov.di.authentication.shared.entity;
+
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+class InternationalSmsSendCountTest {
+
+    private static final String PHONE_NUMBER = "+33777777777";
+    private static final int SENT_COUNT = 7;
+
+    @Test
+    void shouldCreateUserCredentials() {
+        InternationalSmsSendCount internationalSmsSendCount = generateInternationalSmsCount();
+
+        assertThat(internationalSmsSendCount.getPhoneNumber(), equalTo(PHONE_NUMBER));
+        assertThat(internationalSmsSendCount.getSentCount(), equalTo(SENT_COUNT));
+    }
+
+    private InternationalSmsSendCount generateInternationalSmsCount() {
+        return new InternationalSmsSendCount().withPhoneNumber(PHONE_NUMBER).withSentCount(SENT_COUNT);
+    }
+}
