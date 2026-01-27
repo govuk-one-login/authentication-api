991426e33f774c52bd018f4224c2bc318f46166d
Matt W <173997080+sw-mattw@users.noreply.github.com>
2026-01-27 09:02:09 +0000

    AUT-5144: Add international sms request limit service
    Intending for both the producer and consumer lambdas to call `hasReachedInternationalSmsLimit`.
    
    Additionally, intending for the consumer lambda to call `recordSmsSent` after receiving a successful response to the request, noting that this should be after the HTTP 429 handling logic.

---
+++ integration-tests/src/test/java/uk/gov/di/authentication/services/InternationalSmsSendLimitServiceIntegrationTest.java
@@ -0,0 +1,171 @@
+package uk.gov.di.authentication.services;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import uk.gov.di.authentication.shared.helpers.PhoneNumberHelper;
+import uk.gov.di.authentication.sharedtest.extensions.InternationalSmsSendCountExtension;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class InternationalSmsSendLimitServiceIntegrationTest {
+
+    private static final int TEST_SEND_LIMIT = 3;
+
+    @RegisterExtension
+    protected static final InternationalSmsSendCountExtension extension =
+            new InternationalSmsSendCountExtension(TEST_SEND_LIMIT);
+
+    @ParameterizedTest
+    @MethodSource("phoneNumberVariations")
+    void recordSmsSentShouldFormatPhoneNumberBeforeStoring(String rawPhoneNumber) {
+        String formattedPhoneNumber = PhoneNumberHelper.formatPhoneNumber(rawPhoneNumber);
+
+        extension.recordSmsSent(rawPhoneNumber);
+
+        assertTrue(extension.hasRecordForPhoneNumber(formattedPhoneNumber));
+        if (!rawPhoneNumber.equals(formattedPhoneNumber)) {
+            assertFalse(extension.hasRecordForPhoneNumber(rawPhoneNumber));
+        }
+    }
+
+    @ParameterizedTest
+    @MethodSource("phoneNumberVariations")
+    void recordSmsSentShouldCreateNewItemIfNotExists(String internationalPhoneNumber) {
+        String formattedPhoneNumber = PhoneNumberHelper.formatPhoneNumber(internationalPhoneNumber);
+
+        assertFalse(extension.hasRecordForPhoneNumber(formattedPhoneNumber));
+
+        extension.recordSmsSent(internationalPhoneNumber);
+
+        assertTrue(extension.hasRecordForPhoneNumber(formattedPhoneNumber));
+    }
+
+    @ParameterizedTest
+    @MethodSource("phoneNumberVariations")
+    void recordSmsSentShouldIncrementExistingItemIfExists(String internationalPhoneNumber) {
+        String formattedPhoneNumber = PhoneNumberHelper.formatPhoneNumber(internationalPhoneNumber);
+
+        extension.recordSmsSent(internationalPhoneNumber);
+        assertTrue(extension.canSendSms(internationalPhoneNumber));
+
+        for (int i = 0; i < TEST_SEND_LIMIT; i++) {
+            extension.recordSmsSent(internationalPhoneNumber);
+        }
+
+        assertFalse(extension.canSendSms(internationalPhoneNumber));
+        assertTrue(extension.hasRecordForPhoneNumber(formattedPhoneNumber));
+    }
+
+    @Test
+    void differentPhoneNumbersShouldBeTrackedSeparately() {
+        String phoneNumber1 = "+33777777001";
+        String phoneNumber2 = "+33777777002";
+
+        for (int i = 0; i <= TEST_SEND_LIMIT; i++) {
+            extension.recordSmsSent(phoneNumber1);
+        }
+
+        assertFalse(extension.canSendSms(phoneNumber1));
+        assertTrue(extension.canSendSms(phoneNumber2));
+    }
+
+    @ParameterizedTest
+    @MethodSource("phoneNumberVariations")
+    void canSendSmsShouldAllowForNewPhoneNumber(String rawPhoneNumber) {
+        boolean canSendSms = extension.canSendSms(rawPhoneNumber);
+
+        assertTrue(canSendSms);
+    }
+
+    @ParameterizedTest
+    @MethodSource("phoneNumberVariations")
+    void canSendSmsShouldAllowWhenBelowLimit(String rawPhoneNumber) {
+        for (int i = 0; i < TEST_SEND_LIMIT - 1; i++) {
+            extension.recordSmsSent(rawPhoneNumber);
+        }
+
+        boolean canSendSms = extension.canSendSms(rawPhoneNumber);
+
+        assertTrue(canSendSms);
+    }
+
+    @ParameterizedTest
+    @MethodSource("phoneNumberVariations")
+    void canSendSmsShouldBlockWhenAtLimit(String rawPhoneNumber) {
+        for (int i = 0; i < TEST_SEND_LIMIT; i++) {
+            extension.recordSmsSent(rawPhoneNumber);
+        }
+
+        boolean canSendSms = extension.canSendSms(rawPhoneNumber);
+
+        assertFalse(canSendSms);
+    }
+
+    @ParameterizedTest
+    @MethodSource("phoneNumberVariations")
+    void canSendSmsShouldBlockWhenAboveLimit(String rawPhoneNumber) {
+        for (int i = 0; i < TEST_SEND_LIMIT + 1; i++) {
+            extension.recordSmsSent(rawPhoneNumber);
+        }
+
+        boolean canSendSms = extension.canSendSms(rawPhoneNumber);
+
+        assertFalse(canSendSms);
+    }
+
+    @ParameterizedTest
+    @MethodSource("domesticPhoneNumberVariations")
+    void canSendSmsShouldAllowDomesticNumbers(String domesticPhoneNumber) {
+        boolean canSendSms = extension.canSendSms(domesticPhoneNumber);
+
+        assertTrue(canSendSms);
+    }
+
+    @ParameterizedTest
+    @MethodSource("domesticPhoneNumberVariations")
+    void canSendSmsShouldAlwaysAllowDomesticNumbersRegardlessOfLimit(String domesticPhoneNumber) {
+        for (int i = 0; i < TEST_SEND_LIMIT + 5; i++) {
+            extension.recordSmsSent(domesticPhoneNumber);
+        }
+
+        assertTrue(extension.canSendSms(domesticPhoneNumber));
+    }
+
+    @ParameterizedTest
+    @MethodSource("phoneNumberVariations")
+    void canSendSmsShouldNotCreateRecord(String internationalPhoneNumber) {
+        String formattedPhoneNumber = PhoneNumberHelper.formatPhoneNumber(internationalPhoneNumber);
+
+        extension.canSendSms(internationalPhoneNumber);
+
+        assertFalse(extension.hasRecordForPhoneNumber(formattedPhoneNumber));
+    }
+
+    @ParameterizedTest
+    @MethodSource("domesticPhoneNumberVariations")
+    void recordSmsSentShouldIgnoreDomesticNumbers(String domesticPhoneNumber) {
+        extension.recordSmsSent(domesticPhoneNumber);
+        String formattedPhoneNumber = PhoneNumberHelper.formatPhoneNumber(domesticPhoneNumber);
+
+        assertFalse(
+                extension.hasRecordForPhoneNumber(formattedPhoneNumber),
+                "Domestic phone numbers should not be stored in the database");
+    }
+
+    private static Stream<Arguments> phoneNumberVariations() {
+        return Stream.of(
+                Arguments.of("+33 777 777 777"),
+                Arguments.of("+33777777777"),
+                Arguments.of("+49 30 12345678"));
+    }
+
+    private static Stream<Arguments> domesticPhoneNumberVariations() {
+        return Stream.of(Arguments.of("+44 7700 900000"), Arguments.of("+447700900000"));
+    }
+}
