2cdc512a7c2829cadf5d04d2fc9e4b16d847f8a7
Matt W <173997080+sw-mattw@users.noreply.github.com>
2026-01-27 09:00:49 +0000

    AUT-5144: Add `isDomesticPhoneNumber` helper

--- shared/src/test/java/uk/gov/di/authentication/shared/helpers/PhoneNumberHelperTest.java
+++ shared/src/test/java/uk/gov/di/authentication/shared/helpers/PhoneNumberHelperTest.java
@@ -1,9 +1,15 @@
 package uk.gov.di.authentication.shared.helpers;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class PhoneNumberHelperTest {
 
@@ -24,5 +30,31 @@ public class PhoneNumberHelperTest {
                 RuntimeException.class,
                 () -> PhoneNumberHelper.formatPhoneNumber(phoneNumber),
                 "Expected to throw exception");
+    }
+
+    @ParameterizedTest
+    @MethodSource("domesticPhoneNumbers")
+    void isDomesticPhoneNumberShouldReturnTrueForDomesticNumbers(String phoneNumber) {
+        assertTrue(PhoneNumberHelper.isDomesticPhoneNumber(phoneNumber));
+    }
+
+    @ParameterizedTest
+    @MethodSource("internationalPhoneNumbers")
+    void isDomesticPhoneNumberShouldReturnFalseForInternationalNumbers(String phoneNumber) {
+        assertFalse(PhoneNumberHelper.isDomesticPhoneNumber(phoneNumber));
+    }
+
+    @Test
+    void isDomesticPhoneNumberShouldDefaultToFalse() {
+        assertFalse(PhoneNumberHelper.isDomesticPhoneNumber("invalid"));
+    }
+
+    private static Stream<String> domesticPhoneNumbers() {
+        return Stream.of(
+                "+447316763843", "+447700900000", "+44 7700 900000", "07777777777", "07123 456789");
+    }
+
+    private static Stream<String> internationalPhoneNumbers() {
+        return Stream.of("+33777777777", "+1234567890", "+33645453322");
     }
 }
