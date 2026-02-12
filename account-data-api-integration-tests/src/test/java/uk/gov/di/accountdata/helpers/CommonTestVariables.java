128b4f87ced931a468ee7a40b01d59a18362297f
Christian Sophocleous <145482922+ChristianSoftwire@users.noreply.github.com>
2026-02-12 17:23:17 +0000

    AUT-5136: Create DynamoPasskeyService to access the Passkeys entities within the Authenticator table
    - This is a specific DynamoPasskeyService, to access Passkey entities
    - This is needed because Authenticator is abstract and will be inherited from. We can't set up an DynamoAuthenticatorService as we need to know if it's a passkey or another authenticator (for example MFA)
    
    Signed-off-by: Christian Sophocleous <145482922+ChristianSoftwire@users.noreply.github.com>

---
+++ account-data-api-integration-tests/src/test/java/uk/gov/di/accountdata/helpers/CommonTestVariables.java
@@ -0,0 +1,14 @@
+package uk.gov.di.accountdata.helpers;
+
+import java.util.List;
+
+public class CommonTestVariables {
+
+    public static final String PUBLIC_SUBJECT_ID = "public-subject-id";
+    public static final String ANOTHER_PUBLIC_SUBJECT_ID = "another-public-subject-id";
+    public static final String PRIMARY_PASSKEY_ID = "primary-passkey-id";
+    public static final String SECONDARY_PASSKEY_ID = "secondary-passkey-id";
+    public static final String ANOTHER_USER_PASSKEY_ID = "another-user-passkey-id";
+    public static final String PASSKEY_AAGUID = "passkey-aaguid";
+    public static final List<String> PASSKEY_TRANSPORTS = List.of("internal", "hybrid");
+}
