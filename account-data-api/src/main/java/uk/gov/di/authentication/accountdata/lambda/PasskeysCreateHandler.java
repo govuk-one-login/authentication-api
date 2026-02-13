46e65ca8fc1c72097d77e4c84678f495a668566d
Christian Sophocleous <145482922+ChristianSoftwire@users.noreply.github.com>
2026-02-13 17:02:38 +0000

    AUT-5169: setup the basic create passkey endpoint
    Signed-off-by: Christian Sophocleous <145482922+ChristianSoftwire@users.noreply.github.com>

--- account-data-api/src/main/java/uk/gov/di/authentication/accountdata/lambda/PasskeysCreateHandler.java
+++ account-data-api/src/main/java/uk/gov/di/authentication/accountdata/lambda/PasskeysCreateHandler.java
@@ -8,10 +8,9 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.ThreadContext;
 import uk.gov.di.authentication.accountdata.entity.passkey.PasskeysCreateFailureReason;
-import uk.gov.di.authentication.accountdata.entity.passkey.PasskeysCreateRequest;
+import uk.gov.di.authentication.accountdata.services.PasskeyService;
 import uk.gov.di.authentication.shared.entity.ErrorResponse;
 import uk.gov.di.authentication.shared.entity.Result;
-import uk.gov.di.authentication.shared.serialization.Json;
 import uk.gov.di.authentication.shared.services.ConfigurationService;
 import uk.gov.di.authentication.shared.services.SerializationService;
 
@@ -24,21 +23,22 @@ public class PasskeysCreateHandler
 
     private static final Logger LOG = LogManager.getLogger(PasskeysCreateHandler.class);
     private final ConfigurationService configurationService;
-    protected final SerializationService objectMapper;
+    private final PasskeyService passkeyService;
 
     public PasskeysCreateHandler() {
         this(ConfigurationService.getInstance());
     }
 
     public PasskeysCreateHandler(
-            ConfigurationService configurationService, SerializationService serializationService) {
+            ConfigurationService configurationService, PasskeyService passkeyService) {
         this.configurationService = configurationService;
-        this.objectMapper = serializationService;
+        this.passkeyService = passkeyService;
     }
 
     public PasskeysCreateHandler(ConfigurationService configurationService) {
         this.configurationService = configurationService;
-        this.objectMapper = SerializationService.getInstance();
+        this.passkeyService =
+                new PasskeyService(SerializationService.getInstance(), configurationService);
     }
 
     @Override
@@ -55,27 +55,12 @@ public class PasskeysCreateHandler
 
         LOG.info("PasskeysCreateHandler called");
 
-        Result<PasskeysCreateFailureReason, PasskeysCreateRequest> result =
-                validateCreateRequest(input);
+        Result<PasskeysCreateFailureReason, Void> result = passkeyService.createPasskey(input);
 
         return result.fold(
                 failure ->
                         generateApiGatewayProxyErrorResponse(
                                 500, ErrorResponse.UNEXPECTED_ACCOUNT_DATA_API_ERROR),
                 passkeyCreateResult -> generateApiGatewayProxyResponse(201, ""));
-    }
-
-    public Result<PasskeysCreateFailureReason, PasskeysCreateRequest> validateCreateRequest(
-            APIGatewayProxyRequestEvent input) {
-        PasskeysCreateRequest passkeysCreateRequest;
-        try {
-            passkeysCreateRequest =
-                    objectMapper.readValue(input.getBody(), PasskeysCreateRequest.class, true);
-
-        } catch (Json.JsonException e) {
-            return Result.failure(PasskeysCreateFailureReason.PARSING_PASSKEY_CREATE_REQUEST_ERROR);
-        }
-
-        return Result.success(passkeysCreateRequest);
     }
 }
