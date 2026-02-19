75a6ed7439d4cf974382841a997afc57ba18c117
Christian Sophocleous <145482922+ChristianSoftwire@users.noreply.github.com>
2026-02-19 18:02:37 +0000

    AUT-5169: Changes to PasskeysCreateService
    Signed-off-by: Christian Sophocleous <145482922+ChristianSoftwire@users.noreply.github.com>

--- account-data-api/src/main/java/uk/gov/di/authentication/accountdata/services/PasskeysCreateService.java
+++ account-data-api/src/main/java/uk/gov/di/authentication/accountdata/services/PasskeysCreateService.java
@@ -1,22 +1,17 @@
 package uk.gov.di.authentication.accountdata.services;
 
-import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import uk.gov.di.authentication.accountdata.entity.passkey.PasskeysCreateFailureReason;
 import uk.gov.di.authentication.accountdata.entity.passkey.PasskeysCreateRequest;
 import uk.gov.di.authentication.shared.entity.Result;
-import uk.gov.di.authentication.shared.serialization.Json;
 import uk.gov.di.authentication.shared.services.ConfigurationService;
-import uk.gov.di.authentication.shared.services.SerializationService;
 
-import java.util.List;
 import java.util.UUID;
 
 public class PasskeysCreateService {
 
     private static final Logger LOG = LogManager.getLogger(PasskeysCreateService.class);
-    private final Json objectMapper = SerializationService.getInstance();
     private final DynamoPasskeyService dynamoPasskeyService;
     private final ConfigurationService configurationService;
 
@@ -32,14 +27,7 @@ public class PasskeysCreateService {
     }
 
     public Result<PasskeysCreateFailureReason, Void> createPasskey(
-            APIGatewayProxyRequestEvent input) {
-        PasskeysCreateRequest passkeysCreateRequest;
-        try {
-            passkeysCreateRequest =
-                    objectMapper.readValue(input.getBody(), PasskeysCreateRequest.class, true);
-        } catch (Json.JsonException e) {
-            return Result.failure(PasskeysCreateFailureReason.REQUEST_MISSING_PARAMS);
-        }
+            PasskeysCreateRequest passkeysCreateRequest, String publicSubjectId) {
 
         var validatePasskeysCreateRequestResult =
                 validatePasskeysCreateRequest(passkeysCreateRequest);
@@ -47,24 +35,19 @@ public class PasskeysCreateService {
             return validatePasskeysCreateRequestResult;
         }
 
-        var publicSubjectId = input.getPathParameters().get("publicSubjectId");
-        if (publicSubjectId.isEmpty()) {
-            LOG.error("Request does not include public subject id");
-            return Result.failure(PasskeysCreateFailureReason.REQUEST_MISSING_PARAMS);
-        }
-
         boolean passkeyCreated;
         try {
             passkeyCreated =
                     dynamoPasskeyService.savePasskeyIfUnique(
                             publicSubjectId,
+                            passkeysCreateRequest.getCredential(),
                             passkeysCreateRequest.getPasskeyId(),
                             passkeysCreateRequest.getAaguid(),
-                            true,
-                            0,
-                            List.of("SomeTransport"),
-                            true,
-                            false);
+                            passkeysCreateRequest.getIsAttested(),
+                            passkeysCreateRequest.getSignCount(),
+                            passkeysCreateRequest.getTransports(),
+                            passkeysCreateRequest.getIsBackUpEligible(),
+                            passkeysCreateRequest.getIsBackedUp());
         } catch (Exception e) {
             LOG.error("Failed to save passkey", e);
             return Result.failure(PasskeysCreateFailureReason.FAILED_TO_SAVE_PASSKEY);
@@ -85,8 +68,6 @@ public class PasskeysCreateService {
             PasskeysCreateRequest passkeysCreateRequest) {
         if (!isAaguidValid(passkeysCreateRequest.getAaguid())) {
             return Result.failure(PasskeysCreateFailureReason.INVALID_AAGUID);
-        } else if (passkeysCreateRequest.getCredential().isEmpty()) {
-            return Result.failure(PasskeysCreateFailureReason.INVALID_CREDENTIAL);
         } else {
             return Result.success(null);
         }
--- account-data-api/src/test/java/uk/gov/di/authentication/accountdata/services/PasskeysCreateServiceTest.java
+++ account-data-api/src/test/java/uk/gov/di/authentication/accountdata/services/PasskeysCreateServiceTest.java
@@ -1,34 +1,32 @@
 package uk.gov.di.authentication.accountdata.services;
 
-import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import uk.gov.di.authentication.shared.helpers.CommonTestVariables;
-import uk.gov.di.authentication.shared.serialization.Json;
+import uk.gov.di.authentication.accountdata.entity.passkey.PasskeysCreateFailureReason;
+import uk.gov.di.authentication.accountdata.entity.passkey.PasskeysCreateRequest;
 import uk.gov.di.authentication.shared.services.ConfigurationService;
-import uk.gov.di.authentication.shared.services.SerializationService;
 
-import java.util.Map;
+import java.util.List;
 
-import static java.lang.String.format;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.mockito.ArgumentMatchers.anyBoolean;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.anyList;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+import static uk.gov.di.authentication.accountdata.helpers.CommonTestVariables.CREDENTIAL;
+import static uk.gov.di.authentication.accountdata.helpers.CommonTestVariables.PRIMARY_PASSKEY_ID;
 import static uk.gov.di.authentication.accountdata.helpers.CommonTestVariables.PUBLIC_SUBJECT_ID;
 import static uk.gov.di.authentication.accountdata.helpers.CommonTestVariables.TEST_AAGUID;
-import static uk.gov.di.authentication.sharedtest.helper.RequestEventHelper.contextWithSourceIp;
 
 class PasskeysCreateServiceTest {
 
     private final DynamoPasskeyService dynamoPasskeyService = mock(DynamoPasskeyService.class);
     private final ConfigurationService configurationService = mock(ConfigurationService.class);
-    private final Json objectMapper = SerializationService.getInstance();
 
     private PasskeysCreateService passkeysCreateService;
 
@@ -39,52 +37,165 @@ class PasskeysCreateServiceTest {
     }
 
     @Nested
-    class CreatePasskey {
+    class Success {
 
-        @Nested
-        class Success {
+        @Test
+        void shouldCreatePasskeyGivenValidRequest() {
+            // Given
+            var passkeysCreateRequest =
+                    buildPasskeysCreateRequest(
+                            CREDENTIAL,
+                            PRIMARY_PASSKEY_ID,
+                            TEST_AAGUID,
+                            false,
+                            0,
+                            List.of("transport1", "transport2"),
+                            false,
+                            false,
+                            false);
+            when(dynamoPasskeyService.savePasskeyIfUnique(
+                            eq(PUBLIC_SUBJECT_ID),
+                            eq(CREDENTIAL),
+                            eq(PRIMARY_PASSKEY_ID),
+                            eq(TEST_AAGUID),
+                            anyBoolean(),
+                            anyInt(),
+                            anyList(),
+                            anyBoolean(),
+                            anyBoolean()))
+                    .thenReturn(true);
 
-            @Test
-            void shouldCreatePasskeyGivenValidRequest() {
-                // Given
-                var createPasskeysRequest =
-                        passkeysCreateRequest(
-                                format(
-                                        """
-                            {
-                                "credential": "some-credential",
-                                "id": "some-id",
-                                "aaguid": %s,
-                                "attestationSignature": "attestation-signature"
-                            }
-                        """,
-                                        TEST_AAGUID));
-                when(dynamoPasskeyService.savePasskeyIfUnique(
-                                eq(PUBLIC_SUBJECT_ID),
-                                eq("some-id"),
-                                eq(TEST_AAGUID),
-                                anyBoolean(),
-                                anyInt(),
-                                anyList(),
-                                anyBoolean(),
-                                anyBoolean()))
-                        .thenReturn(true);
+            // When
+            var result =
+                    passkeysCreateService.createPasskey(passkeysCreateRequest, PUBLIC_SUBJECT_ID);
 
-                // When
-                var result = passkeysCreateService.createPasskey(createPasskeysRequest);
-
-                // Then
-                assertThat(result.isSuccess(), equalTo(true));
-            }
+            // Then
+            assertThat(result.isSuccess(), equalTo(true));
         }
     }
 
-    private APIGatewayProxyRequestEvent passkeysCreateRequest(String requestBody) {
-        return new APIGatewayProxyRequestEvent()
-                .withPathParameters(
-                        Map.of("publicSubjectId", CommonTestVariables.PUBLIC_SUBJECT_ID))
-                .withHeaders(CommonTestVariables.VALID_HEADERS)
-                .withBody(requestBody)
-                .withRequestContext(contextWithSourceIp(CommonTestVariables.IP_ADDRESS));
+    @Nested
+    class Validation {
+        @Test
+        void shouldReturnInvalidAaguidIfInvalidUUID() {
+            // Given
+            var passkeysCreateRequest =
+                    buildPasskeysCreateRequest(
+                            "some-credential",
+                            "passkey-id",
+                            "invalid-aaguid",
+                            false,
+                            0,
+                            List.of("transport1", "transport2"),
+                            false,
+                            false,
+                            false);
+
+            // When
+            var result =
+                    passkeysCreateService.createPasskey(passkeysCreateRequest, PUBLIC_SUBJECT_ID);
+
+            // Then
+            assertThat(result.isFailure(), equalTo(true));
+            assertThat(result.getFailure(), equalTo(PasskeysCreateFailureReason.INVALID_AAGUID));
+        }
+
+        @Test
+        void shouldReturnPasskeyExistsIfPasskeyWithCredentialIdExistsForSubjectId() {
+            // Given
+            var passkeysCreateRequest =
+                    buildPasskeysCreateRequest(
+                            "some-credential",
+                            "passkey-id",
+                            TEST_AAGUID,
+                            false,
+                            0,
+                            List.of("transport1", "transport2"),
+                            false,
+                            false,
+                            false);
+            when(dynamoPasskeyService.savePasskeyIfUnique(
+                            eq(PUBLIC_SUBJECT_ID),
+                            anyString(),
+                            anyString(),
+                            anyString(),
+                            anyBoolean(),
+                            anyInt(),
+                            anyList(),
+                            anyBoolean(),
+                            anyBoolean()))
+                    .thenReturn(false);
+
+            // When
+            var result =
+                    passkeysCreateService.createPasskey(passkeysCreateRequest, PUBLIC_SUBJECT_ID);
+
+            // Then
+            assertThat(result.isFailure(), equalTo(true));
+            assertThat(result.getFailure(), equalTo(PasskeysCreateFailureReason.PASSKEY_EXISTS));
+        }
+    }
+
+    @Nested
+    class Error {
+
+        @Test
+        void shouldReturnFailedToSavePasskeyIfExceptionThrown() {
+            // Given
+            var passkeysCreateRequest =
+                    buildPasskeysCreateRequest(
+                            "some-credential",
+                            "passkey-id",
+                            TEST_AAGUID,
+                            false,
+                            0,
+                            List.of("transport1", "transport2"),
+                            false,
+                            false,
+                            false);
+            when(dynamoPasskeyService.savePasskeyIfUnique(
+                            eq(PUBLIC_SUBJECT_ID),
+                            anyString(),
+                            anyString(),
+                            anyString(),
+                            anyBoolean(),
+                            anyInt(),
+                            anyList(),
+                            anyBoolean(),
+                            anyBoolean()))
+                    .thenThrow(new RuntimeException());
+
+            // When
+            var result =
+                    passkeysCreateService.createPasskey(passkeysCreateRequest, PUBLIC_SUBJECT_ID);
+
+            // Then
+            assertThat(result.isFailure(), equalTo(true));
+            assertThat(
+                    result.getFailure(),
+                    equalTo(PasskeysCreateFailureReason.FAILED_TO_SAVE_PASSKEY));
+        }
+    }
+
+    private PasskeysCreateRequest buildPasskeysCreateRequest(
+            String credential,
+            String passkeyId,
+            String aaguid,
+            boolean isAttested,
+            int signCount,
+            List<String> transports,
+            boolean isBackUpEligible,
+            boolean isBackedUp,
+            boolean isResidentKey) {
+        return new PasskeysCreateRequest(
+                credential,
+                passkeyId,
+                aaguid,
+                isAttested,
+                signCount,
+                transports,
+                isBackUpEligible,
+                isBackedUp,
+                isResidentKey);
     }
 }
