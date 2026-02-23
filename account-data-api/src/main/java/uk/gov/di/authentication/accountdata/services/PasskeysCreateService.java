8c69603d2fd5fd1787f2003c562527f2ea94df2b
Christian Sophocleous <145482922+ChristianSoftwire@users.noreply.github.com>
2026-02-23 11:17:29 +0000

    AUT-5169: Create PasskeysCreateService
    - Responsible for light validation of the request body, building Passkey object to be saved and calling the Dynamo service to save this object
    - Use the Result pattern
    
    Signed-off-by: Christian Sophocleous <145482922+ChristianSoftwire@users.noreply.github.com>

---
+++ account-data-api/src/main/java/uk/gov/di/authentication/accountdata/services/PasskeysCreateService.java
@@ -0,0 +1,68 @@
+package uk.gov.di.authentication.accountdata.services;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import uk.gov.di.authentication.accountdata.entity.passkey.Passkey;
+import uk.gov.di.authentication.accountdata.entity.passkey.PasskeysCreateRequest;
+import uk.gov.di.authentication.accountdata.entity.passkey.failurereasons.PasskeysCreateServiceFailureReason;
+import uk.gov.di.authentication.shared.entity.Result;
+import uk.gov.di.authentication.shared.services.ConfigurationService;
+
+import java.time.LocalDateTime;
+
+import static uk.gov.di.authentication.accountdata.helpers.PasskeysHelper.buildSortKey;
+
+public class PasskeysCreateService {
+
+    private static final Logger LOG = LogManager.getLogger(PasskeysCreateService.class);
+    private final DynamoPasskeyService dynamoPasskeyService;
+    private final ConfigurationService configurationService;
+
+    public PasskeysCreateService(ConfigurationService configurationService) {
+        this.configurationService = configurationService;
+        this.dynamoPasskeyService = new DynamoPasskeyService(configurationService);
+    }
+
+    public PasskeysCreateService(
+            ConfigurationService configurationService, DynamoPasskeyService dynamoPasskeyService) {
+        this.configurationService = configurationService;
+        this.dynamoPasskeyService = dynamoPasskeyService;
+    }
+
+    public Result<PasskeysCreateServiceFailureReason, Void> createPasskey(
+            PasskeysCreateRequest passkeysCreateRequest, String publicSubjectId) {
+        boolean passkeyCreated;
+        var passkeyId = passkeysCreateRequest.passkeyId();
+
+        try {
+            var created = LocalDateTime.now().toString();
+            var passkey =
+                    new Passkey()
+                            .withPublicSubjectId(publicSubjectId)
+                            .withSortKey(buildSortKey(passkeyId))
+                            .withCredentialId(passkeyId)
+                            .withCreated(created)
+                            .withCredential(passkeysCreateRequest.credential())
+                            .withPasskeyAaguid(passkeysCreateRequest.aaguid())
+                            .withPasskeyIsAttested(passkeysCreateRequest.isAttested())
+                            .withPasskeySignCount(passkeysCreateRequest.signCount())
+                            .withPasskeyTransports(passkeysCreateRequest.transports())
+                            .withPasskeyBackupEligible(passkeysCreateRequest.isBackUpEligible())
+                            .withPasskeyBackedUp(passkeysCreateRequest.isBackedUp());
+            passkeyCreated = dynamoPasskeyService.savePasskeyIfUnique(passkey);
+        } catch (Exception e) {
+            LOG.error("Failed to save passkey", e);
+            return Result.failure(PasskeysCreateServiceFailureReason.FAILED_TO_SAVE_PASSKEY);
+        }
+
+        if (!passkeyCreated) {
+            LOG.error(
+                    "Passkey with id {} already exists for user with subjectId {}",
+                    passkeyId,
+                    publicSubjectId);
+            return Result.failure(PasskeysCreateServiceFailureReason.PASSKEY_EXISTS);
+        }
+
+        return Result.success(null);
+    }
+}
