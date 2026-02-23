97cc58eccc2c86df19634dfe6933c4f0b1da36a3
Christian Sophocleous <145482922+ChristianSoftwire@users.noreply.github.com>
2026-02-23 11:19:01 +0000

    AUT-5169: Add PasskeysCreateHandler
    - Map the request body and validate the publicSubjectId is not null
    - Call the service to save
    - Use the result pattern to map Failures to ErrorResponses
    
    Signed-off-by: Christian Sophocleous <145482922+ChristianSoftwire@users.noreply.github.com>

--- account-data-api/src/main/java/uk/gov/di/authentication/accountdata/lambda/PasskeysCreateHandler.java
+++ account-data-api/src/main/java/uk/gov/di/authentication/accountdata/lambda/PasskeysCreateHandler.java
@@ -7,14 +7,20 @@ import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.ThreadContext;
-import uk.gov.di.authentication.accountdata.entity.passkey.PasskeysCreateFailureReason;
 import uk.gov.di.authentication.accountdata.entity.passkey.PasskeysCreateRequest;
+import uk.gov.di.authentication.accountdata.entity.passkey.failurereasons.PasskeysCreateHandlerFailureReason;
+import uk.gov.di.authentication.accountdata.entity.passkey.failurereasons.PasskeysCreateServiceFailureReason;
+import uk.gov.di.authentication.accountdata.services.PasskeysCreateService;
 import uk.gov.di.authentication.shared.entity.ErrorResponse;
 import uk.gov.di.authentication.shared.entity.Result;
 import uk.gov.di.authentication.shared.serialization.Json;
 import uk.gov.di.authentication.shared.services.ConfigurationService;
 import uk.gov.di.authentication.shared.services.SerializationService;
 
+import java.util.UUID;
+
+import static uk.gov.di.authentication.accountdata.entity.passkey.failurereasons.PasskeysCreateHandlerFailureReason.INVALID_AAGUID;
+import static uk.gov.di.authentication.accountdata.entity.passkey.failurereasons.PasskeysCreateHandlerFailureReason.REQUEST_MISSING_PARAMS;
 import static uk.gov.di.authentication.shared.helpers.ApiGatewayResponseHelper.generateApiGatewayProxyErrorResponse;
 import static uk.gov.di.authentication.shared.helpers.ApiGatewayResponseHelper.generateApiGatewayProxyResponse;
 import static uk.gov.di.authentication.shared.helpers.InstrumentationHelper.segmentedFunctionCall;
@@ -24,21 +30,22 @@ public class PasskeysCreateHandler
 
     private static final Logger LOG = LogManager.getLogger(PasskeysCreateHandler.class);
     private final ConfigurationService configurationService;
-    protected final SerializationService objectMapper;
+    private final PasskeysCreateService passkeysCreateService;
+    private final Json objectMapper = SerializationService.getInstance();
 
     public PasskeysCreateHandler() {
         this(ConfigurationService.getInstance());
     }
 
     public PasskeysCreateHandler(
-            ConfigurationService configurationService, SerializationService serializationService) {
+            ConfigurationService configurationService, PasskeysCreateService passkeyService) {
         this.configurationService = configurationService;
-        this.objectMapper = serializationService;
+        this.passkeysCreateService = passkeyService;
     }
 
     public PasskeysCreateHandler(ConfigurationService configurationService) {
         this.configurationService = configurationService;
-        this.objectMapper = SerializationService.getInstance();
+        this.passkeysCreateService = new PasskeysCreateService(configurationService);
     }
 
     @Override
@@ -55,27 +62,87 @@ public class PasskeysCreateHandler
 
         LOG.info("PasskeysCreateHandler called");
 
-        Result<PasskeysCreateFailureReason, PasskeysCreateRequest> result =
-                validateCreateRequest(input);
-
-        return result.fold(
-                failure ->
-                        generateApiGatewayProxyErrorResponse(
-                                500, ErrorResponse.UNEXPECTED_ACCOUNT_DATA_API_ERROR),
-                passkeyCreateResult -> generateApiGatewayProxyResponse(201, ""));
+        return parseRequest(input)
+                .flatMap(this::validateRequest)
+                .flatMap(this::createPasskey)
+                .fold(
+                        failure ->
+                                switch (failure) {
+                                    case REQUEST_MISSING_PARAMS -> generateApiGatewayProxyErrorResponse(
+                                            400, ErrorResponse.REQUEST_MISSING_PARAMS);
+                                    case PASSKEY_EXISTS -> generateApiGatewayProxyErrorResponse(
+                                            409, ErrorResponse.PASSKEY_ALREADY_EXISTS);
+                                    case INVALID_AAGUID -> generateApiGatewayProxyErrorResponse(
+                                            422, ErrorResponse.INVALID_AAGUID);
+                                    case FAILED_TO_SAVE_PASSKEY -> generateApiGatewayProxyErrorResponse(
+                                            500, ErrorResponse.UNEXPECTED_ACCOUNT_DATA_API_ERROR);
+                                },
+                        passkeyCreateResult -> generateApiGatewayProxyResponse(201, ""));
     }
 
-    public Result<PasskeysCreateFailureReason, PasskeysCreateRequest> validateCreateRequest(
+    private Result<PasskeysCreateHandlerFailureReason, PasskeysCreateContext> parseRequest(
             APIGatewayProxyRequestEvent input) {
         PasskeysCreateRequest passkeysCreateRequest;
         try {
             passkeysCreateRequest =
                     objectMapper.readValue(input.getBody(), PasskeysCreateRequest.class, true);
-
         } catch (Json.JsonException e) {
-            return Result.failure(PasskeysCreateFailureReason.PARSING_PASSKEY_CREATE_REQUEST_ERROR);
+            return Result.failure(REQUEST_MISSING_PARAMS);
         }
 
-        return Result.success(passkeysCreateRequest);
+        var publicSubjectId = input.getPathParameters().get("publicSubjectId");
+        if (publicSubjectId == null || publicSubjectId.isEmpty()) {
+            LOG.error("Request does not include public subject id");
+            return Result.failure(REQUEST_MISSING_PARAMS);
+        }
+
+        return Result.success(new PasskeysCreateContext(publicSubjectId, passkeysCreateRequest));
     }
+
+    private Result<PasskeysCreateHandlerFailureReason, PasskeysCreateContext> validateRequest(
+            PasskeysCreateContext context) {
+        var passkeysCreateRequest = context.passkeysCreateRequest();
+
+        if (!isAaguidValid(passkeysCreateRequest.aaguid())) {
+            return Result.failure(INVALID_AAGUID);
+        }
+
+        return Result.success(context);
+    }
+
+    private Result<PasskeysCreateHandlerFailureReason, Void> createPasskey(
+            PasskeysCreateContext context) {
+        var passkeysCreateRequest = context.passkeysCreateRequest();
+        var publicSubjectId = context.publicSubjectId();
+
+        Result<PasskeysCreateServiceFailureReason, Void> result =
+                passkeysCreateService.createPasskey(passkeysCreateRequest, publicSubjectId);
+
+        return result.fold(
+                failure ->
+                        switch (failure) {
+                            case FAILED_TO_SAVE_PASSKEY -> Result.failure(
+                                    PasskeysCreateHandlerFailureReason.FAILED_TO_SAVE_PASSKEY);
+                            case PASSKEY_EXISTS -> Result.failure(
+                                    PasskeysCreateHandlerFailureReason.PASSKEY_EXISTS);
+                        },
+                success -> Result.success(null));
+    }
+
+    private boolean isAaguidValid(String aaguid) {
+        if (aaguid == null || aaguid.trim().isEmpty()) {
+            return false;
+        }
+
+        try {
+            UUID.fromString(aaguid);
+        } catch (IllegalArgumentException e) {
+            return false;
+        }
+
+        return true;
+    }
+
+    private record PasskeysCreateContext(
+            String publicSubjectId, PasskeysCreateRequest passkeysCreateRequest) {}
 }
