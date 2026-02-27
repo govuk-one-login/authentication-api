f8032db1e9ee1a746a4e01b4f178cf02de3b9bf0
Christian Sophocleous <145482922+ChristianSoftwire@users.noreply.github.com>
2026-02-27 15:43:37 +0000

    AUT-5169: Move isAaguidValid to helper class
    - Add test for isAaguidValid
    - Add test for buildSortKey
    
    Signed-off-by: Christian Sophocleous <145482922+ChristianSoftwire@users.noreply.github.com>

--- account-data-api/src/main/java/uk/gov/di/authentication/accountdata/helpers/PasskeysHelper.java
+++ account-data-api/src/main/java/uk/gov/di/authentication/accountdata/helpers/PasskeysHelper.java
@@ -2,11 +2,27 @@ package uk.gov.di.authentication.accountdata.helpers;
 
 import uk.gov.di.authentication.accountdata.constants.AccountDataConstants;
 
+import java.util.UUID;
+
 public class PasskeysHelper {
 
     private PasskeysHelper() {}
 
     public static String buildSortKey(String passkeyId) {
         return AccountDataConstants.PASSKEY_TYPE + "#" + passkeyId;
+    }
+
+    public static boolean isAaguidValid(String aaguid) {
+        if (aaguid == null || aaguid.trim().isEmpty()) {
+            return false;
+        }
+
+        try {
+            UUID.fromString(aaguid);
+        } catch (IllegalArgumentException e) {
+            return false;
+        }
+
+        return true;
     }
 }
