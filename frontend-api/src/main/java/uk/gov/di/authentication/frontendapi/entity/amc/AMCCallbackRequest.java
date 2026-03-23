2393d1f354ae766a672d5a57c8d0bd7d63e227f2
Aidan Comer <alhcomer@hotmail.co.uk>
2026-03-23 12:35:41 +0000

    BAU: add serialized name annotation for usedRedirectUrl
    - The backend serialized using LOWER_CASE_WITH_UNDERSCORES field naming scheme
    - as the frontend sends using camel case, we must specify the serialized name

--- frontend-api/src/main/java/uk/gov/di/authentication/frontendapi/entity/amc/AMCCallbackRequest.java
+++ frontend-api/src/main/java/uk/gov/di/authentication/frontendapi/entity/amc/AMCCallbackRequest.java
@@ -1,9 +1,10 @@
 package uk.gov.di.authentication.frontendapi.entity.amc;
 
 import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
 import uk.gov.di.authentication.shared.validation.Required;
 
 public record AMCCallbackRequest(
         @Expose @Required String code,
         @Expose @Required String state,
-        @Expose @Required String usedRedirectUrl) {}
+        @Expose @Required @SerializedName("usedRedirectUrl") String usedRedirectUrl) {}
