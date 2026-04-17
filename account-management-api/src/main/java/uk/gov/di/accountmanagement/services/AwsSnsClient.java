073f55086971e49d733fe8c56f29e60820b81e3d
Ryan Andrews <ryan.andrews@digital.cabinet-office.gov.uk>
2026-04-17 12:24:18 +0100

    ATO-2508: Use default credentials provider, not Environment variable based
    We previously enabled snapstart on the token handler[1] which started
    throwing errors due to the usage of EnvironmentVariableCredentialsProvider
    here.
    
    When using Snapstart[2], the environment variables based credentials
    are not set in the lambda environment, as these would have likely
    expired due to the cached execution environment.
    
    When explicitly setting EnvironmentVariableCredentialsProvider here,
    we overide the default credential provider chain[3] lambda uses, which
    means lambda only tries to use the Environment variables to invoke the
    SDK client, causing exceptions to be thrown because the env vars are not
    set when Snapstart is enabled.
    
    The fix here is to use the Default Credential provider class, as
    there is no real benefit to us overriding the default here. This means
    that once snapstart is re-enabled, we will no longer throw an exception
    here.
    
    [1] - https://github.com/govuk-one-login/authentication-api/pull/8168
    [2] - https://docs.aws.amazon.com/lambda/latest/dg/snapstart-activate.html#snapstart-credentials
    [3] - https://docs.aws.amazon.com/sdk-for-java/latest/developer-guide/credentials-chain.html

--- account-management-api/src/main/java/uk/gov/di/accountmanagement/services/AwsSnsClient.java
+++ account-management-api/src/main/java/uk/gov/di/accountmanagement/services/AwsSnsClient.java
@@ -1,5 +1,6 @@
 package uk.gov.di.accountmanagement.services;
 
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.auth.credentials.EnvironmentVariableCredentialsProvider;
 import software.amazon.awssdk.core.exception.SdkClientException;
 import software.amazon.awssdk.regions.Region;
@@ -21,9 +22,10 @@ public class AwsSnsClient {
         var builder =
                 SnsClient.builder()
                         .region(Region.of(region))
-                        .credentialsProvider(EnvironmentVariableCredentialsProvider.create());
+                        .credentialsProvider(DefaultCredentialsProvider.builder().build());
         if (endpointUri != null && !endpointUri.isEmpty()) {
-            builder.endpointOverride(URI.create(endpointUri));
+            builder.endpointOverride(URI.create(endpointUri))
+                    .credentialsProvider(EnvironmentVariableCredentialsProvider.create());
         }
         this.snsClient = builder.build();
         this.topicArn = topicArn;
