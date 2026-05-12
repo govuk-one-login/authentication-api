1d2ef0a329a026ad91d3ef2d8bc48909052318c7
BeckaL <25515510+BeckaL@users.noreply.github.com>
2026-05-12 13:01:09 +0100

    BAU: Remove helper methods which are now no longer used
    Used by methods which have been removed as they were only referenced in test

--- shared/src/main/java/uk/gov/di/authentication/shared/services/TokenService.java
+++ shared/src/main/java/uk/gov/di/authentication/shared/services/TokenService.java
@@ -8,24 +8,9 @@ import com.nimbusds.jose.crypto.impl.ECDSA;
 import com.nimbusds.jose.util.Base64URL;
 import com.nimbusds.jwt.JWTClaimsSet;
 import com.nimbusds.jwt.SignedJWT;
-import com.nimbusds.oauth2.sdk.ErrorObject;
-import com.nimbusds.oauth2.sdk.GrantType;
-import com.nimbusds.oauth2.sdk.OAuth2Error;
-import com.nimbusds.oauth2.sdk.Scope;
-import com.nimbusds.oauth2.sdk.id.Audience;
-import com.nimbusds.oauth2.sdk.id.Issuer;
 import com.nimbusds.oauth2.sdk.id.Subject;
 import com.nimbusds.oauth2.sdk.token.AccessToken;
 import com.nimbusds.oauth2.sdk.token.BearerAccessToken;
-import com.nimbusds.oauth2.sdk.token.RefreshToken;
-import com.nimbusds.openid.connect.sdk.OIDCClaimsRequest;
-import com.nimbusds.openid.connect.sdk.OIDCScopeValue;
-import com.nimbusds.openid.connect.sdk.OIDCTokenResponse;
-import com.nimbusds.openid.connect.sdk.claims.AccessTokenHash;
-import com.nimbusds.openid.connect.sdk.claims.ClaimsSetRequest;
-import com.nimbusds.openid.connect.sdk.claims.IDTokenClaimsSet;
-import com.nimbusds.openid.connect.sdk.claims.SessionID;
-import com.nimbusds.openid.connect.sdk.token.OIDCTokens;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import software.amazon.awssdk.core.SdkBytes;
@@ -33,42 +18,23 @@ import software.amazon.awssdk.services.kms.model.GetPublicKeyRequest;
 import software.amazon.awssdk.services.kms.model.SignRequest;
 import software.amazon.awssdk.services.kms.model.SignResponse;
 import software.amazon.awssdk.services.kms.model.SigningAlgorithmSpec;
-import uk.gov.di.authentication.shared.entity.AccessTokenStore;
-import uk.gov.di.authentication.shared.entity.RefreshTokenStore;
-import uk.gov.di.authentication.shared.helpers.IdGenerator;
 import uk.gov.di.authentication.shared.helpers.NowHelper;
-import uk.gov.di.authentication.shared.helpers.RequestBodyHelper;
-import uk.gov.di.authentication.shared.serialization.Json;
-import uk.gov.di.authentication.shared.serialization.Json.JsonException;
 
-import java.net.URI;
 import java.nio.charset.StandardCharsets;
 import java.time.temporal.ChronoUnit;
 import java.util.Date;
 import java.util.List;
-import java.util.Map;
-import java.util.Objects;
 import java.util.Optional;
 import java.util.UUID;
-import java.util.stream.Collectors;
 
-import static uk.gov.di.authentication.shared.helpers.ConstructUriHelper.buildURI;
 import static uk.gov.di.authentication.shared.helpers.HashHelper.hashSha256String;
-import static uk.gov.di.authentication.shared.helpers.InstrumentationHelper.segmentedFunctionCall;
 
 public class TokenService {
 
     private final ConfigurationService configService;
     private final RedisConnectionService redisConnectionService;
     private final KmsConnectionService kmsConnectionService;
-    private static final JWSAlgorithm TOKEN_ALGORITHM = JWSAlgorithm.ES256;
     private static final Logger LOG = LogManager.getLogger(TokenService.class);
-    private static final String REFRESH_TOKEN_PREFIX = "REFRESH_TOKEN:";
-    private static final String ACCESS_TOKEN_PREFIX = "ACCESS_TOKEN:";
-    private static final List<String> ALLOWED_GRANTS =
-            List.of(GrantType.AUTHORIZATION_CODE.getValue(), GrantType.REFRESH_TOKEN.getValue());
-
-    private final Json objectMapper = SerializationService.getInstance();
 
     public TokenService(
             ConfigurationService configService,
@@ -77,61 +43,6 @@ public class TokenService {
         this.configService = configService;
         this.redisConnectionService = redisConnectionService;
         this.kmsConnectionService = kmsConnectionService;
-    }
-
-    private Optional<ErrorObject> validateRefreshRequestParams(Map<String, String> requestBody) {
-        if (!requestBody.containsKey("refresh_token")) {
-            return Optional.of(
-                    new ErrorObject(
-                            OAuth2Error.INVALID_REQUEST_CODE, "Request is missing refresh token"));
-        }
-        try {
-            new RefreshToken(requestBody.get("refresh_token"));
-        } catch (IllegalArgumentException e) {
-            LOG.warn("Invalid RefreshToken", e);
-            return Optional.of(
-                    new ErrorObject(OAuth2Error.INVALID_REQUEST_CODE, "Invalid refresh token"));
-        }
-        return Optional.empty();
-    }
-
-    private SignedJWT generateIDToken(
-            String clientId,
-            Subject subject,
-            Map<String, Object> additionalTokenClaims,
-            AccessTokenHash accessTokenHash,
-            String vot,
-            boolean isDocAppJourney,
-            JWSAlgorithm signingAlgorithm,
-            String journeyId) {
-
-        LOG.info("Generating IdToken");
-        URI trustMarkUri = buildURI(configService.getOidcApiBaseURL().get(), "/trustmark");
-        Date expiryDate = NowHelper.nowPlus(configService.getIDTokenExpiry(), ChronoUnit.SECONDS);
-        IDTokenClaimsSet idTokenClaims =
-                new IDTokenClaimsSet(
-                        new Issuer(configService.getOidcApiBaseURL().get()),
-                        subject,
-                        List.of(new Audience(clientId)),
-                        expiryDate,
-                        NowHelper.now());
-
-        idTokenClaims.setAccessTokenHash(accessTokenHash);
-        idTokenClaims.setSessionID(new SessionID(journeyId));
-
-        idTokenClaims.putAll(additionalTokenClaims);
-        if (!isDocAppJourney) {
-            idTokenClaims.setClaim("vot", vot);
-        }
-        idTokenClaims.setClaim("vtm", trustMarkUri.toString());
-
-        try {
-            return generateSignedJwtUsingExternalKey(
-                    idTokenClaims.toJWTClaimsSet(), Optional.empty(), signingAlgorithm);
-        } catch (com.nimbusds.oauth2.sdk.ParseException e) {
-            LOG.error("Error when trying to parse IDTokenClaims to JWTClaimSet", e);
-            throw new RuntimeException(e);
-        }
     }
 
     public AccessToken generateStorageTokenForMfaReset(Subject internalPairwiseSubject) {
@@ -159,117 +70,6 @@ public class TokenService {
 
         return new BearerAccessToken(
                 signedJWT.serialize(), configService.getAccessTokenExpiry(), null);
-    }
-
-    private AccessToken generateAndStoreAccessToken(
-            String clientId,
-            Subject internalSubject,
-            List<String> scopes,
-            Subject rpPairwiseSubject,
-            Subject internalPairwiseSubject,
-            OIDCClaimsRequest claimsRequest,
-            JWSAlgorithm signingAlgorithm) {
-
-        LOG.info("Generating AccessToken");
-        Date expiryDate =
-                NowHelper.nowPlus(configService.getAccessTokenExpiry(), ChronoUnit.SECONDS);
-        var jwtID = UUID.randomUUID().toString();
-
-        LOG.info("AccessToken being created with JWTID: {}", jwtID);
-
-        JWTClaimsSet.Builder claimSetBuilder =
-                new JWTClaimsSet.Builder()
-                        .claim("scope", scopes)
-                        .issuer(configService.getOidcApiBaseURL().get())
-                        .expirationTime(expiryDate)
-                        .issueTime(NowHelper.now())
-                        .claim("client_id", clientId)
-                        .subject(rpPairwiseSubject.getValue())
-                        .jwtID(jwtID);
-
-        if (Objects.nonNull(claimsRequest)) {
-            LOG.info("Populating identity claims in access token");
-            claimSetBuilder.claim(
-                    "claims",
-                    claimsRequest.getUserInfoClaimsRequest().getEntries().stream()
-                            .map(ClaimsSetRequest.Entry::getClaimName)
-                            .collect(Collectors.toList()));
-        } else {
-            LOG.info("No identity claims to populate in access token");
-        }
-
-        SignedJWT signedJWT =
-                generateSignedJwtUsingExternalKey(
-                        claimSetBuilder.build(), Optional.empty(), signingAlgorithm);
-        AccessToken accessToken =
-                new BearerAccessToken(
-                        signedJWT.serialize(), configService.getAccessTokenExpiry(), null);
-
-        try {
-            redisConnectionService.saveWithExpiry(
-                    ACCESS_TOKEN_PREFIX + clientId + "." + rpPairwiseSubject.getValue(),
-                    objectMapper.writeValueAsString(
-                            new AccessTokenStore(
-                                    accessToken.getValue(),
-                                    internalSubject.getValue(),
-                                    internalPairwiseSubject.getValue())),
-                    configService.getAccessTokenExpiry());
-        } catch (JsonException e) {
-            LOG.error("Unable to save access token to Redis");
-            throw new RuntimeException(e);
-        }
-        return accessToken;
-    }
-
-    private RefreshToken generateAndStoreRefreshToken(
-            String clientId,
-            Subject internalSubject,
-            List<String> scopes,
-            Subject rpPairwiseSubject,
-            Subject internalPairwiseSubject,
-            JWSAlgorithm signingAlgorithm) {
-        LOG.info("Generating RefreshToken");
-        Date expiryDate = NowHelper.nowPlus(configService.getSessionExpiry(), ChronoUnit.SECONDS);
-        var jwtId = IdGenerator.generate();
-        JWTClaimsSet claimsSet =
-                new JWTClaimsSet.Builder()
-                        .claim("scope", scopes)
-                        .issuer(configService.getOidcApiBaseURL().get())
-                        .expirationTime(expiryDate)
-                        .issueTime(NowHelper.now())
-                        .claim("client_id", clientId)
-                        .subject(rpPairwiseSubject.getValue())
-                        .jwtID(jwtId)
-                        .build();
-        SignedJWT signedJWT =
-                generateSignedJwtUsingExternalKey(claimsSet, Optional.empty(), signingAlgorithm);
-        RefreshToken refreshToken = new RefreshToken(signedJWT.serialize());
-
-        String redisKey = REFRESH_TOKEN_PREFIX + jwtId;
-        var store =
-                new RefreshTokenStore(
-                        refreshToken.getValue(),
-                        internalSubject.toString(),
-                        internalPairwiseSubject.toString());
-        try {
-            redisConnectionService.saveWithExpiry(
-                    redisKey,
-                    objectMapper.writeValueAsString(store),
-                    configService.getSessionExpiry());
-        } catch (JsonException e) {
-            throw new RuntimeException("Error serializing refresh token store", e);
-        }
-
-        return refreshToken;
-    }
-
-    private SignedJWT generateSignedJwtUsingExternalKey(
-            JWTClaimsSet claimsSet, Optional<String> type, JWSAlgorithm algorithm) {
-        String alias =
-                algorithm == JWSAlgorithm.ES256
-                        ? configService.getTokenSigningKeyAlias()
-                        : configService.getTokenSigningKeyRsaAlias();
-        return generateSignedJWT(claimsSet, type, algorithm, alias);
     }
 
     private SignedJWT generateSignedJwtUsingStorageKey(
