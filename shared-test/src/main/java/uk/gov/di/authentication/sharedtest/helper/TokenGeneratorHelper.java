966e37327cf9a2c1ab2b89d7c03e46a4597f75ef
BeckaL <25515510+BeckaL@users.noreply.github.com>
2026-05-13 14:17:31 +0100

    BAU: Remove unused methods and fields from Token Generator Helper
    No longer needed

--- shared-test/src/main/java/uk/gov/di/authentication/sharedtest/helper/TokenGeneratorHelper.java
+++ shared-test/src/main/java/uk/gov/di/authentication/sharedtest/helper/TokenGeneratorHelper.java
@@ -1,23 +1,14 @@
 package uk.gov.di.authentication.sharedtest.helper;
 
-import com.nimbusds.jose.JOSEException;
 import com.nimbusds.jose.JWSAlgorithm;
 import com.nimbusds.jose.JWSHeader;
 import com.nimbusds.jose.JWSSigner;
-import com.nimbusds.jose.crypto.ECDSASigner;
 import com.nimbusds.jose.crypto.RSASSASigner;
-import com.nimbusds.jose.jwk.ECKey;
-import com.nimbusds.jose.jwk.JWK;
-import com.nimbusds.jose.jwk.RSAKey;
 import com.nimbusds.jwt.JWTClaimsSet;
 import com.nimbusds.jwt.SignedJWT;
-import com.nimbusds.oauth2.sdk.ParseException;
-import com.nimbusds.oauth2.sdk.id.Audience;
-import com.nimbusds.oauth2.sdk.id.Issuer;
 import com.nimbusds.oauth2.sdk.id.Subject;
 import com.nimbusds.openid.connect.sdk.OIDCClaimsRequest;
 import com.nimbusds.openid.connect.sdk.claims.ClaimsSetRequest;
-import com.nimbusds.openid.connect.sdk.claims.IDTokenClaimsSet;
 import uk.gov.di.authentication.shared.helpers.NowHelper;
 
 import java.time.temporal.ChronoUnit;
@@ -30,47 +21,6 @@ import java.util.stream.Collectors;
 import static uk.gov.di.authentication.sharedtest.exceptions.Unchecked.unchecked;
 
 public class TokenGeneratorHelper {
-
-    private static final String KEY_ID = "14342354354353";
-
-    public static SignedJWT generateIDToken(
-            String clientId, Subject subject, String issuerUrl, JWK signingKey) {
-        Date expiryDate = NowHelper.nowPlus(2, ChronoUnit.MINUTES);
-        return generateIDToken(clientId, subject, issuerUrl, signingKey, expiryDate);
-    }
-
-    public static SignedJWT generateIDToken(
-            String clientId, Subject subject, String issuerUrl, JWK signingKey, Date expiryDate) {
-        IDTokenClaimsSet idTokenClaims =
-                new IDTokenClaimsSet(
-                        new Issuer(issuerUrl),
-                        subject,
-                        List.of(new Audience(clientId)),
-                        expiryDate,
-                        new Date());
-        try {
-            JWSSigner signer;
-            JWSHeader.Builder jwsHeaderBuilder;
-            if (signingKey instanceof RSAKey) {
-                signer = new RSASSASigner(signingKey.toRSAKey());
-                jwsHeaderBuilder = new JWSHeader.Builder(JWSAlgorithm.RS512);
-            } else if (signingKey instanceof ECKey) {
-                signer = new ECDSASigner(signingKey.toECKey());
-                jwsHeaderBuilder = new JWSHeader.Builder(JWSAlgorithm.ES256);
-            } else {
-                throw new RuntimeException("Invalid JWKKey");
-            }
-            var signedJWT =
-                    new SignedJWT(
-                            jwsHeaderBuilder.keyID(signingKey.getKeyID()).build(),
-                            idTokenClaims.toJWTClaimsSet());
-            signedJWT.sign(signer);
-            return signedJWT;
-
-        } catch (JOSEException | ParseException e) {
-            throw new RuntimeException(e);
-        }
-    }
 
     public static SignedJWT generateSignedToken(
             String clientId,
