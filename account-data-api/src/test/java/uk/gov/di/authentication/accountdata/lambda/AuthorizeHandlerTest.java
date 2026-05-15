070e906b47dec4f2cc8dd3ce5beb4612dd9b6d42
Aidan Comer <alhcomer@hotmail.co.uk>
2026-05-15 12:56:56 +0100

    BAU: Use AWS SDK classes for policy document creation

--- account-data-api/src/test/java/uk/gov/di/authentication/accountdata/lambda/AuthorizeHandlerTest.java
+++ account-data-api/src/test/java/uk/gov/di/authentication/accountdata/lambda/AuthorizeHandlerTest.java
@@ -89,11 +89,11 @@ class AuthorizeHandlerTest {
                             {
                                 "principalId": "%s",
                                 "policyDocument": {
-                                    "Version": "2012-10-17",
-                                    "Statement": [{
-                                        "Action": "execute-api:Invoke",
-                                        "Effect": "Allow",
-                                        "Resource": "%s"
+                                    "version": "2012-10-17",
+                                    "statement": [{
+                                        "action": "execute-api:Invoke",
+                                        "effect": "Allow",
+                                        "resource": ["%s"]
                                     }]
                                 }
                             }
