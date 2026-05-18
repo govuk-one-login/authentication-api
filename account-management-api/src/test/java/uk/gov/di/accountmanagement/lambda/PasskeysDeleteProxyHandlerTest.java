0c8e9d1c9e10dc94cc314455c874feeffeb71073
Christian Sophocleous <145482922+ChristianSoftwire@users.noreply.github.com>
2026-05-18 16:59:12 +0100

    AUT-5359: Retrieve passkeys before deleting and use that value to send correct notification once passkey is deleted
    - In order to be able to send the notification that the users passkey was deleted, we need to retrieve the number of passkeys post deletion in order to know what email template to send to the user. However, there could be a failure when retrieving the passkeys following deletion which would mean we can't send the email notification. Therefore, retrieving passkeys is done before deleting and after a successful deletion, we assume the number of passkeys currently on the users' account is the value retrieved prior to deletion - 1. This means that if the retrieve step fails, we don't delete the passkeys and return a generic 500 to the caller
    
    Signed-off-by: Christian Sophocleous <145482922+ChristianSoftwire@users.noreply.github.com>

--- account-management-api/src/test/java/uk/gov/di/accountmanagement/lambda/PasskeysDeleteProxyHandlerTest.java
+++ account-management-api/src/test/java/uk/gov/di/accountmanagement/lambda/PasskeysDeleteProxyHandlerTest.java
@@ -5,19 +5,38 @@ import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.mockito.ArgumentCaptor;
+import uk.gov.di.accountmanagement.entity.NotificationType;
+import uk.gov.di.accountmanagement.entity.NotifyRequest;
+import uk.gov.di.accountmanagement.services.AwsSqsClient;
 import uk.gov.di.authentication.shared.entity.ErrorResponse;
+import uk.gov.di.authentication.shared.entity.UserProfile;
+import uk.gov.di.authentication.shared.entity.passkeys.PasskeysRetrieveResponse;
 import uk.gov.di.authentication.shared.exceptions.UnsuccessfulAccountDataApiResponseException;
+import uk.gov.di.authentication.shared.helpers.LocaleHelper.SupportedLanguage;
+import uk.gov.di.authentication.shared.serialization.Json;
 import uk.gov.di.authentication.shared.services.AccountDataApiService;
 import uk.gov.di.authentication.shared.services.ConfigurationService;
+import uk.gov.di.authentication.shared.services.DynamoService;
+import uk.gov.di.authentication.shared.services.SerializationService;
 
 import java.net.http.HttpResponse;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Stream;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static uk.gov.di.accountmanagement.entity.NotificationType.PASSKEY_DELETED_NONE_REMAINING;
+import static uk.gov.di.accountmanagement.entity.NotificationType.PASSKEY_DELETED_SOME_REMAINING;
 import static uk.gov.di.accountmanagement.helpers.CommonTestVariables.VALID_HEADERS;
 import static uk.gov.di.authentication.sharedtest.helper.CommonTestVariables.IP_ADDRESS;
 import static uk.gov.di.authentication.sharedtest.helper.CommonTestVariables.PUBLIC_SUBJECT_ID;
@@ -30,25 +49,41 @@ class PasskeysDeleteProxyHandlerTest {
     private final Context context = mock(Context.class);
     private final ConfigurationService configurationService = mock(ConfigurationService.class);
     private final AccountDataApiService accountDataApiService = mock(AccountDataApiService.class);
+    private final AwsSqsClient sqsClient = mock(AwsSqsClient.class);
+    private final DynamoService dynamoService = mock(DynamoService.class);
 
     private static final String ADAPI_TOKEN_HEADER = "X-ADAPI-AccessToken";
     private static final String TOKEN = "token";
+    private static final String TEST_EMAIL_ADDRESS = "joe.bloggs@digital.cabinet-office.gov.uk";
 
     private PasskeysDeleteProxyHandler handler;
 
     @BeforeEach
     void setUp() {
-        handler = new PasskeysDeleteProxyHandler(configurationService, accountDataApiService);
+        var userProfile =
+                new UserProfile().withSubjectID(PUBLIC_SUBJECT_ID).withEmail(TEST_EMAIL_ADDRESS);
+        when(dynamoService.getOptionalUserProfileFromPublicSubject(PUBLIC_SUBJECT_ID))
+                .thenReturn(Optional.of(userProfile));
+
+        handler =
+                new PasskeysDeleteProxyHandler(
+                        configurationService, accountDataApiService, sqsClient, dynamoService);
     }
 
     private static final String PASSKEY_IDENTIFIER = "test-passkey-id";
+    private static final String OTHER_PASSKEY_IDENTIFIER = "other-test-passkey-id";
 
     @Nested
     class SuccessfulRequest {
         @Test
-        void shouldProxyResponseFromService() throws UnsuccessfulAccountDataApiResponseException {
+        void shouldProxyResponseFromService()
+                throws UnsuccessfulAccountDataApiResponseException, Json.JsonException {
             // Arrange
             var mockHttpResponse = mock(HttpResponse.class);
+            when(accountDataApiService.retrievePasskeys(PUBLIC_SUBJECT_ID, TOKEN))
+                    .thenReturn(
+                            new PasskeysRetrieveResponse(
+                                    List.of(aPasskeyResponse(PASSKEY_IDENTIFIER))));
             when(mockHttpResponse.statusCode()).thenReturn(200);
             when(mockHttpResponse.body()).thenReturn("{\"deleted\": true}");
             when(accountDataApiService.deletePasskey(PUBLIC_SUBJECT_ID, PASSKEY_IDENTIFIER, TOKEN))
@@ -63,14 +98,60 @@ class PasskeysDeleteProxyHandlerTest {
             verify(accountDataApiService)
                     .deletePasskey(PUBLIC_SUBJECT_ID, PASSKEY_IDENTIFIER, TOKEN);
         }
+
+        private static Stream<Arguments> expectedNotificationTypeForUserPasskeys() {
+            return Stream.of(
+                    Arguments.of(
+                            List.of(aPasskeyResponse(PASSKEY_IDENTIFIER)),
+                            PASSKEY_DELETED_NONE_REMAINING),
+                    Arguments.of(
+                            List.of(
+                                    aPasskeyResponse(PASSKEY_IDENTIFIER),
+                                    aPasskeyResponse(OTHER_PASSKEY_IDENTIFIER)),
+                            PASSKEY_DELETED_SOME_REMAINING));
+        }
+
+        @ParameterizedTest
+        @MethodSource("expectedNotificationTypeForUserPasskeys")
+        void shouldRetrievePasskeyCountAndSendCorrectNotification(
+                List<PasskeysRetrieveResponse.PasskeyResponse> userPasskeys,
+                NotificationType expectedNotificationType)
+                throws UnsuccessfulAccountDataApiResponseException, Json.JsonException {
+            // Arrange
+            var mockHttpResponse = mock(HttpResponse.class);
+            when(accountDataApiService.retrievePasskeys(PUBLIC_SUBJECT_ID, TOKEN))
+                    .thenReturn(new PasskeysRetrieveResponse(userPasskeys));
+            when(mockHttpResponse.statusCode()).thenReturn(204);
+            when(mockHttpResponse.body()).thenReturn("");
+            when(accountDataApiService.deletePasskey(PUBLIC_SUBJECT_ID, PASSKEY_IDENTIFIER, TOKEN))
+                    .thenReturn(mockHttpResponse);
+
+            // Act
+            handler.handleRequest(passkeysDeleteProxyRequest(), context);
+
+            // Assert
+            var sqsMessageCaptor = ArgumentCaptor.forClass(String.class);
+            verify(sqsClient).send(sqsMessageCaptor.capture());
+
+            var sentNotifyRequest =
+                    SerializationService.getInstance()
+                            .readValue(sqsMessageCaptor.getValue(), NotifyRequest.class);
+            assertThat(sentNotifyRequest.getDestination(), equalTo(TEST_EMAIL_ADDRESS));
+            assertThat(sentNotifyRequest.getNotificationType(), equalTo(expectedNotificationType));
+            assertThat(sentNotifyRequest.getLanguage(), equalTo(SupportedLanguage.EN));
+        }
     }
 
     @Nested
     class FailedRequest {
         @Test
         void shouldReturn500IfServiceThrowsException()
-                throws UnsuccessfulAccountDataApiResponseException {
+                throws UnsuccessfulAccountDataApiResponseException, Json.JsonException {
             // Arrange
+            when(accountDataApiService.retrievePasskeys(PUBLIC_SUBJECT_ID, TOKEN))
+                    .thenReturn(
+                            new PasskeysRetrieveResponse(
+                                    List.of(aPasskeyResponse(PASSKEY_IDENTIFIER))));
             when(accountDataApiService.deletePasskey(PUBLIC_SUBJECT_ID, PASSKEY_IDENTIFIER, TOKEN))
                     .thenThrow(new UnsuccessfulAccountDataApiResponseException("service error", 0));
 
@@ -96,5 +177,20 @@ class PasskeysDeleteProxyHandlerTest {
                                 "passkeyIdentifier", PASSKEY_IDENTIFIER))
                 .withHeaders(headersWithToken)
                 .withRequestContext(contextWithSourceIp(IP_ADDRESS));
+    }
+
+    private static PasskeysRetrieveResponse.PasskeyResponse aPasskeyResponse(String passkeyId) {
+        return new PasskeysRetrieveResponse.PasskeyResponse(
+                passkeyId,
+                "cHVibGljLWtleS1jb3Nl",
+                "some-aaguid",
+                true,
+                5,
+                List.of(),
+                true,
+                true,
+                true,
+                "some-timestamp",
+                "another-timestamp");
     }
 }
--- account-management-integration-tests/src/test/java/uk/gov/di/accountmanagement/api/PasskeysDeleteProxyHandlerIntegrationTest.java
+++ account-management-integration-tests/src/test/java/uk/gov/di/accountmanagement/api/PasskeysDeleteProxyHandlerIntegrationTest.java
@@ -7,27 +7,55 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import uk.gov.di.accountmanagement.entity.NotifyRequest;
 import uk.gov.di.accountmanagement.lambda.PasskeysDeleteProxyHandler;
+import uk.gov.di.authentication.shared.helpers.LocaleHelper;
 import uk.gov.di.authentication.sharedtest.basetest.ApiGatewayHandlerIntegrationTest;
 import uk.org.webcompere.systemstubs.environment.EnvironmentVariables;
 import uk.org.webcompere.systemstubs.jupiter.SystemStub;
 import uk.org.webcompere.systemstubs.jupiter.SystemStubsExtension;
 
 import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
 import static com.github.tomakehurst.wiremock.client.WireMock.delete;
 import static com.github.tomakehurst.wiremock.client.WireMock.deleteRequestedFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlPathMatching;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
+import static uk.gov.di.accountmanagement.entity.NotificationType.PASSKEY_DELETED_NONE_REMAINING;
+import static uk.gov.di.accountmanagement.testsupport.helpers.NotificationAssertionHelper.assertNotificationsReceived;
 import static uk.gov.di.authentication.sharedtest.matchers.APIGatewayProxyResponseEventMatcher.hasStatus;
 
 @ExtendWith(SystemStubsExtension.class)
 class PasskeysDeleteProxyHandlerIntegrationTest extends ApiGatewayHandlerIntegrationTest {
     private static WireMockServer accountDataApiWireMockServer;
+
+    private static final String TEST_EMAIL = "joe.bloggs@digital.cabinet-office.gov.uk";
+    private static final String TEST_PASSWORD = "password";
+    private static final String passkeyRetrieveResponse =
+            """
+                    {
+                      "passkeys": [
+                        {
+                          "id": "123456",
+                          "credential": "credential1",
+                          "aaguid": "some-aaguid",
+                          "isAttested": true,
+                          "signCount": 1,
+                          "transports": [],
+                          "isBackupEligible": true,
+                          "isBackedUp": true,
+                          "createdAt": "some-timestamp",
+                          "lastUsedAt": "another-timestamp"
+                        }
+                      ]
+                    }
+                    """;
 
     @SystemStub static EnvironmentVariables environment = new EnvironmentVariables();
 
@@ -39,6 +67,8 @@ class PasskeysDeleteProxyHandlerIntegrationTest extends ApiGatewayHandlerIntegra
 
         environment.set(
                 "ACCOUNT_DATA_API_URI", "http://localhost:" + accountDataApiWireMockServer.port());
+
+        notificationsQueue.clear();
     }
 
     @AfterAll
@@ -51,9 +81,9 @@ class PasskeysDeleteProxyHandlerIntegrationTest extends ApiGatewayHandlerIntegra
     @Test
     void shouldProxy204ResponseFromAccountDataApi() {
         // Arrange
+        var publicSubjectId = userStore.signUp(TEST_EMAIL, TEST_PASSWORD);
         handler = new PasskeysDeleteProxyHandler(TEST_CONFIGURATION_SERVICE);
 
-        var publicSubjectId = "abc";
         var passkeyId = "def";
         var token = "hij";
 
@@ -66,6 +96,10 @@ class PasskeysDeleteProxyHandlerIntegrationTest extends ApiGatewayHandlerIntegra
                                                 + passkeyId))
                         .withHeader("Authorization", WireMock.equalTo("Bearer " + token))
                         .willReturn(aResponse().withStatus(204)));
+
+        accountDataApiWireMockServer.stubFor(
+                get(urlPathMatching("/accounts/" + publicSubjectId + "/authenticators/passkeys"))
+                        .willReturn(aResponse().withStatus(200).withBody(passkeyRetrieveResponse)));
 
         // Act
         var response =
@@ -91,11 +125,85 @@ class PasskeysDeleteProxyHandlerIntegrationTest extends ApiGatewayHandlerIntegra
     }
 
     @Test
-    void shouldProxy404ResponseFromAccountDataApi() {
+    void shouldSendPasskeyDeletedEmailNotification() {
         // Arrange
+        var publicSubjectId = userStore.signUp(TEST_EMAIL, TEST_PASSWORD);
         handler = new PasskeysDeleteProxyHandler(TEST_CONFIGURATION_SERVICE);
 
-        var publicSubjectId = "abc";
+        var passkeyId = "def";
+        var token = "hij";
+
+        accountDataApiWireMockServer.stubFor(
+                delete(
+                                urlPathMatching(
+                                        "/accounts/"
+                                                + publicSubjectId
+                                                + "/authenticators/passkeys/"
+                                                + passkeyId))
+                        .withHeader("Authorization", WireMock.equalTo("Bearer " + token))
+                        .willReturn(aResponse().withStatus(204)));
+
+        var passkeyRetrieveResponse =
+                String.format(
+                        """
+                        {
+                          "passkeys": [
+                            {
+                              "id": "%s",
+                              "credential": "credential1",
+                              "aaguid": "some-aaguid",
+                              "isAttested": true,
+                              "signCount": 1,
+                              "transports": [],
+                              "isBackupEligible": true,
+                              "isBackedUp": true,
+                              "createdAt": "some-timestamp",
+                              "lastUsedAt": "another-timestamp"
+                            }
+                          ]
+                        }
+                        """,
+                        passkeyId);
+        accountDataApiWireMockServer.stubFor(
+                get(urlPathMatching("/accounts/" + publicSubjectId + "/authenticators/passkeys"))
+                        .willReturn(aResponse().withStatus(200).withBody(passkeyRetrieveResponse)));
+
+        // Act
+        var response =
+                makeRequest(
+                        Optional.empty(),
+                        Map.of("X-ADAPI-AccessToken", token),
+                        Collections.emptyMap(),
+                        Map.of("publicSubjectId", publicSubjectId, "passkeyIdentifier", passkeyId),
+                        Collections.emptyMap(),
+                        Optional.of("delete"));
+
+        // Assert
+        assertThat(response, hasStatus(204));
+        accountDataApiWireMockServer.verify(
+                1,
+                deleteRequestedFor(
+                                urlPathMatching(
+                                        "/accounts/"
+                                                + publicSubjectId
+                                                + "/authenticators/passkeys/"
+                                                + passkeyId))
+                        .withHeader("Authorization", WireMock.equalTo("Bearer " + token)));
+        assertNotificationsReceived(
+                notificationsQueue,
+                List.of(
+                        new NotifyRequest(
+                                TEST_EMAIL,
+                                PASSKEY_DELETED_NONE_REMAINING,
+                                LocaleHelper.SupportedLanguage.EN)));
+    }
+
+    @Test
+    void shouldProxy404ResponseFromAccountDataApi() {
+        // Arrange
+        var publicSubjectId = userStore.signUp(TEST_EMAIL, TEST_PASSWORD);
+        handler = new PasskeysDeleteProxyHandler(TEST_CONFIGURATION_SERVICE);
+
         var passkeyId = "def";
         var token = "hij";
         var adapiResponse =
@@ -115,6 +223,10 @@ class PasskeysDeleteProxyHandlerIntegrationTest extends ApiGatewayHandlerIntegra
                                                 + passkeyId))
                         .withHeader("Authorization", WireMock.equalTo("Bearer " + token))
                         .willReturn(aResponse().withStatus(404).withBody(adapiResponse)));
+
+        accountDataApiWireMockServer.stubFor(
+                get(urlPathMatching("/accounts/" + publicSubjectId + "/authenticators/passkeys"))
+                        .willReturn(aResponse().withStatus(200).withBody(passkeyRetrieveResponse)));
 
         // Act
         var response =
@@ -143,9 +255,9 @@ class PasskeysDeleteProxyHandlerIntegrationTest extends ApiGatewayHandlerIntegra
     @Test
     void shouldProxy500ResponseFromAccountDataApi() {
         // Arrange
+        var publicSubjectId = userStore.signUp(TEST_EMAIL, TEST_PASSWORD);
         handler = new PasskeysDeleteProxyHandler(TEST_CONFIGURATION_SERVICE);
 
-        var publicSubjectId = "abc";
         var passkeyId = "def";
         var token = "hij";
         var adapiResponse =
@@ -166,6 +278,10 @@ class PasskeysDeleteProxyHandlerIntegrationTest extends ApiGatewayHandlerIntegra
                         .withHeader("Authorization", WireMock.equalTo("Bearer " + token))
                         .willReturn(aResponse().withStatus(500).withBody(adapiResponse)));
 
+        accountDataApiWireMockServer.stubFor(
+                get(urlPathMatching("/accounts/" + publicSubjectId + "/authenticators/passkeys"))
+                        .willReturn(aResponse().withStatus(200).withBody(passkeyRetrieveResponse)));
+
         // Act
         var response =
                 makeRequest(
@@ -181,6 +297,42 @@ class PasskeysDeleteProxyHandlerIntegrationTest extends ApiGatewayHandlerIntegra
         assertThat(response.getBody(), equalTo(adapiResponse));
         accountDataApiWireMockServer.verify(
                 1,
+                deleteRequestedFor(
+                                urlPathMatching(
+                                        "/accounts/"
+                                                + publicSubjectId
+                                                + "/authenticators/passkeys/"
+                                                + passkeyId))
+                        .withHeader("Authorization", WireMock.equalTo("Bearer " + token)));
+    }
+
+    @Test
+    void shouldNotDeletePasskeyIfPasskeysRetrievalFails() {
+        // Arrange
+        var publicSubjectId = userStore.signUp(TEST_EMAIL, TEST_PASSWORD);
+        handler = new PasskeysDeleteProxyHandler(TEST_CONFIGURATION_SERVICE);
+
+        var passkeyId = "def";
+        var token = "hij";
+
+        accountDataApiWireMockServer.stubFor(
+                get(urlPathMatching("/accounts/" + publicSubjectId + "/authenticators/passkeys"))
+                        .willReturn(aResponse().withStatus(500)));
+
+        // Act
+        var response =
+                makeRequest(
+                        Optional.empty(),
+                        Map.of("X-ADAPI-AccessToken", token),
+                        Collections.emptyMap(),
+                        Map.of("publicSubjectId", publicSubjectId, "passkeyIdentifier", passkeyId),
+                        Collections.emptyMap(),
+                        Optional.of("delete"));
+
+        // Assert
+        assertThat(response, hasStatus(500));
+        accountDataApiWireMockServer.verify(
+                0,
                 deleteRequestedFor(
                                 urlPathMatching(
                                         "/accounts/"
