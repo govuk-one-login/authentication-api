af2e07e194022ad36852e838b1109074ffc72298
Christian Sophocleous <145482922+ChristianSoftwire@users.noreply.github.com>
2026-05-18 16:59:12 +0100

    AUT-5359: Retrieve passkeys before deleting and use that value to send correct notification once passkey is deleted
    - In order to be able to send the notification that the users passkey was deleted, we need to retrieve the number of passkeys post deletion in order to know what email template to send to the user. However, there could be a failure when retrieving the passkeys following deletion which would mean we can't send the email notification. Therefore, retrieving passkeys is done before deleting and after a successful deletion, we assume the number of passkeys currently on the users' account is the value retrieved prior to deletion - 1. This means that if the retrieve step fails, we don't delete the passkeys and return a generic 500 to the caller
    
    Signed-off-by: Christian Sophocleous <145482922+ChristianSoftwire@users.noreply.github.com>

--- account-management-api/src/main/java/uk/gov/di/accountmanagement/lambda/PasskeysDeleteProxyHandler.java
+++ account-management-api/src/main/java/uk/gov/di/accountmanagement/lambda/PasskeysDeleteProxyHandler.java
@@ -7,14 +7,27 @@ import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.ThreadContext;
+import uk.gov.di.accountmanagement.entity.NotificationType;
+import uk.gov.di.accountmanagement.entity.NotifyRequest;
+import uk.gov.di.accountmanagement.entity.PasskeysDeleteProxyFailureReason;
+import uk.gov.di.accountmanagement.services.AwsSqsClient;
 import uk.gov.di.authentication.shared.entity.ErrorResponse;
+import uk.gov.di.authentication.shared.entity.Result;
 import uk.gov.di.authentication.shared.exceptions.UnsuccessfulAccountDataApiResponseException;
+import uk.gov.di.authentication.shared.helpers.LocaleHelper;
+import uk.gov.di.authentication.shared.serialization.Json;
 import uk.gov.di.authentication.shared.services.AccountDataApiService;
 import uk.gov.di.authentication.shared.services.ConfigurationService;
+import uk.gov.di.authentication.shared.services.DynamoService;
+import uk.gov.di.authentication.shared.services.SerializationService;
+
+import java.net.http.HttpResponse;
 
 import static uk.gov.di.authentication.shared.helpers.ApiGatewayResponseHelper.generateApiGatewayProxyErrorResponse;
 import static uk.gov.di.authentication.shared.helpers.ApiGatewayResponseHelper.generateApiGatewayProxyResponse;
 import static uk.gov.di.authentication.shared.helpers.InstrumentationHelper.segmentedFunctionCall;
+import static uk.gov.di.authentication.shared.helpers.LocaleHelper.getUserLanguageFromRequestHeaders;
+import static uk.gov.di.authentication.shared.helpers.LocaleHelper.matchSupportedLanguage;
 
 public class PasskeysDeleteProxyHandler
         implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
@@ -22,6 +35,9 @@ public class PasskeysDeleteProxyHandler
     private static final Logger LOG = LogManager.getLogger(PasskeysDeleteProxyHandler.class);
     private final ConfigurationService configurationService;
     private final AccountDataApiService accountDataApiService;
+    private final SerializationService serializationService = SerializationService.getInstance();
+    private final AwsSqsClient sqsClient;
+    private final DynamoService dynamoService;
 
     public PasskeysDeleteProxyHandler() {
         this(ConfigurationService.getInstance());
@@ -30,13 +46,23 @@ public class PasskeysDeleteProxyHandler
     public PasskeysDeleteProxyHandler(ConfigurationService configurationService) {
         this.configurationService = configurationService;
         this.accountDataApiService = new AccountDataApiService(configurationService);
+        this.sqsClient =
+                new AwsSqsClient(
+                        configurationService.getAwsRegion(),
+                        configurationService.getEmailQueueUri(),
+                        configurationService.getSqsEndpointUri());
+        this.dynamoService = new DynamoService(configurationService);
     }
 
     public PasskeysDeleteProxyHandler(
             ConfigurationService configurationService,
-            AccountDataApiService accountDataApiService) {
+            AccountDataApiService accountDataApiService,
+            AwsSqsClient sqsClient,
+            DynamoService dynamoService) {
         this.configurationService = configurationService;
         this.accountDataApiService = accountDataApiService;
+        this.sqsClient = sqsClient;
+        this.dynamoService = dynamoService;
     }
 
     @Override
@@ -52,6 +78,43 @@ public class PasskeysDeleteProxyHandler
             APIGatewayProxyRequestEvent input, Context context) {
         LOG.info("PasskeysDeleteProxyHandler invoked");
 
+        PasskeysDeleteRequest request = extractPasskeyDeleteRequest(input);
+
+        var userEmailResult = getUserEmailFromPublicSubjectId(request.publicSubjectId);
+        if (userEmailResult.isFailure()) {
+            return generateApiGatewayProxyErrorResponse(500, ErrorResponse.INTERNAL_SERVER_ERROR);
+        }
+        var userEmail = userEmailResult.getSuccess();
+
+        var currentPasskeyCountResult = getPasskeyCount(request);
+        if (currentPasskeyCountResult.isFailure()) {
+            return generateApiGatewayProxyErrorResponse(500, ErrorResponse.INTERNAL_SERVER_ERROR);
+        }
+        var currentPasskeyCount = currentPasskeyCountResult.getSuccess();
+
+        var deletePasskeyResponseResult = deletePasskey(request);
+        if (deletePasskeyResponseResult.isFailure()) {
+            return generateApiGatewayProxyErrorResponse(500, ErrorResponse.INTERNAL_SERVER_ERROR);
+        }
+        HttpResponse<String> deletePasskeyResponse = deletePasskeyResponseResult.getSuccess();
+
+        var deletePasskeyProxyResponse =
+                generateApiGatewayProxyResponse(
+                        deletePasskeyResponse.statusCode(), deletePasskeyResponse.body());
+
+        if (deletePasskeyResponse.statusCode() != 204) {
+            LOG.warn(
+                    "Passkey Deleted Email notification not sent because delete passkey response was {} for Public Subject ID {}",
+                    deletePasskeyResponse.statusCode(),
+                    request.publicSubjectId);
+        } else {
+            sendEmailNotification(request, userEmail, currentPasskeyCount);
+        }
+
+        return deletePasskeyProxyResponse;
+    }
+
+    private PasskeysDeleteRequest extractPasskeyDeleteRequest(APIGatewayProxyRequestEvent input) {
         var publicSubjectId = input.getPathParameters().getOrDefault("publicSubjectId", "");
         if (publicSubjectId.isEmpty()) {
             LOG.warn("No publicSubjectId in path parameters, request will likely fail");
@@ -67,16 +130,76 @@ public class PasskeysDeleteProxyHandler
             LOG.warn("No X-ADAPI-AccessToken in headers, request will likely fail");
         }
 
+        var userLanguage =
+                matchSupportedLanguage(
+                        getUserLanguageFromRequestHeaders(
+                                input.getHeaders(), configurationService));
+
+        return new PasskeysDeleteRequest(publicSubjectId, token, passkeyIdentifier, userLanguage);
+    }
+
+    private Result<PasskeysDeleteProxyFailureReason, String> getUserEmailFromPublicSubjectId(String publicSubjectId) {
+        var userProfile =
+                dynamoService.getOptionalUserProfileFromPublicSubject(publicSubjectId);
+
+        if (userProfile.isEmpty()) {
+            LOG.warn("No user profile found for public subject ID{}", publicSubjectId);
+            return Result.failure(PasskeysDeleteProxyFailureReason.FAILED_TO_FIND_USER_PROFILE);
+        }
+
+        return Result.success(userProfile.get().getEmail());
+    }
+
+    private Result<PasskeysDeleteProxyFailureReason, Integer> getPasskeyCount(
+            PasskeysDeleteRequest request) {
+        try {
+            var userPasskeys =
+                    accountDataApiService.retrievePasskeys(request.publicSubjectId, request.token);
+            return Result.success(userPasskeys.passkeys().size());
+        } catch (UnsuccessfulAccountDataApiResponseException | Json.JsonException e) {
+            LOG.warn(
+                    "Attempted to retrieve passkeys for user with publicSubjectId '{}' but failed due to '{}'",
+                    request.publicSubjectId,
+                    e.getMessage());
+            return Result.failure(
+                    PasskeysDeleteProxyFailureReason.FAILED_TO_RETRIEVE_PASSKEY_COUNT);
+        }
+    }
+
+    private Result<PasskeysDeleteProxyFailureReason, HttpResponse<String>> deletePasskey(
+            PasskeysDeleteRequest request) {
         try {
             var response =
-                    accountDataApiService.deletePasskey(publicSubjectId, passkeyIdentifier, token);
-            return generateApiGatewayProxyResponse(response.statusCode(), response.body());
+                    accountDataApiService.deletePasskey(
+                            request.publicSubjectId, request.passkeyId, request.token);
+            return Result.success(response);
         } catch (UnsuccessfulAccountDataApiResponseException e) {
             LOG.warn(
                     "Attempted to delete passkey with ID '{}' but failed due to '{}'",
-                    passkeyIdentifier,
+                    request.passkeyId,
                     e.getMessage());
-            return generateApiGatewayProxyErrorResponse(500, ErrorResponse.INTERNAL_SERVER_ERROR);
+            return Result.failure(PasskeysDeleteProxyFailureReason.FAILED_TO_DELETE_PASSKEY);
         }
     }
+
+    private void sendEmailNotification(PasskeysDeleteRequest request, String email, int passkeyCount) {
+        var passkeyCountPostDelete = passkeyCount - 1;
+        var notificationType =
+                passkeyCountPostDelete <= 0
+                        ? NotificationType.PASSKEY_DELETED_NONE_REMAINING
+                        : NotificationType.PASSKEY_DELETED_SOME_REMAINING;
+
+        var notifyRequest = new NotifyRequest(email, notificationType, request.supportedLanguage);
+        sqsClient.send(serializationService.writeValueAsString((notifyRequest)));
+        LOG.info(
+                "Notify request sent with notification type '{}' and publicSubjectId '{}'",
+                notificationType,
+                request.publicSubjectId);
+    }
+
+    private record PasskeysDeleteRequest(
+            String publicSubjectId,
+            String token,
+            String passkeyId,
+            LocaleHelper.SupportedLanguage supportedLanguage) {}
 }
