1d2f9c794c0611a9e9ca36a95e083289617eeede
BeckaL <25515510+BeckaL@users.noreply.github.com>
2026-06-01 17:30:49 +0100

    BAU: Remove redundant helper method
    We already retrieve the default mfa method in the body of the method that this method is called in. This helper method is redundant and we can just directly call sendAuditEvent

--- account-management-api/src/main/java/uk/gov/di/accountmanagement/lambda/MFAMethodsPutHandler.java
+++ account-management-api/src/main/java/uk/gov/di/accountmanagement/lambda/MFAMethodsPutHandler.java
@@ -534,8 +534,11 @@ public class MFAMethodsPutHandler
                         .orElseThrow();
 
         return switch (updateTypeIdentifier) {
-            case SWITCHED_MFA_METHODS -> handleSwitchedMfaMethodsAuditEvents(
-                    auditContext, putRequest, updatedMfaMethods);
+            case SWITCHED_MFA_METHODS -> sendAuditEvent(
+                    AUTH_MFA_METHOD_SWITCH_COMPLETED,
+                    putRequest,
+                    postUpdateDefaultMfaMethod,
+                    auditContext);
             case CHANGED_SMS -> sendAuditEvent(
                     AUTH_UPDATE_PHONE_NUMBER, putRequest, postUpdateDefaultMfaMethod, auditContext);
             case CHANGED_DEFAULT_MFA -> {
@@ -555,23 +558,6 @@ public class MFAMethodsPutHandler
             }
             default -> Result.success(null);
         };
-    }
-
-    private Result<APIGatewayProxyResponseEvent, Void> handleSwitchedMfaMethodsAuditEvents(
-            AuditContext auditContext,
-            ValidPutRequest putRequest,
-            List<MFAMethod> updatedMfaMethods) {
-        var postUpdateDefaultMfaMethod =
-                updatedMfaMethods.stream()
-                        .filter(mfaMethod -> DEFAULT.name().equals(mfaMethod.getPriority()))
-                        .findFirst()
-                        .orElseThrow();
-
-        return sendAuditEvent(
-                        AUTH_MFA_METHOD_SWITCH_COMPLETED,
-                        putRequest,
-                        postUpdateDefaultMfaMethod,
-                        auditContext);
     }
 
     private Result<APIGatewayProxyResponseEvent, Void> sendAuditEvent(
