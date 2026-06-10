591561ede38f6707bb608ae834141aecb15f23b5
BeckaL <25515510+BeckaL@users.noreply.github.com>
2026-06-10 15:30:09 +0100

    AUT-5467: Introduce structured audit event for auth passkey delete successful event
    This event needs an additional field (passkey count) on the user, so using the new structured audit event means that we can set this without setting it for every audit event we emit.
    
    Not yet used, this just introduces the types

---
+++ audit-events/src/main/java/uk/gov/di/authentication/auditevents/entity/AuthPasskeyDeleteSuccessful.java
@@ -0,0 +1,74 @@
+package uk.gov.di.authentication.auditevents.entity;
+
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.GsonBuilder;
+import com.google.gson.annotations.SerializedName;
+import uk.gov.di.audit.AuditContext;
+import uk.gov.di.authentication.auditevents.entity.shared.EncodedDeviceInformation;
+import uk.gov.di.authentication.auditevents.entity.shared.PasskeyWithCredentialId;
+import uk.gov.di.authentication.shared.entity.JourneyType;
+
+import java.time.Clock;
+import java.time.Instant;
+
+public record AuthPasskeyDeleteSuccessful(
+        String eventName,
+        long timestamp,
+        long eventTimestampMs,
+        String clientId,
+        String componentId,
+        User user,
+        Restricted restricted,
+        Extensions extensions)
+        implements StructuredAuditEvent {
+
+    public static AuthPasskeyDeleteSuccessful create(
+            AuditContext auditContext, int passkeyCount, String deletedCredentialId, Clock clock) {
+        Instant now = clock.instant();
+        var user =
+                new User(
+                        auditContext.email(),
+                        auditContext.clientSessionId(),
+                        auditContext.ipAddress(),
+                        auditContext.persistentSessionId(),
+                        auditContext.sessionId(),
+                        auditContext.subjectId(),
+                        passkeyCount);
+        var restrictedSection =
+                new Restricted(
+                        new EncodedDeviceInformation(auditContext.txmaAuditEncoded()),
+                        new PasskeyWithCredentialId(deletedCredentialId));
+        var extensions = new Extensions(JourneyType.ACCOUNT_MANAGEMENT.getValue());
+        return new AuthPasskeyDeleteSuccessful(
+                "AUTH_PASSKEY_DELETE_SUCCESSFUL",
+                now.getEpochSecond(),
+                now.toEpochMilli(),
+                auditContext.clientId(),
+                ComponentId.HOME.getValue(),
+                user,
+                restrictedSection,
+                extensions);
+    }
+
+    public record User(
+            String email,
+            String govukSigninJourneyId,
+            String ipAddress,
+            String persistentSessionId,
+            String sessionId,
+            String userId,
+            int passkeyCount) {}
+
+    public record Restricted(
+            EncodedDeviceInformation deviceInformation, PasskeyWithCredentialId passkey) {}
+
+    public record Extensions(@SerializedName("journey-type") String journeyType) {}
+
+    public String serialize() {
+        var gson =
+                new GsonBuilder()
+                        .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
+                        .create();
+        return gson.toJson(this);
+    }
+}
---
+++ audit-events/src/main/java/uk/gov/di/authentication/auditevents/entity/shared/PasskeyWithCredentialId.java
@@ -0,0 +1,3 @@
+package uk.gov.di.authentication.auditevents.entity.shared;
+
+public record PasskeyWithCredentialId(String passkeyCredentialId) {}
