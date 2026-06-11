ce99b256a65d4f562bdd5fe0bc6414ec5fdd6aef
BeckaL <25515510+BeckaL@users.noreply.github.com>
2026-06-11 15:11:54 +0100

    BAU: Introduce mini helper method
    Reduces a tiny bit of noise

--- audit-events/src/main/java/uk/gov/di/authentication/auditevents/entity/shared/EncodedDeviceInformation.java
+++ audit-events/src/main/java/uk/gov/di/authentication/auditevents/entity/shared/EncodedDeviceInformation.java
@@ -1,3 +1,9 @@
 package uk.gov.di.authentication.auditevents.entity.shared;
 
-public record EncodedDeviceInformation(String encoded) {}
+import uk.gov.di.audit.AuditContext;
+
+public record EncodedDeviceInformation(String encoded) {
+    public static EncodedDeviceInformation from(AuditContext auditContext) {
+        return new EncodedDeviceInformation(auditContext.txmaAuditEncoded());
+    }
+}
