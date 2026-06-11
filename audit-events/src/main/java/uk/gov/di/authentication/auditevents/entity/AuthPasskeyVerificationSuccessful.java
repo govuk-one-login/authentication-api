75709aa7f64968652405a7d1efcc9103912f58fc
BeckaL <25515510+BeckaL@users.noreply.github.com>
2026-06-11 15:06:15 +0100

    AUT-5466: Add Auth Passkey Verification Successful audit event as structured audit event
    Not yet used. Possible that we'll be able to tidy up / share more of the nested records we had to create in here when it's being used.

---
+++ audit-events/src/main/java/uk/gov/di/authentication/auditevents/entity/AuthPasskeyVerificationSuccessful.java
@@ -0,0 +1,72 @@
+package uk.gov.di.authentication.auditevents.entity;
+
+import com.google.gson.annotations.SerializedName;
+import uk.gov.di.audit.AuditContext;
+import uk.gov.di.authentication.auditevents.entity.shared.EncodedDeviceInformation;
+import uk.gov.di.authentication.auditevents.entity.shared.Users.UserWithoutPhone;
+import uk.gov.di.authentication.shared.entity.JourneyType;
+
+import java.time.Clock;
+import java.time.Instant;
+import java.util.List;
+
+public record AuthPasskeyVerificationSuccessful(
+        String eventName,
+        long timestamp,
+        long eventTimestampMs,
+        String clientId,
+        String componentId,
+        UserWithoutPhone user,
+        Restricted restricted,
+        Extensions extensions)
+        implements StructuredAuditEvent {
+
+    public static AuthPasskeyVerificationSuccessful create(
+            AuditContext auditContext,
+            JourneyType journeyType,
+            List<PasskeyAllowedCredential> passkeyAllowedCredentials,
+            Passkey passkey,
+            String credentialId,
+            Clock clock) {
+        var eventName = "AUTH_PASSKEY_VERIFICATION_SUCCESSFUL";
+        Instant now = clock.instant();
+        var user = UserWithoutPhone.fromAuditContext(auditContext);
+        var restricted =
+                new Restricted(
+                        new EncodedDeviceInformation(auditContext.txmaAuditEncoded()),
+                        new RestrictedPasskeySection(passkeyAllowedCredentials),
+                        credentialId);
+        var extensions = new Extensions(journeyType.getValue(), passkey);
+        return new AuthPasskeyVerificationSuccessful(
+                eventName,
+                now.getEpochSecond(),
+                now.toEpochMilli(),
+                auditContext.clientId(),
+                ComponentId.AUTH.getValue(),
+                user,
+                restricted,
+                extensions);
+    }
+
+    public record PasskeyAuthenticationRequest(String passkeyRequestUserVerification) {}
+
+    public record Passkey(
+            int passkeyCounter,
+            boolean passkeyCredentialBackedUp,
+            String passkeyCredentialDeviceType,
+            boolean passkeyUserVerified,
+            PasskeyAuthenticationRequest passkeyAuthenticationRequest) {}
+
+    public record RestrictedPasskeySection(
+            List<PasskeyAllowedCredential> passkeyAllowedCredentials) {}
+
+    public record PasskeyAllowedCredential(
+            String passkeyCredentialId, List<String> passkeyCredentialTransports) {}
+
+    public record Restricted(
+            EncodedDeviceInformation deviceInformation,
+            RestrictedPasskeySection passkey,
+            String passkeyCredentialId) {}
+
+    public record Extensions(@SerializedName("journey-type") String journeyType, Passkey passkey) {}
+}
