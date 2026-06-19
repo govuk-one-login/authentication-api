62fd827ca9e863d257fb93cce3f979671b7fd977
BeckaL <25515510+BeckaL@users.noreply.github.com>
2026-06-19 11:49:01 +0100

    AUT-5466: Emit verification succesful event when passkey verification succeeds

--- frontend-api/src/main/java/uk/gov/di/authentication/frontendapi/services/webauthn/PasskeyAssertionService.java
+++ frontend-api/src/main/java/uk/gov/di/authentication/frontendapi/services/webauthn/PasskeyAssertionService.java
@@ -13,12 +13,20 @@ import com.yubico.webauthn.data.UserVerificationRequirement;
 import com.yubico.webauthn.exception.AssertionFailedException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import uk.gov.di.audit.AuditContext;
+import uk.gov.di.authentication.auditevents.entity.AuthPasskeyVerificationSuccessful;
+import uk.gov.di.authentication.auditevents.entity.shared.passkeys.PasskeyDetail;
 import uk.gov.di.authentication.auditevents.services.StructuredAuditService;
 import uk.gov.di.authentication.frontendapi.entity.FinishPasskeyAssertionFailureReason;
+import uk.gov.di.authentication.shared.entity.JourneyType;
 import uk.gov.di.authentication.shared.entity.Result;
+import uk.gov.di.authentication.shared.services.AuditService;
 
 import java.nio.charset.StandardCharsets;
+import java.time.Clock;
 import java.util.Optional;
+
+import static uk.gov.di.authentication.frontendapi.helpers.PasskeyAuditExtensionsHelper.passkeyAllowedCredentialsFrom;
 
 public class PasskeyAssertionService {
     private final RelyingParty relyingParty;
@@ -81,6 +89,43 @@ public class PasskeyAssertionService {
             return Result.failure(FinishPasskeyAssertionFailureReason.ASSERTION_FAILED_ERROR);
         }
 
+        //TODO pass in audit context to this function
+        emitAuthPasskeyVerificationSuccessEvent(
+                AuditContext.emptyAuditContext(), assertionRequest, assertionResult, credential);
+
         return Result.success(assertionResult);
+    }
+
+    @SuppressWarnings("deprecation")
+    private void emitAuthPasskeyVerificationSuccessEvent(
+            AuditContext auditContext,
+            AssertionRequest assertionRequest,
+            AssertionResult assertionResult,
+            PublicKeyCredential<AuthenticatorAssertionResponse, ClientAssertionExtensionOutputs>
+                    publicKeyCredential) {
+        var passkeyAllowedCredentials = passkeyAllowedCredentialsFrom(assertionRequest);
+        var userVerification =
+                assertionRequest
+                        .getPublicKeyCredentialRequestOptions()
+                        .getUserVerification()
+                        .map(UserVerificationRequirement::getValue)
+                        .orElse(AuditService.UNKNOWN);
+        var passkeyCredentialDeviceType =
+                assertionResult.isBackupEligible() ? "multi-device" : "single-device";
+        var passkeyDetail =
+                PasskeyDetail.verificationSuccessful(
+                        userVerification,
+                        (int) assertionResult.getSignatureCount(), // TODO change to long
+                        assertionResult.isBackedUp(),
+                        passkeyCredentialDeviceType);
+        var event =
+                AuthPasskeyVerificationSuccessful.create(
+                        auditContext,
+                        JourneyType.SIGN_IN,
+                        passkeyAllowedCredentials,
+                        passkeyDetail,
+                        publicKeyCredential.getId().getBase64Url(),
+                        Clock.systemUTC());
+        structuredAuditService.submitAuditEvent(event);
     }
 }
--- frontend-api/src/test/java/uk/gov/di/authentication/frontendapi/services/webauthn/PasskeyAssertionServiceTest.java
+++ frontend-api/src/test/java/uk/gov/di/authentication/frontendapi/services/webauthn/PasskeyAssertionServiceTest.java
@@ -5,37 +5,62 @@ import com.yubico.webauthn.AssertionRequest;
 import com.yubico.webauthn.AssertionResult;
 import com.yubico.webauthn.RelyingParty;
 import com.yubico.webauthn.StartAssertionOptions;
+import com.yubico.webauthn.data.AuthenticatorAssertionResponse;
+import com.yubico.webauthn.data.AuthenticatorTransport;
 import com.yubico.webauthn.data.ByteArray;
+import com.yubico.webauthn.data.ClientAssertionExtensionOutputs;
 import com.yubico.webauthn.data.PublicKeyCredential;
+import com.yubico.webauthn.data.PublicKeyCredentialDescriptor;
+import com.yubico.webauthn.data.PublicKeyCredentialRequestOptions;
 import com.yubico.webauthn.data.UserVerificationRequirement;
+import com.yubico.webauthn.data.exception.Base64UrlException;
 import com.yubico.webauthn.exception.AssertionFailedException;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import uk.gov.di.authentication.auditevents.entity.AuthPasskeyVerificationSuccessful;
+import uk.gov.di.authentication.auditevents.entity.StructuredAuditEvent;
+import uk.gov.di.authentication.auditevents.entity.shared.passkeys.PasskeyAllowCredentials;
+import uk.gov.di.authentication.auditevents.entity.shared.passkeys.PasskeyDetail;
 import uk.gov.di.authentication.auditevents.services.StructuredAuditService;
 import uk.gov.di.authentication.frontendapi.entity.FinishPasskeyAssertionFailureReason;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.reset;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static uk.gov.di.authentication.sharedtest.helper.CommonTestVariables.PUBLIC_SUBJECT_ID;
 
-public class PasskeyAssertionServiceTest {
+class PasskeyAssertionServiceTest {
     private final RelyingParty relyingParty = mock(RelyingParty.class);
     private final PasskeyJsonParser jsonParser = mock(PasskeyJsonParser.class);
     private PasskeyAssertionService passkeyAssertionService;
+    private static final String CREDENTIAL_ID = "Q6pkKSucKCqDzOuFky3pQAA";
+    private static final StructuredAuditService structuredAuditService =
+            mock(StructuredAuditService.class);
 
     @BeforeEach
     void setup() {
         passkeyAssertionService =
-                new PasskeyAssertionService(
-                        relyingParty, jsonParser, mock(StructuredAuditService.class));
+                new PasskeyAssertionService(relyingParty, jsonParser, structuredAuditService);
+    }
+
+    @AfterEach
+    void tearDown() {
+        reset(structuredAuditService);
     }
 
     @Nested
@@ -76,13 +101,13 @@ public class PasskeyAssertionServiceTest {
             @Test
             @SuppressWarnings("unchecked")
             void shouldReturnAssertionResultIfAssertionSucceeded()
-                    throws IOException, AssertionFailedException {
+                    throws IOException, AssertionFailedException, Base64UrlException {
                 // Given
-                when(jsonParser.parseAssertionRequest(any()))
-                        .thenReturn(mock(AssertionRequest.class));
-                when(jsonParser.parsePublicKeyCredential(any()))
-                        .thenReturn(mock(PublicKeyCredential.class));
-                AssertionResult mockAssertionResult = mock(AssertionResult.class);
+                var assertionRequest = setupAssertionRequest();
+                when(jsonParser.parseAssertionRequest(any())).thenReturn(assertionRequest);
+                var publicKeyCredential = setupPublicKeyCredential(CREDENTIAL_ID);
+                when(jsonParser.parsePublicKeyCredential(any())).thenReturn(publicKeyCredential);
+                AssertionResult mockAssertionResult = setupAnySuccessfulMockAssertionResult();
                 when(mockAssertionResult.isSuccess()).thenReturn(true);
                 when(relyingParty.finishAssertion(any())).thenReturn(mockAssertionResult);
 
@@ -92,6 +117,63 @@ public class PasskeyAssertionServiceTest {
 
                 // Then
                 assertEquals(actualAssertionResult, mockAssertionResult);
+            }
+
+            @Test
+            void shouldEmitAuditEventIfAssertionSucceeded()
+                    throws IOException, AssertionFailedException, Base64UrlException {
+                // Given
+                var signCount = 10;
+                var isBackedUp = false;
+                var isBackupEligible = true;
+                var userVerification = UserVerificationRequirement.REQUIRED;
+                var assertionIsSuccess = true;
+                var allowedCredentialsMap = Map.of(CREDENTIAL_ID, "BLE");
+
+                var assertionRequest =
+                        setupAssertionRequest(allowedCredentialsMap, userVerification);
+                var publicKeyCredential = setupPublicKeyCredential(CREDENTIAL_ID);
+                var assertionResult =
+                        setupMockAssertionResult(
+                                signCount, isBackupEligible, isBackedUp, assertionIsSuccess);
+
+                when(jsonParser.parseAssertionRequest(any())).thenReturn(assertionRequest);
+                when(jsonParser.parsePublicKeyCredential(any())).thenReturn(publicKeyCredential);
+                when(relyingParty.finishAssertion(any())).thenReturn(assertionResult);
+
+                // When
+                passkeyAssertionService.finishAssertion("", "").getSuccess();
+
+                // Then
+                var argCaptor = ArgumentCaptor.forClass(StructuredAuditEvent.class);
+
+                verify(structuredAuditService).submitAuditEvent(argCaptor.capture());
+                var capturedAuditEvent = argCaptor.getValue();
+
+                assertEquals(
+                        "AUTH_PASSKEY_VERIFICATION_SUCCESSFUL", capturedAuditEvent.eventName());
+
+                var authPasskeyVerificationSuccessful =
+                        (AuthPasskeyVerificationSuccessful) capturedAuditEvent;
+
+                var expectedPasskeyDetail =
+                        PasskeyDetail.verificationSuccessful(
+                                userVerification.getValue(), signCount, isBackedUp, "multi-device");
+                assertEquals(
+                        expectedPasskeyDetail,
+                        authPasskeyVerificationSuccessful.extensions().passkey());
+
+                assertEquals(
+                        CREDENTIAL_ID,
+                        authPasskeyVerificationSuccessful.restricted().passkeyCredentialId());
+                var restrictedPasskeySection =
+                        new AuthPasskeyVerificationSuccessful.RestrictedPasskeySection(
+                                List.of(
+                                        new PasskeyAllowCredentials(
+                                                CREDENTIAL_ID, List.of("BLE"))));
+                assertEquals(
+                        restrictedPasskeySection,
+                        authPasskeyVerificationSuccessful.restricted().passkey());
             }
         }
 
@@ -112,6 +194,8 @@ public class PasskeyAssertionServiceTest {
                 assertEquals(
                         FinishPasskeyAssertionFailureReason.PARSING_ASSERTION_REQUEST_ERROR,
                         actualFailureReason);
+
+                verify(structuredAuditService, never()).submitAuditEvent(any());
             }
 
             @Test
@@ -128,6 +212,7 @@ public class PasskeyAssertionServiceTest {
                 // Then
                 assertEquals(
                         FinishPasskeyAssertionFailureReason.PARSING_PKC_ERROR, actualFailureReason);
+                verify(structuredAuditService, never()).submitAuditEvent(any());
             }
 
             @Test
@@ -149,6 +234,7 @@ public class PasskeyAssertionServiceTest {
                 assertEquals(
                         FinishPasskeyAssertionFailureReason.ASSERTION_FAILED_ERROR,
                         actualFailureReason);
+                verify(structuredAuditService, never()).submitAuditEvent(any());
             }
 
             @Test
@@ -172,7 +258,72 @@ public class PasskeyAssertionServiceTest {
                 assertEquals(
                         FinishPasskeyAssertionFailureReason.ASSERTION_FAILED_ERROR,
                         actualFailureReason);
+                verify(structuredAuditService, never()).submitAuditEvent(any());
             }
         }
+    }
+
+    private static AssertionResult setupAnySuccessfulMockAssertionResult() {
+        return setupMockAssertionResult(10, false, false, true);
+    }
+
+    @SuppressWarnings("deprecation")
+    private static AssertionResult setupMockAssertionResult(
+            long signCount, boolean isBackupEligible, boolean isBackedUp, boolean isSuccess) {
+        var mock = mock(AssertionResult.class);
+        when(mock.getSignatureCount()).thenReturn(signCount);
+        when(mock.isBackupEligible()).thenReturn(isBackupEligible);
+        when(mock.isBackedUp()).thenReturn(isBackedUp);
+        when(mock.isSuccess()).thenReturn(isSuccess);
+        return mock;
+    }
+
+    private static AssertionRequest setupAssertionRequest(
+            Map<String, String> credentials,
+            UserVerificationRequirement userVerificationRequirement) {
+        var assertionRequest = mock(AssertionRequest.class);
+        var publicKeyCredentialRequestOptions = mock(PublicKeyCredentialRequestOptions.class);
+        var allPublicKeyCredentialDescriptors = new ArrayList<PublicKeyCredentialDescriptor>();
+        credentials.forEach(
+                (k, v) -> {
+                    try {
+                        var descriptor =
+                                PublicKeyCredentialDescriptor.builder()
+                                        .id(ByteArray.fromBase64Url(CREDENTIAL_ID))
+                                        .transports(Set.of(AuthenticatorTransport.of(v)))
+                                        .build();
+                        allPublicKeyCredentialDescriptors.add(descriptor);
+                    } catch (Base64UrlException e) {
+                        throw new RuntimeException(e);
+                    }
+                });
+        when(publicKeyCredentialRequestOptions.getAllowCredentials())
+                .thenReturn(Optional.of(allPublicKeyCredentialDescriptors));
+        when(publicKeyCredentialRequestOptions.getUserVerification())
+                .thenReturn(Optional.of(userVerificationRequirement));
+        when(assertionRequest.getPublicKeyCredentialRequestOptions())
+                .thenReturn(publicKeyCredentialRequestOptions);
+
+        return assertionRequest;
+    }
+
+    private static AssertionRequest setupAssertionRequest() {
+        var assertionRequest = mock(AssertionRequest.class);
+        var publicKeyCredentialRequestOptions = mock(PublicKeyCredentialRequestOptions.class);
+        when(publicKeyCredentialRequestOptions.getAllowCredentials()).thenReturn(Optional.empty());
+        when(publicKeyCredentialRequestOptions.getUserVerification())
+                .thenReturn(Optional.of(UserVerificationRequirement.REQUIRED));
+        when(assertionRequest.getPublicKeyCredentialRequestOptions())
+                .thenReturn(publicKeyCredentialRequestOptions);
+
+        return assertionRequest;
+    }
+
+    private static PublicKeyCredential<
+                    AuthenticatorAssertionResponse, ClientAssertionExtensionOutputs>
+            setupPublicKeyCredential(String credentialId) throws Base64UrlException {
+        var credential = mock(PublicKeyCredential.class);
+        when(credential.getId()).thenReturn(ByteArray.fromBase64Url(credentialId));
+        return credential;
     }
 }
