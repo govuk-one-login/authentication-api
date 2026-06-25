dd259c815675efedeb5cf77682a6d36e683ab881
BeckaL <25515510+BeckaL@users.noreply.github.com>
2026-06-25 12:29:07 +0100

    AUT-5446: Increment counters for passkey delete success / failures
    This will help us track the behaviour of passkeys journeys

--- shared/src/main/java/uk/gov/di/authentication/shared/domain/CloudwatchMetrics.java
+++ shared/src/main/java/uk/gov/di/authentication/shared/domain/CloudwatchMetrics.java
@@ -39,7 +39,9 @@ public enum CloudwatchMetrics {
     PASSKEY_AUTHENTICATION_REQUESTED("PasskeyAuthenticationRequested"),
     AMC_AUTHORISATION_REQUESTED("AMCAuthorisationRequested"),
     AMC_AUTHORISATION_RECEIVED("AMCAuthorisationReceived"),
-    AMC_FAILURE_GETTING_AUTHORISATION("AMCFailureGettingAuthorisation");
+    AMC_FAILURE_GETTING_AUTHORISATION("AMCFailureGettingAuthorisation"),
+    PASSKEY_DELETION_SUCCESSFUL("PasskeyDeletionSuccessful"),
+    PASSKEY_DELETION_FAILED("PasskeyDeletionFailed");
 
     private String value;
 
