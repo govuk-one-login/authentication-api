d3007188a832244fc5ce6c89032c8348df3c327c
Craig Earl <craig.earl@digital.cabinet-office.gov.uk>
2026-06-26 09:30:19 +0100

    ATO-2710: Add unit test for SISJwksHandler

--- sis-api/src/test/java/uk/gov/di/orchestration/sis/lambda/SISJwksHandlerTest.java
+++ sis-api/src/test/java/uk/gov/di/orchestration/sis/lambda/SISJwksHandlerTest.java
@@ -2,21 +2,60 @@ package uk.gov.di.orchestration.sis.lambda;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.nimbusds.jose.jwk.Curve;
+import com.nimbusds.jose.jwk.ECKey;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.gen.ECKeyGenerator;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import uk.gov.di.orchestration.shared.services.JwksService;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.List;
+import java.util.UUID;
+
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static uk.gov.di.orchestration.sharedtest.matchers.APIGatewayProxyResponseEventMatcher.hasBody;
+import static uk.gov.di.orchestration.sharedtest.matchers.APIGatewayProxyResponseEventMatcher.hasHeader;
+import static uk.gov.di.orchestration.sharedtest.matchers.APIGatewayProxyResponseEventMatcher.hasStatus;
 
-public class SISJwksHandlerTest {
+class SISJwksHandlerTest {
+
+    private final Context context = mock(Context.class);
+    private final JwksService jwksService = mock(JwksService.class);
+    private SISJwksHandler handler;
+    private ECKey sisTokenSigningKey;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        sisTokenSigningKey =
+                new ECKeyGenerator(Curve.P_256).keyID(UUID.randomUUID().toString()).generate();
+        handler = new SISJwksHandler(jwksService);
+
+        when(jwksService.getPublicSisTokenJwkWithOpaqueId()).thenReturn(sisTokenSigningKey);
+    }
+
     @Test
-    void shouldCheckLambdaReturnsExpectedValue() {
-        var sisJwksHandler = new SISJwksHandler();
+    void shouldReturnASingleJwk() {
+        var event = new APIGatewayProxyRequestEvent();
+        var result = handler.handleRequest(event, context);
 
-        var response =
-                sisJwksHandler.handleRequest(
-                        new APIGatewayProxyRequestEvent(), mock(Context.class));
+        var expectedJWKSet = new JWKSet(List.of(sisTokenSigningKey));
 
-        assertEquals(200, response.getStatusCode());
-        assertEquals("Test lambda", response.getBody());
+        assertThat(result, hasStatus(200));
+        assertThat(result, hasBody(expectedJWKSet.toString(true)));
+        assertThat(result, hasHeader("Cache-Control", "max-age=86400"));
+    }
+
+    @Test
+    void shouldReturn500WhenSigningKeyIsNotPresent() {
+        when(jwksService.getPublicSisTokenJwkWithOpaqueId()).thenReturn(null);
+
+        var event = new APIGatewayProxyRequestEvent();
+        var result = handler.handleRequest(event, context);
+
+        assertThat(result, hasStatus(500));
+        assertThat(result, hasBody("Error providing SISJwks data"));
     }
 }
