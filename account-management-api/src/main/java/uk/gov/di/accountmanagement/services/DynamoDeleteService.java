3b76955d3d34507f3f14ece54bada509137571a6
Matt <173997080+sw-mattw@users.noreply.github.com>
2026-07-06 16:58:19 +0100

    AUT-5551: Update `DynamoDeleteService` to query and delete authenticator items within the transaction

--- account-management-api/src/main/java/uk/gov/di/accountmanagement/services/DynamoDeleteService.java
+++ account-management-api/src/main/java/uk/gov/di/accountmanagement/services/DynamoDeleteService.java
@@ -4,7 +4,10 @@ import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbTable;
 import software.amazon.awssdk.enhanced.dynamodb.Key;
 import software.amazon.awssdk.enhanced.dynamodb.TableSchema;
+import software.amazon.awssdk.enhanced.dynamodb.model.QueryConditional;
+import software.amazon.awssdk.enhanced.dynamodb.model.QueryEnhancedRequest;
 import software.amazon.awssdk.enhanced.dynamodb.model.TransactWriteItemsEnhancedRequest;
+import uk.gov.di.accountmanagement.entity.AuthenticatorItemKey;
 import uk.gov.di.authentication.shared.dynamodb.DynamoClientHelper;
 import uk.gov.di.authentication.shared.entity.AccountModifiers;
 import uk.gov.di.authentication.shared.entity.UserCredentials;
@@ -12,6 +15,7 @@ import uk.gov.di.authentication.shared.entity.UserProfile;
 import uk.gov.di.authentication.shared.helpers.TableNameHelper;
 import uk.gov.di.authentication.shared.services.ConfigurationService;
 
+import java.util.List;
 import java.util.Locale;
 import java.util.Optional;
 
@@ -22,9 +26,11 @@ public class DynamoDeleteService {
     private static final String USER_PROFILE_TABLE = "user-profile";
     private static final String USER_CREDENTIAL_TABLE = "user-credentials";
     private static final String ACCOUNT_MODIFIERS_TABLE_NAME = "account-modifiers";
+    private static final String AUTHENTICATOR_TABLE = "authenticator";
     private final DynamoDbTable<AccountModifiers> dynamoAccountModifiersTable;
     private final DynamoDbTable<UserProfile> dynamoUserProfileTable;
     private final DynamoDbTable<UserCredentials> dynamoUserCredentialsTable;
+    private final DynamoDbTable<AuthenticatorItemKey> dynamoAuthenticatorTable;
     private final DynamoDbEnhancedClient dynamoDbEnhancedClient;
 
     public DynamoDeleteService(ConfigurationService configurationService) {
@@ -35,6 +41,8 @@ public class DynamoDeleteService {
         var accountModifiersTableName =
                 TableNameHelper.getFullTableName(
                         ACCOUNT_MODIFIERS_TABLE_NAME, configurationService);
+        var authenticatorTableName =
+                TableNameHelper.getFullTableName(AUTHENTICATOR_TABLE, configurationService);
 
         this.dynamoDbEnhancedClient =
                 DynamoClientHelper.createDynamoEnhancedClient(configurationService);
@@ -47,13 +55,17 @@ public class DynamoDeleteService {
         this.dynamoAccountModifiersTable =
                 dynamoDbEnhancedClient.table(
                         accountModifiersTableName, TableSchema.fromBean(AccountModifiers.class));
+        this.dynamoAuthenticatorTable =
+                dynamoDbEnhancedClient.table(
+                        authenticatorTableName, TableSchema.fromBean(AuthenticatorItemKey.class));
 
         warmUp(dynamoUserProfileTable);
         warmUp(dynamoUserCredentialsTable);
         warmUp(dynamoAccountModifiersTable);
+        warmUp(dynamoAuthenticatorTable);
     }
 
-    public void deleteAccount(String email, String internalSubPairwiseId) {
+    public void deleteAccount(String email, String internalSubPairwiseId, String publicSubjectId) {
         var transactionWriterBuilder =
                 TransactWriteItemsEnhancedRequest.builder()
                         .addDeleteItem(
@@ -75,6 +87,26 @@ public class DynamoDeleteService {
                                 transactionWriterBuilder.addDeleteItem(
                                         dynamoAccountModifiersTable, t));
 
+        getAuthenticatorItems(publicSubjectId)
+                .forEach(
+                        item ->
+                                transactionWriterBuilder.addDeleteItem(
+                                        dynamoAuthenticatorTable,
+                                        Key.builder()
+                                                .partitionValue(item.getPublicSubjectId())
+                                                .sortValue(item.getSortKey())
+                                                .build()));
+
         dynamoDbEnhancedClient.transactWriteItems(transactionWriterBuilder.build());
+    }
+
+    private List<AuthenticatorItemKey> getAuthenticatorItems(String publicSubjectId) {
+        var queryConditional =
+                QueryConditional.keyEqualTo(Key.builder().partitionValue(publicSubjectId).build());
+        return dynamoAuthenticatorTable
+                .query(QueryEnhancedRequest.builder().queryConditional(queryConditional).build())
+                .items()
+                .stream()
+                .toList();
     }
 }
