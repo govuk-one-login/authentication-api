d039ed14306c662018a99076cc04a281a3b23361
Craig Earl <craig.earl@digital.cabinet-office.gov.uk>
2026-07-17 10:10:57 +0100

    ATO-2755: Copy spot waiting code to identity-shared

---
+++ identity-shared/src/main/java/uk/gov/di/orchestration/identity/entity/ProcessingIdentityStatus.java
@@ -0,0 +1,9 @@
+package uk.gov.di.orchestration.identity.entity;
+
+public enum ProcessingIdentityStatus {
+    COMPLETED,
+    PROCESSING,
+    ERROR,
+    NO_ENTRY,
+    INTERVENTION
+}
--- identity-shared/src/test/java/uk/gov/di/orchestration/identity/testsupport/TestAuditEvent.java
+++ identity-shared/src/test/java/uk/gov/di/orchestration/identity/testsupport/TestAuditEvent.java
@@ -4,7 +4,8 @@ import uk.gov.di.orchestration.shared.domain.AuditableEvent;
 
 public enum TestAuditEvent implements AuditableEvent {
     TEST_UNSUCCESSFUL_TOKEN_RESPONSE_RECEIVED,
-    TEST_SUCCESSFUL_TOKEN_RESPONSE_RECEIVED;
+    TEST_SUCCESSFUL_TOKEN_RESPONSE_RECEIVED,
+    TEST_PROCESSING_IDENTITY_REQUEST;
 
     @Override
     public AuditableEvent parseFromName(String name) {
