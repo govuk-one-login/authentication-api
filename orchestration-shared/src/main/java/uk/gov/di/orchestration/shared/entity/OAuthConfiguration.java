afed19ad993cfbd20ddbae857c5e135b9b806804
Ryan Andrews <ryan.andrews@digital.cabinet-office.gov.uk>
2026-08-05 17:31:14 +0100

    BAU: Add getIpvConfig helper method
    Helps reduce some of the visual bulk here

--- orchestration-shared/src/main/java/uk/gov/di/orchestration/shared/entity/OAuthConfiguration.java
+++ orchestration-shared/src/main/java/uk/gov/di/orchestration/shared/entity/OAuthConfiguration.java
@@ -1,5 +1,8 @@
 package uk.gov.di.orchestration.shared.entity;
 
+import uk.gov.di.orchestration.shared.helpers.ConstructUriHelper;
+import uk.gov.di.orchestration.shared.services.ConfigurationService;
+
 import java.net.URI;
 
 public record OAuthConfiguration(
@@ -10,4 +13,20 @@ public record OAuthConfiguration(
         URI redirectURI,
         String signingKeyAlias,
         String privateKeyJwtAudience,
-        String statePrefix) {}
+        String statePrefix) {
+    public static final String IPV_STATE_STORAGE_PREFIX = "state:";
+
+    public static OAuthConfiguration getIPVConfig(ConfigurationService configurationService) {
+        return new OAuthConfiguration(
+                configurationService.getIPVAuthorisationClientId(),
+                configurationService.getIPVAuthorisationURI(),
+                ConstructUriHelper.buildURI(
+                        configurationService.getIPVBackendURI().toString(), "token"),
+                ConstructUriHelper.buildURI(
+                        configurationService.getIPVBackendURI().toString(), "user-identity"),
+                configurationService.getIPVAuthorisationCallbackURI(),
+                configurationService.getIPVTokenSigningKeyAlias(),
+                configurationService.getIPVAudience(),
+                IPV_STATE_STORAGE_PREFIX);
+    }
+}
