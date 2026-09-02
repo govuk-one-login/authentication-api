901814d1311b3f6e44723e1b6de3e8afe8370e97
Christian Sophocleous <145482922+csophocleous@users.noreply.github.com>
2026-09-02 16:27:13 +0100

    BAU: add log for when there is an existing session with the new session id
    We are seeing an issue where 2 quick requests made to the StartHandler are overwriting a users session with a blank session. The following is an example of how this happens
    
    - User has existing sessionA.
    - A request to the StartHandler is made with SessionA id as previousSessionId in requestBody and SessionB ID is a header
    - Get SessionA with id and copy everything from it into SessionB
    - Delete SessionA
    - Immediately another request is sent with SessionA id as previousSessionId and SessionB as sessionId in header
    - SessionA was deleted in the first request, therefore user gets a fresh session which suggests they haven't authenticated
    - User doesn't do a silent login and instead has to log in again
    
    We want to add a log to check our theory about this issue being caused by our implementation of using the previousSessionId for the existing user

--- shared/src/main/java/uk/gov/di/authentication/shared/services/AuthSessionService.java
+++ shared/src/main/java/uk/gov/di/authentication/shared/services/AuthSessionService.java
@@ -94,6 +94,14 @@ public class AuthSessionService extends BaseDynamoService<AuthSessionItem> {
 
                 return updatedSession;
             } else {
+                var existingSessionWithNewSessionId = getSession(newSessionId);
+                if (existingSessionWithNewSessionId.isPresent()) {
+                    LOG.info(
+                            "Session already exists with newSessionId {} for previousSessionId {} may cause problems",
+                            newSessionId,
+                            previousSessionId);
+                }
+
                 LOG.info("New Auth session item created with sessionId: {}", newSessionId);
                 return generateNewAuthSession(newSessionId);
             }
