45f6f73cf74736953dbf141334a2bf5a6f4f40a9
Pawankumar Kushwaha <pawankumar.kushwaha@digital.cabinet-office.gov.uk>
2026-09-24 13:03:06 +0100

    AUT-5846: Remove indentity shared code and update workflows

--- identity-shared/src/main/java/uk/gov/di/orchestration/identity/entity/AuditEventConfiguration.java
+++
@@ -1,7 +0,0 @@
-package uk.gov.di.orchestration.identity.entity;
-
-import uk.gov.di.orchestration.shared.domain.AuditableEvent;
-
-public record AuditEventConfiguration(
-        AuditableEvent unsuccessfulTokenResponseReceived,
-        AuditableEvent successfulTokenResponseReceived) {}
--- identity-shared/src/main/java/uk/gov/di/orchestration/identity/entity/IdentityProgressStatus.java
+++
@@ -1,8 +0,0 @@
-package uk.gov.di.orchestration.identity.entity;
-
-public enum IdentityProgressStatus {
-    COMPLETED,
-    PROCESSING,
-    ERROR,
-    NO_ENTRY,
-}
